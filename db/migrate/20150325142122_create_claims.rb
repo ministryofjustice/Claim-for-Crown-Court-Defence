1b041c4fef190b8b55cf0a658f43753dd232b5cd
Himal Mandalia <himalmandalia@gmail.com>
2015-03-26 17:09:20 +0000

    vat_required added to Claim

--- db/migrate/20150325142122_create_claims.rb
+++ db/migrate/20150325142122_create_claims.rb
@@ -2,6 +2,7 @@ class CreateClaims < ActiveRecord::Migration
   def change
     create_table :claims do |t|
       t.text :additional_information
+      t.boolean :vat_required
       t.references :advocate, index: true
 
       t.timestamps

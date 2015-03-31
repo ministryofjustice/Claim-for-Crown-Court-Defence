86d1abe58075f00a733c2e85263afab5c53f4e2b
Himal Mandalia <himalmandalia@gmail.com>
2015-03-31 09:54:33 +0100

    Sign in feature

---
+++ features/step_definitions/sign_in_steps.rb
@@ -0,0 +1,23 @@
+Given(/^an? "(.*?)" user account exists$/) do |role|
+  @password = 'password'
+  create(role.gsub(/\s/, '_').to_sym, password: @password, password_confirmation: @password)
+end
+
+When(/^I vist the user sign in page$/) do
+  visit new_user_session_path
+end
+
+When(/^I enter my email, password and click log in$/) do
+  fill_in 'Email', with: User.first.email
+  fill_in 'Password', with: @password
+  click_on 'Log in'
+end
+
+Then(/^I should be redirected to the "(.*?)" claims index$/) do |namespace|
+  case namespace.gsub(/\s/, '_')
+  when 'advocates'
+    expect(current_url).to eq(advocates_root_url)
+  when 'case_workers'
+    expect(current_url).to eq(case_workers_root_url)
+  end
+end

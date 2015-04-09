8fc4394e8ff661da3ccc3bcac0cc4f5a620011bf
Todd Tyree <tatyree@gmail.com>
2015-04-09 15:21:27 +0100

    Document the config. Override bucket if needed.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
   if (ENV['cbo_use_s3'] || Rails.env.production?)
-    #unless (ENV['cbo_aws_access_key'] && ENV['cbo_secret_access_key'])
-      puts "************************"
-      puts "You have not set `cbo_aws_access_key` or `cbo_secret_access_key` environment variables."
-      puts "************************"
-    #end
+    unless (ENV['cbo_aws_access_key'] && ENV['cbo_secret_access_key'])
+      warn "************************************************"
+      warn "You have not set `cbo_aws_access_key` or `cbo_secret_access_key` environment variables."
+      warn "These are required if you want to run in production mode or use S3 in any other mode."
+      warn "************************************************"
+      exit
+    end
 
     config.fog_credentials = {
       provider:               'AWS',
@@ -12,7 +14,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key:  ENV['cbo_secret_access_key'],
       region:                 'eu-west-1'
     }
-    config.fog_directory  = "moj_cbo_documents_#{Rails.env}"
+    config.fog_directory  = ENV['cbo_bucket_name'] || "moj_cbo_documents_#{Rails.env}"
     config.fog_public     = false
     config.fog_attributes = {'Cache-Control': 'no-cache'}
 

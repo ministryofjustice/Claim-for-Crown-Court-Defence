f969ac41c0a5ff7963a6ee83351f9f2309f6d969
Todd Tyree <tatyree@gmail.com>
2015-04-09 13:59:40 +0100

    WIP: getting config right

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
+  if (ENV['cbo_use_s3'] || Rails.env.production?)
+    #unless (ENV['cbo_aws_access_key'] && ENV['cbo_secret_access_key'])
+      puts "************************"
+      puts "You have not set `cbo_aws_access_key` or `cbo_secret_access_key` environment variables."
+      puts "************************"
+    #end
 
-  if Rails.env.test? || Rails.env.cucumber?
-    config.storage = :file
-    config.enable_processing = false
-  elsif Rails.env.development? && !ENV['cbo_use_s3']
-    config.storage = :file
-  else
     config.fog_credentials = {
       provider:               'AWS',
       aws_access_key_id:      ENV['cbo_aws_access_key'],
@@ -15,6 +15,15 @@ CarrierWave.configure do |config|
     config.fog_directory  = "moj_cbo_documents_#{Rails.env}"
     config.fog_public     = false
     config.fog_attributes = {'Cache-Control': 'no-cache'}
-  end
 
+  elsif (Rails.env.test? || Rails.env.cucumber?)
+
+    config.storage = :file
+    config.enable_processing = false
+
+  else
+
+    config.storage = :file
+
+  end
 end

487fb8a251ba4581e069de99dad1dbbc2f229f1f
Todd Tyree <tatyree@gmail.com>
2015-04-10 13:13:29 +0100

    Make idempotent

--- db/seeds/case_workers.rb
+++ db/seeds/case_workers.rb
@@ -2,4 +2,4 @@ User.create!(
   email: 'caseworker@example.com',
   password: ENV['CASE_WORKER_PASSWORD'],
   password_confirmation: ENV['CASE_WORKER_PASSWORD'],
-  role: 'case_worker')
+  role: 'case_worker') if User.find_by(email: 'caseworker@example.com').blank?

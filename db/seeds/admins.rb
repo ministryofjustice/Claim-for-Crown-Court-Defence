d6bc3bed211b2c9181e8b43237747814681bde94
Himal Mandalia <himalmandalia@gmail.com>
2015-04-10 13:28:57 +0100

    Make admin seeds idempotent

--- db/seeds/admins.rb
+++ db/seeds/admins.rb
@@ -2,4 +2,4 @@ User.create!(
   email: 'admin@example.com',
   password: ENV['ADMIN_PASSWORD'],
   password_confirmation: ENV['ADMIN_PASSWORD'],
-  role: 'admin')
+  role: 'admin') if User.find_by(email: 'admin@example.com').blank?

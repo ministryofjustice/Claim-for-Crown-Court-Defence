a465fbbe48160ae72c4beb3d33499429df8016be
Todd Tyree <tatyree@gmail.com>
2015-04-10 13:13:29 +0100

    Make idempotent

--- db/seeds/courts.rb
+++ db/seeds/courts.rb
@@ -1,2 +1,2 @@
-Court.create!(code: 'AAA', name: 'Some court')
-Court.create!(code: 'BBB', name: 'Another court')
+Court.find_or_create_by!(code: 'AAA', name: 'Some court')
+Court.find_or_create_by!(code: 'BBB', name: 'Another court')

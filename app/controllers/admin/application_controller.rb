cd30be6d08d3119db18d5efcedadf7975495a691
Himal Mandalia <himalmandalia@gmail.com>
2015-04-10 08:31:34 +0100

    Scaffold basic admin users controller and templates

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,1 @@
+class Admin::ApplicationController < ApplicationController; end
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,8 @@ Rails.application.routes.draw do
 
     resources :claims, only: [:index, :show]
   end
+
+  namespace :admin do
+    resources :users
+  end
 end

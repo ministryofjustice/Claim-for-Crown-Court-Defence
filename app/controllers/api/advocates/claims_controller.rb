832b44e724cbfbfaf4144df47d84f34a99bf7e35
Eddie Andress <eddieandress@Admins-MacBook-Pro-7.local>
2015-04-14 15:44:17 +0100

    error handling for validation fail during update on API

--- app/controllers/api/advocates/claims_controller.rb
+++ app/controllers/api/advocates/claims_controller.rb
@@ -2,6 +2,15 @@ module Api
   module Advocates
     class ClaimsController < ::Advocates::ClaimsController
       respond_to :json
+
+      def update
+        if @claim.update(claim_params)
+          render json: @claim, message: 'Claim sucessfully updated'
+        else
+          render json: { errors: @claim.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+
     end
   end
 end

73f2313e6529f962935d05f619000745d7c63c89
Todd Tyree <tatyree@gmail.com>
2015-04-16 16:33:12 +0100

    Make paperclip work at a basic level.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,10 +1,19 @@
 class Document < ActiveRecord::Base
   has_attached_file :document
 
+  validates_attachment :document,
+    presence: true,
+    content_type: {
+      content_type: ['application/pdf',
+                     'application/msword',
+                     'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                     'application/vnd.oasis.opendocument.text',
+                     'text/rtf',
+                     'application/rtf']}
 
-  belongs_to :claim
+    belongs_to :claim
 
-  #validates :claim, presence: true
-  #validates :description, presence: true
-  #validates :document, presence: true
+    #validates :claim, presence: true
+    #validates :description, presence: true
+    #validates :document, presence: true
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Document, type: :model do
-  subject { build(:document) }
   it { should belong_to(:claim) }
+  it { should have_attached_file(:document) }
+  it { should validate_attachment_presence(:document) }
 
+  it do
+    should validate_attachment_content_type(:document).
+      allowing('application/pdf',
+               'application/msword',
+               'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+               'application/vnd.oasis.opendocument.text',
+               'text/rtf',
+               'application/rtf').
+      rejecting('text/plain',
+                'text/html')
+  end
 
   #it { should validate_presence_of(:claim) }
   #it { should validate_presence_of(:description) }

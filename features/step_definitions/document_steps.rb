3238dd6b2751dc64b93406003cffe3c78488594e
Todd Tyree <tatyree@gmail.com>
2015-04-16 15:52:22 +0100

    Refactor upload feature.

--- features/step_definitions/document_steps.rb
+++ features/step_definitions/document_steps.rb
@@ -1,5 +1,6 @@
 When(/^I upload an example document$/) do
-  attach_file(:document, 'features/examples/shorter_lorem.docx')
+  attach_file(:document_document, 'features/examples/shorter_lorem.docx')
+  click_on('Upload')
 end
 
 Then(/^The example document should exist on the system$/) do

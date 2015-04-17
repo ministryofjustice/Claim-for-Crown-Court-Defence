a2016d013e40661eb16bd9b5fe5f987fa1e6525c
Himal Mandalia <himalmandalia@gmail.com>
2015-04-17 12:10:27 +0100

    Court type

--- spec/models/court_spec.rb
+++ spec/models/court_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe Court, type: :model do
   it { should validate_uniqueness_of(:code) }
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
+  it { should validate_presence_of(:court_type) }
+  it { should validate_inclusion_of(:court_type).in_array(%w( crown magistrate )) }
 end

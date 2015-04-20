1864be47fdb777892a33da39764c42811f0d9dfb
Edward Andress <eddie_andress@hotmail.com>
2015-04-20 16:33:01 +0100

    added validations for fee rate quantity and amount - added random expense trait

--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -8,5 +8,12 @@ FactoryGirl.define do
     rate "9.99"
     hours "9.99"
     amount "9.99"
+
+    trait :random_values do
+        quantity { rand(1..10) }
+        rate { rand(1.0..9.99) }
+        hours { rand(1.0..10.0) }
+        amount { quantity * rate * hours}
+    end
   end
 end

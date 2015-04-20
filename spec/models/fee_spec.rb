b80a1f7cebdff91faf90337d8ebbbf762d5db42d
Eddie Andress <eddieandress@Admins-MacBook-Pro-7.local>
2015-04-20 16:33:01 +0100

    added validations for fee rate quantity and amount - added random expense trait

--- spec/models/fee_spec.rb
+++ spec/models/fee_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Fee, type: :model do
   it { should belong_to(:claim) }
   it { should belong_to(:fee_type) }
+  it { should validate_presence_of(:rate) }
+  it { should validate_presence_of(:quantity) }
+  it { should validate_presence_of(:amount) }
 end

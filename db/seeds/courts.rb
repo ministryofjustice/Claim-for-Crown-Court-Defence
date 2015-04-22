6951a1f0c567b685dea67b1166ef9672daca0b6c
Eddie Andress <eddieandress@Admins-MacBook-Pro-7.local>
2015-04-22 13:09:19 +0100

    added real court info to court seed file

--- db/seeds/courts.rb
+++ db/seeds/courts.rb
@@ -1,2 +1,2 @@
-Court.find_or_create_by!(code: 'AAA', name: 'Some court', court_type: 'crown')
-Court.find_or_create_by!(code: 'BBB', name: 'Another court', court_type: 'magistrate')
+Court.find_or_create_by!(code: '467', name: 'York', court_type: 'crown')
+Court.find_or_create_by!(code: '2351', name: 'Newcastle upon Tyne', court_type: 'magistrate')

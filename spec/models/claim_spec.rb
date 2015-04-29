33278a748e438a8a20d64b91b21eedb775a5db90
Edward Andress <eddie_andress@hotmail.com>
2015-04-29 17:25:41 +0100

    fixing tests which rely on seeds

--- spec/models/claim_spec.rb
+++ spec/models/claim_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe Claim, type: :model do
   subject { create(:claim) }
 
   describe '.find_by_maat_reference' do
-    let!(:other_claim) { create(:claim) }
+    let!(:other_claim) { create(:claim, advocate: create(:advocate, chamber: (create(:chamber)))) }
 
     before do
       create(:defendant, maat_reference: '111111', claim_id: subject.id)

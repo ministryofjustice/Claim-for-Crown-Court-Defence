f81c0e7bbb12afa4868c1e565952faa1c8801f92
Himal Mandalia <himalmandalia@gmail.com>
2015-04-29 11:07:21 +0100

    Add vat registered and account number to Chamber

--- app/models/chamber.rb
+++ app/models/chamber.rb
@@ -2,5 +2,5 @@ class Chamber < ActiveRecord::Base
   has_many :advocates, -> { advocates }, class_name: 'User'
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :supplier_no, presence: true, uniqueness: { case_sensitive: false }
+  validates :account_number, presence: true, uniqueness: { case_sensitive: false }
 end
--- db/migrate/20150325120724_create_chambers.rb
+++ db/migrate/20150325120724_create_chambers.rb
@@ -2,11 +2,12 @@ class CreateChambers < ActiveRecord::Migration
   def change
     create_table :chambers do |t|
       t.string :name
-      t.string :supplier_no
+      t.string :account_number
+      t.boolean :vat_registered
 
       t.timestamps
     end
     add_index :chambers, :name
-    add_index :chambers, :supplier_no
+    add_index :chambers, :account_number
   end
 end

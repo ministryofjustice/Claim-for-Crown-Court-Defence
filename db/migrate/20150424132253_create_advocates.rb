2e149cc4f4eabd5976a52f2edba3ce70dbf8bb23
Himal Mandalia <himalmandalia@gmail.com>
2015-04-30 09:17:56 +0100

    Add Advocate first name and last name

--- db/migrate/20150424132253_create_advocates.rb
+++ db/migrate/20150424132253_create_advocates.rb
@@ -1,6 +1,9 @@
 class CreateAdvocates < ActiveRecord::Migration
   def change
     create_table :advocates do |t|
+      t.string :first_name
+      t.string :last_name
+
       t.references :chamber, index: true
 
       t.timestamps

86a2fc3967eadd605c38012730fcef2e02de863e
Himal Mandalia <himalmandalia@gmail.com>
2015-04-30 09:17:56 +0100

    Add Advocate first name and last name

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 20150428123404) do
   enable_extension "plpgsql"
 
   create_table "advocates", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
     t.integer  "chamber_id"
     t.datetime "created_at"
     t.datetime "updated_at"

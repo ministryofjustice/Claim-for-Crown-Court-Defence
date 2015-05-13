947ccbb3a2aacd5326f3ce2507ce35880b6d4118
Himal Mandalia <himalmandalia@gmail.com>
2015-05-13 09:21:12 +0100

    Find claims by advocate name

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -42,6 +42,11 @@ class Claim < ActiveRecord::Base
     def find_by_maat_reference(maat_reference)
       joins(:defendants).where('defendants.maat_reference = ?', maat_reference.upcase.strip)
     end
+
+    def find_by_advocate_name(advocate_name)
+      joins(:advocate)
+        .where("lower(first_name || ' ' || last_name) LIKE ?", "%#{advocate_name.downcase}%")
+    end
   end
 
   def calculate_fees_total

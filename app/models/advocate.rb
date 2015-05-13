4c6a5f322567fb6bbd250042b6999968be1ce196
Himal Mandalia <himalmandalia@gmail.com>
2015-05-13 09:52:56 +0100

    Remove Authenticatable concern and restore logic to Advocate and CaseWorker models

--- app/models/advocate.rb
+++ app/models/advocate.rb
@@ -1,12 +1,17 @@
 class Advocate < ActiveRecord::Base
   ROLES = %w{ admin advocate }
   include UserRoles
-  include Authenticatable
 
   belongs_to :chamber
+  has_one :user, as: :persona, inverse_of: :persona, dependent: :destroy
   has_many :claims, dependent: :destroy
 
+  validates :user, presence: true
   validates :first_name, :last_name, presence: true
+
+  accepts_nested_attributes_for :user
+
+  delegate :email, to: :user
 
   def name
     [first_name, last_name] * ' '

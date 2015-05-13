964148517ecb4359cc33e638069b4a6fe8d6bf32
Himal Mandalia <himalmandalia@gmail.com>
2015-05-13 17:13:53 +0100

    Remove "pending" scope and spec

--- app/models/claims/state_machine.rb
+++ app/models/claims/state_machine.rb
@@ -71,8 +71,6 @@ module Claims::StateMachine
     klass.state_machine.states.map(&:name).each do |s|
       klass.scope s, -> { klass.where(state: s) }
     end
-
-    klass.scope :pending, -> { klass.where(state: ['allocated', 'submitted']) }
   end
 
   private

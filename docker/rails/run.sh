b2115d0ad7643030a83e5529eec87e267da49c73
Himal Mandalia <himalmandalia@gmail.com>
2015-05-14 13:34:42 +0100

    Reset DB and execute demo data task

--- docker/rails/run.sh
+++ docker/rails/run.sh
@@ -7,12 +7,12 @@ case ${DOCKER_STATE} in
 migrate)
     echo "running migrate"
     bundle exec rake db:migrate
-    ;;  
+    ;;
 seed)
     echo "running seed"
-    bundle exec rake db:migrate
+    bundle exec rake db:reset
     bundle exec rake db:seed
-    bundle exec rake claims:all_states[25]
+    bundle exec rake claims:demo_data
     ;;
 esac
 exec bundle exec unicorn -p 80

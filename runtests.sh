e6e0a33e31fb5cc42d2d7ac42b6fc12b94224215
Ash Berlin <ash_github@firemirror.com>
2015-05-15 11:32:50 +0100

    Set Travis and Jenkins to run tests in the same way.
    We're running tests in Travis and on Jenkins (inside a docker container)
    and we need to make sure we run the same commands in both, else we'll
    change one and not the other
    
    Like just happened.

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -e
 # Script executing all the test tasks.
-rake db:clear
-rake db:migrate
-rake db:seed
-rake claims:allocated[38]
-rake claims:completed[10]
-rake test
+bundle exec rake db:migrate test

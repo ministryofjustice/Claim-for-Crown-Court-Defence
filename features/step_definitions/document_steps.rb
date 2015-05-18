69c73f2ddf9f52b4e52467ad30884cdc50f77de2
Eddie Andress <eddieandress@Admins-MacBook-Pro-7.local>
2015-05-18 15:54:22 +0100

    use pdf in integration tests and test conversion method in unit tests, stubbing the call to Libreoffice

--- features/step_definitions/document_steps.rb
+++ features/step_definitions/document_steps.rb
@@ -5,19 +5,13 @@ end
 When(/^I upload an example document "shorter_lorem.docx"$/) do
   @notes = SecureRandom.uuid
   select 'Other', from: 'document_document_type_id'
-  attach_file(:document_document, 'features/examples/shorter_lorem.docx')
+  attach_file(:document_document, 'features/examples/longer_lorem.pdf')
   fill_in('Notes', with: @notes)
   click_on('Upload')
 end
 
 Then(/^The example document should exist on the system$/) do
   expect(Document.find_by(notes: @notes)).to be_present
-end
-
-Then(/^the document should have a duplicate pdf version$/) do
-  @doc = Document.find_by(notes: @notes)
-  @doc_duplicate = @doc.document.path.slice('.')[0] + '.pdf'
-  expect(@doc_duplicate).to be_present
 end
 
 Then(/^I should be told I need to select a claim first$/) do

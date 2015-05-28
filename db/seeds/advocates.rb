fb5b24bc58a87b283bc8062a05e2b2804fe0d209
Himal Mandalia <himalmandalia@gmail.com>
2015-05-28 12:28:17 +0100

    Refactor first and last name fields into User model in order to avoid duplication in Advocate and CaseWorker models.

--- db/seeds/advocates.rb
+++ db/seeds/advocates.rb
@@ -2,24 +2,28 @@ chamber = Chamber.find_or_create_by!(name: 'Test chamber/firm', account_number: 
 
 if User.find_by(email: 'advocate@example.com').blank?
   user = User.create!(
+    first_name: 'John',
+    last_name: 'Smith',
     email: 'advocate@example.com',
     password: ENV['ADVOCATE_PASSWORD'],
     password_confirmation: ENV['ADVOCATE_PASSWORD']
   )
 
-  advocate = Advocate.new(first_name: 'John', last_name: 'Smith', role: 'advocate', chamber_id: chamber.id)
+  advocate = Advocate.new(role: 'advocate', chamber_id: chamber.id)
   advocate.user = user
   advocate.save!
 end
 
 if User.find_by(email: 'advocateadmin@example.com').blank?
   user = User.create!(
+    first_name: 'John',
+    last_name: 'Smith',
     email: 'advocateadmin@example.com',
     password: ENV['ADMIN_PASSWORD'],
     password_confirmation: ENV['ADMIN_PASSWORD']
   )
 
-  advocate = Advocate.new(first_name: 'John', last_name: 'Smith', role: 'admin', chamber_id: chamber.id)
+  advocate = Advocate.new(role: 'admin', chamber_id: chamber.id)
   advocate.user = user
   advocate.save!
 end
--- spec/factories/case_workers.rb
+++ spec/factories/case_workers.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :case_worker do
     after(:build) do |case_worker|
-      case_worker.user ||= build(:user, email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
+      case_worker.user ||= build(:user, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: 'password', password_confirmation: 'password')
     end
 
     role 'case_worker'

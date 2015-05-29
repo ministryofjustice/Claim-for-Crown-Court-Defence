a416e6ee2718d10f37e155663e08450ee77c2120
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-05-29 09:38:07 +0100

    resolve merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150528152825) do
-=======
 ActiveRecord::Schema.define(version: 20150528151004) do
->>>>>>> develop
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

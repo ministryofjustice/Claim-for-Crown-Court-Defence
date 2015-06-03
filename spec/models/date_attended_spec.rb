64c13102db5e379663646c13a14fa1d0705ad94c
Himal Mandalia <himalmandalia@gmail.com>
2015-06-03 15:47:11 +0100

    Display dates attended on claim detail summary

--- spec/models/date_attended_spec.rb
+++ spec/models/date_attended_spec.rb
@@ -15,4 +15,22 @@ require 'rails_helper'
 RSpec.describe DateAttended, type: :model do
   it { should belong_to(:fee) }
   it { should validate_presence_of(:date) }
+
+  describe '#to_s' do
+    context 'when date_to present' do
+      subject { create(:date_attended, date: Date.parse('1/1/2015'), date_to: Date.parse('5/1/2015')) }
+
+      it 'formats the date and date_to' do
+        expect(subject.to_s).to eq('01/01/15 - 05/01/15')
+      end
+    end
+
+    context 'when only date present' do
+      subject { create(:date_attended, date: Date.parse('1/1/2015')) }
+
+      it 'formats the date' do
+        expect(subject.to_s).to eq('01/01/15')
+      end
+    end
+  end
 end

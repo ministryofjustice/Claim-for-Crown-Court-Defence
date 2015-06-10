28e4098ba8336fd8b7cf528f708fa976fb0d44f7
Mark Ford <mark.ford@digital.justice.gov.uk>
2015-06-10 14:13:57 +0100

    add reminder behaviour to additional defendants

--- app/assets/javascripts/judicialapportionment.js
+++ app/assets/javascripts/judicialapportionment.js
@@ -2,19 +2,19 @@ var moj = moj || {};
 moj.Modules.judicialApportionment = {
   reminder: 'Remember: You must attach the Order for judicial apportionment in the Supporting evidence documents section',
   init: function(){
-    moj.Modules.judicialApportionment.state();
-    $('input[id$="judicial_apportionment"]').change(function(){
-      moj.Modules.judicialApportionment.state();
+    $('#claim-form').on('click', 'input[id$="judicial_apportionment"]', function(){
+      var whut = $(this).attr('id');
+      moj.Modules.judicialApportionment.state(whut);
     });
   },
-  state: function(){
-    if ($('input[id$="judicial_apportionment"]').is(':checked')){
+  state: function(whut){
+    if ($('#' + whut).is(':checked')){
       // show reminder
-      $('input[id$="judicial_apportionment"]').parent('label').append('<span class="reminder">' + moj.Modules.judicialApportionment.reminder +'</span>')
+      $('#' + whut).parent('label').append('<span class="reminder">' + moj.Modules.judicialApportionment.reminder +'</span>')
     }
     else{
       // hide reminder
-      $('input[id$="judicial_apportionment"]').parent('label').find('.reminder').remove();
+      $('#' + whut).parent('label').find('.reminder').remove();
     }
   }
 };

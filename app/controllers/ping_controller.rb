1cc3eb8859588e78f12b5e3bddbebd28548dbf9a
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2015-06-16 16:31:24 +0100

    Added the BUILD_ID to the ping.json status page
    It should display the build timestamp.

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -6,6 +6,7 @@ class PingController  < ApplicationController
       respond_with(
         {
           'Build Number' => ENV['BUILD_NUMBER'] || "Not Avaialble",
+          'Build Date'   => ENV['BUILD_ID'] || 'Not Available',
           'Commit SHA'   => ENV['GIT_COMMIT'] || 'Not Available'
         }
       )

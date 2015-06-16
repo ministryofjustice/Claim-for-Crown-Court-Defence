c93531623e412d164fd8d617b30d0a5578bed187
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-06-16 09:32:52 +0100

    fix identation issue on evidence list item spec

--- spec/models/evidence_list_item_spec.rb
+++ spec/models/evidence_list_item_spec.rb
@@ -3,41 +3,41 @@ require 'rails_helper'
 
 RSpec.describe EvidenceListItem, type: :model do
 
-	let(:evidence_list_item) { create(:evidence_list_item) }
-	let(:evidence_list_item_dup) { evidence_list_item.dup }
+  let(:evidence_list_item) { create(:evidence_list_item) }
+  let(:evidence_list_item_dup) { evidence_list_item.dup }
 
- 	it { should have_many :evidence_list_item_claims }
+  it { should have_many :evidence_list_item_claims }
 
-	it "has a valid factory" do
-		expect(create(:evidence_list_item)).to be_valid
-	end
+  it "has a valid factory" do
+    expect(create(:evidence_list_item)).to be_valid
+  end
 
-	context "description" do
+  context "description" do
 
-		it { should validate_presence_of :description }
+    it { should validate_presence_of :description }
 
-		# note: shoulda-matcher validate_uniqueness_of fails due to two unique columns//attributes
-		it "must be unique" do
-				evidence_list_item_dup.item_order = evidence_list_item_dup.item_order+1
-				expect { evidence_list_item_dup.save! }.to raise_error(ActiveRecord::RecordInvalid,/Description has already been taken/)
-		end
+    # note: shoulda-matcher validate_uniqueness_of fails due to two unique columns//attributes
+    it "must be unique" do
+      evidence_list_item_dup.item_order = evidence_list_item_dup.item_order+1
+      expect { evidence_list_item_dup.save! }.to raise_error(ActiveRecord::RecordInvalid,/Description has already been taken/)
+    end
 
-		it "is case-insensitive" do
-			expect(create(:evidence_list_item, description: 'Evidence Test Document')).to be_valid
-		end
+    it "is case-insensitive" do
+      expect(create(:evidence_list_item, description: 'Evidence Test Document')).to be_valid
+    end
 
-	end
+  end
 
-	context "item_order" do
+  context "item_order" do
 
-		it { should validate_presence_of :item_order }
+    it { should validate_presence_of :item_order }
 
-		# note: shoulda-matcher validate_uniqueness_of fails due to two unique columns//attributes
-		it "must be unique" do
-			evidence_list_item_dup.description = evidence_list_item_dup.description << 'modifier'
-			expect { evidence_list_item_dup.save! }.to raise_error(ActiveRecord::RecordInvalid,/Item order has already been taken/)
-		end
+    # note: shoulda-matcher validate_uniqueness_of fails due to two unique columns//attributes
+    it "must be unique" do
+      evidence_list_item_dup.description = evidence_list_item_dup.description << 'modifier'
+      expect { evidence_list_item_dup.save! }.to raise_error(ActiveRecord::RecordInvalid,/Item order has already been taken/)
+    end
 
-	end
+  end
 
 end

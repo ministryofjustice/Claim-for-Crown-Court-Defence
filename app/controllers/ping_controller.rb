90967f14c4d7a3a49dde8f8dc0c1a931193be38e
Benedetto Lo Giudice <benedetto.lo-giudice@digital.justice.gov.uk>
2015-06-17 12:21:58 +0100

    Attempt to solve Rails error in ping.json

--- app/controllers/ping_controller.rb
+++ app/controllers/ping_controller.rb
@@ -2,14 +2,12 @@ class PingController  < ApplicationController
   respond_to :json
 
   def index
-    Rails.logger.silence do
       respond_with(
         {
           'Build Number' => ENV['BUILD_NUMBER'] || "Not Avaialble",
-          'Build Date'   => ENV['BUILD_ID'] || 'Not Available',
+          'Build Date'   => ENV['BUILD_DATE'] || 'Not Available',
           'Commit SHA'   => ENV['GIT_COMMIT'] || 'Not Available'
         }
       )
-    end
   end
 end

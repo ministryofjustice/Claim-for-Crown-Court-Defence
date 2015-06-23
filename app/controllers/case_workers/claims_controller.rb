5b84006db1ea0d969b3bdd5006de9b5a3f6759fb
Himal Mandalia <himalmandalia@gmail.com>
2015-06-23 15:53:52 +0100

    Refactor search mappings

--- app/controllers/case_workers/claims_controller.rb
+++ app/controllers/case_workers/claims_controller.rb
@@ -36,19 +36,20 @@ class CaseWorkers::ClaimsController < CaseWorkers::ApplicationController
 
   def search
     params[:search_field] ||= 'All'
+    @claims = @claims.search(*search_option_mappings[params[:search_field]], params[:search])
+  end
 
-    @claims = case params[:search_field]
-      when 'All'
-        options = [:maat_reference, :defendant_name]
-        options << :case_worker_name_or_email if current_user.persona.admin?
-        @claims.search(*options,  params[:search])
-      when 'MAAT Reference'
-        @claims.search(:maat_reference, params[:search])
-      when 'Defendant'
-        @claims.search(:defendant_name, params[:search])
-      when 'Case worker'
-        @claims.search(:case_worker_name_or_email, params[:search])
-    end
+  def search_option_mappings
+    option_mappings = {
+      'All' => [:maat_reference, :defendant_name],
+      'MAAT Reference' => [:maat_reference],
+      'Defendant' => [:defendant_name],
+      'Case worker' => [:case_worker_name_or_email]
+    }
+
+    option_mappings['All'] << :case_worker_name_or_email if current_user.persona.admin?
+
+    option_mappings
   end
 
   def claim_params

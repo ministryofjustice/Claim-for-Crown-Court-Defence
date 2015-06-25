3516cb29c0fd84b1f4427be9f9957d279c7db821
Edward Andress <eddie_andress@hotmail.com>
2015-06-25 14:40:17 +0100

    fix failing tests

--- spec/models/defendant_spec.rb
+++ spec/models/defendant_spec.rb
@@ -61,7 +61,7 @@ RSpec.describe Defendant, type: :model do
       it 'should not be valid if there are no representation orders' do
         defendant.representation_orders = []
         expect(defendant).not_to be_valid
-        expect(defendant.errors[:representation_orders]).to eq [ 'There must be at least one representation order per defendant' ]
+        expect(defendant.errors[:representation_orders]).to eq [ " - there must be at least one representation order per defendant" ]
       end
     end
   end

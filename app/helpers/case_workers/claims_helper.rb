b8bf6141a3f48e53e37d00660ff498339674e7bd
Edward Andress <eddie_andress@hotmail.com>
2015-06-26 14:15:05 +0100

    include all states that imply assessment-completion in caseworker's 'completed tab'

--- app/helpers/case_workers/claims_helper.rb
+++ app/helpers/case_workers/claims_helper.rb
@@ -1,17 +1,17 @@
 module CaseWorkers::ClaimsHelper
   def current_claims_count
-    current_user.claims.allocated.count
+    current_user.claims.caseworker_dashboard_under_assessment.count
   end
 
   def completed_claims_count
-    current_user.claims.completed.count
+    current_user.claims.caseworker_dashboard_completed.count
   end
 
   def allocated_claims_count
-    Claim.allocated.count
+    Claim.caseworker_dashboard_under_assessment.count
   end
 
   def unallocated_claims_count
-    Claim.submitted.count
+    Claim.submitted.count # doesn't include appealed claims
   end
 end

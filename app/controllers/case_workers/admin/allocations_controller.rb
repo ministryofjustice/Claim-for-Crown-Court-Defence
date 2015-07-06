e443c821f2ba74673478dd9ced109cfc1ff60c61
Himal Mandalia <himalmandalia@gmail.com>
2015-07-06 15:41:21 +0100

    Refactor methods

--- app/controllers/case_workers/admin/allocations_controller.rb
+++ app/controllers/case_workers/admin/allocations_controller.rb
@@ -1,19 +1,14 @@
 class CaseWorkers::Admin::AllocationsController < CaseWorkers::Admin::ApplicationController
   before_action :set_case_workers, only: [:new, :create]
   before_action :set_claims, only: [:new, :create]
+  before_action :set_summary_values, only: [:new], if: :summary_from_previous_request?
+  before_action :process_claim_ids, only: [:create], if: :quantity_allocation?
 
   def new
-    if params[:case_worker_id].present? && params[:claim_ids].present?
-      @case_worker = CaseWorker.find(params[:case_worker_id])
-      @allocated_claims = Claim.find(params[:claim_ids].reject(&:blank?))
-    end
-
     @allocation = Allocation.new
   end
 
   def create
-    process_claim_ids
-
     @allocation = Allocation.new(allocation_params)
 
     if @allocation.save
@@ -25,11 +20,22 @@ class CaseWorkers::Admin::AllocationsController < CaseWorkers::Admin::Applicatio
 
   private
 
+  def summary_from_previous_request?
+    params[:case_worker_id].present? && params[:claim_ids].present?
+  end
+
+  def set_summary_values
+    @case_worker = CaseWorker.find(params[:case_worker_id])
+    @allocated_claims = Claim.find(params[:claim_ids].reject(&:blank?))
+  end
+
+  def quantity_allocation?
+    params[:quantity_to_allocate].present? && params[:quantity_to_allocate].to_i.is_a?(Integer)
+  end
+
   def process_claim_ids
-    if params[:quantity_to_allocate].present? && params[:quantity_to_allocate].to_i.is_a?(Integer)
-      quantity_to_allocate = params[:quantity_to_allocate].to_i
-      params[:allocation][:claim_ids] = @claims.limit(quantity_to_allocate).map(&:id).map(&:to_s)
-    end
+    quantity_to_allocate = params[:quantity_to_allocate].to_i
+    params[:allocation][:claim_ids] = @claims.limit(quantity_to_allocate).map(&:id).map(&:to_s)
   end
 
   def set_case_workers

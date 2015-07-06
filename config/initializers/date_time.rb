beaec4225a72d9dc4bb70df6997274dfa8dce428
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-07-06 14:49:43 +0100

    add default date and time formats
    This has already been applied through constants in the settings.yml
    file but this provides a catchall for other dates.

---
+++ config/initializers/date_time.rb
@@ -0,0 +1,8 @@
+# Date
+Date::DATE_FORMATS[:default] = Settings.date_format
+
+# Time
+DateTime::DATE_FORMATS[:default] = Settings.date_time_format
+
+# Time
+Time::DATE_FORMATS[:default] = Settings.date_time_format

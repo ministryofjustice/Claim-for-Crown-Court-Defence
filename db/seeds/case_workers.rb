143844e30207c9253db4975d83e35ca61bd4b1fe
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-07-09 17:43:05 +0100

    fix accordion bug and case worker allocation bug

--- db/seeds/case_workers.rb
+++ db/seeds/case_workers.rb
@@ -2,8 +2,8 @@ require 'csv'
 require Rails.root.join('db','seeds','seed_helper')
 
 # create test/dummy case workers
-SeedHelper.find_or_create_caseworker!(first_name: 'Bill', last_name: 'Smith', email: 'caseworkeradmin@example.com', location: 'Nottingham', role: 'admin', password_env_var: 'ADMIN_PASSWORD')
 SeedHelper.find_or_create_caseworker!(first_name: 'Dave', last_name: 'Smith', email: 'caseworker@example.com', location: 'Nottingham', role: 'case_worker', password_env_var: 'CASE_WORKER_PASSWORD')
+SeedHelper.find_or_create_caseworker!(first_name: 'Bill', last_name: 'Smith', email: 'caseworkeradmin@example.com', location: 'Nottingham', role: 'admin', password_env_var: 'ADMIN_PASSWORD')
 
 # create actual case workers
 file_path = Rails.root.join('lib', 'assets', 'data', 'case_workers.csv')

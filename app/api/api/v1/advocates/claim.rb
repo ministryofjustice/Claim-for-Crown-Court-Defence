c63ce2b7e7db36eb10fba822df29739f144eb688
Stephen Strudwick <stevestrudwick@Admins-MacBook-Pro-9.local>
2015-07-15 11:57:19 +0100

    fixes indention in api claims

--- app/api/api/v1/advocates/claim.rb
+++ app/api/api/v1/advocates/claim.rb
@@ -1,60 +1,60 @@
 module API
   module V1
-  module Advocates
-    class Claim < Grape::API
+    module Advocates
+      class Claim < Grape::API
 
-    version 'v1', using: :header, vendor: 'Advocate Defence Payments'
-    format :json
-    prefix 'api/advocates'
-    content_type :json, 'application/json'
+      version 'v1', using: :header, vendor: 'Advocate Defence Payments'
+      format :json
+      prefix 'api/advocates'
+      content_type :json, 'application/json'
 
-    resource :claims do
+      resource :claims do
 
-      helpers do
-        params :claim_creation do
-          requires :advocate_id, type: Integer, desc: "Your unique identifier as an adavocate."
-          requires :creator_id, type: Integer, desc: "Your unique identifier as a creator of the claim."
-          requires :case_number, type: String, desc: "The case number"
-          requires :case_type, type: String, desc: "The case type i.e trial"
-          optional :cms_number, type: String, desc: "The CMS number"
+        helpers do
+          params :claim_creation do
+            requires :advocate_id, type: Integer, desc: "Your unique identifier as an adavocate."
+            requires :creator_id, type: Integer, desc: "Your unique identifier as a creator of the claim."
+            requires :case_number, type: String, desc: "The case number"
+            requires :case_type, type: String, desc: "The case type i.e trial"
+            optional :cms_number, type: String, desc: "The CMS number"
+          end
+
+          def args
+            {
+              advocate_id: params[:advocate_id],
+              creator_id: params[:creator_id],
+              case_number: params[:case_number],
+              case_type: params[:case_type],
+              cms_number: params[:cms_number]
+            }
+          end
+
         end
 
-        def args
-          {
-            advocate_id: params[:advocate_id],
-            creator_id: params[:creator_id],
-            case_number: params[:case_number],
-            case_type: params[:case_type],
-            cms_number: params[:cms_number]
-          }
+        desc "Create a claim."
+
+        params do
+          use :claim_creation
+        end
+
+        post do
+          ::Claim.create!(args)
+        end
+
+        desc "Validate a claim."
+
+        params do
+          use :claim_creation
+        end
+
+        post '/validate' do
+          ::Claim.new(args).valid?
         end
 
       end
 
-      desc "Create a claim."
-
-      params do
-        use :claim_creation
+      add_swagger_documentation
       end
-
-      post do
-        ::Claim.create!(args)
-      end
-
-      desc "Validate a claim."
-
-      params do
-        use :claim_creation
-      end
-
-      post '/validate' do
-        ::Claim.new(args).valid?
-      end
-
     end
-
-    add_swagger_documentation
-    end
-  end
   end
 end

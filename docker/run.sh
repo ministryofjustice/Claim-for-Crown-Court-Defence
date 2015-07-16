7fdd1774032c92359efaea01d29e948602e60667
Benedetto Lo Giudice <benedetto.lo-giudice@digital.justice.gov.uk>
2015-07-16 13:20:17 +0100

    Changes to run.sh for container   - if then else statement contained error

--- docker/run.sh
+++ docker/run.sh
@@ -4,14 +4,9 @@ set -e
 if [ "$DOCKER_STATE" = "none" ]; then
   SLEEP_TIME=0
 else
-  if [ "$RUN_MIGRATE_OR_SEED" = "True" ]; then
-    # if DOCKER_STATE is none, no need to wait
-    if [ "$DOCKER_STATE" = "none" ]; then
-        SLEEP_TIME=0
-    fi
-  else
-    DOCKER_STATE=none
-    SLEEP_TIME=2m
+  if [ "$RUN_MIGRATE_OR_SEED" != "True" ]; then
+      SLEEP_TIME=5m
+      DOCKER_STATE=none
   fi
 fi
 
@@ -27,4 +22,5 @@ esac
 if [ -n "$SLEEP_TIME" ]; then
   sleep $SLEEP_TIME
 fi
+
 exec bundle exec unicorn -p 80

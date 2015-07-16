0e392d4b6dcab4ae3ff473770250dd5947925403
Stephen Strudwick <stevestrudwick@Admins-MacBook-Pro-9.local>
2015-07-16 16:44:02 +0100

    tidied api claim specs

--- spec/api/v1/claims/claim_spec.rb
+++ spec/api/v1/claims/claim_spec.rb
@@ -12,22 +12,28 @@ describe API::V1::Advocates::Claim do
     describe "POST /api/advocates/claims/validate" do
 
       let!(:current_advocate) { create(:advocate) }
+      let!(:claim_params) { {:advocate_id => current_advocate.id, :creator_id => current_advocate.id, :case_type => 'trial', :case_number => 12345} }
 
       it "returns success" do
-        post "/api/advocates/claims/validate", {:advocate_id => current_advocate.id, :creator_id => current_advocate.id, :case_type => 'trial', :case_number => 12345}
+        post "/api/advocates/claims/validate", claim_params
         expect(last_response.status).to eq(200)
         expect(last_response.body).to eq("true")
       end
 
       it "returns an error when advocate id is invalid" do
-        post "/api/advocates/claims/validate", {:advocate_id => "324YUGH2", :creator_id => 1, :case_type => 'trial', :case_number => 12345 }
+        bad_claim_params = claim_params
+        bad_claim_params[:advocate_id] = "32FGHJ4757"
+
+        post "/api/advocates/claims/validate", bad_claim_params
         expect(last_response.status).to eq(400)
         error = JSON.parse(last_response.body)['error']
         expect(error).to eq("advocate_id is invalid")
       end
 
       it "returns an error when required param is missing" do
-        post "/api/advocates/claims/validate", {:advocate_id => current_advocate.id, :creator_id => current_advocate.id, :case_type => 'trial' }
+        claim_params.delete(:case_number) 
+
+        post "/api/advocates/claims/validate", claim_params
         expect(last_response.status).to eq(400)
         error = JSON.parse(last_response.body)['error']
         expect(error).to eq("case_number is missing")

ba226bd45ba210b55e83eb580c028ece00ab90c0
Stephen Strudwick <stevestrudwick@Admins-MacBook-Pro-9.local>
2015-07-16 12:12:06 +0100

    better tests for claims api

--- spec/api/v1/claims/claim_spec.rb
+++ spec/api/v1/claims/claim_spec.rb
@@ -19,11 +19,18 @@ describe API::V1::Advocates::Claim do
         expect(last_response.body).to eq("true")
       end
 
-      it "returns false" do
-        post "/api/advocates/claims/validate", {:advocate_id => 1, :creator_id => 1, :case_type => 'trial'}
+      it "returns an error when advocate id is invalid" do
+        post "/api/advocates/claims/validate", {:advocate_id => "324YUGH2", :creator_id => 1, :case_type => 'trial', :case_number => 12345 }
         expect(last_response.status).to eq(400)
-        puts JSON.parse(last_response.body)
-        #expect(last_response.body).to eq("false")
+        error = JSON.parse(last_response.body)['error']
+        expect(error).to eq("advocate_id is invalid")
+      end
+
+      it "returns an error when required param is missing" do
+        post "/api/advocates/claims/validate", {:advocate_id => current_advocate.id, :creator_id => current_advocate.id, :case_type => 'trial' }
+        expect(last_response.status).to eq(400)
+        error = JSON.parse(last_response.body)['error']
+        expect(error).to eq("case_number is missing")
       end
 
     end

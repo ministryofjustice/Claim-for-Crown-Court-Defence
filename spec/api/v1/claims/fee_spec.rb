98a802d719781dcfbeb78589fee4e64432d48119
Edward Andress <eddie_andress@hotmail.com>
2015-07-27 17:23:06 +0100

    create fees over api

---
+++ spec/api/v1/claims/fee_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe API::V1::Advocates::Fee do
+
+  include Rack::Test::Methods
+
+  CREATE_FEE_ENDPOINT = "/api/advocates/fees"
+  VALIDATE_FEE_ENDPOINT = "/api/advocates/fees/validate"
+
+  let!(:fee_type)            { create(:fee_type, id: 1) }
+  let!(:claim)               { create(:claim) }
+  let!(:valid_fee_params)    { {claim_id: claim.id, fee_type_id: fee_type.id, quantity: 1, rate: 54321} }
+  let!(:invalid_fee_params)  { {claim_id: claim.id} }
+
+  describe 'POST api/advocates/fees' do
+
+    def post_to_create_endpoint
+      post CREATE_FEE_ENDPOINT, valid_fee_params, format: :json
+    end
+
+    context 'when fee params are valid' do
+
+      it 'returns 201 and creates a new fee record' do
+        response = post_to_create_endpoint
+        expect(response.status).to eq 201
+        expect(Fee.last.rate.to_i).to eq 54321
+      end
+
+    end
+
+  end
+
+  describe "POST /api/advocates/fees/validate" do
+
+    def post_to_validate_endpoint(params)
+      post VALIDATE_FEE_ENDPOINT, params, format: :json
+    end
+
+    it 'returns 200 when the params are valid' do
+        response = post_to_validate_endpoint(valid_fee_params)
+        expect(response.status).to eq 200
+    end
+
+    it 'returns 400 when the params are invalid' do
+        invalid_response = post_to_validate_endpoint(invalid_fee_params)
+        expect(invalid_response.status).to eq 400
+    end
+
+  end
+
+end

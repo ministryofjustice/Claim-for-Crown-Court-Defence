c707ed1a6f852022aed4e38fbb6f8ec3f9aeab5b
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2015-08-13 22:03:47 +0100

    remove breadcrumbs in the controller

--- app/controllers/case_workers/claims_controller.rb
+++ app/controllers/case_workers/claims_controller.rb
@@ -6,16 +6,11 @@ class CaseWorkers::ClaimsController < CaseWorkers::ApplicationController
   before_action :set_claim_ids_and_count, only: [:show]
 
   def index
-    add_breadcrumb 'Dashboard', case_workers_root_path
-
     search if params[:search].present?
     @claims = @claims.order("#{sort_column} #{sort_direction}")
   end
 
   def show
-    add_breadcrumb 'Dashboard', case_workers_root_path
-    add_breadcrumb "Claim: #{@claim.case_number}", case_workers_claim_path(@claim)
-
     @doc_types = DocType.all
     @messages = @claim.messages.most_recent_first
     @message = @claim.messages.build

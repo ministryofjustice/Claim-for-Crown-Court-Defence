eb999e4cc0deca0fab7eec31625afc7a70e38e73
Himal Mandalia <himalmandalia@gmail.com>
2015-08-20 16:21:31 +0100

    Display MAAT reference in claim detail

--- spec/presenters/claim_presenter_spec.rb
+++ spec/presenters/claim_presenter_spec.rb
@@ -113,15 +113,15 @@ RSpec.describe ClaimPresenter do
       defendant_2 = FactoryGirl.build :defendant
       Timecop.freeze 5.days.ago do
         defendant_1.representation_orders = [
-          FactoryGirl.build(:representation_order, representation_order_date: Date.new(2015,3,1), granting_body: "Crown Court"),
-          FactoryGirl.build(:representation_order, representation_order_date: Date.new(2015,8,13), granting_body: "Magistrate's Court"),
+          FactoryGirl.build(:representation_order, representation_order_date: Date.new(2015,3,1), granting_body: "Crown Court", maat_reference: '1234abc'),
+          FactoryGirl.build(:representation_order, representation_order_date: Date.new(2015,8,13), granting_body: "Magistrate's Court", maat_reference: 'abc1234'),
         ]
       end
       Timecop.freeze 2.days.ago do
-        defendant_2.representation_orders =[ FactoryGirl.build(:representation_order, representation_order_date: Date.new(2015,3,1), granting_body: "Magistrate's Court") ]
+        defendant_2.representation_orders =[ FactoryGirl.build(:representation_order, representation_order_date: Date.new(2015,3,1), granting_body: "Magistrate's Court", maat_reference: 'xyz4321') ]
       end
       claim.defendants = [ defendant_1, defendant_2 ]
-      expect(subject.representation_order_details).to eq( "Crown Court 01/03/2015<br/>Magistrate's Court 13/08/2015<br/>Magistrate's Court 01/03/2015" )
+      expect(subject.representation_order_details).to eq( "Crown Court 01/03/2015 1234abc<br/>Magistrate's Court 13/08/2015 abc1234<br/>Magistrate's Court 01/03/2015 xyz4321" )
     end
   end
 

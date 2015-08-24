a1f513e3f70936e2f20e01dbc179cb3a3b7ddec0
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-08-24 11:02:05 +0100

    refactor duplicate code

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -286,14 +286,14 @@ class Claim < ActiveRecord::Base
   def opened_for_redetermination?
     return true if self.redetermination?
 
-    transition = claim_state_transitions.order(created_at: :asc).last
+    transition = last_state_transition
     transition && transition.from == 'redetermination'
   end
 
   def written_reasons_outstanding?
     return true if self.awaiting_written_reasons?
 
-    transition = claim_state_transitions.order(created_at: :asc).last
+    transition = last_state_transition
     transition && transition.from == 'awaiting_written_reasons'
   end
 

e341009cbf4cbab2fb37a17877ad07398aed6008
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2015-08-28 11:34:41 +0100

    Further refactoring to reuse code for the onload/init function

--- app/assets/javascripts/case_worker/claims/determination_calculator.js
+++ app/assets/javascripts/case_worker/claims/determination_calculator.js
@@ -5,6 +5,23 @@ var adp = adp || {};
 adp.determination = {
   init : function(container_id) {
     this.addChangeEvent(container_id);
+
+    $(container_id)
+      //Find all the rows
+      .find('tr')
+      //that have input fields
+      .has(':text')
+      //Work out the total for each row
+      .each(function(){
+        //cache the current row
+        var $tr = $(this),
+        //get the first text element
+        firstInput = $tr.find(':text').get(0);
+
+        //Calculate the rows total.
+        adp.redetermination.calculateRow(firstInput);
+      })
+
   },
   calculateAmount: function(fee, expenses) {
 
@@ -18,29 +35,31 @@ adp.determination = {
 
   },
   addChangeEvent: function(container_id) {
+
     $('#' + container_id).on('change', ':text', function(e) {
-
-      //Cache the element that triggered the event
-      var $element = $(this),
-      //Find the row the element is in
-      $tr = $element.closest('tr'),
-      //Find the fees column
-      $fees = $tr.find('.js-fees'),
-      //Parse the value
-      fees = parseFloat($fees.val().replace(/,/g, "")),
-      //Find the Expenses column
-      $expenses = $tr.find('.js-expenses'),
-      // Parse the value
-      expenses = parseFloat($expenses.val().replace(/,/g, "")),
-      //Work out the total
-      total = adp.determination.calculateAmount(fees,expenses);
-
-      if (isNaN(total) ){
-        $('.js-total-determination').text('£0.00');
-      }
-      else{
-        $('.js-total-determination').text('£ '+ total);
-      }
+      adp.determination.calculateRow(this);
     });
+  },
+  calculateRow : function(element){
+    //Cache the element that triggered the event
+    var $element = $(element),
+    //Find the row the element is in
+    $tr = $element.closest('tr'),
+    //Find the fees column
+    $fees = $tr.find('.js-fees'),
+    //Parse the value
+    fees = parseFloat($fees.val().replace(/,/g, "")),
+    //Find the Expenses column
+    $expenses = $tr.find('.js-expenses'),
+    // Parse the value
+    expenses = parseFloat($expenses.val().replace(/,/g, "")),
+    //Work out the total
+    total = adp.determination.calculateAmount(fees,expenses);
+
+    if (isNaN(total) ){
+      $('.js-total-determination').text('£0.00');
+    }else{
+      $('.js-total-determination').text('£ '+ total);
+    }
   }
 };

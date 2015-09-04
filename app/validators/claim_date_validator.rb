3fc27c4d750a07ce935919c64e3d58e85afcbace
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-09-04 13:07:07 +0100

    refactored common validation routines into BaseClaimValidator

--- app/validators/claim_date_validator.rb
+++ app/validators/claim_date_validator.rb
@@ -1,19 +1,11 @@
-class ClaimDateValidator < ActiveModel::Validator
+# class ClaimDateValidator < ActiveModel::Validator
+class ClaimDateValidator < BaseClaimValidator
 
-  @@date_fields = [ 
+  @@claim_date_validator_fields = [ 
     :trial_fixed_notice_at, 
     :trial_fixed_at 
   ]
-
-  def validate(record)
-    @record = record
-    if @record.perform_validation?
-      @@date_fields.each do |date_field|
-        self.send("validate_#{date_field}")
-      end
-    end
-  end
-
+ 
 
   private
 
@@ -44,9 +36,6 @@ class ClaimDateValidator < ActiveModel::Validator
 
 
 
-
-
-
   def validate_not_after(date, attribute, message)
     return if @record.send(attribute).nil? || date.nil?
     add_error(attribute, message) if @record.send(attribute) > date
@@ -59,14 +48,6 @@ class ClaimDateValidator < ActiveModel::Validator
 
   def case_type_in(*case_types)
     case_types.include?(@record.case_type.name)
-  end
-
-  def validate_presence(attribute, message)
-    add_error(attribute, message) if @record.send(attribute).blank?
-  end
-
-  def add_error(attribute, message)
-    @record.errors[attribute] << message
   end
 
   def earliest_rep_order

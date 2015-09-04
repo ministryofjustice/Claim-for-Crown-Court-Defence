2dcb0f6ff5dfc051a2aa89dab299ac90086f2761
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-09-04 12:49:52 +0100

    ClaimDateValidator - trial_fixed_at

--- app/validators/claim_date_validator.rb
+++ app/validators/claim_date_validator.rb
@@ -28,8 +28,24 @@ class ClaimDateValidator < ActiveModel::Validator
     validate_not_before(earliest_rep_order, :trial_fixed_notice_at, "Date notice of first fixed/warned issued may not be earlier than the first representation order date")
   end
 
+
+  # required when case type is cracked, cracked before retrieal
+  # cannot be in the future
+  # cannot be before earliest rep order
+  # cannot be more than 5 years old
+  # cannot be before trial_fixed_notice_at
   def validate_trial_fixed_at
+    validate_presence(:trial_fixed_at, "Please enter valid date of first fixed/warned") if case_type_in("Cracked Trial", "Cracked before retrial")
+    validate_not_after(Date.today, :trial_fixed_at, "Date of first fixed/warned may not be in the future")
+    validate_not_before(5.years.ago, :trial_fixed_at, "Date of first fixed/warned may not be older than 5 years")
+    validate_not_before(earliest_rep_order, :trial_fixed_at, "Date of first fixed/warned may not be earlier than the first representation order date")
+    validate_not_before(@record.trial_fixed_notice_at, :trial_fixed_at, "Date of first fixed/warned may not be earlier than the date notice of first fixed/warned issued")
   end
+
+
+
+
+
 
   def validate_not_after(date, attribute, message)
     return if @record.send(attribute).nil? || date.nil?
--- spec/validators/claim_date_validator_spec.rb
+++ spec/validators/claim_date_validator_spec.rb
@@ -41,6 +41,47 @@ describe ClaimDateValidator do
       expect(cracked_trial_claim.valid?).to be false
       expect(cracked_trial_claim.errors[:trial_fixed_notice_at]).to eq( [ 'Date notice of first fixed/warned issued may not be earlier than the first representation order date' ])
     end
+  end
+
+  context 'trial_fixed_at' do
+    it 'should error if not present for cracked trial' do
+      expect(cracked_trial_claim.valid?).to be false
+      expect(cracked_trial_claim.errors[:trial_fixed_at]).to eq( [ 'Please enter valid date of first fixed/warned' ])
+    end
+
+    it 'should error if not present for cracked before retrial' do
+      expect(cracked_before_retrial_claim.valid?).to be false
+      expect(cracked_before_retrial_claim.errors[:trial_fixed_at]).to eq( [ 'Please enter valid date of first fixed/warned' ])
+    end
+
+    it 'should error if in the future' do
+      cracked_trial_claim.trial_fixed_at = 3.days.from_now.to_date
+      expect(cracked_trial_claim.valid?).to be false
+      expect(cracked_trial_claim.errors[:trial_fixed_at]).to eq( [ 'Date of first fixed/warned may not be in the future' ])
+    end
+
+    it 'should error if more than 5 years old' do
+      cracked_trial_claim.trial_fixed_at = 61.months.ago.to_date
+      expect(cracked_trial_claim.valid?).to be false
+      expect(cracked_trial_claim.errors[:trial_fixed_at]).to eq( [ 'Date of first fixed/warned may not be older than 5 years' ])
+    end
+
+    it 'should error if before earliest rep order' do
+      repo = double RepresentationOrder
+      allow(cracked_trial_claim).to receive(:earliest_representation_order).and_return(repo)
+      allow(repo).to receive(:representation_order_date).and_return(1.year.ago.to_date)
+      cracked_trial_claim.trial_fixed_at = 13.months.ago.to_date
+      expect(cracked_trial_claim.valid?).to be false
+      expect(cracked_trial_claim.errors[:trial_fixed_at]).to eq( [ 'Date of first fixed/warned may not be earlier than the first representation order date' ])
+    end
+
+    it 'should error if before the trial_fixed_notice_at' do
+      cracked_trial_claim.trial_fixed_notice_at = 180.days.ago.to_date
+      cracked_trial_claim.trial_fixed_at = 181.days.ago.to_date
+      expect(cracked_trial_claim.valid?).to be false
+      expect(cracked_trial_claim.errors[:trial_fixed_at]).to eq( [ 'Date of first fixed/warned may not be earlier than the date notice of first fixed/warned issued' ])
+    end
+
 
   end
   

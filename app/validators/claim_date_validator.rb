457d3af05145839af6336554c33b7a2f32b72b81
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-09-04 12:11:56 +0100

    ClaimDateValidator - WIP

---
+++ app/validators/claim_date_validator.rb
@@ -0,0 +1,60 @@
+class ClaimDateValidator < ActiveModel::Validator
+
+  @@date_fields = [ 
+    :trial_fixed_notice_at, 
+    :trial_fixed_at 
+  ]
+
+  def validate(record)
+    @record = record
+    if @record.perform_validation?
+      @@date_fields.each do |date_field|
+        self.send("validate_#{date_field}")
+      end
+    end
+  end
+
+
+  private
+
+  # required when case type is cracked, cracked before retrial
+  # cannot be in the future
+  # cannot be before earliest rep order
+  # cannot be more than 5 years old
+  def validate_trial_fixed_notice_at
+    validate_presence(:trial_fixed_notice_at, "Please enter valid date notice of first fixed/warned issued") if case_type_in("Cracked Trial", "Cracked before retrial")
+    validate_not_after(Date.today, :trial_fixed_notice_at, "Date notice of first fixed/warned issued may not be in the future")
+    validate_not_before(5.years.ago, :trial_fixed_notice_at, "Date notice of first fixed/warned issued may not be older than 5 years")
+    # validate_not_before(earliest_rep_order, :trial_fixed_notice_at, "Date notice of first fixed/warned issued may not be earlier than the first representation order date")
+  end
+
+  def validate_trial_fixed_at
+  end
+
+  def validate_not_after(date, attribute, message)
+    return if @record.send(attribute).nil?
+    add_error(attribute, message) if @record.send(attribute) > date
+  end
+
+  def validate_not_before(date, attribute, message)
+    return if @record.send(attribute).nil?
+    add_error(attribute, message) if @record.send(attribute) < date
+  end
+
+  def case_type_in(*case_types)
+    case_types.include?(@record.case_type.name)
+  end
+
+  def validate_presence(attribute, message)
+    add_error(attribute, message) if @record.send(attribute).blank?
+  end
+
+  def add_error(attribute, message)
+    @record.errors[attribute] << message
+  end
+
+  def earliest_rep_order
+    @record.try(:earliest_representation_order).try(:representation_order_date)
+  end
+
+end
---
+++ spec/validators/claim_date_validator_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe ClaimDateValidator do 
+
+  
+  let(:cracked_case_type)                 { FactoryGirl.build :case_type, name: "Cracked Trial" }
+  let(:cracked_before_retrial_case_type)  { FactoryGirl.build :case_type, name: "Cracked before retrial" }
+
+  let(:claim)                             { FactoryGirl.build :claim, force_validation: true }
+  let(:cracked_trial_claim)               { FactoryGirl.build :claim, force_validation: true, case_type: cracked_case_type }
+  let(:cracked_before_retrial_claim)      { FactoryGirl.build :claim, force_validation: true, case_type: cracked_before_retrial_case_type }
+
+  context 'trial_fixed_notice_at' do
+    it 'should error if not present for cracked trials' do
+      expect(cracked_trial_claim.valid?).to be false
+      expect(cracked_trial_claim.errors[:trial_fixed_notice_at]).to eq( [ 'Please enter valid date notice of first fixed/warned issued' ])
+    end
+
+    it 'should error if not present for Cracked before retrial' do
+      expect(cracked_before_retrial_claim.valid?).to be false
+      expect(cracked_before_retrial_claim.errors[:trial_fixed_notice_at]).to eq( [ 'Please enter valid date notice of first fixed/warned issued' ])
+    end
+
+    it 'should error if in the future' do
+      cracked_trial_claim.trial_fixed_notice_at = 3.days.from_now.to_date
+      expect(cracked_trial_claim.valid?).to be false
+      expect(cracked_trial_claim.errors[:trial_fixed_notice_at]).to eq( [ 'Date notice of first fixed/warned issued may not be in the future' ])
+    end
+
+    it 'should error if more than 5 years old' do
+      cracked_trial_claim.trial_fixed_notice_at = 61.months.ago.to_date
+      expect(cracked_trial_claim.valid?).to be false
+      expect(cracked_trial_claim.errors[:trial_fixed_notice_at]).to eq( [ 'Date notice of first fixed/warned issued may not be older than 5 years' ])
+    end
+
+    # it 'should error if earlier than the earliest representation order date' do
+    #   repo = double RepresentationOrder
+    #   expect(cracked_trial_claim).to receive(:earliest_representation_order).and_return(repo)
+    #   expect(repo).to receive(:representation_order_date).and_return(1.year.ago.to_date)
+    #   cracked_trial_claim.trial_fixed_notice_at = 13.months.ago.to_date
+    #   expect(cracked_trial_claim.valid?).to be false
+    #   expect(cracked_trial_claim.errors[:trial_fixed_notice_at]).to eq( [ 'Date notice of first fixed/warned issued may not be older than 5 years' ])
+
+    # end
+
+  end
+  
+end

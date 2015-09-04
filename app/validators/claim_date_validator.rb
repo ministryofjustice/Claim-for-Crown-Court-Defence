ddc74e7e88e08100ae1262fd82b73d242a897bac
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-09-04 15:25:29 +0100

    Validation specs for trial fixed at, trial cracked at

--- app/validators/claim_date_validator.rb
+++ app/validators/claim_date_validator.rb
@@ -3,7 +3,8 @@ class ClaimDateValidator < BaseClaimValidator
 
   @@claim_date_validator_fields = [ 
     :trial_fixed_notice_at, 
-    :trial_fixed_at 
+    :trial_fixed_at,
+    :trial_cracked_at
   ]
  
 
@@ -27,12 +28,26 @@ class ClaimDateValidator < BaseClaimValidator
   # cannot be more than 5 years old
   # cannot be before trial_fixed_notice_at
   def validate_trial_fixed_at
-    validate_presence(:trial_fixed_at, "Please enter valid date of first fixed/warned") if case_type_in("Cracked Trial", "Cracked before retrial")
-    validate_not_after(Date.today, :trial_fixed_at, "Date of first fixed/warned may not be in the future")
-    validate_not_before(5.years.ago, :trial_fixed_at, "Date of first fixed/warned may not be older than 5 years")
-    validate_not_before(earliest_rep_order, :trial_fixed_at, "Date of first fixed/warned may not be earlier than the first representation order date")
-    validate_not_before(@record.trial_fixed_notice_at, :trial_fixed_at, "Date of first fixed/warned may not be earlier than the date notice of first fixed/warned issued")
+    validate_presence(:trial_fixed_at, "Please enter valid date first fixed/warned") if case_type_in("Cracked Trial", "Cracked before retrial")
+    validate_not_after(Date.today, :trial_fixed_at, "Date first fixed/warned may not be in the future")
+    validate_not_before(5.years.ago, :trial_fixed_at, "Date first fixed/warned may not be older than 5 years")
+    validate_not_before(earliest_rep_order, :trial_fixed_at, "Date first fixed/warned may not be earlier than the first representation order date")
+    validate_not_before(@record.trial_fixed_notice_at, :trial_fixed_at, "Date first fixed/warned may not be earlier than the date notice of first fixed/warned issued")
   end
+
+  # required when case type is cracked, cracked before retrial
+  # cannot be in the future
+  # cannot be before the rep order was granted
+  # cannot be more than 5 years in the past
+  # cannot be before the trial fixed/warned issued
+  def validate_trial_cracked_at
+    validate_presence(:trial_cracked_at, "Please enter valid date when case cracked") if case_type_in("Cracked Trial", "Cracked before retrial")
+    validate_not_after(Date.today, :trial_cracked_at, "Date case cracked may not be in the future")
+    validate_not_before(5.years.ago, :trial_cracked_at, "Date case cracked may not be older than 5 years")
+    validate_not_before(earliest_rep_order, :trial_cracked_at, "Date case cracked may not be earlier than the first representation order date")
+    validate_not_before(@record.trial_fixed_notice_at, :trial_cracked_at, "Date case cracked may not be earlier than the date notice of first fixed/warned issued")
+  end
+
 
 
 
@@ -47,6 +62,7 @@ class ClaimDateValidator < BaseClaimValidator
   end
 
   def case_type_in(*case_types)
+    return false if @record.case_type.nil?
     case_types.include?(@record.case_type.name)
   end
 

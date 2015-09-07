71580c78a8e9545775de143fec233fe653ac5c1a
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-09-07 17:28:31 +0100

    Validate defendant date of birth

--- app/validators/base_claim_validator.rb
+++ app/validators/base_claim_validator.rb
@@ -3,7 +3,7 @@ class BaseClaimValidator < ActiveModel::Validator
 
    def validate(record)
     @record = record
-    if record.perform_validation?
+    if @record.perform_validation?
       fields = self.class.class_variable_get("@@#{self.class.to_s.underscore}_fields".to_sym)
       fields.each do |field|
         self.send("validate_#{field}")
@@ -20,6 +20,17 @@ class BaseClaimValidator < ActiveModel::Validator
 
   def add_error(attribute, message)
     @record.errors[attribute] << message
+  end
+
+  # throws an error if record.attribute > date
+  def validate_not_after(date, attribute, message)
+    return if @record.send(attribute).nil? || date.nil?
+    add_error(attribute, message) if @record.send(attribute) > date
+  end
+
+  def validate_not_before(date, attribute, message)
+    return if @record.send(attribute).nil? || date.nil?
+    add_error(attribute, message) if @record.send(attribute) < date
   end
 
 
---
+++ spec/validators/defendant_date_validator_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+require File.dirname(__FILE__) + '/date_validation_helpers'
+
+describe DefendantDateValidator do 
+
+  include RspecDateValidationHelpers
+
+  let(:defendant)           { FactoryGirl.build :defendant, claim: FactoryGirl.build(:claim, force_validation: true) }
+
+  context 'date of birth' do
+    it { should_error_if_not_present(defendant, :date_of_birth, 'Please enter valid date of birth') }
+    it { should_error_if_before_specified_date(defendant, :date_of_birth, 120.years.ago, 'Date of birth must not be more than 120 years ago') }
+    it { should_error_if_after_specified_date(defendant, :date_of_birth, 10.years.ago, 'Date of birth must be at least 10 years ago') }
+  end
+
+
+
+end

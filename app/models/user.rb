3728d7e884f82693b631c5709d267e65b49db53a
Himal Mandalia <himalmandalia@gmail.com>
2015-09-14 16:27:38 +0100

    Registrations controller for api-sandbox user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, #:registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   belongs_to :persona, polymorphic: true

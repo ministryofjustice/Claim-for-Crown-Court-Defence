67eaaa242229172c6488e1e97d8019ab1b2f2aff
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-09-14 15:56:45 +0100

    uncomment code

--- app/validators/fee_validator.rb
+++ app/validators/fee_validator.rb
@@ -98,8 +98,8 @@ class FeeValidator < BaseClaimValidator
       add_error(:amount, 'Fee amount cannot be zero or blank if a fee quantity has been specified, please enter the relevant amount')
     elsif @record.quantity == 0 && @record.amount > 0
       add_error(:amount, 'Fee amounts cannot be specified if the fee quanitity is zero')
-    # elsif @record.amount.to_i != @record.amount
-    #   add_error(:amount, 'Fee amount must be whole numbers only')
+    elsif @record.amount.to_i != @record.amount
+      add_error(:amount, 'Fee amount must be whole numbers only')
     end
   end
 

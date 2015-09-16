ebb7d9c9ad598ace03e7601971bc0e16b613e06c
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2015-09-16 10:05:30 +0100

    DB migration: Remove Subject attribute from Messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915110319) do
+ActiveRecord::Schema.define(version: 20150916083058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -292,7 +292,6 @@ ActiveRecord::Schema.define(version: 20150915110319) do
   add_index "locations", ["name"], name: "index_locations_on_name", using: :btree
 
   create_table "messages", force: :cascade do |t|
-    t.string   "subject"
     t.text     "body"
     t.integer  "claim_id"
     t.integer  "sender_id"

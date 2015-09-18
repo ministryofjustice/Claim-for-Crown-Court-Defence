4c8d30fe3b888b08766dd4a52d9fc1ac56e8a382
Edward Andress <eddie_andress@hotmail.com>
2015-09-18 16:13:31 +0100

    allow api errors to bubble up and collate in errors att

--- app/validators/json_format_validator.rb
+++ app/validators/json_format_validator.rb
@@ -1,7 +1,7 @@
 class JsonFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value.content_type == 'application/json' && JSON.parse(File.open(value.tempfile).read)
-      record.errors[attribute.to_s.capitalize] = ("is either in the wrong format or contains errors")
+      record.errors[attribute.to_s.capitalize] = "is either in the wrong format or contains errors"
     end
   end
 end

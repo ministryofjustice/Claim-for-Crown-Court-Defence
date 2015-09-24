071175f7bd004f558cf2ae1690347e46d0c5adaf
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-09-24 13:53:13 +0100

    modify acceptable date format for API to permit optional time element
    IS0 8601 compliant date format YYYY-MM-DD[T00:00:00]
    enforced with optional time element to assist
    different api consumer apps.

--- app/interfaces/api/custom_validations/date_format.rb
+++ app/interfaces/api/custom_validations/date_format.rb
@@ -1,7 +1,13 @@
 class StandardJsonFormat < Grape::Validations::Base
   def validate_param!(attr_name, params)
-    unless params[attr_name] =~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/
-      fail Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: "is not in standard JSON date format (YYYY-MM-DD)"
+
+    #
+    # regex pattern: "sortable" e.g.
+    #   match     - 2015-05-21
+    #   match     - 2015-05-21T14:00:00
+    #
+    unless params[attr_name] =~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}(T[0-9]{2}:[0-9]{2}:[0-9]{2})?$/
+      fail Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: "is not in an acceptable date format (YYYY-MM-DD[T00:00:00])"
     end
   end
 end

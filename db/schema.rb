ced81a8c447104e1c2fe47153b6b24881e71c6d8
Himal Mandalia <himalmandalia@gmail.com>
2015-09-28 16:02:23 +0100

    Devise lockable module with 10 minutes account lockout after 3 failed attempts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916083058) do
+ActiveRecord::Schema.define(version: 20150928133144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -374,6 +374,8 @@ ActiveRecord::Schema.define(version: 20150916083058) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.datetime "locked_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

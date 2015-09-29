f4b26e31a039926f9b6817da4619f4037588d34f
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-09-29 14:12:49 +0100

    added banner and tidied up

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( grape_swagger_rails/custom.css )

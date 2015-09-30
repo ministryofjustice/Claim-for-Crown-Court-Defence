c256f56853cca5368743ff684a11a179d05c506e
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-09-30 10:12:24 +0100

    comment change

--- app/assets/javascripts/adp_swagger_application.js
+++ app/assets/javascripts/adp_swagger_application.js
@@ -1,8 +1,5 @@
 //
-// to provide grape swagger rails use of certain rails methods,
-// particulalry link_to with methid: delete, we need
-// swagger to have jquery_ujs. However, since grape swagger rails
-// already includes jquery and does not require other application
-// js, we do not wan tto include apd's application.js here
+// To provide grape swagger rails use of certain rails methods
+// without adding all of ADP's application.js
 //
 //= require jquery_ujs

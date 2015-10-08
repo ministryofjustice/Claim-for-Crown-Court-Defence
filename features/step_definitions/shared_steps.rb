6e37b321f05387546ad531786752d2246710f51b
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-10-08 14:07:37 +0100

    add contact us landing page and link from all advocate pages

--- features/step_definitions/shared_steps.rb
+++ features/step_definitions/shared_steps.rb
@@ -1,3 +1,9 @@
 When(/^I click "(.*?)"$/) do |link_or_button|
   click_on link_or_button
 end
+
+Then(/^I should( not)? see the get in touch contact link$/) do |have|
+  to_or_not_to = have.nil? ? 'to' : have.gsub(/\s+/,'').downcase == 'not' ? 'to_not' : 'to'
+  expect(page).method(to_or_not_to).call have_content("if you're having trouble with this")
+  expect(page).method(to_or_not_to).call have_link("get in touch", href: contact_us_page_path)
+end

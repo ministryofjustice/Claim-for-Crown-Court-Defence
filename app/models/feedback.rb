036f3c4680722719c0bdcde2bd788ef3f7d6b7c3
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2015-10-27 13:42:22 +0000

    Feedback page content, model and layout

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,13 @@
 class Feedback
   include ActiveModel::Model
   include ActiveModel::Validations
+
+  attr_accessor :text, :email, :referrer, :user_agent, :comment, :rating
+
+  RATINGS = { 5 => "Very satisfied",
+    4 => "Satisfied",
+    3 => "Neither satisfied nor dissatisfied",
+    2 => "Dissatisfied",
+    1 =>"Very dissatisfied"}
+
 end

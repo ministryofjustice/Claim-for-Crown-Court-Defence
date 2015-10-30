73364f4d979d9084cfb912051c986d0cbbc78421
Himal Mandalia <himalmandalia@gmail.com>
2015-10-30 11:53:30 +0000

    Rescue from nil GRAPE_SWAGGER_ROOT_URL

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "admin@#{ENV['GRAPE_SWAGGER_ROOT_URL'].gsub(/https?:\/\//, '')}"
+  config.mailer_sender = "admin@#{ENV['GRAPE_SWAGGER_ROOT_URL'].gsub(/https?:\/\//, '') rescue 'example.com'}"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'

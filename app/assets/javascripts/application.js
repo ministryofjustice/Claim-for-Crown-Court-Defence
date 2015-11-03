4743b76ec8aa6e7b31392a441c83ead496c413d7
Tyom Semonov <inspiro@gmail.com>
2015-11-03 15:40:11 +0000

    Fix JSHint warnings, refactor accordion, remove footer margins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,34 +8,31 @@
 //= require modules/moj.cookie-message
 //= require_tree .
 
-/*For JSHint to ignore ADP object*/
-/* globals rorData */
+(function () {
+  "use strict";
 
-var moj = moj || {};
+  delete moj.Modules.devs;
 
-moj.Modules.devs.init = function(){};
+  // Accordion
+  $('#claim-accordion')
+    .find('h2')
+      .next('section').hide()
+      .parent()
+    .on('click', 'h2', function(e, animationDuration) {
+      $(this).toggleClass('open').next('section').slideToggle(animationDuration);
+    })
+    .find('h2:first-of-type').trigger('click', 0);
 
-// Accordion
-$('#claim-accordion h2').each(function(){
-  $(this).next('section').hide();
-  $(this).click(function(){
-    $(this).toggleClass('open').next('section').slideToggle('slow');
+  $('#fixed-fees, #misc-fees, #expenses, #documents').on('cocoon:after-insert', function (e, insertedItem) {
+    $(insertedItem).find('.select2').select2();
   });
-});
-$('#claim-accordion h2:first-of-type').addClass('open').next('section').show();
 
-
-$('#footer').css('margin-top', $(document).height() - ($('#global-header').height() + $('.outer-block:eq(1)').height()  ) - $('#footer').height());
-
-$('#fixed-fees, #misc-fees, #expenses, #documents').on('cocoon:after-insert', function(e, insertedItem) {
-  $(insertedItem).find('.select2').select2();
-});
-
-//Stops the form from submitting when the user presses 'Enter' key
-$("#claim-form form").on("keypress", function (e) {
-  if (e.keyCode === 13) {
+  //Stops the form from submitting when the user presses 'Enter' key
+  $("#claim-form form").on("keypress", function (e) {
+    if (e.keyCode === 13) {
       return false;
-  }
-});
-
-moj.init();
+    }
+  });
+  
+  moj.init();
+}());

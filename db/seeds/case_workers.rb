5a48732dcf618c440382a027d463f7cbac0e47e6
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2015-11-05 15:28:10 +0000

    Add case worker roles to seed data

--- db/seeds/case_workers.rb
+++ db/seeds/case_workers.rb
@@ -11,42 +11,42 @@ days_worked = [
 
 # create test/dummy case workers
 SeedHelper.find_or_create_caseworker!(
-  first_name: 'Dave', 
-  last_name: 'Smith', 
-  email: 'caseworker@example.com', 
+  first_name: 'Dave',
+  last_name: 'Smith',
+  email: 'caseworker@example.com',
   days_worked: [ 1,1,1,1,0],
-  location: 'Nottingham', 
-  role: 'case_worker', 
+  location: 'Nottingham',
+  role: 'case_worker',
   password_env_var: 'CASE_WORKER_PASSWORD')
 
 SeedHelper.find_or_create_caseworker!(
-  first_name: 'Bill', 
-  last_name: 'Smith', 
-  email: 'caseworkeradmin@example.com', 
+  first_name: 'Bill',
+  last_name: 'Smith',
+  email: 'caseworkeradmin@example.com',
   days_worked: [ 1,1,1,1,1],
-  location: 'Nottingham', 
-  role: 'admin', 
+  location: 'Nottingham',
+  role: 'admin',
   password_env_var: 'ADMIN_PASSWORD')
 
- 
+
  # create actual case workers
  file_path = Rails.root.join('lib', 'assets', 'data', 'case_workers.csv')
  data = CSV.read(file_path)
  data.shift
- 
+
 # create actual case workers
 file_path = Rails.root.join('lib', 'assets', 'data', 'case_workers.csv')
 data = CSV.read(file_path)
 data.shift
 
 data.each_with_index do |row, index|
-  fname, lname, email, location = row
+  fname, lname, email, location, role = row
   SeedHelper.find_or_create_caseworker!(
     first_name: fname, 
-    last_name: lname, 
-    email: email, 
-    location: location, 
-    role: 'case_worker', 
+    last_name: lname,
+    email: email,
+    location: location,
+    role: role,
     days_worked: days_worked[ index % 3 ],
     password_env_var: 'CASE_WORKER_PASSWORD')
 end

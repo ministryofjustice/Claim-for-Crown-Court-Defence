bd927e3fe4b0ae7d58590f68a3dd613b0b8ad6e3
Edward Andress <eddie_andress@hotmail.com>
2015-11-10 12:13:04 +0000

    DRY up ZenDesk sending functionality and spec new class

---
+++ app/services/zendesk_sender.rb
@@ -0,0 +1,26 @@
+class ZendeskSender
+  attr_accessor :ticket_payload
+
+  class << self
+    def send!(ticket_payload)
+      ZendeskSender.new(ticket_payload).send!
+    end
+  end
+
+  def initialize(ticket_payload)
+    @ticket_payload = ticket_payload
+  end
+
+  def send!
+    ZendeskAPI::Ticket.create!(
+      ZENDESK_CLIENT,
+      subject: ticket_payload.subject,
+      description: ticket_payload.description,
+      custom_fields: [
+        { id: '26047167', value: ticket_payload.referrer   },
+        { id: '23757677', value: 'advocate_defence_payments' },
+        { id: '23791776', value: ticket_payload.user_agent }
+      ]
+    )
+  end
+end

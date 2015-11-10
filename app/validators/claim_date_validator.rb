dcd51e4769cb1ee5e0ba9dfe9b382791467684ed
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-11-10 14:02:54 +0000

    modify translator to handle gov_uk_date_fields error
    the gov_uk_date_fields gem raises an "Invalid date"
    for dates that of an illegal format. This needed
    separate handling.

--- app/validators/claim_date_validator.rb
+++ app/validators/claim_date_validator.rb
@@ -23,7 +23,7 @@ class ClaimDateValidator < BaseClaimValidator
   # cannot be more than 5 years old
   def validate_trial_fixed_notice_at
     if @record.case_type  && @record.case_type.requires_cracked_dates?
-      validate_presence(:trial_fixed_notice_at, "blank_#{snake_case_type}_date") 
+      validate_presence(:trial_fixed_notice_at, "blank_#{snake_case_type}_date")
       validate_not_after(Date.today, :trial_fixed_notice_at, "check_#{snake_case_type}_date")
       validate_not_before(Settings.earliest_permitted_date, :trial_fixed_notice_at, "check_#{snake_case_type}_date")
       validate_not_before(earliest_rep_order, :trial_fixed_notice_at, "check_#{snake_case_type}_date")
@@ -37,7 +37,7 @@ class ClaimDateValidator < BaseClaimValidator
   # cannot be before trial_fixed_notice_at
   def validate_trial_fixed_at
     if @record.case_type  && @record.case_type.requires_cracked_dates?
-      validate_presence(:trial_fixed_at, "blank_#{snake_case_type}_date") 
+      validate_presence(:trial_fixed_at, "blank_#{snake_case_type}_date")
       validate_not_after(Date.today, :trial_fixed_at, "check_#{snake_case_type}_date")
       validate_not_before(Settings.earliest_permitted_date, :trial_fixed_at, "check_#{snake_case_type}_date")
       validate_not_before(earliest_rep_order, :trial_fixed_at, "check_#{snake_case_type}_date")
@@ -52,7 +52,7 @@ class ClaimDateValidator < BaseClaimValidator
   # cannot be before the trial fixed/warned issued
   def validate_trial_cracked_at
     if @record.case_type && @record.case_type.requires_cracked_dates?
-      validate_presence(:trial_cracked_at, "blank_#{snake_case_type}_date") 
+      validate_presence(:trial_cracked_at, "blank_#{snake_case_type}_date")
       validate_not_after(Date.today, :trial_cracked_at, "check_#{snake_case_type}_date")
       validate_not_before(Settings.earliest_permitted_date, :trial_cracked_at, "check_#{snake_case_type}_date")
       validate_not_before(earliest_rep_order, :trial_cracked_at, "check_#{snake_case_type}_date")
@@ -65,8 +65,8 @@ class ClaimDateValidator < BaseClaimValidator
   # cannot be before first rep order date
   # cannot be more than 5 years in the past
   def validate_first_day_of_trial
-    if @record.case_type && @record.case_type.requires_trial_dates? 
-      validate_presence(:first_day_of_trial, "blank") 
+    if @record.case_type && @record.case_type.requires_trial_dates?
+      validate_presence(:first_day_of_trial, "blank")
       validate_not_after(@record.trial_concluded_at, :first_day_of_trial, "blank")
       validate_not_before(earliest_rep_order, :first_day_of_trial, "blank")
       validate_not_before(Settings.earliest_permitted_date, :first_day_of_trial, "blank")
@@ -78,7 +78,7 @@ class ClaimDateValidator < BaseClaimValidator
   # cannot be before the first rep order was granted
   # cannot be more than 5 years in sthe past
   def validate_trial_concluded_at
-    if @record.case_type && @record.case_type.requires_trial_dates? 
+    if @record.case_type && @record.case_type.requires_trial_dates?
       validate_presence(:trial_concluded_at, "blank")
       validate_not_before(@record.first_day_of_trial, :trial_concluded_at, "blank")
       validate_not_before(earliest_rep_order, :trial_concluded_at, "blank")

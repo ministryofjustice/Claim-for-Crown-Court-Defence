f69cf2f7cf9335baa87c798060788fbd66b5323e
Himal Mandalia <himalmandalia@gmail.com>
2015-11-12 17:56:42 +0000

    Filter params in sentry

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,7 +2,8 @@ require 'raven'
 
 unless %w( test ).include?(Rails.env)
   Raven.configure do |config|
-    config.environments = %w( production )
     config.dsn = ENV['SENTRY_DSN']
+    config.environments = %w( production )
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end

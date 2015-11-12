cf6e85123508fbc3491fd7d1411c4fb50b9ed7d1
Himal Mandalia <himalmandalia@gmail.com>
2015-11-12 16:37:08 +0000

    Only allow production for sentry

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,7 +1,6 @@
 require 'raven'
 
-unless %w( test ).include?(Rails.env)
-  Raven.configure do |config|
-    config.dsn = ENV['SENTRY_DSN']
-  end
+Raven.configure do |config|
+  config.environments = %w( production )
+  config.dsn = ENV['SENTRY_DSN']
 end

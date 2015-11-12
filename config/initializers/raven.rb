7aa887faf68ad219be7d2eabc8b2e35eb81137d5
Himal Mandalia <himalmandalia@gmail.com>
2015-11-12 14:08:07 +0000

    Do not load Sentry config in test env

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,7 @@
 require 'raven'
 
-Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
+unless %w( test ).include?(Rails.env)
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+  end
 end

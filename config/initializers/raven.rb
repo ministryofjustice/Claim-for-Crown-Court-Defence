e54eab3a41ac5072c5bc5b95c8aa2666f0b25620
Himal Mandalia <himalmandalia@gmail.com>
2015-11-12 18:01:07 +0000

    Refactor conditional

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,9 +1,11 @@
 require 'raven'
 
-unless %w( test ).include?(Rails.env) && (Rails.host.gamma? || Rails.host.staging?)
-  Raven.configure do |config|
-    config.dsn = ENV['SENTRY_DSN']
-    config.environments = %w( production )
-    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+unless %w( test ).include?(Rails.env)
+  if Rails.host.gamma? || Rails.host.staging?
+    Raven.configure do |config|
+      config.dsn = ENV['SENTRY_DSN']
+      config.environments = %w( production )
+      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    end
   end
 end

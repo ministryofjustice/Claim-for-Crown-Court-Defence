083b504644f1126022967cc93eb0f450dda10406
Edward Andress <eddie_andress@hotmail.com>
2015-11-17 13:08:17 +0000

    refactor json_document_importer_spec stubbed API responses

--- spec/models/json_document_importer_spec.rb
+++ spec/models/json_document_importer_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 describe JsonDocumentImporter do
 
-  let(:schema)                            { JsonSchema.generate(JsonTemplate.generate) }
-  let(:exported_claim)                    { double 'cms_export', tempfile: './spec/examples/exported_claim.json', content_type: 'application/json'}
-  let(:exported_claim_with_errors)        { double 'cms_export', tempfile: './spec/examples/exported_claim_with_errors.json', content_type: 'application/json'}
-  let(:exported_claims)                   { double 'cms_export', tempfile: './spec/examples/exported_claims.json', content_type: 'application/json'}
-  let(:wrong_format_file)                 { double 'erroneous_file_selection', tempfile: './features/examples/shorter_lorem.pdf', content_type: 'application/pdf' }
-  let(:exported_claim_with_schema_error)  { double 'invalid_json_file', tempfile: './spec/examples/exported_claim_with_schema_error.json', content_type: 'application/json'}
-  let(:exported_claim_with_nulls)         { double 'cms_export_with_nulls', tempfile: './spec/examples/exported_claim_with_nulls.json', content_type: 'application/json'}
-  let(:claim_params)                      { {:source=>"json_import", "creator_email"=>"advocateadmin@example.com", "advocate_email"=>"advocate@example.com", "case_number"=>"A12345678", "case_type_id"=>1, "indictment_number"=>"12345678", "first_day_of_trial"=>"2015-06-01", "estimated_trial_length"=>3, "actual_trial_length"=>3, "trial_concluded_at"=>"2015-06-03", "advocate_category"=>"QC", "offence_id"=>1, "court_id"=>1, "cms_number"=>"12345678", "additional_information"=>"string", "apply_vat"=>true, "trial_fixed_notice_at"=>"2015-06-01", "trial_fixed_at"=>"2015-06-01", "trial_cracked_at"=>"2015-06-01", api_key: 'test_key'} }
-  let(:defendant_params)                  { {"first_name"=>"case", "last_name"=>"system", "date_of_birth"=>"1979-12-10", "order_for_judicial_apportionment"=>true, "claim_id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7", api_key: 'test_key'} }
-  let(:rep_order_params)                  { {"granting_body"=>"Crown Court", "maat_reference"=>"1234567891", "representation_order_date"=>"2015-05-01", "defendant_id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7", api_key: 'test_key'} }
-  let(:fee_params)                        { {"fee_type_id"=>2, "quantity"=>1, "amount"=>1.1, "claim_id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7", api_key: 'test_key'} }
-  let(:expense_params)                    { {"expense_type_id"=>1, "quantity"=>1, "rate"=>1.1, "location"=>"London", "claim_id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7", api_key: 'test_key'} }
-  let(:date_attended_params)              { {"attended_item_type"=>/Fee|Expense/, "date"=>"2015-06-01", "date_to"=>"2015-06-01", "attended_item_id"=>"1234", api_key: 'test_key'} }
-  let(:successful_claim_response)         { double 'api_response', code: 201, body: {"id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7"}.to_json }
-  let(:successful_defendant_response)     { double 'api_response', code: 201, body: {"id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7"}.to_json }
-  let(:successful_rep_order_response)     { double 'api_response', code: 201 }
-  let(:successful_fee_response)           { double 'api_response', code: 201, body: {"id"=> "1234"}.to_json }
-  let(:successful_expense_response)       { double 'api_response', code: 201, body: {"id"=> "1234"}.to_json }
-  let(:successful_date_attended_response) { double 'api_response', code: 201 }
-  let(:failed_claim_creation)             { double 'api_error_response', code: 400, body: [{"error"=>"Advocate email is invalid"}].to_json }
-  let(:failed_claim_creation2)            { double 'api_error_response', code: 400, body: [{"error"=>"Case type cannot be blank, you must select a case type"}, {"error"=>"Court cannot be blank, you must select a court"}, {"error"=>"Case number cannot be blank, you must enter a case number"}, {"error"=>"Advocate category cannot be blank, you must select an appropriate advocate category"}, {"error"=>"Offence Category cannot be blank, you must select an offence category"}].to_json }
-  let(:failed_defendant_creation)         { double 'api_error_response', code: 400, body: [{"error"=> "Claim cannot be blank"}].to_json } 
+  let(:schema)                              { JsonSchema.generate(JsonTemplate.generate) }
+  let(:exported_claim)                      { double 'cms_export', tempfile: './spec/examples/exported_claim.json', content_type: 'application/json'}
+  let(:exported_claim_with_errors)          { double 'cms_export', tempfile: './spec/examples/exported_claim_with_errors.json', content_type: 'application/json'}
+  let(:exported_claims)                     { double 'cms_export', tempfile: './spec/examples/exported_claims.json', content_type: 'application/json'}
+  let(:wrong_format_file)                   { double 'erroneous_file_selection', tempfile: './features/examples/shorter_lorem.pdf', content_type: 'application/pdf' }
+  let(:exported_claim_with_schema_error)    { double 'invalid_json_file', tempfile: './spec/examples/exported_claim_with_schema_error.json', content_type: 'application/json'}
+  let(:exported_claim_with_nulls)           { double 'cms_export_with_nulls', tempfile: './spec/examples/exported_claim_with_nulls.json', content_type: 'application/json'}
+  let(:claim_params)                        { {:source=>"json_import", "creator_email"=>"advocateadmin@example.com", "advocate_email"=>"advocate@example.com", "case_number"=>"A12345678", "case_type_id"=>1, "indictment_number"=>"12345678", "first_day_of_trial"=>"2015-06-01", "estimated_trial_length"=>3, "actual_trial_length"=>3, "trial_concluded_at"=>"2015-06-03", "advocate_category"=>"QC", "offence_id"=>1, "court_id"=>1, "cms_number"=>"12345678", "additional_information"=>"string", "apply_vat"=>true, "trial_fixed_notice_at"=>"2015-06-01", "trial_fixed_at"=>"2015-06-01", "trial_cracked_at"=>"2015-06-01", api_key: 'test_key'} }
+  let(:defendant_params)                    { {"first_name"=>"case", "last_name"=>"system", "date_of_birth"=>"1979-12-10", "order_for_judicial_apportionment"=>true, "claim_id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7", api_key: 'test_key'} }
+  let(:rep_order_params)                    { {"granting_body"=>"Crown Court", "maat_reference"=>"1234567891", "representation_order_date"=>"2015-05-01", "defendant_id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7", api_key: 'test_key'} }
+  let(:fee_params)                          { {"fee_type_id"=>2, "quantity"=>1, "amount"=>1.1, "claim_id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7", api_key: 'test_key'} }
+  let(:expense_params)                      { {"expense_type_id"=>1, "quantity"=>1, "rate"=>1.1, "location"=>"London", "claim_id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7", api_key: 'test_key'} }
+  let(:date_attended_params)                { {"attended_item_type"=>/Fee|Expense/, "date"=>"2015-06-01", "date_to"=>"2015-06-01", "attended_item_id"=>"1234", api_key: 'test_key'} }
+  let(:successful_claim_response)           { double 'api_response', code: 201, body: {"id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7"}.to_json }
+  let(:successful_defendant_response)       { double 'api_response', code: 201, body: {"id"=>"642ec639-5037-4d64-a3aa-27c377e51ea7"}.to_json }
+  let(:successful_rep_order_response)       { double 'api_response', code: 201 }
+  let(:successful_fee_response)             { double 'api_response', code: 201, body: {"id"=> "1234"}.to_json }
+  let(:successful_expense_response)         { double 'api_response', code: 201, body: {"id"=> "1234"}.to_json }
+  let(:successful_date_attended_response)   { double 'api_response', code: 201 }
+  let(:failed_claim_response)               { double 'api_error_response', code: 400, body: [{"error"=>"Advocate email is invalid"}].to_json }
+  let(:failed_claim_response_2)             { double 'api_error_response', code: 400, body: [{"error"=>"Case type cannot be blank, you must select a case type"}, {"error"=>"Court cannot be blank, you must select a court"}, {"error"=>"Case number cannot be blank, you must enter a case number"}, {"error"=>"Advocate category cannot be blank, you must select an appropriate advocate category"}, {"error"=>"Offence Category cannot be blank, you must select an offence category"}].to_json }
+  let(:failed_defendant_response)           { double 'api_error_response', code: 400, body: [{"error"=> "Claim cannot be blank"}].to_json } 
 
   context 'parses a json document and' do
 
@@ -71,7 +71,7 @@ describe JsonDocumentImporter do
       let(:subject) { JsonDocumentImporter.new(json_file: exported_claim_with_errors, schema: schema, api_key: 'test_key') }
 
       before {
-        allow(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).and_return(failed_claim_creation)
+        allow(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).and_return(failed_claim_response)
       }
 
       it 'and errors are stored' do
@@ -126,20 +126,20 @@ describe JsonDocumentImporter do
         let(:subject) { JsonDocumentImporter.new(json_file: exported_claims, schema: schema, api_key: 'test_key') }
 
         it 'one Claim model error from each of two claims' do
-          allow(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).and_return(failed_claim_creation)
+          allow(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).and_return(failed_claim_response)
           subject.import!
           expect(subject.errors).to eq({:claim_1=>[{"error"=>"Advocate email is invalid"}], :claim_2=>[{"error"=>"Advocate email is invalid"}]})
         end
 
         it 'multiple Claim model errors from each of two claims' do
-          allow(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).and_return(failed_claim_creation2)
+          allow(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).and_return(failed_claim_response_2)
           subject.import!
           expect(subject.errors).to eq({:claim_1=>[{"error"=>"Case type cannot be blank, you must select a case type"}, {"error"=>"Court cannot be blank, you must select a court"}, {"error"=>"Case number cannot be blank, you must enter a case number"}, {"error"=>"Advocate category cannot be blank, you must select an appropriate advocate category"}, {"error"=>"Offence Category cannot be blank, you must select an offence category"}], :claim_2=>[{"error"=>"Case type cannot be blank, you must select a case type"}, {"error"=>"Court cannot be blank, you must select a court"}, {"error"=>"Case number cannot be blank, you must enter a case number"}, {"error"=>"Advocate category cannot be blank, you must select an appropriate advocate category"}, {"error"=>"Offence Category cannot be blank, you must select an offence category"}]})
         end
 
         it "but stops when the first validation fail is met" do
-          allow(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).and_return(failed_claim_creation)
-          allow(JsonDocumentImporter::DEFENDANT_CREATION).to receive(:post).and_return(failed_defendant_creation)
+          allow(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).and_return(failed_claim_response)
+          allow(JsonDocumentImporter::DEFENDANT_CREATION).to receive(:post).and_return(failed_defendant_response)
           expect(JsonDocumentImporter::CLAIM_CREATION).to receive(:post).exactly(2).times # claim creation end point is hit and returns an error
           expect(JsonDocumentImporter::DEFENDANT_CREATION).not_to receive(:post) # defendant creation is, therefore, not hit
           subject.import!

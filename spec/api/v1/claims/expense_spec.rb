b5abbc04a7ad9dc692aa0f22d11d964c8c9c8910
Edward Andress <eddie_andress@hotmail.com>
2015-11-23 11:58:27 +0000

    fix broken test

--- spec/api/v1/claims/expense_spec.rb
+++ spec/api/v1/claims/expense_spec.rb
@@ -90,7 +90,7 @@ describe API::V1::Advocates::Expense do
 
       context "unexpected error" do
         it "should return 400 and JSON error array of error message" do
-          params[:quantity] = 1000000000000000000000000
+          params[:location] = 1000000000000000000000000
           post_to_create_endpoint
           expect(last_response.status).to eq(400)
           json = JSON.parse(last_response.body)

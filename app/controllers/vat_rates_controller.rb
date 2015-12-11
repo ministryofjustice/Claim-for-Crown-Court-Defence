6f1a1cd4b78cfb4739aca473bfe5422a518b3558
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2015-12-11 14:13:49 +0000

    Javascript changes to suppose the extra detail on the page and update to the Vat Rate endpoint to return formatted data for js to inject

--- app/controllers/vat_rates_controller.rb
+++ app/controllers/vat_rates_controller.rb
@@ -10,15 +10,31 @@ class VatRatesController < ApplicationController
 
   def index
     net_amount = params['net_amount'].to_f.round(2)
+    apply_vat = params['apply_vat']
     date = Date.parse(params['date'])
-    respond_with(
-      {
-        'net_amount' => ActionController::Base.helpers.number_to_currency(net_amount),
-        'date'       => date.strftime(Settings.date_format),
-        'rate'       => VatRate.pretty_rate(date),
-        'vat_amount' => ActionController::Base.helpers.number_to_currency(VatRate.vat_amount(net_amount, date))
-      }
-    )
+    vat_amount = VatRate.vat_amount(net_amount, date)
+    total_inc_vat = (net_amount || 0) + (vat_amount || 0)
+    if apply_vat == 'true'
+      respond_with(
+        {
+          'net_amount'    => ActionController::Base.helpers.number_to_currency(net_amount),
+          'date'          => date.strftime(Settings.date_format),
+          'rate'          => VatRate.pretty_rate(date),
+          'vat_amount'    => ActionController::Base.helpers.number_to_currency(VatRate.vat_amount(net_amount, date)),
+          'total_inc_vat' => ActionController::Base.helpers.number_to_currency(total_inc_vat)
+        }
+      )
+    else
+      respond_with(
+        {
+          'net_amount'    => ActionController::Base.helpers.number_to_currency(net_amount),
+          'date'          => '',
+          'rate'          => "0%",
+          'vat_amount'    => ActionController::Base.helpers.number_to_currency(0),
+          'total_inc_vat' => ActionController::Base.helpers.number_to_currency(net_amount)
+        }
+      )
+    end
   end
 
 

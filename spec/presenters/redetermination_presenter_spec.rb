bfffccae47d0d2983beb9098857647f8a55366a2
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2015-12-11 13:38:00 +0000

    Remove "created_at" spec because its no longer in the presenter nor is it actually used

--- spec/presenters/redetermination_presenter_spec.rb
+++ spec/presenters/redetermination_presenter_spec.rb
@@ -1,20 +1,13 @@
 require 'rails_helper'
 
 describe RedeterminationPresenter do
-  
+
   let(:rd)              { FactoryGirl.create :redetermination, fees: 1452.33, expenses: 2455.77 }
   let(:presenter)       { RedeterminationPresenter.new(rd, view) }
 
-  describe '#created_at' do
-    it 'should properly format the time' do
-      allow(rd).to receive(:created_at).and_return(Time.local(2015, 8, 13, 13, 15, 22))
-      expect(presenter.created_at).to eq('13/08/2015 13:15')
-    end
-  end
 
-  
   context 'currency fields' do
-  
+
     it 'should format currency amount' do
       expect(presenter.fees_total).to eq '£1,452.33'
       expect(presenter.expenses_total).to eq '£2,455.77'

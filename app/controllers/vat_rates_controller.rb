79556b8d5010bd7747721d4d24ff5a3eb31b562e
Edward Andress <eddie_andress@hotmail.com>
2015-12-14 15:47:26 +0000

    refactor vat controller

--- app/controllers/vat_rates_controller.rb
+++ app/controllers/vat_rates_controller.rb
@@ -9,33 +9,53 @@ class VatRatesController < ApplicationController
   respond_to :json
 
   def index
-    net_amount = params['net_amount'].to_f.round(2)
-    apply_vat = params['apply_vat']
-    date = Date.parse(params['date'])
-    vat_amount = VatRate.vat_amount(net_amount, date)
-    total_inc_vat = (net_amount || 0) + (vat_amount || 0)
-    if apply_vat == 'true'
       respond_with(
         {
-          'net_amount'    => ActionController::Base.helpers.number_to_currency(net_amount),
-          'date'          => date.strftime(Settings.date_format),
-          'rate'          => VatRate.pretty_rate(date),
-          'vat_amount'    => ActionController::Base.helpers.number_to_currency(VatRate.vat_amount(net_amount, date)),
-          'total_inc_vat' => ActionController::Base.helpers.number_to_currency(total_inc_vat)
+          'net_amount'    => number_to_currency(net_amount),
+          'date'          => formatted_date,
+          'rate'          => rate,
+          'vat_amount'    => number_to_currency(vat_amount),
+          'total_inc_vat' => total
         }
       )
-    else
-      respond_with(
-        {
-          'net_amount'    => ActionController::Base.helpers.number_to_currency(net_amount),
-          'date'          => '',
-          'rate'          => "0%",
-          'vat_amount'    => ActionController::Base.helpers.number_to_currency(0),
-          'total_inc_vat' => ActionController::Base.helpers.number_to_currency(net_amount)
-        }
-      )
-    end
   end
 
+  private
+
+  def apply_vat
+    params['apply_vat'] == 'true' ? true : false
+  end
+
+  def date
+    Date.parse(params['date'])
+  end
+
+  def formatted_date
+    apply_vat ? date.strftime(Settings.date_format) : ''
+  end
+
+  def net_amount
+    params['net_amount'].to_f.round(2)
+  end
+
+  def rate
+    apply_vat ? VatRate.pretty_rate(date) : '0%'
+  end
+
+  def vat_amount
+    apply_vat ? VatRate.vat_amount(net_amount, date) : 0
+  end
+
+  def total_inc_vat
+    (net_amount || 0) + (vat_amount || 0)
+  end
+
+  def total
+    apply_vat ? number_to_currency(total_inc_vat) : number_to_currency(net_amount)
+  end
+
+  def number_to_currency(number)
+    ActionController::Base.helpers.number_to_currency(number)
+  end
 
 end

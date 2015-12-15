c068cbcb77bdcf192cffa6909b4c9a896dbe215d
Himal Mandalia <himalmandalia@gmail.com>
2015-12-15 15:47:23 +0000

    Add unicode up/down arrows to indicate sort direction

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -50,14 +50,11 @@ module ApplicationHelper
     end
   end
 
-  def sort_direction
-    params[:direction] == 'asc' ? 'desc' : 'asc'
-  end
-
   def sortable(column, title = nil)
     title ||= column.titleize
+    title = column == sort_column ? ("#{title} " + (sort_direction == 'asc' ? "\u25B2" : "\u25BC")) : title
     css_class = column == sort_column ? "current #{sort_direction}" : nil
-    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
     link_to title, { sort: column, direction: direction }, { class: css_class }
   end
 end

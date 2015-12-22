ea3ba2ef15767942fb9ca6000d80296829a77123
Himal Mandalia <himalmandalia@gmail.com>
2015-12-22 11:17:33 +0000

    Persist apply_vat on claim when calculating VAT

--- app/models/claims/calculations.rb
+++ app/models/claims/calculations.rb
@@ -43,7 +43,7 @@ module Claims::Calculations
 
   def update_vat
     return if self.advocate.nil?
-    self.apply_vat = self.advocate.apply_vat
+    update_column(:apply_vat, self.advocate.apply_vat) if self.advocate.apply_vat
     if self.apply_vat?
       update_column(:vat_amount, calculate_total_vat)
     else

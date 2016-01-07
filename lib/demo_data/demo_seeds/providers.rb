bd431306cf6f91755330cc21ef1433bd98c02137
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-01-07 10:02:58 +0000

    Fix malformed seed data firms missing vat registered values

--- lib/demo_data/demo_seeds/providers.rb
+++ lib/demo_data/demo_seeds/providers.rb
@@ -1,2 +1,2 @@
-Provider.find_or_create_by!(provider_type: 'chamber', name: 'Doughty Street Chambers', supplier_number: '1234' )
-Provider.find_or_create_by!(provider_type: 'chamber', name: 'Matrix Chambers', supplier_number:  '5678' )
+Provider.find_or_create_by!(provider_type: 'chamber', name: 'Doughty Street Chambers')
+Provider.find_or_create_by!(provider_type: 'chamber', name: 'Matrix Chambers' )

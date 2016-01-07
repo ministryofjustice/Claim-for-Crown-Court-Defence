34e346a0dc6614c71650ead5366f2d399df97780
Edward Andress <eddie_andress@hotmail.com>
2016-01-07 17:48:54 +0000

    Flip gem for feature toggling not needed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106161350) do
+ActiveRecord::Schema.define(version: 20160107163538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,13 +237,6 @@ ActiveRecord::Schema.define(version: 20160106161350) do
 
   add_index "expenses", ["claim_id"], name: "index_expenses_on_claim_id", using: :btree
   add_index "expenses", ["expense_type_id"], name: "index_expenses_on_expense_type_id", using: :btree
-
-  create_table "features", force: :cascade do |t|
-    t.string   "key",                        null: false
-    t.boolean  "enabled",    default: false, null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
 
   create_table "fee_categories", force: :cascade do |t|
     t.string   "name"

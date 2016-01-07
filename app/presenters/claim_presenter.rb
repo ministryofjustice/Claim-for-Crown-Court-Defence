509092f0991b4a7d549e5a14afadec117f91105a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-01-07 17:17:39 +0000

    add help/hint text to case worker redetermination form
    case workers were only entering any increase in amount
    authorised from the previous amount authorised when they
    should have entered the new total authorised amount.
    
    also:
    
      - removed humanized state as redundant
      - remove \n line break in presenter as redundant

--- app/presenters/claim_presenter.rb
+++ app/presenters/claim_presenter.rb
@@ -19,12 +19,8 @@ class ClaimPresenter < BasePresenter
     end
   end
 
-  def humanized_state
-    claim.state.humanize
-  end
-
   def case_type_name
-    claim.opened_for_redetermination? ? claim.case_type.name + "\n(redetermination)" : claim.case_type.name
+    claim.opened_for_redetermination? ? claim.case_type.name + ' (redetermination)' : claim.case_type.name
   end
 
   def defendant_names

bb6ec9125c9de30c1af91a8342affe97f83f9cf8
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-01-12 12:38:04 +0000

    remove repeated JsonDocumentImporter instantiation from index

--- app/controllers/external_users/claims_controller.rb
+++ app/controllers/external_users/claims_controller.rb
@@ -19,7 +19,6 @@ class ExternalUsers::ClaimsController < ExternalUsers::ApplicationController
   include MessageControlsDisplay
 
   def index
-    @json_document_importer = JsonDocumentImporter.new
     @claims = @context.claims.dashboard_displayable_states.order('last_submitted_at asc NULLS FIRST, created_at asc').
       page(params[:page]).
       per(10)

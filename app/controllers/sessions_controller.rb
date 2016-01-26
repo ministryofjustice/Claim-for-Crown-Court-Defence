b87fb1021be2bf84145cff4bb1ef330017f3c7ed
Himal Mandalia <himalmandalia@gmail.com>
2016-01-26 12:20:16 +0000

    Refactor feedback signout user persistence

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,17 @@
 class SessionsController < Devise::SessionsController
   skip_load_and_authorize_resource only: [:new, :create, :destroy]
-  before_action :set_user_email, only: [:destroy]
+  before_action :set_user_id, only: [:destroy]
 
   private
 
-  def set_user_email
-    @current_user_email = current_user.email
+  def set_user_id
+    @current_user_id = current_user.id
   end
 
   def respond_to_on_destroy
     respond_to do |format|
       format.all { head :no_content }
-      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name, email: @current_user_email) }
+      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name, user_id: @current_user_id) }
     end
   end
 end

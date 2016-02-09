89140cb1bec1393cf880062c8e72f1e91dad8919
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-02-09 15:41:58 +0000

    disable confirmation and unlock inistruction previews
    These mailers are not required since they are not implemented

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -1,16 +1,17 @@
 class Devise::MailerPreview < ActionMailer::Preview
 
-  def confirmation_instructions
-    Devise::Mailer.confirmation_instructions(User.first, "faketoken")
-  end
+  # NOTE: confirmation instructions not required unless Devise :confirmabl is implemented
+  # def confirmation_instructions
+  #   Devise::Mailer.confirmation_instructions(User.first, "faketoken")
+  # end
 
   def reset_password_instructions
     Devise::Mailer.reset_password_instructions(User.first, "faketoken")
   end
 
-  def unlock_instructions
-    ap "UNLOCK_INSTRUCTIONS"
-    Devise::Mailer.unlock_instructions(User.first, "faketoken")
-  end
+  # NOTE: unlock instructions is not required unless locking strategy is changed to :email or :both - see config
+  # def unlock_instructions
+  #   Devise::Mailer.unlock_instructions(User.first, "faketoken")
+  # end
 
 end

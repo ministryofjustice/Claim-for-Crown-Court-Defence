7ee75a4797314ff3119158311570d5a9e4d0e479
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-02-09 22:09:08 +0000

    implement customer mailer and apply conditional subject
    the reset password instructions mailer needs a conditional
    subject based on whether the email is a welcome/on-boarding
    email or a password reset.

--- spec/mailers/previews/adp_mailer_preview.rb
+++ spec/mailers/previews/adp_mailer_preview.rb
@@ -1,13 +1,13 @@
-class Devise::MailerPreview < ActionMailer::Preview
+class AdpMailerPreview < ActionMailer::Preview
 
-  # NOTE: confirmation instructions not required unless Devise :confirmabl is implemented
+  # NOTE: confirmation instructions not required unless Devise :confirmable is implemented
   # def confirmation_instructions
   #   Devise::Mailer.confirmation_instructions(User.first, "faketoken")
   # end
 
-  # def reset_password_instructions
-  #   Devise::Mailer.reset_password_instructions(User.first, "faketoken")
-  # end
+  def reset_password_instructions
+    AdpMailer.reset_password_instructions(User.first, "faketoken")
+  end
 
   # NOTE: unlock instructions is not required unless locking strategy is changed to :email or :both - see config
   # def unlock_instructions

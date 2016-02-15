93649ea75ce3e387943542e0c51f2235a49c4f2e
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-02-15 16:25:50 +0000

    fix presenter bug for existing claims
    existing data (as on production) was failing
    on calls to the base claim presenter class

---
+++ app/presenters/claim/advocate_claim_presenter.rb
@@ -0,0 +1,3 @@
+class Claim::AdvocateClaimPresenter < Claim::BaseClaimPresenter
+  # TODO: Any differences in baseclaimpresenters for litigators and advocates to be handled here
+end
---
+++ app/presenters/claim/litigator_claim_presenter.rb
@@ -0,0 +1,3 @@
+class Claim::LitigatorClaimPresenter < Claim::BaseClaimPresenter
+  # TODO: Any differences in baseclaimpresenters for litigators and advocates to be handled here
+end

38b9bbc5ded7b97bad7f39b5d175f6334ba0fd37
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-02-19 14:32:13 +0000

    ExpensePresenter had no specs but those are now added along with tests for the new "name" method.

---
+++ spec/presenters/expense_presenter_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe ExpensePresenter do
+
+  let(:claim)     { create(:claim) }
+  let(:expense_type)  { create(:expense_type) }
+  let(:expense)       { create(:expense, quantity: 4, claim: claim, expense_type: expense_type) }
+  let(:presenter) {ExpensePresenter.new(expense, view) }
+
+  describe '#dates_attended_delimited_string' do
+
+    before {
+      claim.expenses .each do |fee|
+        expense.dates_attended << create(:date_attended, attended_item: fee, date: Date.parse('21/05/2015'), date_to: Date.parse('23/05/2015'))
+        expense.dates_attended << create(:date_attended, attended_item: fee, date: Date.parse('25/05/2015'), date_to: nil)
+      end
+    }
+
+    it 'outputs string of dates or date ranges separated by comma' do
+      claim.expenses.each do |fee|
+        expense = ExpensePresenter.new(fee, view)
+        expect(expense.dates_attended_delimited_string).to eql('21/05/2015 - 23/05/2015, 25/05/2015')
+      end
+    end
+  end
+
+  describe '#amount' do
+    it 'formats as currency' do
+      expense.amount = 32456.3
+      expect(presenter.amount).to eq '£32,456.30'
+    end
+  end
+
+  describe '#name' do
+    it 'outputs "Not selected" if there is no expense type' do
+      expense.expense_type_id = nil
+      expect(presenter.name).to eql('Not selected')
+    end
+
+    it 'outputs the type name is there is an expense type' do
+      expense.expense_type_id = expense_type.id
+      expect(presenter.name).to eql(expense.expense_type.name)
+    end
+  end
+
+
+
+
+
+end

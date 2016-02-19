cad075e8012b686ca94ce8bc32d786830eef28a3
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-02-19 14:31:25 +0000

    Created a new method in expense presenter called "name", defaults to "Not selected"

--- app/presenters/expense_presenter.rb
+++ app/presenters/expense_presenter.rb
@@ -5,7 +5,16 @@ class ExpensePresenter < BasePresenter
     expense.dates_attended.order(date: :asc).map(&:to_s).join(', ')
   end
 
- def amount
+  def amount
     h.number_to_currency(expense.amount)
- end
+  end
+
+  def name
+    if expense.expense_type.blank?
+      "Not selected"
+    else
+      expense.expense_type.name
+    end
+  end
+
 end

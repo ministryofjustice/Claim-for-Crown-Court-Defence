ec89a4a4639d708d3f2d87685996b0b9fb0a1359
Himal Mandalia <himalmandalia@gmail.com>
2016-02-23 13:29:48 +0000

    Add roles 'agfs' and 'lgfs' to expense types

--- spec/models/expense_type_spec.rb
+++ spec/models/expense_type_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id         :integer          not null, primary key
 #  name       :string
+#  roles      :string
 #  created_at :datetime
 #  updated_at :datetime
 #
@@ -11,8 +12,16 @@
 require 'rails_helper'
 
 RSpec.describe ExpenseType, type: :model do
+  it_behaves_like 'roles', ExpenseType, ExpenseType::ROLES
+
   it { should have_many(:expenses) }
 
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
+
+  context 'ROLES' do
+    it 'should have "agfs" and "lgfs"' do
+      expect(Provider::ROLES).to match_array(%w( agfs lgfs ))
+    end
+  end
 end

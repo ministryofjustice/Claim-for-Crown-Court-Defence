6a2c8777f66719c7a9850a01b6e4dbbf65730b0e
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-03-10 15:06:09 +0000

    WIP

--- app/controllers/case_workers/admin/management_information_controller.rb
+++ app/controllers/case_workers/admin/management_information_controller.rb
@@ -1,23 +1,28 @@
 require 'csv'
 
 class CaseWorkers::Admin::ManagementInformationController < CaseWorkers::Admin::ApplicationController
-  skip_load_and_authorize_resource only: [:index, :report]
-  before_action -> { authorize! :view, :management_information }, only: [:index, :report]
-  before_action :set_claims, only: :report
+  skip_load_and_authorize_resource only: [:index, :download, :generate]
+  before_action -> { authorize! :view, :management_information }, only: [:index, :download, :generate]
 
   def index
-
+    @filename = Stats::ManagementInformationGenerator.current_csv_filename
+    @time_generated = Stats::ManagementInformationGenerator.creation_time(@filename) unless @filename.nil?
   end
 
-  def report
-    respond_to do |format|
-      format.csv { send_data csv_report, filename: "all_claims_report.csv" }
-    end
+  def download
+    @filename = Stats::ManagementInformationGenerator.current_csv_filename
+    send_file @filename, type: 'text/csv; charset=iso-8859-1; header=present'
+  end
+
+  def generate
+    ManagemenInformationGenerationJob.perform_later
+    redirect_to case_workers_admin_management_information_url, alert: 'A background job has been scheduled to regenerate the Management Information report'
   end
 
   private
 
   def csv_report
+    @claims = Claim::BaseClaim.non_draft
     CSV.generate(headers: true) do |csv|
       csv << Settings.claim_csv_headers.map {|header| header.to_s.humanize}
       @claims.each do |claim|
@@ -28,10 +33,6 @@ class CaseWorkers::Admin::ManagementInformationController < CaseWorkers::Admin::
         end
       end
     end
-  end
-
-  def set_claims
-    @claims = Claim::BaseClaim.non_draft
   end
 
 end
--- app/models/stats/management_information_generator.rb
+++ app/models/stats/management_information_generator.rb
@@ -4,20 +4,76 @@ module Stats
 
   class ManagementInformationGenerator
 
+    STATS_DIR = File.join(Rails.root, 'tmp', 'stats')
+    PIDFILE_NAME = File.join(STATS_DIR, 'management_information.pid')
+
     def initialize
-      @stats_dir = File.join(Rails.root, 'tmp', 'stats')
-      FileUtils.mkdir @stats_dir unless File.exist?(@stats_dir)
-      @filename = File.join(@stats_dir, "management_information_#{Time.now.strftime('%Y_%m_%d_%H_%M_%S')}.csv")
+      FileUtils.mkdir STATS_DIR unless Dir.exist?(STATS_DIR)
+      @filename = File.join(STATS_DIR, "management_information_#{Time.now.strftime('%Y_%m_%d_%H_%M_%S')}.csv")
     end
 
     def run
-      generate_new_report
-      delete_old_reports
+      if drop_pidfile_ok?
+        generate_new_report
+        delete_old_reports
+        clean_up_pidfile
+      end
+    end
+
+    def self.current_csv_filename
+      files = files = Dir["#{STATS_DIR}/**/*.csv"]
+      files.sort.first           # return the first file, because if there are two, then the second hasn't finished generating
+    end
+
+
+    def self.creation_time(filename)
+      unless filename =~ /management_information_(\d{4})_(\d{2})_(\d{2})_(\d{2})_(\d{2})_(\d{2})\.csv$/
+        raise ArgumentError.new("Invalid filename for management information file: #{filename}")
+      end
+      Time.new($1.to_i, $2.to_i, $3.to_i, $4.to_i, $5.to_i, $6.to_i)
     end
 
   private
+    def drop_pidfile_ok?
+      if pidfile_exists? 
+        return false if duplicate_job_running?
+        clean_up_pidfile
+      end
+      create_pidfile
+      true
+    end 
+
+    def pidfile_exists?
+      File.exist?(PIDFILE_NAME)
+    end
+
+    def duplicate_job_running?
+      pid = File.read(PIDFILE_NAME).chomp
+      active_process_with_pid?(pid)
+    end
+
+    def clean_up_pidfile
+      FileUtils.rm PIDFILE_NAME
+    end
+
+    def active_process_with_pid?(pid)
+      begin
+        Process.getpgid(pid.to_i)
+        true
+      rescue Errno::ESRCH
+        false
+      end
+    end
+
+    def create_pidfile
+      File.open(PIDFILE_NAME, 'w') do |fp|
+        fp.puts Process.pid
+      end
+    end
+
+
     def delete_old_reports
-      files = Dir["#{@stats_dir}/**/*"] - [ @filename ]
+      files = Dir["#{STATS_DIR}/**/*.csv"] - [ @filename ]
       FileUtils.rm files
     end
 

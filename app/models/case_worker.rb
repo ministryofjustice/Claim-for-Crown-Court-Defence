33dd50ccbf8e8eb4f3af1ca6b1ff5ba478a03bde
Himal Mandalia <himalmandalia@gmail.com>
2016-03-10 17:03:33 +0000

    Remove days worked from case worker

--- app/models/case_worker.rb
+++ app/models/case_worker.rb
@@ -6,7 +6,6 @@
 #  created_at  :datetime
 #  updated_at  :datetime
 #  location_id :integer
-#  days_worked :string
 #  roles       :string
 #
 
@@ -17,8 +16,6 @@ class CaseWorker < ActiveRecord::Base
 
   include Roles
 
-  serialize :days_worked, Array
-
   belongs_to :location
   has_one :user, as: :persona, inverse_of: :persona, dependent: :destroy
   has_many :case_worker_claims, dependent: :destroy
@@ -28,7 +25,6 @@ class CaseWorker < ActiveRecord::Base
 
   validates :location, presence: {message: 'Location cannot be blank'}
   validates :user, presence: {message: 'User cannot be blank'}
-  validate  :days_worked_valid
 
   accepts_nested_attributes_for :user
 
@@ -37,49 +33,9 @@ class CaseWorker < ActiveRecord::Base
   delegate :last_name, to: :user
   delegate :name, to: :user
 
-
-  def method_missing(method, *args)
-    if method.to_s =~ /^days_worked_(.)$/
-      self.days_worked[$1.to_i]
-    elsif method.to_s =~/^days_worked_(.)=$/
-      self.days_worked[$1.to_i]= args.first.to_i
-    else
-      super
-    end
-  end
-
   protected
 
   def unallocate!(record)
     record.submit! if record.allocated? && (record.case_workers - [self]).none?
-  end
-
-
-  private
-
-  def days_worked_valid
-    unless days_worked_size? && days_worked_valid_class_and_values?
-      errors[:base] << 'Days worked invalid'
-    end
-    unless at_least_one_day_specified_as_working?
-      errors[:base] << 'At least one day must be specified as a working day'
-    end
-  end
-
-  def at_least_one_day_specified_as_working?
-    self.days_worked.uniq != [ 0 ]
-  end
-
-  def days_worked_size?
-    self.days_worked.size == 5
-  end
-
-  def days_worked_valid_class_and_values?
-    self.days_worked.map(&:class).uniq == [ Fixnum ] && days_worked_valid_values?
-  end
-
-  def days_worked_valid_values?
-    uniqs = self.days_worked.uniq.sort
-    uniqs == [1] || uniqs == [0, 1] || uniqs == [0]
   end
 end
--- db/seeds/seed_helper.rb
+++ db/seeds/seed_helper.rb
@@ -10,7 +10,7 @@ module SeedHelper
         password:   ENV.fetch(attrs[:password_env_var]),
         password_confirmation: ENV[attrs[:password_env_var]]
       )
-      case_worker = CaseWorker.new(roles: attrs[:roles], days_worked: attrs[:days_worked])
+      case_worker = CaseWorker.new(roles: attrs[:roles])
       case_worker.user = user
       case_worker.location = Location.find_or_create_by!(name: attrs[:location].capitalize)
       case_worker.save!

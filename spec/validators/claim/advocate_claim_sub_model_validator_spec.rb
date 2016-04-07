2998f66cdc1c14299652a1ea894eb788b2dc5416
Jesus Laiz <jesus.laiz@digital.justice.gov.uk>
2016-04-07 09:37:37 +0100

    Re-organizing multi-step validation tests.

---
+++ spec/validators/claim/advocate_claim_sub_model_validator_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+require_relative '../validation_helpers'
+
+describe Claim::AdvocateClaimSubModelValidator do
+
+  let(:claim) { FactoryGirl.create :claim }
+
+  before(:each) do
+    claim.force_validation = true
+  end
+
+  context 'partial validation' do
+    let(:step1_has_one) { [] }
+    let(:step2_has_one) { [ :assessment, :certification ] }
+
+    let(:step1_has_many) { [ :defendants ] }
+    let(:step2_has_many) { [ :basic_fees, :misc_fees, :fixed_fees, :expenses, :messages, :redeterminations, :documents ] }
+
+    context 'from web' do
+      before do
+        claim.source = 'web'
+      end
+
+      context 'step 1' do
+        before do
+          claim.form_step = 1
+        end
+
+        it 'should validate has_one associations for this step' do
+          step1_has_one.each do |association|
+            expect_any_instance_of(described_class).to receive(:validate_association_for).with(claim, association)
+          end
+
+          step2_has_one.each do |association|
+            expect_any_instance_of(described_class).not_to receive(:validate_association_for).with(claim, association)
+          end
+
+          claim.valid?
+        end
+
+        it 'should validate has_many associations for this step' do
+          step1_has_many.each do |association|
+            expect_any_instance_of(described_class).to receive(:validate_collection_for).with(claim, association)
+          end
+
+          step2_has_many.each do |association|
+            expect_any_instance_of(described_class).not_to receive(:validate_collection_for).with(claim, association)
+          end
+
+          claim.valid?
+        end
+      end
+
+      context 'step 2' do
+        before do
+          claim.form_step = 2
+        end
+
+        it 'should validate has_one associations for this step' do
+          step2_has_one.each do |association|
+            expect_any_instance_of(described_class).to receive(:validate_association_for).with(claim, association)
+          end
+
+          claim.valid?
+        end
+
+        it 'should validate has_many associations for this step' do
+          step2_has_many.each do |association|
+            expect_any_instance_of(described_class).to receive(:validate_collection_for).with(claim, association)
+          end
+
+          claim.valid?
+        end
+      end
+    end
+
+    context 'from API' do
+      before do
+        claim.source = 'api'
+      end
+
+      it 'should validate all the has_one associations for all the steps' do
+        (step1_has_one + step2_has_one).each do |association|
+          expect_any_instance_of(described_class).to receive(:validate_association_for).with(claim, association)
+        end
+
+        claim.valid?
+      end
+
+      it 'should validate all the has_many associations for all the steps' do
+        (step1_has_many + step2_has_many).each do |association|
+          expect_any_instance_of(described_class).to receive(:validate_collection_for).with(claim, association)
+        end
+
+        claim.valid?
+      end
+    end
+  end
+end

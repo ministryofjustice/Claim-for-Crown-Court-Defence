208cb7a63cbeaa40abf812f2bb56220127b7d560
Jesus Laiz <jesus.laiz@digital.justice.gov.uk>
2016-04-11 13:55:43 +0100

    Update cuke css selector for Continue button on summary page.

--- features/page_objects/claim_summary_page.rb
+++ features/page_objects/claim_summary_page.rb
@@ -1,5 +1,5 @@
 class ClaimSummaryPage < SitePrism::Page
   set_url "/external_users/claims/{claim_id}/summary"
 
-  element :continue, "div.claim-summary-sidebar > div.claim-summary-hgroup > a:nth-of-type(1)"
+  element :continue, "div.new-claim-sidebar > div.new-claim-hgroup > a:nth-of-type(1)"
 end

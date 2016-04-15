7063c3d197e898e5c8a596b3cae0b02368b580ac
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-04-15 13:11:35 +0100

    replace claim_detail feature test with with view spec

---
+++ spec/support/view_spec_helpers.rb
@@ -0,0 +1,21 @@
+# This is necessary in order to stub out ApplicationController
+# methods that are declared as helpers.
+#
+# Declare the method here to raise an exception, and
+# then in the spec, stub out the behaviour you need.
+#
+# Remember to include ViewSpecHelper in the spec, and call
+# initialize_view_helpers, probably as a before(:each)
+#
+
+module ViewSpecHelper
+  module ControllerViewHelpers
+    def current_user_persona_is?(klass)
+      raise 'Stub current_user if you want to test the behavior.'
+    end
+  end
+
+  def initialize_view_helpers(view)
+    view.extend ControllerViewHelpers
+  end
+end

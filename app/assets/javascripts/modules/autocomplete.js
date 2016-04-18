b350c2a08a430658e6cf73aa1ade46b1f6e7405a
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2016-04-18 10:52:37 +0100

    Amend testing helper for dropdowns

--- app/assets/javascripts/modules/autocomplete.js
+++ app/assets/javascripts/modules/autocomplete.js
@@ -8,8 +8,9 @@
     
     this.init = function(select) {
       
-      var input = Awesomplete.$.create('input');
-      $(input).addClass('form-control');
+      var id = $(select).attr('id'),
+        input = Awesomplete.$.create('input');
+      $(input).addClass('form-control').attr('id', id+'_autocomplete');
       input.disabled = $(select).attr('disabled')? true : false;
       select.parentElement.insertBefore(input, select);
       select.setAttribute("hidden", "");
@@ -41,6 +42,10 @@
         'blur': function(){
           self.copySelectedValue(input, select);
         }
+      });
+
+      $(select).on('change', function(){
+        self.copySelectedValue(input, select);
       });
 
     };

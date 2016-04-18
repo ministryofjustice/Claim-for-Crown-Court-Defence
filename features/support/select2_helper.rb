3cf547f32df8b38a5082fee2d2f1a8f7a54c88f2
Chris Hanes <chris.hanes@digital.justice.gov.uk>
2016-04-18 10:52:37 +0100

    Amend testing helper for dropdowns

--- features/support/select2_helper.rb
+++ features/support/select2_helper.rb
@@ -6,18 +6,26 @@ module Select2Helper
   # e.g. select2 'value-to-select', from: 'select2-list-id'
   #
   def select2(value, options)
-    sleep 1 # Allow dropdown to build - we can get flickers without this
-    page.find("#s2id_#{options[:from]} a").click
-    downcase_value = value.downcase
+    sleep 2 # Allow dropdown to build - we can get flickers without this
+    # page.find("##{options[:from]}").set(value)
+    # page.find("##{options[:from]}", :visible => false).set('#{value}')
+    select('#{value}', :from => page.find("##{options[:from]}", :visible => false))
 
-    page.all("ul.select2-results li").each do |e|
-      if e.text.downcase == downcase_value
-        e.click
-        return
-      end
-    end
+    script = "$('#{options[:from]}').trigger('change');"
+    page.execute_script(script);
 
-    raise "Value not found in select list: #{value}"
+    sleep 2
+
+    # downcase_value = value.downcase
+
+    # page.all("##{options[:from]}_autocomplete + ul li").each do |e|
+    #   if e.text.downcase == downcase_value
+    #     e.click
+    #     return
+    #   end
+    # end
+
+    # raise "Value not found in select list: #{value}"
   end
 end
 

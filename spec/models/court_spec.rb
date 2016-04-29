71025568bddd3390b39ba0a780321812ea12109e
Himal Mandalia <himalmandalia@gmail.com>
2016-04-29 11:55:47 +0100

    Scope to return courts alphabetised

--- spec/models/court_spec.rb
+++ spec/models/court_spec.rb
@@ -21,4 +21,14 @@ RSpec.describe Court, type: :model do
   it { should validate_uniqueness_of(:name).with_message('Court name must be unique') }
   it { should validate_presence_of(:court_type) }
   it { should validate_inclusion_of(:court_type).in_array(%w( crown magistrate )) }
+
+  describe '.alphabetical' do
+    let(:court_1) { create(:court, name: 'Oxford') }
+    let(:court_2) { create(:court, name: 'Reading') }
+    let(:court_3) { create(:court, name: 'Cambridge') }
+
+    it 'returns the courts in alphabetical order' do
+      expect(Court.alphabetical).to eq([court_3, court_1, court_2])
+    end
+  end
 end

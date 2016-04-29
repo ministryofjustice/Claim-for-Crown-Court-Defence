6df4b3290d48eabe9af03d2fae9fc365e534a434
Jesus Laiz <jesus.laiz@digital.justice.gov.uk>
2016-04-29 16:00:29 +0100

    Interim claim detail view amendments.

--- app/presenters/claim/interim_claim_presenter.rb
+++ app/presenters/claim/interim_claim_presenter.rb
@@ -1,2 +1,15 @@
 class Claim::InterimClaimPresenter < Claim::BaseClaimPresenter
+
+  def requires_trial_dates?
+    false
+  end
+
+  def requires_retrial_dates?
+    false
+  end
+
+  def can_have_expenses?
+    false
+  end
+
 end

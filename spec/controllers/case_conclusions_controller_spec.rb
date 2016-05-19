d16efc5976a4bec3aa3f172eb7f51fea1f0f36e3
jsugarman <joel.sugarman@digital.justice.gov.uk>
2016-05-19 13:34:23 +0100

    refactor case conclusion controller specs

--- spec/controllers/case_conclusions_controller_spec.rb
+++ spec/controllers/case_conclusions_controller_spec.rb
@@ -6,38 +6,44 @@ RSpec.describe CaseConclusionsController, type: :controller do
   let(:transfer_detail) { build :transfer_detail, litigator_type: 'new', elected_case: false, transfer_stage_id: 10 }
 
   describe 'GET index' do
-    it 'should assign @transfer_details' do
-      xhr :get, :index, params
-      expect(assigns(:transfer_detail)).to have_attributes(litigator_type: 'new', elected_case: false, transfer_stage_id: 30)
+
+    context 'basics' do
+      before { xhr :get, :index, params }
+      it 'should assign @transfer_details' do
+        expect(assigns(:transfer_detail)).to have_attributes(litigator_type: 'new', elected_case: false, transfer_stage_id: 30)
+      end
+
+      it 'should render the index template' do
+        expect(response).to render_template(:index)
+      end
     end
 
-    it 'should assign @transfer_stage_label_text' do
-      xhr :get, :index, params
-      expect(assigns(:transfer_stage_label_text)).to_not be_nil
-      expect(assigns(:transfer_stage_label_text)).to eql 'When did you start acting?'
+    context 'for new litigator_type' do
+      before { xhr :get, :index, params }
+      it 'should assign @transfer_stage_label_text to say start' do
+        expect(assigns(:transfer_stage_label_text)).to_not be_nil
+        expect(assigns(:transfer_stage_label_text)).to eql 'When did you start acting?'
+      end
+
+      it 'should assign @transfer_date_label_text to say started' do
+        expect(assigns(:transfer_date_label_text)).to_not be_nil
+        expect(assigns(:transfer_date_label_text)).to eql 'Date started acting'
+      end
     end
 
-    it 'should modify transfer stage label text based on the litigator type' do
-      params[:litigator_type] = 'original'
-      xhr :get, :index, params
-      expect(assigns(:transfer_stage_label_text)).to eql 'When did you stop acting?'
-    end
+    context 'for original litigator type' do
+      before do
+        params[:litigator_type] = 'original'
+        xhr :get, :index, params
+      end
+      it 'should assign @transfer_stage_label_text to say stop' do
+        expect(assigns(:transfer_stage_label_text)).to eql 'When did you stop acting?'
+      end
 
-    it 'should assign @transfer_date_label' do
-      xhr :get, :index, params
-      expect(assigns(:transfer_date_label_text)).to_not be_nil
-      expect(assigns(:transfer_date_label_text)).to eql 'Date started acting'
-    end
-
-    it 'should modify transfer date label text based on the litigator type' do
-      params[:litigator_type] = 'original'
-      xhr :get, :index, params
-      expect(assigns(:transfer_date_label_text)).to eql 'Date stopped acting'
-    end
-
-    it 'should render the index template' do
-      xhr :get, :index, params
-      expect(response).to render_template(:index)
+      it 'should assign @transfer_date_label_text to say stopped' do
+        expect(assigns(:transfer_date_label_text)).to eql 'Date stopped acting'
+      end
     end
   end
+
 end

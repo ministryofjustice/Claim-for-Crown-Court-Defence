953492b81306ddaeb7f90e4e91fbc30a4ca8e91c
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-06-09 09:08:03 +0100

    Protect against nil expense array in JSON importer (#1508)
    * Protect against nil expense array in JSON importer
    
    * replace wait_for_ajax with sleep to make cukes pass on travis

--- features/step_definitions/advocate_claim_steps.rb
+++ features/step_definitions/advocate_claim_steps.rb
@@ -70,13 +70,12 @@ When(/^I add another defendant, representation order and MAAT reference$/) do
 end
 
 When(/^I add a basic fee with dates attended$/) do
-  using_wait_time 6 do
-    @claim_form_page.initial_fees.basic_fee.quantity.set "1"
-    @claim_form_page.initial_fees.basic_fee.rate.set "3.45"
-    # @claim_form_page.initial_fees.basic_fee.add_dates.click
-    # @claim_form_page.initial_fees.basic_fee_dates.from.set_date "2016-01-02"
-    # @claim_form_page.initial_fees.basic_fee_dates.to.set_date "2016-01-03"
-  end
+  sleep 1
+  @claim_form_page.initial_fees.basic_fee.quantity.set "1"
+  @claim_form_page.initial_fees.basic_fee.rate.set "3.45"
+  # @claim_form_page.initial_fees.basic_fee.add_dates.click
+  # @claim_form_page.initial_fees.basic_fee_dates.from.set_date "2016-01-02"
+  # @claim_form_page.initial_fees.basic_fee_dates.to.set_date "2016-01-03"
 end
 
 When(/^I add a daily attendance fee with dates attended$/) do

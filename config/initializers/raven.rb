391324c99eeff7a5da1c0dc58b76ea1692957668
Jesus Laiz <zheileman@users.noreply.github.com>
2016-06-13 13:05:33 +0100

    Only configuring Sentry on prod and when token is available.

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,11 +1,9 @@
 require 'raven'
 
-unless %w( test ).include?(Rails.env)
-  if Rails.host.gamma? || Rails.host.staging? || Rails.host.api_sandbox?
-    Raven.configure do |config|
-      config.dsn = ENV['SENTRY_DSN']
-      config.environments = %w( production )
-      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-    end
+if %w( production ).include?(Rails.env) && ENV['SENTRY_DSN'].present?
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.environments = %w( production )
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
 end

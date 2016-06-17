92f5928049762a8ebf09d7d2ed6a696e5883ff08
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-06-17 16:28:24 +0100

    AdpFormBuilder#adp_text_field
    This PR makes a method adp_text_field() avaialble on the AdpFormBuilder which outputs the HTML for a text field wrapped in all the GDS specific markup
    to allow labels, hints, error messages, etc.

---
+++ app/form_builders/adp_text_field.rb
@@ -0,0 +1,134 @@
+
+# class to create specialized text field wrapped in all the GDS gubbins
+class AdpTextField
+
+  include ExternalUsers::ClaimsHelper
+  include ActionView::Helpers::TagHelper
+
+
+  # instantiate an AdpTextField object
+  # * form: the instance of AdpFormBuilder that this is called from
+  # * method: the method on the object that the form is wrapping that this input field is for
+  # * options:
+  #   * label: label to be provided for the input field
+  #   * hint_text: Hint text displayed underneath the label
+  #   * errors: An ErrorPresenter for the form object, or the form object itself
+
+  def initialize(form, method, options)
+    @form = form
+    @method = method
+    @options = options
+    @form_field_id = generate_form_field_id
+    @form_field_name = generate_form_field_name
+    @errors = options[:errors]
+    @anchor_id = generate_anchor_id
+  end
+
+
+  # the methods output_buffer= and output_buffer are required by the content_tag
+  # methods, called from the validation_error_message in ExternalUsers::ClaimsHelper
+  #
+  def output_buffer=(value)
+    @errors = value
+  end
+
+  def output_buffer
+    @errors
+  end
+
+  def has_errors?
+    @errors.errors_for?(@anchor_id.to_sym)
+  end
+
+  def to_html
+    result = div_start
+    result += anchor
+    result += label
+    result += hint
+    result += error_message
+    result += label_close
+    result += input_field
+    result += div_close
+    result.html_safe
+  end
+
+  private
+
+  def generate_form_field_id
+    # @form.object_name either returns a symbol for top level fields (e.g. :claim), or
+    # a string like "claim[defendants_attributes][0]" for cocoon nested objects
+    if @form.object_name.is_a?(Symbol)
+      "#{@form.object_name}_#{@method}"
+    else
+      # translates e.g. claim[defendants_attributes][0]_last_name to claim_defendants_attributes_0_last_name
+      @form.object_name.to_s.gsub(/\[/, '_').gsub(/\]/, '_').gsub('__', '_') + "#{@method}"
+    end
+  end
+
+  def generate_form_field_name
+    # @form.object_name either returns a symbol for top level fields (e.g. :claim), or
+    # a string like "claim[defendants_attributes][0]" for cocoon nested objects
+    "#{@form.object_name}[#{@method}]"
+  end
+
+  def generate_anchor_id
+    # translates e.g. claim_defendants_attributes_0_last_name to defendant_1_last_name
+    anchor = @form_field_id.sub(/^claim_/, '').gsub('s_attributes', '')
+    parts = anchor.split('_')
+    incremented_anchor_parts = []
+    parts.each do |part|
+      if part =~ /^[0-9]{1,2}$/
+        incremented_anchor_parts << (part.to_i + 1).to_s
+      else
+        incremented_anchor_parts << part
+      end
+    end
+    incremented_anchor_parts.join('_')
+  end
+
+  def div_start
+    result = %Q|<div class="form-group #{@method}|
+    result += %Q| field_with_errors| if has_errors?
+    result += %Q|">|
+    result
+  end
+
+  def anchor
+    %Q|<a id="#{@anchor_id}"></a>|
+  end
+
+  def label
+    %Q|<label class="form-label" for="#{@form_field_id}">#{@options[:label]}|
+  end
+
+  def label_close
+    %Q|</label>|
+  end
+
+  def error_message
+    has_errors? ? validation_error_message(@errors, @anchor_id) : ''
+  end
+
+  def hint
+    if @options[:hint_text]
+      %Q|<div class="form-hint">#{@options[:hint_text]}</div>|
+    else
+      ''
+    end
+  end
+
+  def input_field
+    result = %Q|<input class="form-control" type="text" name="#{@form_field_name}" id="#{@form_field_id}" |
+    result += %Q|value="#{@form.object.__send__(@method)}" | unless @form.object.__send__(@method).nil?
+    result += %Q|/>|
+    result
+  end
+
+  def div_close
+    '</div>'
+  end
+end
+
+
+
+

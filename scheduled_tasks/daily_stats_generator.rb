8bea2c8a1cdc34af60de754b7e7be854d35c2d8b
Jesus Laiz <zheileman@users.noreply.github.com>
2016-07-04 17:39:27 +0100

    Tidy up scheduler task.

--- scheduled_tasks/daily_stats_generator.rb
+++ scheduled_tasks/daily_stats_generator.rb
@@ -3,18 +3,33 @@ require 'chronic'
 # https://github.com/ssoroka/scheduler_daemon for help
 class DailyStatsGenerator < Scheduler::SchedulerTask
 
-  every '1d', first_at: Chronic.parse('next 4 am')
+  every '1d', first_at: Chronic.parse('next 5 am')
 
   def run
     log('Daily stats generation started')
-    Stats::Collector::ClaimCreationSourceCollector.new(Date.yesterday).collect
-    Stats::Collector::ClaimSubmissionsCollector.new(Date.yesterday).collect
-    Stats::Collector::MultiSessionSubmissionCollector.new(Date.yesterday).collect
-    Stats::Collector::InfoRequestCountCollector.new(Date.yesterday).collect
-    Stats::Collector::TimeFromRejectToAuthCollector.new(Date.yesterday).collect
+
+    collectors.each do |klass|
+      klass.new(date).collect
+    end
   rescue => ex
     log('There was an error: ' + ex.message)
   ensure
     log('Daily stats generation finished')
+  end
+
+  private
+
+  def date
+    Date.yesterday
+  end
+
+  def collectors
+    [
+      Stats::Collector::ClaimCreationSourceCollector,
+      Stats::Collector::ClaimSubmissionsCollector,
+      Stats::Collector::MultiSessionSubmissionCollector,
+      Stats::Collector::InfoRequestCountCollector,
+      Stats::Collector::TimeFromRejectToAuthCollector
+    ]
   end
 end

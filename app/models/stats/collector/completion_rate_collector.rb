0e24e8fe69d962b9a89566007c5d5142e42d4978
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-07-05 13:46:46 +0100

    Rework to use claim intentions as basis of statistic

--- app/models/stats/collector/completion_rate_collector.rb
+++ app/models/stats/collector/completion_rate_collector.rb
@@ -11,27 +11,21 @@ module Stats
 
       def collect
         begin
-          num_abandoned = claims_created_at_start_of_period.where(last_submitted_at: nil).count
-          num_completed = claims_created_at_start_of_period.where.not(last_submitted_at: nil).count
-          num_created = num_abandoned + num_completed
-          if num_created == 0
+          form_ids_created_at_period_start = ClaimIntention.where(created_at: @start_date.beginning_of_day..@start_date.end_of_day).pluck(:form_id)
+          num_started = form_ids_created_at_period_start.size
+          num_completed = Claim::BaseClaim.where(form_id: form_ids_created_at_period_start).where.not(last_submitted_at: nil).count
+          if num_started == 0
             percentage_completed_e2 = 10000
           else
-            percentage_completed_e2 = ((num_completed.to_f / num_created) * 10000).to_i # 0.2546, i.e. 25.46% is stored as 2546
+            percentage_completed_e2 = ((num_completed.to_f / num_started) * 10000).to_i # 0.2546, i.e. 25.46% is stored as 2546
           end
-          Statistic.create_or_update(@date, 'completion_percentage', 'Claim::BaseClaim', percentage_completed_e2, num_created)
+          Statistic.create_or_update(@date, 'completion_percentage', 'Claim::BaseClaim', percentage_completed_e2, num_completed)
         rescue => err
           puts "Error processing for date #{@date}"
           puts err.class
           puts err.message
         end
 
-      end
-
-      private
-
-      def claims_created_at_start_of_period
-        Claim::BaseClaim.where(created_at: @start_date.beginning_of_day..@start_date.end_of_day)
       end
 
     end

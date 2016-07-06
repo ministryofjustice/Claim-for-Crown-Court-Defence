b235662f18c4464528338f637da4e2456717f1e2
Jesus Laiz <zheileman@users.noreply.github.com>
2016-07-06 11:10:01 +0100

    Hide and do not validate defendant date of birth for Breach of Crown Court order case types.

---
+++ app/assets/javascripts/modules/external_users/claims/CaseTypeFieldsDisplay.js
@@ -0,0 +1,32 @@
+moj.Modules.CaseTypeFieldsDisplay = {
+  $caseTypeSelect: {},
+
+  init: function () {
+    var self = this;
+    //initialise handles
+    this.$caseTypeSelect = $('#claim_case_type_id');
+    this.$trialFieldSet = $('#trial-details');
+    this.$retrialFieldSet = $('#retrial-details');
+    this.$defendantFieldSet = $('div.defendants');
+
+    if (!this.$caseTypeSelect.exists()) {
+      return
+    }
+
+    // add change listener
+    this.$caseTypeSelect.change(function () {
+      self.caseTypeChanged();
+    });
+
+    this.caseTypeChanged();
+  },
+  caseTypeChanged: function () {
+    if (this.$caseTypeSelect.val()) {
+      $.getScript("/case_types/" + this.$caseTypeSelect.val());
+    } else {
+      this.$trialFieldSet.hide();
+      this.$retrialFieldSet.hide();
+      this.$defendantFieldSet.removeClass('hide-dob');
+    }
+  }
+};
--- app/interfaces/api/v1/external_users/defendant.rb
+++ app/interfaces/api/v1/external_users/defendant.rb
@@ -10,7 +10,7 @@ module API
           optional :claim_id, type: String, desc: "REQUIRED: Unique identifier for the claim associated with this defendant."
           optional :first_name, type: String, desc: "REQUIRED: First name of the defedant."
           optional :last_name, type: String, desc: "REQUIRED: Last name of the defendant."
-          optional :date_of_birth, type: String, desc: "REQUIRED: Defendant's date of birth (YYYY-MM-DD).", standard_json_format: true
+          optional :date_of_birth, type: String, desc: "REQUIRED/UNREQUIRED: Defendant's date of birth (YYYY-MM-DD). Not required for Breach of Crown Court order case types.", standard_json_format: true
           optional :order_for_judicial_apportionment, type: Boolean, desc: "OPTIONAL: whether or not the defendant is impacted by an order for judicial apportionment (JSON Boolean data type: true or false)"
         end
 

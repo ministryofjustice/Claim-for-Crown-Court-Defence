6ea49c36bbbcf37bbcd5322d589ce23804889e23
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-07-15 17:39:27 +0100

    Don't graph current month in money claimed per month geckoboard widget

--- app/services/stats/money_claimed_per_month_data_generator.rb
+++ app/services/stats/money_claimed_per_month_data_generator.rb
@@ -6,7 +6,7 @@ module Stats
     end
 
     def run
-      stats = Statistic.where(report_name: 'money_claimed_per_month').where('date > ?', 1.year.ago).order(:date)
+      stats = Statistic.where(report_name: 'money_claimed_per_month').where(date: 1.year.ago..@date).order(:date)
       line_graph = Stats::GeckoWidgets::LineGraph.new
       line_graph.x_axis_labels = stats.pluck(:date).map{ |d| d.strftime('%b %y') }
       line_graph.add_dataset('Claimed (£ millions)', stats.pluck(:value_1).map{ |v| (v / 1_000_000.00).round(2)})

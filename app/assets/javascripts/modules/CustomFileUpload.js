7b2e97b9b614320ed2df8a6571ed31f092f73e7c
David Plews (ClearGif) <freelance@cleargif.co.uk>
2016-07-18 13:40:34 +0100

    DAC: Hidden elements recieve focus
    https://www.pivotaltracker.com/story/show/126302631
    
    - fixed the focus issue
    - refactors to the aria attrs for better screen reading

--- app/assets/javascripts/modules/CustomFileUpload.js
+++ app/assets/javascripts/modules/CustomFileUpload.js
@@ -1,14 +1,18 @@
 moj.Modules.CustomFileUpload = {
   el: 'input:file.custom-uploader',
+  tpml: {
+    button: ['<button class="button button-secondary external-user-json-export" type="button">Choose file</button>'].join(''),
+    text: ['<p tabindex="0" class="file-upload-name" aria-hidden="true" ><span class="visuallyhidden">File name:</span></p>'].join('')
+  },
 
   init: function() {
+    $('.btn-import-file').hide().attr('aria-hidden', 'true');
     this.cacheEls();
     this.bindEvents();
     this.setUp();
   },
 
   setUp: function() {
-    this.$forms.addClass('no-file-selected');
     this.$fileInputs
       .after(this.$chooseFileButtonTmpl.clone())
       .after(this.$chooseFileNameTmpl.clone());
@@ -25,10 +29,13 @@ moj.Modules.CustomFileUpload = {
       .on('change', ':file', function(e) {
         var fileName = this.value.replace('C:\\fakepath\\', '');
         $(e.target).closest('form')
-          .removeClass('no-file-selected')
-          .find('.file-upload-name').text(fileName)
+          .find('.file-upload-name').append(fileName).attr('aria-hidden','')
           .parent()
           .find('.errors').empty();
+
+        $('.file-upload-name').focus();
+        $('.btn-import-file').show().attr('aria-hidden', '');
+
       })
       .on('submit', function() {
         $('input.btn-import-file').prop('disabled', true).val('Please wait...');
@@ -38,7 +45,7 @@ moj.Modules.CustomFileUpload = {
   cacheEls: function() {
     this.$fileInputs = $(this.el);
     this.$forms = this.$fileInputs.closest('form');
-    this.$chooseFileNameTmpl = $('<p class="file-upload-name" />');
-    this.$chooseFileButtonTmpl = $('<button class="button button-secondary external-user-json-export" type="button">Choose file</button>');
+    this.$chooseFileNameTmpl = $(this.tpml.text);
+    this.$chooseFileButtonTmpl = $(this.tpml.button);
   }
 };

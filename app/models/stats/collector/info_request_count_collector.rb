99e758ca6768927826ce9133710ded36fca5f000
Jesus Laiz <zheileman@users.noreply.github.com>
2016-07-26 10:36:48 +0100

    Exclude paid in full messages from this request info collector.

--- app/models/stats/collector/info_request_count_collector.rb
+++ app/models/stats/collector/info_request_count_collector.rb
@@ -35,22 +35,25 @@ module Stats
       def get_count_for_claims_authorised_after_further_info_requested
         sql = "
           select
-          date(c.authorised_at),
-            count(*)
+            distinct(c.id),
+            date(c.authorised_at),
+            count(m.id)
           from claims c
           left outer join messages m on (m.claim_id = c.id)
           inner join users u on (m.sender_id = u.id)
           where c.authorised_at between '#{@beginning_of_day}' and '#{@end_of_day}'
           and (u.persona_type is null or u.persona_type !='ExternalUser')
           and m.id is not null
-          group by date(c.authorised_at)
+          and m.body not ilike 'paid in full'
+          group by c.id
+          having count(m.id) = 1
         "
         execute_query(sql)
       end
 
       def execute_query(sql)
         result_set = ActiveRecord::Base.connection.execute(sql)
-        result_set.ntuples == 0 ? 0 : result_set.first['count'].to_i
+        result_set.reduce(0) { |sum, rs| sum + rs['count'].to_i }
       end
     end
   end

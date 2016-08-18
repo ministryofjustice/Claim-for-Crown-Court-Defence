f7d6f39f64d8154619e872db495dbc60809ef366
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-08-18 15:20:29 +0300

    Deleted old migration whcih was just a data migration, and crashed now that we have a new field on Users table

--- db/migrate/20160111111547_change_advocate_persona_type_to_external_user.rb
+++
@@ -1,5 +0,0 @@
-class ChangeAdvocatePersonaTypeToExternalUser < ActiveRecord::Migration
-  def change
-    execute(%q{UPDATE users SET persona_type = 'ExternalUser' WHERE persona_type = 'Advocate'} )
-  end
-end

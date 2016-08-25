e2bf614c96f61039744705c12f08ee0d0453883c
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-08-25 14:34:04 +0100

    Suppress showing hotline link on Ts&Cs, service down etc pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,20 @@
 class PagesController < ApplicationController
 
   skip_load_and_authorize_resource
+  before_action :suppress_hotline_link
 
   def tandcs; end
 
-  def contact_us
-    @suppress_contact_us_message = true
-  end
+  def contact_us; end
 
   def api_landing; end
 
   def api_release_notes; end
 
   def servicedown; end
+
+  private
+  def suppress_hotline_link
+    @suppress_contact_us_message = true
+  end
 end

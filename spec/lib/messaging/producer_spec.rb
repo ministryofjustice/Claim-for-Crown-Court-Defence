c856a5e1afd155a75a43e7403717f04d6f1dd3f7
Jesus Laiz <zheileman@users.noreply.github.com>
2016-10-18 17:26:05 +0100

    Fix test failing on CI.

--- spec/lib/messaging/producer_spec.rb
+++ spec/lib/messaging/producer_spec.rb
@@ -15,10 +15,16 @@ describe Messaging::Producer do
     }.to raise_exception(ArgumentError)
   end
 
-  it 'should publish a message' do
-    subject.publish(message: 'test message')
+  context 'publishing a message' do
+    before(:each) do
+      allow(ENV).to receive(:[]).with('ENV').and_return('test')
+    end
 
-    expect(queue.size).to eq(1)
-    expect(queue.last).to eq(target_arn: 'arn:aws:sns:eu-west-1:016649511486:cccd-claims-local', message: 'test message')
+    it 'should publish a message' do
+      subject.publish(message: 'test message')
+
+      expect(queue.size).to eq(1)
+      expect(queue.last).to eq(target_arn: 'arn:aws:sns:eu-west-1:016649511486:cccd-claims-test', message: 'test message')
+    end
   end
 end

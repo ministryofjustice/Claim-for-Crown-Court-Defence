705ad4f30468f59741a3bd0d9c47768c9b56f407
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2016-12-30 10:33:00 +0000

    Fix issue in spec to cater for varying record ids

--- spec/models/redetermination_spec.rb
+++ spec/models/redetermination_spec.rb
@@ -53,9 +53,9 @@ describe Redetermination do
   describe '#to_s' do
     it 'outputs the totals' do
       rd = FactoryGirl.create :redetermination, fees: 123.22, expenses: 301.55, disbursements: 44.33
-      expected  = "  id:            2\n" +
+      expected  = "  id:            #{rd.id}\n" +
                   "  type           Redetermination\n" +
-                  "  claim_id:      1\n" +
+                  "  claim_id:      #{rd.claim.id}\n" +
                   "  expenses:      301.55\n" +
                   "  fees:          123.22\n" +
                   "  disbursements: 44.33\n" +

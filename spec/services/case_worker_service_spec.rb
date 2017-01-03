4489204cf7a1737f9aa0c25184381ce1b3537445
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-01-03 10:45:31 +0000

    Increase coverage on DocumentsController, CaseWorkerService

---
+++ spec/services/case_worker_service_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe CaseWorkerService do
+  describe '#active' do
+    it 'calls Remote::Caseworker with user and criteria' do
+      service = CaseWorkerService.new(current_user: :my_user, criteria: { key: :my_criteria} )
+      expect(Remote::CaseWorker).to receive(:all).with(:my_user, { key: :my_criteria} )
+      service.active
+    end
+  end
+
+end

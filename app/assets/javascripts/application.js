f94c393fb1a8c5463a0a375c6dcbf79d7ce5327e
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-01-10 10:58:13 +0000

    Provider management form JS
    Provider management form JS
    
    PT:
    https://www.pivotaltracker.com/story/show/137252565
    
    **What this PR does**
    - introduced a proxy event that will test the state of the entire form
    - the state is then passed to another listner that will call the correct show/hide
    - clear the value of the agfs supplier number when hidden
    - added specs
    - added module

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,54 +22,7 @@
     return this.length > 0;
   };
 
-  /*! Tiny Pub/Sub - v0.7.0 - 2013-01-29
-   * https://github.com/cowboy/jquery-tiny-pubsub
-   * Copyright (c) 2013 "Cowboy" Ben Alman; Licensed MIT */
-  var o = $({});
-  $.subscribe = function() {
-    o.on.apply(o, arguments);
-  };
 
-  $.unsubscribe = function() {
-    o.off.apply(o, arguments);
-  };
-
-  $.publish = function() {
-    o.trigger.apply(o, arguments);
-  };
-
-  // Publish the agfs event when provider changes
-  // this will show / hide AGFS supplier number
-  $.subscribe('/provider/type/', function(e, obj) {
-    $.publish('/scheme/type/agfs/', obj);
-  });
-
-
-  // Subscribe to the AGFS event and publish the full state
-  // via a proxy listener
-  $.subscribe('/scheme/type/agfs/', function(e, obj) {
-    var provider = $('#provider_provider_type_chamber').is(':checked') ? 'chamber' : 'firm';
-    var $agfs = $('#provider_roles_agfs').is(':checked');
-    $.publish('/scheme/type/agfs/proxy/', {
-      provider: provider,
-      agfs: $agfs
-    });
-  });
-
-  // Proxy listener to conditionally show / hide the supplier
-  // number for agfs
-  $.subscribe('/scheme/type/agfs/proxy/', function(event, obj) {
-    if(obj.provider === 'firm' && obj.agfs === true){
-      $.publish('/scheme/type/agfs/custom/', {eventValue: 'show-agfs-supplier'});
-      return;
-    }
-    $('input#provider_firm_agfs_supplier_number').val('');
-    $.publish('/scheme/type/agfs/custom/', {eventValue: 'hide-agfs-supplier'});
-  });
-
-  $.jqReveal({
-    // options go here
-  });
 
   $('#fixed-fees, #misc-fees, #disbursements, #expenses, #documents').on('cocoon:after-insert', function(e, insertedItem) {
     var $insertedItem = $(insertedItem);

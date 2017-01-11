c9e041c950f1277bcf9e98acac63d2be23fe2089
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2017-01-11 11:46:13 +0000

    Remove unused form builder methods

--- app/form_builders/adp_form_builder.rb
+++ app/form_builders/adp_form_builder.rb
@@ -25,7 +25,6 @@ class AdpFormBuilder < ActionView::Helpers::FormBuilder
     anchor_and_label_markup(anchor_name, nil, options)
   end
 
-
   private
 
   def anchor_and_label_markup(anchor_name, label, options = {})
@@ -44,44 +43,11 @@ class AdpFormBuilder < ActionView::Helpers::FormBuilder
     "#{make_object_name}_#{anchor_name}"
   end
 
-  def make_option(current_value, member, value_method, text_method, data_options)
-    value = member.send(value_method)
-    option = %Q[<option value="#{member.send(value_method)}"]
-    if current_value == member.send(value_method)
-      option += %Q[ selected="selected"]
-    end
-    data_options.each do |data_key, data_method|
-      option += %Q[ data-#{data_key}="#{member.send(data_method)}"]
-    end
-    option += %Q[>#{member.send(text_method)}</option>]
-    option
-  end
 
-
-  def make_select_start(method)
-    %Q/<select id="#{make_id(method)}" name="#{make_name(method)}" class="form-control autocomplete">/
-  end
-
-  def make_select_end
-    '</select>'
-  end
-
-  def make_id(method)
-    "#{make_object_name}_#{method}"
-  end
-
-  def make_name(method)
-    "#{make_object_name}[#{method}]"
-  end
 
   def make_object_name
     klass_name = object.class.to_s
     klass_name = 'Claim' if klass_name =~ /^Claim::/
     klass_name.camelize.downcase
-  end
-
-
-  def make_prompt
-    %q[<option value="">Please select</option>]
   end
 end

27ed2558f4e6fe69afb8ff3f92891d54798cb91d
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2017-05-30 14:40:38 +0100

    reverse the sequence generated for RAG status (#1975)
    The original sequence of data was generated as Green Amber Red, so successful was displayed
    as Red, rather than green.  This reverses the sequence and tests for the new sequence

--- spec/services/stats/claim_percentage_authorised_generator_spec.rb
+++ spec/services/stats/claim_percentage_authorised_generator_spec.rb
@@ -18,9 +18,9 @@ module Stats
       {
         item:
           [
-            { value: 71.20418848167539, text: 'Authorised' },
+            { value: 7.853403141361256, text: 'Rejected/refused'},
             { value: 20.94240837696335, text: 'Part authorised' },
-            { value: 7.853403141361256, text: 'Rejected/refused'}
+            { value: 71.20418848167539, text: 'Authorised' }
           ]
         }
     end

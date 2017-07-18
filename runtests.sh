890998fe6ce5a2058c53633ab5b394d3a16b96ba
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-07-18 17:33:20 +0100

    tidy travis config and script

--- runtests.sh
+++ runtests.sh
@@ -3,21 +3,18 @@ set -ex
 # set variables. It's presumed the absence of these is causing
 # TRAVIS to fail
 if [ "$TRAVIS" = "true" ]; then
-  printf '\e[32mLoading Schema\e[0m'
+  printf '\e[32mInfo: Loading Schema\e[0m'
   bundle exec rake db:schema:load
   bundle exec rake jasmine:ci
   bundle exec rake spec
-  printf "\e[33mSleeping for two seconds to give CPU time to cool down and perhaps not fail on the cuke tasks because drop down lists aren't populated fast enough\e[0m"
+  printf "\e[33mInfo: Sleeping for two seconds to give CPU time to cool down and perhaps not fail on the cuke tasks because drop down lists aren't populated fast enough\e[0m"
   sleep 2
   bundle exec rake cucumber
 
   exit 0
 else
-  # Script executing all the test tasks.
+  printf "\e[33mInfo: Executing smoke test\e[0m"
   bundle exec rake db:migrate
-  # execute smoke test - needs seeded tables
   bundle exec rake db:seed
-
-  printf "\e[33mINFO: EXECUTING SMOKE TEST\e[0m"
   bundle exec rake api:smoke_test
 fi

5d9f77f28db3e8ae091b31609b2ca2c2738b8ee3
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-07-20 16:10:05 +0100

    refactor "in place" anonymisation SQL.

--- db/data/anonymise_db.sql
+++ db/data/anonymise_db.sql
@@ -1,23 +1,25 @@
+\set chars_to_translate '\'' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC '\''
+
 UPDATE providers
-SET name = translate(name, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC', :translation);
+SET name = translate(name, :chars_to_translate, :translation);
 
 UPDATE users
-SET first_name = translate(first_name, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC', :translation)
-  , last_name = translate(last_name, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC', :translation);
+SET first_name = translate(first_name, :chars_to_translate, :translation)
+  , last_name = translate(last_name, :chars_to_translate, :translation);
 
 UPDATE defendants
-SET first_name = translate(first_name, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC', :translation)
-  , last_name = translate(last_name, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC', :translation);
+SET first_name = translate(first_name, :chars_to_translate, :translation)
+  , last_name = translate(last_name, :chars_to_translate, :translation);
 
 -- messages can contain sensitive data
 UPDATE messages
-SET body = translate(body, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC', :translation);
+SET body = translate(body, :chars_to_translate, :translation);
 
 -- message attachment file names can contain sensitive data
 UPDATE messages
 SET attachment_file_name = translate(
       substr(attachment_file_name, 1, char_length(attachment_file_name) - position('.' in reverse(attachment_file_name))),
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC',
+    :chars_to_translate,
     :translation
     )
     || substr(attachment_file_name,char_length(attachment_file_name) - position('.' in reverse(attachment_file_name)) + 1)
@@ -27,19 +29,19 @@ WHERE attachment_file_name IS NOT NULL;
 UPDATE documents
 SET document_file_name = translate(
       substr(document_file_name, 1, char_length(document_file_name) - position('.' in reverse(document_file_name))),
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC',
+    :chars_to_translate,
     :translation
     )
     || substr(document_file_name,char_length(document_file_name) - position('.' in reverse(document_file_name)) + 1),
     converted_preview_document_file_name = translate(
       substr(converted_preview_document_file_name, 1, char_length(converted_preview_document_file_name) - position('.' in reverse(document_file_name))),
-    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC',
+    :chars_to_translate,
     :translation
     )
-    || substr(converted_preview_document_file_name,char_length(converted_preview_document_file_name) - position('.' in reverse(converted_preview_document_file_name)) + 1);
+    || substr(converted_preview_document_file_name, char_length(converted_preview_document_file_name) - position('.' in reverse(converted_preview_document_file_name)) + 1);
 
 -- additional information can contain sensitive data
 UPDATE claims
-SET additional_information = translate(additional_information, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZC', :translation)
+SET additional_information = translate(additional_information, :chars_to_translate, :translation)
 WHERE additional_information IS NOT NULL
-  AND length(regexp_replace(additional_information, '[\s\t\n]+','','g')) > 0;
+  AND length(regexp_replace(additional_information, '[\s\t\n]+', '', 'g')) > 0;

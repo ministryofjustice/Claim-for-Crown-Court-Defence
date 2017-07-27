28084fd833c49fbcb720bc43c016dc6faa8fcb23
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-07-27 10:51:31 +0100

    refactor the search result

--- app/interfaces/api/entities/search_result.rb
+++ app/interfaces/api/entities/search_result.rb
@@ -1,7 +1,6 @@
 module API
   module Entities
     class SearchResult < BaseEntity
-      include SearchResultHelpers
 
       expose :id
       expose :uuid
@@ -14,14 +13,13 @@ module API
       expose :case_type
       expose :total, format_with: :decimal
       expose :total_display
-      expose :opened_for_redetermination?, as: :redetermination
-      expose :disk_evidence
       expose :external_user
       expose :last_submitted_at
       expose :last_submitted_at_display
       expose :defendants
       expose :maat_references
       expose :filter do
+        expose :disk_evidence
         expose :redetermination
         expose :fixed_fee
         expose :awaiting_written_reasons
@@ -37,81 +35,104 @@ module API
 
       private
 
+      def id
+        object['id']
+      end
+
+      def uuid
+        object['uuid']
+      end
+
       def scheme
-        object.type.gsub(/Claim|::/, '')
+        object['scheme']
       end
 
       def scheme_type
-        scheme.eql?('Litigator') ? 'Final' : scheme
+        object['scheme_type']
       end
 
-      def state_display
-        object.state.humanize
+      def state
+        object['state']
       end
 
       def court_name
-        object.court&.name
+        object['court_name']
       end
 
       def case_type
-        object.case_type&.name
+        object['case_type']
       end
 
-      def total_display
-        ActiveSupport::NumberHelper.number_to_currency(object.total, precision: 2, delimiter: ',')
-      end
-
-      def disk_evidence
-        object.disk_evidence.present? && object.disk_evidence
+      def total
+        object['total']
       end
 
       def external_user
-        "#{object&.external_user.first_name} #{object&.external_user.last_name}"
+        object['external_user']
       end
 
       def last_submitted_at
-        object.last_submitted_at.to_i
-      end
-
-      def last_submitted_at_display
-        object.last_submitted_at.strftime('%d/%m/%Y')
+        object['last_submitted_at']
       end
 
       def defendants
-        object&.defendants.map { |defendant| [defendant.first_name, defendant.last_name].join(' ') }.join(', ')
+        object['defendants']
       end
 
       def maat_references
-        object&.representation_orders.map(&:maat_reference).join(', ')
+        object['maat_references']
+      end
+
+      def state_display
+        state.humanize
+      end
+
+      def total_display
+        ActiveSupport::NumberHelper.number_to_currency(total, precision: 2, delimiter: ',')
+      end
+
+      def disk_evidence
+        object['disk_evidence'].eql?(true)
+      end
+
+      def last_submitted_at_display
+        last_submitted_at.strftime('%d/%m/%Y')
       end
 
       def redetermination
-        object.redetermination?
+        state.eql?('redetermination')
       end
 
-      def fixed_fee
-        object&.case_type&.is_fixed_fee
+      def fees
+        object['fees']&.split(',')&.map { |fee| fee.split('~') }
       end
 
       def awaiting_written_reasons
-        object.awaiting_written_reasons?
+        state.eql?('awaiting_written_reasons')
       end
 
       def cracked
-        object&.case_type&.name.eql?('Cracked Trial')
+        case_type.eql?('Cracked Trial')
       end
 
       def trial
-        object&.case_type&.name.eql?('Trial')
+        case_type.eql?('Trial')
       end
 
       def guilty_plea
-        object&.case_type&.name.eql?('Guilty plea')
+        case_type.eql?('Guilty plea')
+      end
+
+      def fixed_fee
+        object['is_fixed_fee'].eql?(true)
       end
 
       def graduated_fees
-        object.allocation_type.eql?('Grad') ||
-          object&.case_type&.fee_type_code.in?(::Fee::GraduatedFeeType.pluck(:unique_code))
+        object['fee_type_code']&.in?(graduated_fee_codes).eql?(true)
+      end
+
+      def graduated_fee_codes
+        object['graduated_fee_types']&.split(',')
       end
 
       def interim_fees
@@ -127,7 +148,43 @@ module API
       end
 
       def risk_based_bills
-        risk_based_class_letter && contains_risk_based_fee
+        (risk_based_class_letter && contains_risk_based_fee).eql?(true)
+      end
+
+      def fee_is_interim_type
+        fees.map do |fee|
+          [
+              fee[2].eql?('Fee::InterimFeeType'),
+              fee[1].downcase.in?(['effective pcmh', 'trial start', 'retrial new solicitor', 'retrial start'])
+          ].all?
+        end.any?
+      end
+
+      def risk_based_class_letter
+        object['class_letter']&.in?(%w(E F H I))
+      end
+
+      def contains_risk_based_fee
+        fees&.map do |fee|
+          [
+              fee[0].to_i.between?(1, 50),
+              fee[1].eql?('Guilty plea'),
+              fee[2].eql?('Fee::GraduatedFeeType')
+          ]&.all?
+        end&.any?
+      end
+
+      def interim_claim?
+        scheme_type.eql?('Interim')
+      end
+
+      def contains_fee_of_type(fee_type_description)
+        fees.map do |fee|
+          [
+              fee[2].eql?('Fee::InterimFeeType'),
+              fee[1].eql?(fee_type_description)
+          ].all?
+        end.any?
       end
     end
   end

e36e03927fbf7138c27145bd88adb95fda3e36de
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-07-27 10:49:40 +0100

    replace AR query with SQL

--- app/interfaces/api/v2/search.rb
+++ app/interfaces/api/v2/search.rb
@@ -15,22 +15,74 @@ module API
 
         helpers do
           def claims
-            ::Claim::BaseClaim
-              .active.__send__(scheme)
-              .submitted_or_redetermination_or_awaiting_written_reasons
-              .includes(:case_type,
-                        :court,
-                        :claim_state_transitions,
-                        :external_user,
-                        :fees,
-                        :messages => :user_message_statuses,
-                        :defendants => :representation_orders,
-                        :offence => :offence_class
-              )
+            built_sql = unallocated_sql.gsub(/REPLACE_MATCHER/, scheme.eql?('agfs') ? ' = ' : ' != ')
+            ActiveRecord::Base.connection.execute(built_sql).to_a
           end
 
           def scheme
             params.scheme
+          end
+
+          def unallocated_sql
+            <<~SQL
+              SELECT
+                c.id,
+                c.uuid,
+                CASE WHEN c.type = 'Claim::AdvocateClaim' THEN 'agfs' ELSE 'lgfs' END AS scheme,
+                CASE
+                  WHEN ltrim(replace(type, 'Claim', ''), '::') = 'Litigator'
+                  THEN 'Final' ELSE ltrim(replace(type, 'Claim', ''), '::')
+                  END AS scheme_type,
+                c.case_number,
+                c.state,
+                court.name AS court_name,
+                ct.name as case_type,
+                c.total,
+                c.disk_evidence,
+                u.first_name || ' ' || u.last_name AS external_user,
+                string_agg(ro.maat_reference, ', ') AS maat_references,
+                string_agg(d.first_name || ' ' || d.last_name, ', ') as defendants,
+                (
+                  SELECT string_agg(fees.quantity || '~' || ft.description || '~' || ft.type, ', ')
+                  FROM fee_types ft INNER JOIN fees ON fees.fee_type_id = ft.id
+                  WHERE fees.claim_id = c.id
+                ) AS fees,
+                c.last_submitted_at,
+                oc.class_letter,
+                ct.is_fixed_fee,
+                ct.fee_type_code,
+                (
+                  SELECT string_agg(unique_code, ',')
+                  FROM fee_types
+                  WHERE type IN ('Fee::GraduatedFeeType')
+                ) AS graduated_fee_types
+              FROM claims AS c
+                LEFT OUTER JOIN defendants AS d
+                  ON c.id = d.claim_id
+                LEFT OUTER JOIN representation_orders AS ro
+                  on d.id = ro.defendant_id
+                LEFT OUTER JOIN external_users AS eu
+                  on c.external_user_id = eu.id
+                LEFT OUTER JOIN users as u
+                  ON u.id = eu.provider_id
+                LEFT OUTER JOIN courts AS court
+                  ON c.court_id = court.id
+                LEFT OUTER JOIN case_types AS ct
+                  ON c.case_type_id = ct.id
+                LEFT OUTER JOIN offences AS o
+                  ON c.offence_id = o.id
+                LEFT OUTER JOIN offence_classes AS oc
+                  ON o.offence_class_id = oc.id
+              WHERE
+                c.deleted_at IS NULL
+                AND c.type REPLACE_MATCHER 'Claim::AdvocateClaim'
+                AND c.state IN ('submitted', 'redetermination' ,'awaiting_written_reasons')
+              GROUP BY
+                c.id, c.uuid, c.allocation_type, court.name,
+                ct.name, ct.is_fixed_fee, ct.fee_type_code, c.disk_evidence,
+                u.first_name, u.last_name, oc.class_letter, d.last_name, d.first_name
+              ;
+            SQL
           end
         end
 

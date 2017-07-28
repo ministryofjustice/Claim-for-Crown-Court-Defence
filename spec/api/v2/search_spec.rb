b5820dc9c4a63fc06464abcea772f6c2bfdc2eba
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-07-28 09:07:46 +0100

    Add a test to restrict endpoint to caseworkers

--- spec/api/v2/search_spec.rb
+++ spec/api/v2/search_spec.rb
@@ -16,10 +16,11 @@ describe API::V2::Search do
 
   let(:get_claims_endpoint) { '/api/search/unallocated' }
   let(:case_worker_admin) { create(:case_worker, :admin) }
-  let(:pagination) { {} }
+  let(:l33t_h4xx0r) { create(:external_user) }
+  let(:api_key) { case_worker_admin.user.api_key }
   let(:params) do
     {
-        api_key: case_worker_admin.user.api_key,
+        api_key: api_key,
         scheme: 'agfs'
     }
   end
@@ -45,14 +46,25 @@ describe API::V2::Search do
       expect(last_response.body).to include('Unauthorised')
     end
 
-    it 'should return a JSON with the required information' do
-      response = do_request
-      expect(response.status).to eq 200
+    context 'when accessed by a CaseWorker' do
+      it 'should return a JSON with the required information' do
+        response = do_request
+        expect(response.status).to eq 200
 
-      body = JSON.parse(response.body, symbolize_names: true)
-      cw = body.first
+        body = JSON.parse(response.body, symbolize_names: true)
+        cw = body.first
 
-      expect(cw.keys).to eq([:id, :uuid, :scheme, :scheme_type, :case_number, :state, :state_display, :court_name, :case_type, :total, :total_display, :external_user, :last_submitted_at, :last_submitted_at_display, :defendants, :maat_references, :filter])
+        expect(cw.keys).to eq([:id, :uuid, :scheme, :scheme_type, :case_number, :state, :state_display, :court_name, :case_type, :total, :total_display, :external_user, :last_submitted_at, :last_submitted_at_display, :defendants, :maat_references, :filter])
+      end
+    end
+    context 'when accessed by a ExternalUser' do
+      let(:api_key) { l33t_h4xx0r.user.api_key }
+
+      it 'returns unauthorised' do
+        do_request
+        expect(last_response.status).to eq 401
+        expect(last_response.body).to include('Unauthorised')
+      end
     end
   end
 end

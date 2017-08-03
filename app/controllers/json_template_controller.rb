3154ec3ef3049f482570992250c90a80b8e6e607
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-08-03 18:08:20 +0100

    enable json schema publishing

--- app/controllers/json_template_controller.rb
+++ app/controllers/json_template_controller.rb
@@ -1,7 +1,24 @@
 class JsonTemplateController < ApplicationController
-  skip_load_and_authorize_resource only: [:index]
+  skip_load_and_authorize_resource only: [:index, :show]
+
+  before_action :set_schema, only: :show
 
   def index
     @schema = ClaimJsonSchemaValidator.full_schema
   end
+
+  def show
+    render json: @schema
+  end
+
+  private
+
+  def set_schema
+    @schema = ClaimJsonSchemaValidator.send(schema_params[:id].to_sym)
+  end
+
+  def schema_params
+    params.permit(:id)
+  end
+
 end

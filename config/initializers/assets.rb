c125d20a88619c61fcd811781351f811b3c4284d
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-08-08 09:25:19 +0100

    Add vendor images path to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( adp_swagger_application.js email.css )
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')

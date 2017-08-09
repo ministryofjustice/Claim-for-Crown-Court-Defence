25e74b9a171ec7cb3a35643f933b64d8afe1c43d
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2017-08-09 11:55:32 +0100

    enable json schema publishing (#2031)
    Enable json schema publishing
    
    To assist development conversations and iteration
    on, in particular, CCR consumption of a GET claim
    JSON response

--- app/controllers/json_template_controller.rb
+++ app/controllers/json_template_controller.rb
@@ -1,7 +1,23 @@
 class JsonTemplateController < ApplicationController
-  skip_load_and_authorize_resource only: [:index]
+  skip_load_and_authorize_resource only: %i[index show]
+
+  before_action :schema, only: :show
 
   def index
     @schema = ClaimJsonSchemaValidator.full_schema
+  end
+
+  def show
+    render json: schema
+  end
+
+  private
+
+  def schema
+    @schema ||= ClaimJsonSchemaValidator.send(schema_params[:schema].to_sym)
+  end
+
+  def schema_params
+    params.permit(:schema)
   end
 end

807f0d2c8cd2aae93711940992f76936b69179bc
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-08-11 13:52:40 +0100

    Extend tests for exclusivity

--- spec/api/entities/search_result_spec.rb
+++ spec/api/entities/search_result_spec.rb
@@ -7,9 +7,16 @@ describe API::Entities::SearchResult do
   describe 'filters' do
     subject(:filter) { JSON.parse(search_result.to_json)['filter'] }
 
+    context 'when passed a submitted case with a graduated fee ' do
+      let(:claim) { OpenStruct.new('id'=>'19932', 'uuid'=>'aec3900f-3e82-4c4f-a7cd-498ad45f11f8', 'scheme'=>'agfs', 'scheme_type'=>'Advocate', 'case_number'=>'T20160427', 'state'=>'submitted', 'court_name'=>'Newcastle', 'case_type'=>'Contempt', 'total'=>'426.36', 'disk_evidence'=>'f', 'external_user'=>'Theodore Schumm', 'maat_references'=>'2320144', 'defendants'=>'Junius Lesch', 'fees'=>'0.0~Daily attendance fee (3 to 40)~Fee::BasicFeeType, 0.0~Daily attendance fee (41 to 50)~Fee::BasicFeeType, 0.0~Daily attendance fee (51+)~Fee::BasicFeeType, 0.0~Standard appearance fee~Fee::BasicFeeType, 0.0~Plea and case management hearing~Fee::BasicFeeType, 0.0~Conferences and views~Fee::BasicFeeType, 0.0~Number of defendants uplift~Fee::BasicFeeType, 0.0~Number of cases uplift~Fee::BasicFeeType, 0.0~Number of prosecution witnesses~Fee::BasicFeeType, 1.0~Basic fee~Fee::BasicFeeType, 34.0~Pages of prosecution evidence~Fee::BasicFeeType', 'last_submitted_at'=>'2017-07-06 09:33:30.932017', 'class_letter'=>'F', 'is_fixed_fee'=>'f', 'fee_type_code'=>'GRRAK', 'graduated_fee_types'=>'GRTRL,GRRTR,GRGLT,GRDIS,GRRAK,GRCBR') }
+      let(:result) { {'disk_evidence'=>false, 'redetermination'=>false, 'fixed_fee'=>false, 'awaiting_written_reasons'=>false, 'cracked'=>false, 'trial'=>false, 'guilty_plea'=>false, 'graduated_fees'=>true, 'interim_fees'=>false, 'warrants'=>false, 'interim_disbursements'=>false, 'risk_based_bills'=>false} }
+
+      it { is_expected.to eql result }
+    end
+
     context 'when passed a redetermination case with a graduated fee ' do
       let(:claim) { OpenStruct.new('id'=>'19932', 'uuid'=>'aec3900f-3e82-4c4f-a7cd-498ad45f11f8', 'scheme'=>'agfs', 'scheme_type'=>'Advocate', 'case_number'=>'T20160427', 'state'=>'redetermination', 'court_name'=>'Newcastle', 'case_type'=>'Contempt', 'total'=>'426.36', 'disk_evidence'=>'f', 'external_user'=>'Theodore Schumm', 'maat_references'=>'2320144', 'defendants'=>'Junius Lesch', 'fees'=>'0.0~Daily attendance fee (3 to 40)~Fee::BasicFeeType, 0.0~Daily attendance fee (41 to 50)~Fee::BasicFeeType, 0.0~Daily attendance fee (51+)~Fee::BasicFeeType, 0.0~Standard appearance fee~Fee::BasicFeeType, 0.0~Plea and case management hearing~Fee::BasicFeeType, 0.0~Conferences and views~Fee::BasicFeeType, 0.0~Number of defendants uplift~Fee::BasicFeeType, 0.0~Number of cases uplift~Fee::BasicFeeType, 0.0~Number of prosecution witnesses~Fee::BasicFeeType, 1.0~Basic fee~Fee::BasicFeeType, 34.0~Pages of prosecution evidence~Fee::BasicFeeType', 'last_submitted_at'=>'2017-07-06 09:33:30.932017', 'class_letter'=>'F', 'is_fixed_fee'=>'f', 'fee_type_code'=>'GRRAK', 'graduated_fee_types'=>'GRTRL,GRRTR,GRGLT,GRDIS,GRRAK,GRCBR') }
-      let(:result) { {'disk_evidence'=>false, 'redetermination'=>true, 'fixed_fee'=>false, 'awaiting_written_reasons'=>false, 'cracked'=>false, 'trial'=>false, 'guilty_plea'=>false, 'graduated_fees'=>true, 'interim_fees'=>false, 'warrants'=>false, 'interim_disbursements'=>false, 'risk_based_bills'=>false} }
+      let(:result) { {'disk_evidence'=>false, 'redetermination'=>true, 'fixed_fee'=>false, 'awaiting_written_reasons'=>false, 'cracked'=>false, 'trial'=>false, 'guilty_plea'=>false, 'graduated_fees'=>false, 'interim_fees'=>false, 'warrants'=>false, 'interim_disbursements'=>false, 'risk_based_bills'=>false} }
 
       it { is_expected.to eql result }
     end

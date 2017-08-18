16ba73e568e955b3462ec533eb9e3ab2cd7f6ee9
David Plews <cleargif@users.noreply.github.com>
2017-08-18 08:16:29 +0100

    Allocation / old allocation swop over (#2054)
    Allocation / old allocation swap over

--- app/assets/javascripts/modules/Modules.AllocationDataTable.js
+++ app/assets/javascripts/modules/Modules.AllocationDataTable.js
@@ -72,6 +72,13 @@ moj.Modules.AllocationDataTable = {
       infoFiltered: "",
       processing: "Table loading, please wait a moment."
     },
+    initComplete: function(settings, json) {
+      // block the row highlight from happening
+      // when a link is clicked
+      $('#dtAllocation tbody tr').on('click', 'a', function(e){
+        e.stopImmediatePropagation();
+      });
+    },
     // $.ajax config object
     // https://datatables.net/reference/option/ajax
     // The url is set during the init procedures
@@ -102,9 +109,7 @@ moj.Modules.AllocationDataTable = {
       targets: 1,
       data: null,
       render: function(data, type, full) {
-        // No link string
-        return data.filter.disk_evidence ? data.case_number + '<br/><span class="disk-evidence">Disk evidence</span>' : data.case_number;
-        //return data.filter.disk_evidence ? '<a href="#noop">' + data.case_number + '</a><br/><span class="disk-evidence">Disk evidence</span>' : '<a href="#noop">' + data.case_number + '</a>';
+        return data.filter.disk_evidence ? '<a href="/case_workers/claims/'+ data.id +'">' + data.case_number + '</a><br/><span class="disk-evidence">Disk evidence</span>' : '<a href="/case_workers/claims/'+ data.id +'">' + data.case_number + '</a>';
       }
 
     }, {
@@ -148,14 +153,11 @@ moj.Modules.AllocationDataTable = {
 
     this.searchConfig.key = $('#api-key').data('api-key');
 
-    // http://localhost:3001/api/search/unallocated?api_key=bbef1c5f-0ded-43d2-8d53-5a6358659dac&scheme=agfs
-
     this.setAjaxURL();
     this.dataTable = moj.Modules.DataTables._init(this.options, '#dtAllocation');
 
     // :(
     $('#dtAllocation_filter').find('input').addClass('form-control');
-
 
     this.bindEvents();
     this.registerCustomSearch();

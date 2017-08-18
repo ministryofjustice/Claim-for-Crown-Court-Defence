8f14e037cc6fa090a041b465124119b91d3afaee
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-08-18 14:05:58 +0100

    Add Rack::Deflater to application

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module AdvocateDefencePayments
   class Application < Rails::Application
+    config.middleware.use Rack::Deflater
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

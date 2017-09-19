b9312b57fecfae17a6778e0ccb1f7639f28fbcd5
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-09-19 12:00:14 +0100

    add data migration to amend offence description typo

---
+++ db/migrate/20170919103557_modify_offence_description.rb
@@ -0,0 +1,15 @@
+class ModifyOffenceDescription < ActiveRecord::Migration
+  # fix typo
+  def up
+    Offence
+      .where(description: 'Possession of false identify documents')
+      .update_all(description: 'Possession of false identity documents')
+  end
+
+  # reinstate typo
+  def down
+    Offence
+      .where(description: 'Possession of false identity documents')
+      .update_all(description: 'Possession of false identify documents')
+  end
+end

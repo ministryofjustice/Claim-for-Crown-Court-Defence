b754916e074f3dbcaaa3bca4a4be2191e9511496
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-09-21 09:33:52 +0100

    update the govuk_notify initialiser
    Change to the new style API key from id and key

--- config/initializers/govuk_notify_rails.rb
+++ config/initializers/govuk_notify_rails.rb
@@ -1,3 +1,2 @@
 ActionMailer::Base.add_delivery_method :govuk_notify, GovukNotifyRails::Delivery,
-  service_id: ENV['GOVUK_NOTIFY_SERVICE_ID'],
-  secret_key: ENV['GOVUK_NOTIFY_API_SECRET']
+                                       api_key: Settings.govuk_notify.api_key

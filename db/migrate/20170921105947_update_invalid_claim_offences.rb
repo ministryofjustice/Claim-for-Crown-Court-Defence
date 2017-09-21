0d6b032f887d16f0e9cb28cde9cdadce0153598e
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-09-21 17:15:15 +0100

    Add migration to delete invalid offences

--- db/migrate/20170921105947_update_invalid_claim_offences.rb
+++ db/migrate/20170921105947_update_invalid_claim_offences.rb
@@ -1,16 +1,16 @@
 module InvalidOffences
-  def invalid_offence_G_and_K_ids
+  def invalid_G_and_K_offences
    Offence.joins(:offence_class).where(description: [
     'Destruction of registers of births etc.',
     'Making false entries in copies of registers sent to register',
     'Possession of false identity documents',
     'Possession (with intention) of false identity documents',
     'Possession (with intention) of apparatus or material for making false identity documents',
-    'Possession (without reasonable excuse) of false identity documents or apparatus or material for making false identity documents',
-  ]).where.not(offence_classes: {class_letter: 'F'}).pluck(:id)
+    'Possession (without reasonable excuse) of false identity documents or apparatus or material for making false identity documents'
+  ]).where.not(offence_classes: {class_letter: 'F'})
   end
 
-  def invalid_offence_F_and_K_ids
+  def invalid_F_and_K_offences
     Offence.joins(:offence_class).where(description: [
       'Undischarged bankrupt being concerned in a company',
       'Counterfeiting notes and coins',
@@ -19,15 +19,15 @@ module InvalidOffences
       'Making, custody or control of counterfeiting materials etc.',
       'Illegal importation: counterfeit notes or coins',
       'Fraudulent evasion: counterfeit notes or coins'
-    ]).where.not(offence_classes: {class_letter: 'G'}).pluck(:id)
+    ]).where.not(offence_classes: {class_letter: 'G'})
   end
 
   def claims_requiring_class_f_of_offence
-    Claim::BaseClaim.joins(:offence).where(offences: {id: invalid_offence_G_and_K_ids })
+    Claim::BaseClaim.joins(:offence).where(offences: { id: invalid_G_and_K_offences.pluck(:id) })
   end
 
   def claims_requiring_class_g_of_offence
-      Claim::BaseClaim.joins(:offence).where(offences: {id: invalid_offence_F_and_K_ids })
+    Claim::BaseClaim.joins(:offence).where(offences: { id: invalid_F_and_K_offences.pluck(:id) })
   end
 end
 
@@ -35,6 +35,8 @@ class UpdateInvalidClaimOffences < ActiveRecord::Migration
   include InvalidOffences
 
   def up
+    # update claims with invalid offences
+    #
     claims_requiring_class_f_of_offence.each do |claim|
       new_offence = Offence.joins(:offence_class).where(description: claim.offence.description).where(offence_class: { class_letter: 'F' }).first
       claim.update_column(:offence_id, new_offence.id)
@@ -44,9 +46,15 @@ class UpdateInvalidClaimOffences < ActiveRecord::Migration
       new_offence = Offence.joins(:offence_class).where(description: claim.offence.description).where(offence_class: { class_letter: 'G' }).first
       claim.update_column(:offence_id, new_offence.id)
     end
+
+    # delete the invalid offences
+    #
+    invalid_G_and_K_offences.delete_all
+    invalid_F_and_K_offences.delete_all
   end
 
   def down
+    puts "Warning: The update of claims with invalid offences and deletion of those offences is irreversible"
     raise ActiveRecord::IrreversibleMigration
   end
 end

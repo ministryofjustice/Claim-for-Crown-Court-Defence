c057148f14a62b2e2fc9ac89b920fdb3ca12a99f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-10-19 14:58:08 +0100

    simplifiy the downloaders file locator

--- app/services/s3_zip_downloader.rb
+++ app/services/s3_zip_downloader.rb
@@ -26,21 +26,8 @@ class S3ZipDownloader
   end
 
   def move_files
-    send("move_files_from_#{Paperclip::Attachment.default_options[:storage]}")
-  end
-
-  def move_files_from_filesystem
     @claim.documents.each do |file|
-      FileUtils.copy(file.file_path, @folder)
-    end
-  end
-
-  def move_files_from_s3
-    s3 = Aws::S3::Resource.new(region: 'eu-west-1')
-    bucket = s3.bucket(ENV['adp-bucket-name'])
-    @claim.documents.each do |file|
-      file_obj = bucket.object(file.file_path)
-      file_obj.get(response_target: "#{@folder}/#{file}")
+      FileUtils.copy(Paperclip.io_adapters.for(file.document).path, @folder)
     end
   end
 end

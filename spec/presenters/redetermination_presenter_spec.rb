4c54f4254454ff01f76cbeed20a1e5888b59b117
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2017-10-20 14:20:16 +0100

    Add handler for vat_amount being nil (#2111)
    The front end allows caseworkers to submit a nil value for VAT amount
    this causes the redetermination presenter to blow up because it
    can't add nil to the total.
    This prevented the display of the claim entirely.
    
    This ensures the vat amount will add zero instead of nil and allow the
    display of the claim

--- spec/presenters/redetermination_presenter_spec.rb
+++ spec/presenters/redetermination_presenter_spec.rb
@@ -6,7 +6,6 @@ describe RedeterminationPresenter do
   let(:rd)              { FactoryGirl.create :redetermination, fees: 1452.33, expenses: 2455.77, disbursements: 2123.55, claim: claim }
   let(:presenter)       { RedeterminationPresenter.new(rd, view) }
 
-
   context 'currency fields' do
     it 'should format currency amount' do
       expect(presenter.fees_total).to eq '£1,452.33'
@@ -18,4 +17,11 @@ describe RedeterminationPresenter do
     end
   end
 
+  context 'when VAT amount is nil' do
+    before { allow(rd).to receive(:vat_amount).and_return(nil) }
+
+    it 'should still calculate' do
+      expect(presenter.total_inc_vat).to eq '£6,031.65'
+    end
+  end
 end

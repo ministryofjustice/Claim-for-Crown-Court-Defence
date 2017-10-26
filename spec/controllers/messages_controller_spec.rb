2034bd5293b62078f4e350323ff7a94b5d0dc89c
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2017-10-26 11:46:12 +0100

    Fix message_added_email (#2116)
    # Why?
    The mailer was attempting to send emails to a softly-deleted user
    As this suffixes `.deleted.{id}` to the users email, this would fail.
    
    # What?
    Add tests to replicate the situation and then handle the situation by
    not attempting to send messages to softly-deleted external users

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -135,6 +135,19 @@ RSpec.describe MessagesController, type: :controller do
           expect(mock_mail).to receive(:deliver_later)
           post :create, message: message_params
         end
+
+        context 'but has been deleted' do
+          before do
+            claim.creator.email_notification_of_message = 'true'
+            claim.creator.soft_delete
+            sign_in sender.user
+          end
+
+          it 'does not send an email' do
+            expect(NotifyMailer).not_to receive(:message_added_email)
+            post :create, message: message_params
+          end
+        end
       end
       context 'external_user_is_not_setup_to_recieve_emails' do
         it 'does not send an email' do

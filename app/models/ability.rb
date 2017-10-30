a636b7482a2452d87c417b855e1a67a46f431d35
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2017-10-30 13:58:49 +0000

    Add find user (#2119)
    * Add route and link for new feature
    * Implement the Controller for finding emails
    * Allow users to find and search in CanCan
    * Handle the views
    * Add the search form
    * Update the display form to add a link to the provider
    * clarify and unify headings

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,7 +8,7 @@ class Ability
 
     if persona.is_a? SuperAdmin
       can %i[show index new create edit update], Provider
-      can %i[show index new create edit update change_password update_password], ExternalUser
+      can %i[show index new create edit update change_password update_password find search], ExternalUser
       can %i[show edit update change_password update_password], SuperAdmin, id: persona.id
       can [:update_settings], User, id: user.id
       return

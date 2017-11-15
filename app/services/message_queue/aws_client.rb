fcc194484d30076b376368f695d4fddb7ee10ed3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-11-15 11:59:26 +0000

    Refactor the AwsClient class
    * split the message template out to separate model
    * refactor the initialiser to allow polling
    * rename send_message! to clarify use with new param
    * add poll! method

--- app/services/message_queue/aws_client.rb
+++ app/services/message_queue/aws_client.rb
@@ -1,43 +1,35 @@
 module MessageQueue
   class AwsClient
-    def initialize(message, to_queue)
+    def initialize(queue)
       @sqs = Aws::SQS::Client.new(access_key_id: Settings.aws.access, secret_access_key: Settings.aws.secret)
-      @message = message
       begin
-        @queue_url = @sqs.get_queue_url(queue_name: to_queue).queue_url
+        @queue_url = @sqs.get_queue_url(queue_name: queue).queue_url
       rescue Aws::SQS::Errors::NonExistentQueue
-        raise StandardError.new, "Non existing queue: #{to_queue}."
+        raise StandardError.new, "Non existing queue: #{queue}."
       end
     end
 
-    def send_message!
+    def send!(message)
       @sqs.send_message(
         queue_url: @queue_url,
-        message_body: @message[:body],
-        message_attributes: @message[:attributes]
+        message_body: message[:body],
+        message_attributes: message[:attributes]
       )
       true
     end
-  end
 
-  class MessageTemplate
-    def self.claim_created(type, uuid)
-      {
-        body: 'Claim submitted',
-        attributes:
-          {
-            'type':
-              {
-                data_type: 'String',
-                string_value: type
-              },
-            'uuid':
-              {
-                data_type: 'String',
-                string_value: uuid
-              }
-          }
-      }
+    def poll!
+      resp = @sqs.receive_message(
+        queue_url: @queue_url,
+        max_number_of_messages: Settings.aws.poll_message_count,
+        wait_time_seconds: Settings.aws.poll_message_wait_time
+      )
+      resp.messages.each do |m|
+        irs = InjectionResponseService.new(JSON.parse(m.body))
+        if irs.run! && %w[demo production].include?(ENV['ENV'])
+          @sqs.delete_message(queue_url: @queue_url, receipt_handle: m.receipt_handle)
+        end
+      end
     end
   end
 end
--- spec/services/message_queue/aws_client_spec.rb
+++ spec/services/message_queue/aws_client_spec.rb
@@ -1,39 +1,58 @@
 require 'rails_helper'
 
 module MessageQueue
-  describe AwsClient do
-    subject(:aws_client) { described_class.new(message, aws_queue_name) }
+  describe AwsClient, slack_bot: true do
+    subject(:aws_client) { described_class.new(aws_queue_name) }
 
     let(:client) do
       Aws::SQS::Client.new(
         region: 'eu_west_1',
         stub_responses:
-        {
-          list_queues: { queue_urls:['valid_queue_name'] },
-          get_queue_url: stub_queue_response,
-          send_message: stub_send_response
-        }
+          {
+            list_queues: { queue_urls:['valid_queue_name'] },
+            get_queue_url: stub_queue_response,
+            send_message: stub_send_response,
+            receive_message: stub_poll_response,
+            delete_message: true
+          }
+      )
+    end
+    let(:claim) { create(:claim) }
+    let(:aws_queue_name) { 'valid_queue_name' }
+    let(:stub_queue_response) { stub_queue_response_success }
+    let(:stub_send_response) {}
+    let(:stub_poll_response) {}
+    let(:stub_queue_response_success)  { { queue_url: 'http://aws_url' } }
+    let(:stub_queue_response_failure)  do
+      Aws::SQS::Errors::NonExistentQueue.new(
+        double('request'),
+        double('response', :status => 400, :body => '<foo/>')
       )
     end
 
-    let(:aws_queue_name) { 'valid_queue_name' }
-    let(:stub_queue_response) { stub_response_success }
-    let(:stub_send_response) {}
-    let(:stub_response_success)  { { queue_url: 'http://aws_url' } }
-    let(:stub_response_failure)  { Aws::SQS::Errors::NonExistentQueue.new(
-                                     double('request'),
-                                     double('response', :status => 400, :body => '<foo/>')
-                                    )
-                                 }
-    let(:message) { { body: 'Claim added', attributes: { 'uuid': { data_type: 'String', string_value: SecureRandom.uuid } } } }
+    let(:stub_poll_response_success) { Aws::SQS::Types::ReceiveMessageResult.new(
+      messages: [
+        Aws::SQS::Types::Message.new(
+          {
+            message_id: "a1dc5042-e8bf-4417-a443-ed9a2c9558e6",
+            receipt_handle: "AQEB4y8lIzE9G2HiEVen7vRjcmv0xFQML6VcyZYDbnOUzOHFO00yLwaZFE0flYhEv2XMTVyURrK3pRNgaHUH4KK7kFd6cGay35L58UljtEHCJNbBEHSJpuWiV98G56fz04DtTDZN5IKG4tBthDjeDlAheUBkMiiBBLESHSOlUsgGj0vu++x9IlcdjO8+pszXH8356DM3/eayvgoOq9i2yCKkSy4piO6tNX9/VHFVH0fyjIwW3knpbWJHNg2ROKs3RloXKIaBmD4boqc8DSPDx4Mx77zh/T0z3UBG/1CXzmtcXt9NfJJzSqzHus11s4l7bY6qEqIheTaQVl1rl6XF5RBN46M+qIR2i4ggV50TINn+629dP7H2J1yPDPWKJmkV/BzNuCF4fXWlsxiuleoM8v1pbQ==",
+            md5_of_body: "06ca6b264e9878e64c76b3b6858a1676",
+            body: "{\"errors\":[{\"error\":\"PPE is a mandatory field for Claim Element of type Advocate Fee.\"}],\"uuid\":\"3d34c071-c19a-4248-93ea-6f0e91002561\",\"messages\":[]}",
+            attributes: {},
+            md5_of_message_attributes: nil,
+            message_attributes: {}
+          })
+      ]
+    )}
 
     before do
       allow(Aws::SQS::Client).to receive(:new).and_return client
+      allow(Claim::BaseClaim).to receive(:find_by) { claim }
     end
 
     context 'when passed a non-existant queue' do
       let(:aws_queue_name) { 'no_such_queue' }
-      let(:stub_queue_response) { stub_response_failure }
+      let(:stub_queue_response) { stub_queue_response_failure }
 
       it 'raises an appropriate error' do
         expect{aws_client}.to raise_error(StandardError, 'Non existing queue: no_such_queue.')
@@ -41,7 +60,9 @@ module MessageQueue
     end
 
     describe '#send_message!' do
-      subject(:send_message!) { aws_client.send_message! }
+      subject(:send!) { aws_client.send!(message) }
+
+      let(:message) { { body: 'Claim added', attributes: { 'uuid': { data_type: 'String', string_value: SecureRandom.uuid } } } }
 
       context 'when values are good' do
         it { is_expected.to eql true }
@@ -54,9 +75,19 @@ module MessageQueue
       end
 
       context 'when an error occurs (simulate someone deleting the queue mid-submission?!)' do
-        let(:stub_send_response) { stub_response_failure }
+        let(:stub_send_response) { stub_queue_response_failure }
 
-        it { expect{ send_message! }.to raise_error(stub_response_failure) }
+        it { expect{ send! }.to raise_error(stub_queue_response_failure) }
+      end
+    end
+
+    describe '.poll!' do
+      subject(:poll!) { aws_client.poll! }
+
+      context 'when there are messages on the queue' do
+        let(:stub_poll_response) { stub_poll_response_success }
+
+        it { expect{ poll! }.to change { InjectionAttempt.count }.by(1) }
       end
     end
   end

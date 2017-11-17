d2e27d3c3067b3c147d79ac6b6c792d2375887fa
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-11-17 13:59:03 +0000

    try slack attachments
    this may help with cognitive load by
    splitting out success and failure and highlighting errors

--- app/services/injection_response_service.rb
+++ app/services/injection_response_service.rb
@@ -5,8 +5,8 @@ class InjectionResponseService
   end
 
   def run!
-    claim = Claim::BaseClaim.find_by(uuid: @response['uuid'])
-    if claim.nil?
+    @claim = Claim::BaseClaim.find_by(uuid: @response['uuid'])
+    if @claim.nil?
       failure_message = 'Failed to inject because no claim found'
       LogStuff.send(:info, 'InjectionResponseService::NonExistentClaim',
                     action: 'run!',
@@ -14,18 +14,18 @@ class InjectionResponseService
       update_slack(failure_message)
       return false
     end
-    ia = InjectionAttempt.create(claim: claim, succeeded: ccr_injected?, error_message: error_message)
-    update_slack(generate_message(ccr_injected?, claim))
+    ia = InjectionAttempt.create(claim: @claim, succeeded: ccr_injected?, error_message: error_message)
+    update_slack(generate_message)
     ia.save
   end
 
   private
 
-  def generate_message(success, claim)
-    if success
-      "Claim #{claim.case_number} successfully injected"
+  def generate_message
+    if ccr_injected?
+      "Claim #{@claim.case_number} successfully injected"
     else
-      "Claim #{claim.case_number} could not be injected because #{error_message}"
+      "Claim #{@claim.case_number} could not be injected"
     end
   end
 
@@ -33,14 +33,42 @@ class InjectionResponseService
     payload = {
       channel: Settings.slack.channel,
       username: Settings.slack.bot_name,
-      text: "#{message} {#{@response['uuid']}}",
-      icon_emoji: Settings.slack.icon
+      icon_emoji: Settings.slack.icon,
+      attachments: [
+        {
+          'fallback': "#{message} {#{@response['uuid']}}",
+          'color': ccr_injected? ? '#36a64f' : '#c41f1f',
+          'title': 'UUID',
+          'text': @response['uuid'],
+          'fields': fields(message)
+        }
+      ]
     }.to_json
     RestClient.post(Settings.slack.bot_url, payload, content_type: :json)
   end
 
-  def ccr_injected?
+  def fields(message)
+    fields = [
+      { 'title': "Injection #{ccr_injected? ? 'succeeded' : 'failed'}", 'value': message, 'short': true },
+      { 'title': 'environment', 'value': ENV['ENV'], 'short': true }
+    ]
+    errors = has_no_errors? ? [] : error_fields
+    fields + errors
+  end
+
+  def error_fields
+    errors = @response['errors'].map { |x| x['error'] }.join('\n')
+    [
+      { 'title': 'Errors', 'value': errors }
+    ]
+  end
+
+  def has_no_errors?
     @response['errors'].empty?
+  end
+
+  def ccr_injected?
+    has_no_errors? && @claim.present?
   end
 
   def error_message

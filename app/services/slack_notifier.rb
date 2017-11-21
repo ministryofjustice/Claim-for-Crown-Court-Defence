7d01b5e0433ba8594270c97fa227a5fbee95e8ce
colinbruce <colin.bruce@digital.justice.gov.uk>
2017-11-21 22:17:31 +0000

    Extend the slack notifier

--- app/services/slack_notifier.rb
+++ app/services/slack_notifier.rb
@@ -2,29 +2,75 @@ class SlackNotifier
   def initialize
     @slack_url = Settings.slack.bot_url
     @ready_to_send = false
+    @payload = {
+      channel: Settings.slack.channel,
+      username: Settings.slack.bot_name
+    }
   end
 
   def send_message!
     raise 'Unable to send without payload' unless @ready_to_send
 
-    RestClient.post(@slack_url, {})
+    RestClient.post(@slack_url, @payload.to_json, content_type: :json)
   end
 
-  def build_injection
-    # {
-    #   channel: Settings.slack.channel,
-    #   username: Settings.slack.bot_name,
-    #   icon_emoji: ccr_injected? ? Settings.slack.success_icon : Settings.slack.fail_icon,
-    #   attachments: [
-    #     {
-    #       'fallback': "#{generate_message} {#{@response['uuid']}}",
-    #       'color': ccr_injected? ? '#36a64f' : '#c41f1f',
-    #       'title': "Injection #{ccr_injected? ? 'succeeded' : 'failed'}",
-    #       'text': @response['uuid'],
-    #       'fields': fields
-    #     }
-    #   ]
-    # }.to_json
+  def build_injection_payload(response)
+    @response = response.stringify_keys
+    @claim = Claim::BaseClaim.find_by(uuid: @response['uuid'])
+    @payload.merge(
+      icon_emoji: ccr_injected? ? Settings.slack.success_icon : Settings.slack.fail_icon,
+      attachments: [
+        {
+          'fallback': "#{generate_message} {#{@response['uuid']}}",
+          'color': ccr_injected? ? '#36a64f' : '#c41f1f',
+          'title': "Injection #{ccr_injected? ? 'succeeded' : 'failed'}",
+          'text': @response['uuid'],
+          'fields': fields
+        }
+      ]
+    )
     @ready_to_send = true
+  rescue StandardError
+    @ready_to_send = false
+  end
+
+  private
+
+  def ccr_injected?
+    has_no_errors? && @claim.present?
+  end
+
+  def has_no_errors?
+    @response['errors'].empty?
+  end
+
+  def error_message
+    @response['errors'].join(' ')
+  end
+
+  def fields
+    fields = [
+      { 'title': 'Claim number', 'value': @claim&.case_number, 'short': true },
+      { 'title': 'environment', 'value': ENV['ENV'], 'short': true }
+    ]
+    errors = has_no_errors? ? [] : error_fields
+    fields + errors
+  end
+
+  def error_fields
+    errors = @response['errors'].map { |x| x['error'] }.join('\n')
+    [
+      { 'title': 'Errors', 'value': errors }
+    ]
+  end
+
+  def generate_message
+    if @claim.nil?
+      'Failed to inject because no claim found'
+    elsif ccr_injected?
+      "Claim #{@claim.case_number} successfully injected"
+    else
+      "Claim #{@claim.case_number} could not be injected"
+    end
   end
 end
--- spec/services/slack_notifier_spec.rb
+++ spec/services/slack_notifier_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe SlackNotifier, slack_bot: true do
   subject(:slack_notifier) { described_class.new() }
 
+  let(:claim) { create :claim }
+  let(:valid_json_on_success) { { "errors":[], "uuid":claim.uuid, "messages":[{'message':'Claim injected successfully.'}]} }
+  let(:valid_json_on_failure) { { "errors":[ {'error':"No defendant found for Rep Order Number: '123456432'."} ],"uuid":claim.uuid,"messages":[] } }
+
   it { is_expected.to be_a described_class }
 
-  it { is_expected.to respond_to :build_injection }
+  it { is_expected.to respond_to :build_injection_payload }
   it { is_expected.to respond_to :send_message! }
 
   describe '#send_message!' do
@@ -18,7 +22,7 @@ RSpec.describe SlackNotifier, slack_bot: true do
     end
 
     context 'after payload set' do
-      before { slack_notifier.build_injection }
+      before { slack_notifier.build_injection_payload(valid_json_on_success) }
       it 'calls the slack api' do
         subject
         expect(a_request(:post, "https://hooks.slack.com/services/fake/endpoint")).to have_been_made.times(1)

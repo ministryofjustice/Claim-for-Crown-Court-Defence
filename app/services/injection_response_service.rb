ed95ceca9c02328099779321505bfa87df7c3f1e
colinbruce <colin.bruce@digital.justice.gov.uk>
2017-11-22 21:54:48 +0000

    remove duplicated slack parts

--- app/services/injection_response_service.rb
+++ app/services/injection_response_service.rb
@@ -6,70 +6,24 @@ class InjectionResponseService
   end
 
   def run!
+    slack = SlackNotifier.new
+    slack.build_injection_payload(@response)
     if @claim.nil?
       LogStuff.send(:info, 'InjectionResponseService::NonExistentClaim',
                     action: 'run!',
-                    uuid: @response['uuid']) { generate_message }
-      update_slack
+                    uuid: @response['uuid']) { 'Failed to inject because no claim found' }
+      slack.send_message!
       return false
     end
     ia = InjectionAttempt.create(claim: @claim, succeeded: ccr_injected?, error_message: error_message)
-    update_slack
+    slack.send_message!
     ia.save
   end
 
   private
 
-  def generate_message
-    if @claim.nil?
-      'Failed to inject because no claim found'
-    elsif ccr_injected?
-      "Claim #{@claim.case_number} successfully injected"
-    else
-      "Claim #{@claim.case_number} could not be injected"
-    end
-  end
-
-  def update_slack
-    payload = {
-      channel: Settings.slack.channel,
-      username: Settings.slack.bot_name,
-      icon_emoji: ccr_injected? ? Settings.slack.success_icon : Settings.slack.fail_icon,
-      attachments: [
-        {
-          'fallback': "#{generate_message} {#{@response['uuid']}}",
-          'color': ccr_injected? ? '#36a64f' : '#c41f1f',
-          'title': "Injection #{ccr_injected? ? 'succeeded' : 'failed'}",
-          'text': @response['uuid'],
-          'fields': fields
-        }
-      ]
-    }.to_json
-    RestClient.post(Settings.slack.bot_url, payload, content_type: :json)
-  end
-
-  def fields
-    fields = [
-      { 'title': 'Claim number', 'value': @claim&.case_number, 'short': true },
-      { 'title': 'environment', 'value': ENV['ENV'], 'short': true }
-    ]
-    errors = has_no_errors? ? [] : error_fields
-    fields + errors
-  end
-
-  def error_fields
-    errors = @response['errors'].map { |x| x['error'] }.join('\n')
-    [
-      { 'title': 'Errors', 'value': errors }
-    ]
-  end
-
-  def has_no_errors?
-    @response['errors'].empty?
-  end
-
   def ccr_injected?
-    has_no_errors? && @claim.present?
+    @response['errors'].empty? && @claim.present?
   end
 
   def error_message

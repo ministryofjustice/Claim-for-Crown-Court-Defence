6fae3ae1befae42394455ff0276bbd52e193e4cf
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-11-23 15:39:36 +0000

    Indictment message muting

--- app/assets/javascripts/modules/external_users/claims/NewClaim.js
+++ app/assets/javascripts/modules/external_users/claims/NewClaim.js
@@ -45,17 +45,18 @@ moj.Modules.NewClaim = {
 
   initSubmitValidation: function() {
     //
-    // Warn the user if 'A copy of the indictment' is not selected in the supporting evidence checklist.
+    // Warn the user if 'A copy of the indictment' is not selected in the
+    // supporting evidence checklist.
     // Tests in /spec/javascripts/supporting-evidence_spec.js
     //
-    $('input[name="commit_submit_claim"]').on('click', function() {
-      if ($('#claim_evidence_checklist_ids_4').exists() && !$('#claim_evidence_checklist_ids_4').prop('checked')) {
-        return confirm(
-          "The evidence checklist suggests that no indictment has been attached.\n" +
-          "This can lead to your claim being rejected.\n\n" +
-          "Do you wish to proceed without attaching an indictment?"
-        );
-      }
+    $('input[name="commit_submit_claim"]').on('click', function(e) {
+        if ($('#claim_evidence_checklist_ids_4').exists() && !$('#claim_evidence_checklist_ids_4').prop('checked') && !$('[data-mute-indictment]').data('mute-indictment')) {
+          return confirm(
+            "The evidence checklist suggests that no indictment has been attached.\n" +
+            "This can lead to your claim being rejected.\n\n" +
+            "Do you wish to proceed without attaching an indictment?"
+          );
+        }
     });
   },
 
@@ -168,7 +169,7 @@ moj.Modules.NewClaim = {
 
     self.$ariaLiveRegion.children().hide().end();
   },
-  toggleMileageRateFields: function () {
+  toggleMileageRateFields: function() {
     var self = this;
     // toggle between bike / car mileage
     if (self.dataAttribute.mileageType === 'bike') {

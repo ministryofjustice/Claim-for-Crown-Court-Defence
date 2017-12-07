5c2ca2a97fed4b0065b7fe12f955dc655270c58d
jsugarman <joel.sugarman@digital.justice.gov.uk>
2017-12-07 16:59:33 +0000

    Expose retrial details

--- app/interfaces/api/entities/ccr_claim.rb
+++ app/interfaces/api/entities/ccr_claim.rb
@@ -21,6 +21,23 @@ module API
       expose :offence, using: API::Entities::CCR::Offence
       expose :defendants_with_main_first, using: API::Entities::CCR::Defendant, as: :defendants
 
+      # TODO: need a new field for retrial reductions
+      expose :dummy_retrial_reduction, as: :retrial_reduction
+      expose :retrial_started_at, :retrial_concluded_at, format_with: :utc
+      with_options(format_with: :string) do
+        expose :retrial_actual_length_or_one, as: :retrial_actual_length
+        expose :retrial_estimated_length_or_one, as: :retrial_estimated_length
+      end
+
+      # TODO: Retrial, original trial details
+      # NOTE: CCR requires original trial case number but defaults this
+      # to the retrials case number as they are usually the same. Similarly
+      # with court. Ideally CCR should infer this information until such time
+      # as new fields are added to CCCD.
+      #
+      # expose :case_number, as: :retrial_original_case_number
+      # expose :court, using: API::Entities::CCR::Court, as: :retrial_original_court
+
       expose :estimated_trial_length_or_one, as: :estimated_trial_length, format_with: :string
       expose :actual_trial_length_or_one, as: :actual_trial_Length, format_with: :string
       expose :adapted_advocate_category, as: :advocate_category
@@ -35,12 +52,28 @@ module API
         object.defendants.order(created_at: :asc)
       end
 
+      def length_or_one(length)
+        [length, 1].compact.max
+      end
+
       def estimated_trial_length_or_one
-        [object.estimated_trial_length, 1].compact.max
+        length_or_one(object.estimated_trial_length)
       end
 
       def actual_trial_length_or_one
-        [object.actual_trial_length, 1].compact.max
+        length_or_one(object.actual_trial_length)
+      end
+
+      def dummy_retrial_reduction
+        'true'
+      end
+
+      def retrial_actual_length_or_one
+        length_or_one(object.retrial_actual_length)
+      end
+
+      def retrial_estimated_length_or_one
+        length_or_one(object.retrial_actual_length)
       end
 
       def bills

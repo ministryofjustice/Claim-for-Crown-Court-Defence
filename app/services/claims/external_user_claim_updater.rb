97ff36eb643eaa9bc7520cf8ceea7a0bf0adcde5
ColinBruce <colin.bruce@digital.justice.gov.uk>
2017-12-15 11:56:58 +0000

    remove raise error from Claims::ExternalUserClaimUpdater

--- app/services/claims/external_user_claim_updater.rb
+++ app/services/claims/external_user_claim_updater.rb
@@ -17,8 +17,6 @@ module Claims
 
     def clone_rejected
       claim.clone_rejected_to_new_draft(audit_attributes)
-    rescue StandardError => error
-      raise "Claims::ExternalUserClaimUpdater.clone_rejected with #{error.message}"
     end
 
     def request_redetermination

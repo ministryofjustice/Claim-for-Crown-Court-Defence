20ac73a9d2cd78b92bfa79f83689ce2ec03cfc39
David Plews (ClearGif) <freelance@cleargif.co.uk>
2017-12-27 13:22:35 +0000

    Reject reasons: JS + FE
    PT:
    https://www.pivotaltracker.com/story/show/153451416
    
    - Update JS modules
    - Reimplemented parts of the form
    - Simple JS testing

--- app/assets/javascripts/modules/Modules.AmountAssessed.js
+++ app/assets/javascripts/modules/Modules.AmountAssessed.js
@@ -1,30 +1,97 @@
 moj.Modules.AmountAssessed = {
-    $assessmentSection: $('#claim-status.js-cw-claim-assessment'),
-    $actionSection: $('div.js-cw-claim-action'),
-    $rejectionReasons: $('div.js-cw-claim-rejection-reasons'),
+  blocks: [],
+  init: function() {
+    this.blocks.push(new moj.Modules.AmountAssessedBlock());
+  }
+};
 
-    init: function () {
-        this.$rejectionReasons.hide();
-        this.$actionSection.find('input:radio').on('change', function () {
-            moj.Modules.AmountAssessed.state();
-        });
+
+moj.Modules.AmountAssessedBlock = function(selector) {
+  var self = this;
+
+  this.config = {
+    hook: selector || '.fx-assesment-hook',
+    form: '.js-cw-claim-assessment',
+    actions: '.js-cw-claim-action',
+    reasons: '.js-cw-claim-rejection-reasons',
+    otherinput: '.js-reject-reason-text',
+    otherCheckbox: '#_state_reason_other',
+    action: 'toggle'
+  };
+
+  this.states = {
+    rejected: {
+      form: false,
+      reasons: true
     },
-    state: function () {
-        var action = this.$actionSection.find('input:radio:checked').val();
-
-        if (action === 'part_authorised' || action === 'authorised') {
-            this.$assessmentSection.slideDown('slow');
-        } else {
-            this.$assessmentSection.slideUp('slow');
-        }
-
-        if (action === 'rejected') {
-            this.$rejectionReasons.show();
-        } else {
-            this.$rejectionReasons.hide();
-            this.$rejectionReasons
-                .find('input:radio:checked').prop('checked', false)
-                .closest('label').removeClass('selected');
-        }
+    refused: {
+      form: false,
+      reasons: false
+    },
+    authorised: {
+      form: true,
+      reasons: false
+    },
+    part_authorised: {
+      form: true,
+      reasons: false
     }
+  };
+
+  this.el = this.config.hook;
+
+  this.init = function() {
+    this.$el = $(this.el);
+    this.$form = $(this.config.form);
+    this.$actions = $(this.config.actions);
+    this.$reasons = $(this.config.reasons);
+    this.$otherinput = $(this.config.otherinput);
+    this.$otherCheckbox = $(this.config.otherCheckbox);
+    this.bindEvents();
+  };
+
+  this.slider = function(state, el) {
+    // open and close slider
+    // true: open
+    // false: close
+    return state ? $(el).slideDown() : $(el).slideUp();
+  };
+
+  this.bindEvents = function() {
+    var self = this;
+
+    this.$actions.on('change', function(e) {
+      var state = $(e.target).val();
+      $.publish('claim.status.change', {
+        state: state
+      })
+    });
+
+    this.$reasons.on('change', function(e) {
+      var reason = self.$otherCheckbox.is(':checked');
+      $.publish('claim.reasons.change', {
+        reason: reason
+      })
+    });
+
+    $.subscribe('claim.reasons.change', function(e, data) {
+      data.reason ? self.slider(true, self.$otherinput) : self.slider(false, self.$otherinput)
+    });
+
+    $.subscribe('claim.status.change', function(e, data) {
+      var state = self.states[data.state]
+
+      self.$form.is(function(idx, el) {
+        self.slider(state.form, el)
+      });
+
+      self.$reasons.is(function(idx, el) {
+        self.slider(state.reasons, el)
+      });
+    });
+
+  };
+
+  this.init();
+  return this;
 };

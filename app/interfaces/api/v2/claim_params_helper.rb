891e16721b6f0b168366e10c65c8dce255c61644
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-01-09 09:59:45 +0000

    Refactor params helpers and full claim
    Isolate API class resolution and apply helpers
    consistently for API V2 claim objects.

--- app/interfaces/api/v2/claim_params_helper.rb
+++ app/interfaces/api/v2/claim_params_helper.rb
@@ -8,12 +8,17 @@ module API
         requires :uuid, type: String, desc: 'REQUIRED: Claim UUID'
       end
 
-      # for a given API class its entity class should be
+      # For a given API claim class its entity class should be
       # in API::Entities::<class_name_of_api_object>
       # e.g. API::V2::Fred --> API::Entities::Fred
       def entity_class
-        class_name = options[:for].to_s.demodulize
-        "API::Entities::#{class_name}".constantize
+        "API::Entities::#{api_class}".constantize
+      end
+
+      private
+
+      def api_class
+        options[:for].to_s.demodulize
       end
     end
   end

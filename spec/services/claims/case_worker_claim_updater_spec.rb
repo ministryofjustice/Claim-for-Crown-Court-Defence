1c6871ca8d7a4f994c17d428a4c643b9c05e1624
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-01-12 11:10:39 +0000

    Reduce the duplication in CaseWorkerClaimUpdater tests

--- spec/services/claims/case_worker_claim_updater_spec.rb
+++ spec/services/claims/case_worker_claim_updater_spec.rb
@@ -1,8 +1,60 @@
 require 'rails_helper'
 
-
 module Claims
   describe CaseWorkerClaimUpdater do
+
+    RSpec.shared_examples 'a successful non-authorised claim with a single reason' do |state|
+      it_behaves_like 'base_test', state, ['no_indictment']
+    end
+
+    RSpec.shared_examples 'a successful non-authorised claim with other as reason' do |state|
+      it_behaves_like 'base_test', state, ['other'], 'a reason'
+    end
+
+    RSpec.shared_examples 'base_test' do |state, state_reason, reason_text=nil|
+      subject(:updater) { CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update! }
+      let(:claim) { create :allocated_claim }
+      let(:current_user) { double(User, id: 12345) }
+      let(:params) { {'state' => state, 'state_reason' => state_reason, 'reason_text' => reason_text, 'assessment_attributes' => {'fees' => '', 'expenses' => '0'}} }
+
+      it 'updates the state to `other` and returns OK' do
+        expect(updater.result).to eq :ok
+        expect(updater.claim.state).to eq state
+        expect(updater.claim.last_state_transition.reason_code).to eq state_reason
+        expect(updater.claim.last_state_transition.reason_text).to eq reason_text
+        expect(updater.claim.last_state_transition.author_id).to eq(current_user.id)
+      end
+    end
+
+    RSpec.shared_examples 'a successful assessment' do |state, fees='128.33', expenses='42.40'|
+      subject(:updater) { CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update! }
+      let(:claim) { create :allocated_claim }
+      let(:current_user) { double(User, id: 12345) }
+      let(:params) { {'state' => state, 'assessment_attributes' => {'fees' => fees, 'expenses' => expenses}} }
+
+      it 'sets the result to error' do
+        expect(updater.result).to eq :ok
+        expect(updater.claim.state).to eq state
+        expect(updater.claim.assessment.fees).to eq fees
+        expect(updater.claim.assessment.expenses).to eq expenses
+        expect(updater.claim.assessment.disbursements).to eq 0.0
+        expect(updater.claim.last_state_transition.author_id).to eq(current_user.id)
+      end
+    end
+
+    RSpec.shared_examples 'a failing assessment' do |state, expected_error, update_type='redeterminations', state_reason=nil, fees='128.33', expenses='42.40'|
+      subject(:updater) { CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update! }
+      let(:claim) { create :allocated_claim }
+      let(:current_user) { double(User, id: 12345) }
+      let(:params) { {'state' => state, 'state_reason' => state_reason, "#{update_type}_attributes" => {'0' => {'fees' => fees, 'expenses' => expenses}}} }
+
+      it 'sets the result to error' do
+        expect(updater.result).to eq :error
+        expect(updater.claim.errors[:determinations]).to eq(expected_error)
+        expect(updater.claim.state).to eq 'allocated'
+        expect(updater.claim.redeterminations).to be_empty
+      end
+    end
 
     context 'assessments' do
 
@@ -11,92 +63,25 @@ module Claims
       let(:current_user) { double(User, id: 12345) }
 
       context 'successful transitions' do
-        it 'advances the claim to part authorised' do
-          params = {'state' => 'part_authorised', 'assessment_attributes' => {'fees' => '45', 'expenses' => '0.00'}}
-          updater = CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update!
-          expect(updater.result).to eq :ok
-          expect(updater.claim.state).to eq 'part_authorised'
-          expect(updater.claim.assessment.fees).to eq 45.0
-          expect(updater.claim.assessment.expenses).to eq 0.0
-          expect(updater.claim.assessment.disbursements).to eq 0.0
-          expect(updater.claim.last_state_transition.author_id).to eq(current_user.id)
+        context 'advances the claim to part authorised' do
+          it_behaves_like 'a successful assessment', 'part_authorised', 45.0, 0
         end
 
-        it 'advances the claim to authorised' do
-          params = {'state' => 'authorised', 'assessment_attributes' => {'fees' => '128.33', 'expenses' => '42.88'}}
-          updater = CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update!
-          expect(updater.result).to eq :ok
-          expect(updater.claim.state).to eq 'authorised'
-          expect(updater.claim.assessment.fees.to_f).to eq 128.33
-          expect(updater.claim.assessment.expenses).to eq 42.88
-          expect(updater.claim.assessment.disbursements).to eq 0.0
-          expect(updater.claim.last_state_transition.author_id).to eq(current_user.id)
+        context 'advances the claim to authorised' do
+          it_behaves_like 'a successful assessment', 'authorised', 128.33, 42.88
         end
 
         context 'rejections' do
           let(:params) { {'state' => 'rejected', 'state_reason' => ['no_indictment'], 'assessment_attributes' => {'fees' => '', 'expenses' => '0'}} }
-
-          it 'advances the claim to rejected with reason supplied' do
-            updater = CaseWorkerClaimUpdater.new(claim.id, params).update!
-            expect(updater.result).to eq :ok
-            expect(updater.claim.state).to eq 'rejected'
-            expect(updater.claim.last_state_transition.reason_code).to eq ['no_indictment']
-          end
-
-          it 'saves audit attributes' do
-            updater = CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update!
-            expect(updater.claim.last_state_transition.author_id).to eq(current_user.id)
-          end
-
-          context 'other rejection with reason' do
-            let(:params) { {'state' => 'rejected', 'state_reason' => ['other'], 'reason_text' => 'a reason', 'assessment_attributes' => {'fees' => '', 'expenses' => '0'}} }
-
-            it 'advances the claim to rejected with reason supplied' do
-              updater = CaseWorkerClaimUpdater.new(claim.id, params).update!
-              expect(updater.result).to eq :ok
-              expect(updater.claim.state).to eq 'rejected'
-              expect(updater.claim.last_state_transition.reason_code).to eq ['other']
-              expect(updater.claim.last_state_transition.reason_text).to eq 'a reason'
-            end
-
-            it 'saves audit attributes' do
-              updater = CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update!
-              expect(updater.claim.last_state_transition.author_id).to eq(current_user.id)
-            end
-          end
+          it_behaves_like 'a successful non-authorised claim with a single reason', 'rejected'
+          it_behaves_like 'a successful non-authorised claim with other as reason', 'rejected'
         end
 
         context 'refusals' do
           let(:params) { {'state' => 'refused', 'state_reason' => ['no_indictment'], 'assessment_attributes' => {'fees' => '', 'expenses' => '0'}} }
 
-          it 'advances the claim to rejected with reason supplied' do
-            updater = CaseWorkerClaimUpdater.new(claim.id, params).update!
-            expect(updater.result).to eq :ok
-            expect(updater.claim.state).to eq 'refused'
-            expect(updater.claim.last_state_transition.reason_code).to eq ['no_indictment']
-          end
-
-          it 'saves audit attributes' do
-            updater = CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update!
-            expect(updater.claim.last_state_transition.author_id).to eq(current_user.id)
-          end
-
-          context 'other rejection with reason' do
-            let(:params) { {'state' => 'refused', 'state_reason' => ['other'], 'reason_text' => 'a reason', 'assessment_attributes' => {'fees' => '', 'expenses' => '0'}} }
-
-            it 'advances the claim to rejected with reason supplied' do
-              updater = CaseWorkerClaimUpdater.new(claim.id, params).update!
-              expect(updater.result).to eq :ok
-              expect(updater.claim.state).to eq 'refused'
-              expect(updater.claim.last_state_transition.reason_code).to eq ['other']
-              expect(updater.claim.last_state_transition.reason_text).to eq 'a reason'
-            end
-
-            it 'saves audit attributes' do
-              updater = CaseWorkerClaimUpdater.new(claim.id, params.merge(current_user: current_user)).update!
-              expect(updater.claim.last_state_transition.author_id).to eq(current_user.id)
-            end
-          end
+          it_behaves_like 'a successful non-authorised claim with a single reason', 'refused'
+          it_behaves_like 'a successful non-authorised claim with other as reason', 'refused'
         end
 
         it 'advances the claim to refused when no values are supplied' do
@@ -254,40 +239,20 @@ module Claims
           expect(updater.claim.errors[:determinations]).to eq(['You must specify authorised or part authorised if you supply values'])
         end
 
-        it 'errors if values are supplied with refused' do
-          params = {'state' => 'refused', 'redeterminations_attributes' => {'0' => {'fees' => '128.33', 'expenses' => '42.40'}}}
-          updater = CaseWorkerClaimUpdater.new(claim.id, params).update!
-          expect(updater.result).to eq :error
-          expect(updater.claim.errors[:determinations]).to eq(['You cannot specify values when refusing a claim'])
-          expect(updater.claim.state).to eq 'allocated'
-          expect(updater.claim.redeterminations).to be_empty
+        context 'if values are supplied with refused' do
+          it_behaves_like 'a failing assessment', 'refused', ['You cannot specify values when refusing a claim']
         end
 
-        it 'errors if values are supplied with rejected' do
-          params = {'state' => 'rejected', 'redeterminations_attributes' => {'0' => {'fees' => '128.33', 'expenses' => '42.40'}}}
-          updater = CaseWorkerClaimUpdater.new(claim.id, params).update!
-          expect(updater.result).to eq :error
-          expect(updater.claim.errors[:determinations]).to eq(['You cannot specify values when rejecting a claim'])
-          expect(updater.claim.state).to eq 'allocated'
-          expect(updater.claim.redeterminations).to be_empty
+        context 'if values are supplied with rejected' do
+          it_behaves_like 'a failing assessment', 'rejected', ['You cannot specify values when rejecting a claim']
         end
 
-        it 'if no state_reason are supplied' do
-          params = {'state' => 'rejected', 'state_reason' => [''], 'redeterminations_attributes' => {'0' => {'fees' => '', 'expenses' => '0'}}}
-          updater = CaseWorkerClaimUpdater.new(claim.id, params).update!
-          expect(updater.result).to eq :error
-          expect(updater.claim.errors[:determinations]).to eq(['requires a reason when rejecting'])
-          expect(updater.claim.state).to eq 'allocated'
-          expect(updater.claim.redeterminations).to be_empty
+        context 'if no state_reason are supplied' do
+          it_behaves_like 'a failing assessment', 'rejected', ['requires a reason when rejecting'], 'redeterminations', [''], '', 0
         end
 
-        it 'if state_reason is other, but no text is supplied' do
-          params = {'state' => 'rejected', 'state_reason' => ['other'], 'assessment_attributes' => {'fees' => '', 'expenses' => '0'}}
-          updater = CaseWorkerClaimUpdater.new(claim.id, params).update!
-          expect(updater.result).to eq :error
-          expect(updater.claim.errors[:determinations]).to eq(['requires details when rejecting with other'])
-          expect(updater.claim.state).to eq 'allocated'
-          expect(updater.claim.redeterminations).to be_empty
+        context 'if state_reason is other, but no text is supplied' do
+          it_behaves_like 'a failing assessment', 'rejected', ['requires details when rejecting with other'], 'assessment', ['other'], 0, 0
         end
       end
     end

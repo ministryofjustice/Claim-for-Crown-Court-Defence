4aa9ef58a267aad66ea14cc1cf96955e656d0d32
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2018-01-16 16:57:44 +0000

    Fix for refusing a claim with other, but no text (#2205)
    * Fix for refusing a claim with other, but no text
    There was an omission in the code that meant the wrong
    values where being checked.

--- app/services/claims/case_worker_claim_updater.rb
+++ app/services/claims/case_worker_claim_updater.rb
@@ -73,7 +73,7 @@ module Claims
     end
 
     def transition_reason_text_missing?
-      @transition_reason&.include?('other') && @transition_reason_text.blank?
+      @transition_reason&.any? { |reason| %w[other other_refuse].include?(reason) } && @transition_reason_text.blank?
     end
 
     def nil_or_empty_zero_or_negative?(determination_params)

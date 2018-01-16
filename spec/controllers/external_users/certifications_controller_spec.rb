e9e8d5ac34ad5c70505fe0a2b05e2bfb4d31ab4f
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2018-01-16 09:57:40 +0000

    Litigator message queuing (#2203)
    * Add an SNS implementation foo Litigator messaging
    * Implement switchable notifications
    AGFS -> SQS
    LGFS -> SNS

--- spec/controllers/external_users/certifications_controller_spec.rb
+++ spec/controllers/external_users/certifications_controller_spec.rb
@@ -52,63 +52,87 @@ RSpec.describe ExternalUsers::CertificationsController, type: :controller, focus
   end
 
   describe 'POST create' do
-    let(:claim) { create(:claim) }
-    let(:aws_client) do
-      Aws::SQS::Client.new(
-        region: 'eu_west_1',
-        stub_responses:
-          {
-            list_queues: { queue_urls:['valid_queue_name'] },
-            get_queue_url: { queue_url: 'http://aws_url' },
-            send_message: stub_send_response
-          }
-      )
+    context 'AGFS' do
+      let(:claim) { create(:claim) }
+      let(:sqs_client) do
+        Aws::SQS::Client.new(
+          region: 'eu_west_1',
+          stub_responses:
+            {
+              list_queues: { queue_urls:['valid_queue_name'] },
+              get_queue_url: { queue_url: 'http://aws_url' },
+              send_message: stub_send_response
+            }
+        )
+      end
+      let(:stub_send_response) { stub_response_success }
+      let(:stub_response_success) { }
+      let(:stub_response_failure)  { Aws::SQS::Errors::NonExistentQueue.new(
+                                       double('request'),
+                                       double('response', :status => 400, :body => '<foo/>')
+                                     )
+      }
+
+      context 'valid certification params for submission' do
+        let(:frozen_time) { Time.new(2015, 8, 20, 13, 54, 22) }
+        before { allow(Aws::SQS::Client).to receive(:new).and_return sqs_client }
+
+        it 'should be a redirect to confirmation' do
+          post :create, valid_certification_params(claim, certification_type)
+          expect(response).to redirect_to(confirmation_external_users_claim_path(claim))
+        end
+
+        it 'should change the state to submitted' do
+          post :create, valid_certification_params(claim, certification_type)
+          expect(claim.reload).to be_submitted
+        end
+
+        it 'should set the submitted at date' do
+          Timecop.freeze(frozen_time) do
+            post :create, valid_certification_params(claim, certification_type)
+          end
+
+          expect(claim.reload.last_submitted_at).to eq(frozen_time)
+        end
+
+        it 'logs a successful message on the queue' do
+          allow(Settings.aws).to receive(:submitted_queue).and_return('valid_queue_name')
+          expect(Rails.logger).to receive(:info).with(/Successfully sent message about submission of claim#/)
+          post :create, valid_certification_params(claim, certification_type)
+        end
+
+        context 'when SQS fails' do
+          let(:stub_send_response) { stub_response_failure }
+
+          it 'logs an error message' do
+            expect(Rails.logger).to receive(:warn).with(/Error: .* while sending message about submission of claim#/)
+            post :create, valid_certification_params(claim, certification_type)
+          end
+        end
+      end
     end
-    let(:stub_send_response) { stub_response_success }
-    let(:stub_response_success) { }
-    let(:stub_response_failure)  { Aws::SQS::Errors::NonExistentQueue.new(
-                                     double('request'),
-                                     double('response', :status => 400, :body => '<foo/>')
-                                   )
-    }
+    context 'LGFS' do
+      let(:claim) { create(:litigator_claim) }
 
-    context 'valid certification params for submission' do
-      let(:frozen_time) { Time.new(2015, 8, 20, 13, 54, 22) }
-      before { allow(Aws::SQS::Client).to receive(:new).and_return aws_client }
+      let(:sns_client) do
+        Aws::SNS::Client.new(
+          region: 'eu_west_1',
+          stub_responses:
+            {
+              publish: {}
+            }
+        )
+      end
 
-      it 'should be a redirect to confirmation' do
+      before do
+        allow(Aws::SNS::Client).to receive(:new).and_return sns_client
+        allow(sns_client).to receive(:publish)
+      end
+
+      it 'calls the SNS notification path' do
         post :create, valid_certification_params(claim, certification_type)
-        expect(response).to redirect_to(confirmation_external_users_claim_path(claim))
+        expect(sns_client).to have_received(:publish).once
       end
-
-      it 'should change the state to submitted' do
-        post :create, valid_certification_params(claim, certification_type)
-        expect(claim.reload).to be_submitted
-      end
-
-      it 'should set the submitted at date' do
-        Timecop.freeze(frozen_time) do
-          post :create, valid_certification_params(claim, certification_type)
-        end
-
-        expect(claim.reload.last_submitted_at).to eq(frozen_time)
-      end
-
-      it 'logs a successful message on the queue' do
-        allow(Settings.aws).to receive(:submitted_queue).and_return('valid_queue_name')
-        expect(Rails.logger).to receive(:info).with(/Successfully sent message about submission of claim#/)
-        post :create, valid_certification_params(claim, certification_type)
-      end
-
-      context 'when SQS fails' do
-        let(:stub_send_response) { stub_response_failure }
-
-        it 'logs an error message' do
-          expect(Rails.logger).to receive(:warn).with(/Error: .* while sending message about submission of claim#/)
-          post :create, valid_certification_params(claim, certification_type)
-        end
-      end
-
     end
 
     context 'invalid certification' do

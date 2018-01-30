5be348212c19ee970e1c60409f582a9ed3e0deb6
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-01-30 09:44:22 +0000

    Add a yesno helper for boolean objects
    This will allow us to display human readable values for
    true/false columns in the MI report, but it could be used
    elsewhere in the future

--- lib/extensions/boolean_extension.rb
+++ lib/extensions/boolean_extension.rb
@@ -3,11 +3,19 @@ module BooleanExtension
     def to_i
       1
     end
+
+    def to_yesno
+      'Yes'
+    end
   end
 
   module False
     def to_i
       0
+    end
+
+    def to_yesno
+      'No'
     end
   end
 end

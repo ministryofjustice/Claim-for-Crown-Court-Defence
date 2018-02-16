fb0800981fd284b7432d45be5a38532e69003865
David Plews (ClearGif) <freelance@cleargif.co.uk>
2018-02-16 13:41:15 +0000

    Re-Allocation page bugs
    PT:
    https://www.pivotaltracker.com/story/show/155178509
    
    What this PR does:
    - fixing the new style radio buttons
    - fixing the select all link on the table

--- app/assets/javascripts/modules/Modules.SelectAll.js
+++ app/assets/javascripts/modules/Modules.SelectAll.js
@@ -2,13 +2,17 @@ moj.Modules.SelectAll = {
   init: function() {
     $('.select-all').click(function() {
       var $element = $(this);
-      var index = $element.parent().index();
       var checkedState = $element.data('all-checked');
+
       $element.data('all-checked', !checkedState);
 
-      $('tr').each(function(i, val) {
-        $(val).children().eq(index).children('input[type=checkbox]').prop('checked', !checkedState);
+      $('tr').each(function(idx, el) {
+        var $el = $(el);
+        if($el.find('input').length){
+          $el.find('input').prop('checked', !checkedState);
+        }
       });
+
 
       $element.text(checkedState ? 'Select all' : 'Select none');
 

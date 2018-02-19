dc14a98f4de301d0cb005cf2627310dfe0b014ab
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-02-19 14:44:56 +0000

    Ensure validator steps end range has a default
    For the cases where the steps end range is not defined to avoid the
    pages raising a 500 page with a BadRange error.

--- app/controllers/external_users/advocates/claims_controller.rb
+++ app/controllers/external_users/advocates/claims_controller.rb
@@ -1,9 +1,16 @@
 class ExternalUsers::Advocates::ClaimsController < ExternalUsers::ClaimsController
   skip_load_and_authorize_resource
+  before_action :set_form_step, only: %i[new create edit update]
 
   resource_klass Claim::AdvocateClaim
 
   private
+
+  def set_form_step
+    # TODO: refactor so it manages step based on it being valid
+    # redirecting to the summary page or the claims page if isn't
+    params[:claim][:form_step] = :fixed_fees if params[:claim] && params[:claim][:form_step] == 'fees'
+  end
 
   def build_nested_resources
     @claim.fixed_fees.build if @claim.fixed_fees.none?

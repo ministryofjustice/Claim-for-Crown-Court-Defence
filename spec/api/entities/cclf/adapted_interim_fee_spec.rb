21315b61ccacecea71f1269071a2671e0d7dd531
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-02-23 08:21:27 +0000

    Refactor adapted interim fee entity spec

--- spec/api/entities/cclf/adapted_interim_fee_spec.rb
+++ spec/api/entities/cclf/adapted_interim_fee_spec.rb
@@ -3,10 +3,21 @@ require 'rails_helper'
 RSpec.describe API::Entities::CCLF::AdaptedInterimFee, type: :adapter do
   subject(:response) { JSON.parse(described_class.represent(interim_fee).to_json, symbolize_names: true) }
 
+  let(:adapter) { instance_double(::CCLF::Fee::InterimFeeAdapter) }
   let(:claim) { instance_double(::Claim::InterimClaim) }
   let(:fee_type) { instance_double(::Fee::InterimFeeType, unique_code: 'INTDT') }
-  let(:interim_fee) { instance_double(::Fee::InterimFee, claim: claim, fee_type: fee_type, quantity: 0.0, amount: 0.0, warrant_issued_date: nil, warrant_executed_date: nil, is_interim_warrant?: false) }
-  let(:adapter) { instance_double(::CCLF::Fee::InterimFeeAdapter) }
+  let(:interim_fee) do
+      instance_double(
+        ::Fee::InterimFee,
+        claim: claim,
+        fee_type: fee_type,
+        quantity: 0.0,
+        amount: 0.0,
+        warrant_issued_date: nil,
+        warrant_executed_date: nil,
+        is_interim_warrant?: false
+      )
+    end
 
   it 'formats amount as string' do
     expect(response).to include(amount: '0.0')
@@ -20,26 +31,23 @@ RSpec.describe API::Entities::CCLF::AdaptedInterimFee, type: :adapter do
     expect(::CCLF::Fee::InterimFeeAdapter).to receive(:new).with(interim_fee).and_return(adapter)
     expect(adapter).to receive(:bill_type)
     expect(adapter).to receive(:bill_subtype)
-    subject
+    response
   end
 
   context 'interim warrants' do
     let(:fee_type) { instance_double(::Fee::InterimFeeType, unique_code: 'INWAR') }
-    let(:interim_fee) do
-      instance_double(
-        ::Fee::InterimFee,
-        claim: claim,
-        fee_type: fee_type,
-        quantity: 0.0,
-        amount: 101.01,
-        warrant_issued_date: '01-Jun-2017'.to_date,
-        warrant_executed_date: '01-Aug-2017'.to_date,
-        is_interim_warrant?: true
-      )
+
+    before do
+      allow(interim_fee).to receive_messages(
+          amount: 101.01,
+          warrant_issued_date: '01-Jun-2017'.to_date,
+          warrant_executed_date: '01-Aug-2017'.to_date,
+          is_interim_warrant?: true
+        )
     end
 
     it 'exposes expected json key-value pairs' do
-      expect(response).to include(
+      expect(response).to match_array(
         bill_type: 'FEE_ADVANCE',
         bill_subtype: 'WARRANT',
         amount: '101.01',
@@ -52,10 +60,13 @@ RSpec.describe API::Entities::CCLF::AdaptedInterimFee, type: :adapter do
 
   context 'effective pcmh' do
     let(:fee_type) { instance_double(::Fee::InterimFeeType, unique_code: 'INPCM') }
-    let(:interim_fee) { instance_double(::Fee::InterimFee, claim: claim, fee_type: fee_type, quantity: 999.0, amount: 202.02, warrant_issued_date: nil, warrant_executed_date: nil, is_interim_warrant?: false) }
+
+    before do
+      allow(interim_fee).to receive_messages(quantity: 999.0, amount: 202.02)
+    end
 
     it 'exposes expected json key-value pairs' do
-      expect(response).to include(
+      expect(response).to match_array(
         bill_type: 'LIT_FEE',
         bill_subtype: 'LIT_FEE',
         quantity: '999',

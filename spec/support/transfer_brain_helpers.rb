124b33ed8c697009ac54f7f773f2830ac58171a8
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-02-26 22:42:29 +0000

    Add Transfer brain mapping specs for all transfer bill scenarios

--- spec/support/transfer_brain_helpers.rb
+++ spec/support/transfer_brain_helpers.rb
@@ -19,11 +19,11 @@ module TransferBrainHelpers
       {
         true=>
           {
-            10=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - up to and including PCMH transfer (new)", :allocation_type=>"Fixed", :bill_scenario=>'ST4TS0T2' }},
-            20=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - before trial transfer (new)", :allocation_type=>"Fixed", :bill_scenario=>'TODO'}},
+            10=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - up to and including PCMH transfer (new)", :allocation_type=>"Fixed", :bill_scenario=>'ST4TS0T3' }},
+            20=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - before trial transfer (new)", :allocation_type=>"Fixed", :bill_scenario=>'ST4TS0T5'}},
             30=>{"*"=>{ :validity=>false, :transfer_fee_full_name=>nil, :allocation_type=>nil, :bill_scenario=>nil}},
             40=>{"*"=>{ :validity=>false, :transfer_fee_full_name=>nil, :allocation_type=>nil, :bill_scenario=>nil}},
-            50=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - transfer before retrial (new)", :allocation_type=>"Fixed", :bill_scenario=>'TODO'}},
+            50=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - transfer before retrial (new)", :allocation_type=>"Fixed", :bill_scenario=>'ST4TS0T7'}},
             60=>{"*"=>{ :validity=>false, :transfer_fee_full_name=>nil, :allocation_type=>nil, :bill_scenario=>nil}},
             70=>{"*"=>{ :validity=>false, :transfer_fee_full_name=>nil, :allocation_type=>nil, :bill_scenario=>nil}}
           },
@@ -37,30 +37,30 @@ module TransferBrainHelpers
               },
             20=>
               {
-                30=>{ :validity=>true, :transfer_fee_full_name=>"before trial transfer (new) - cracked", :allocation_type=>"Grad", :bill_scenario=>'TODO'},
-                10=>{ :validity=>true, :transfer_fee_full_name=>"before trial transfer (new) - trial", :allocation_type=>"Grad", :bill_scenario=>'TODO'}
+                30=>{ :validity=>true, :transfer_fee_full_name=>"before trial transfer (new) - cracked", :allocation_type=>"Grad", :bill_scenario=>'ST3TS2T3'},
+                10=>{ :validity=>true, :transfer_fee_full_name=>"before trial transfer (new) - trial", :allocation_type=>"Grad", :bill_scenario=>'ST3TS2T4'}
               },
             30=>
               {
-                20=>{ :validity=>true, :transfer_fee_full_name=>"during trial transfer (new) - retrial", :allocation_type=>"Grad", :bill_scenario=>'TODO'},
-                10=>{ :validity=>true, :transfer_fee_full_name=>"during trial transfer (new) - trial", :allocation_type=>"Grad", :bill_scenario=>'TODO'}
+                20=>{ :validity=>true, :transfer_fee_full_name=>"during trial transfer (new) - retrial", :allocation_type=>"Grad", :bill_scenario=>'ST3TS3TA'},
+                10=>{ :validity=>true, :transfer_fee_full_name=>"during trial transfer (new) - trial", :allocation_type=>"Grad", :bill_scenario=>'ST3TS3T4'}
               },
             40=>
               {
-                "*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer after trial and before sentence hearing (new)", :allocation_type=>"Grad", :bill_scenario=>'TODO'}
+                "*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer after trial and before sentence hearing (new)", :allocation_type=>"Grad", :bill_scenario=>'ST3TS7T4'}
               },
             50=>
               {
-                40=>{ :validity=>true, :transfer_fee_full_name=>"transfer before retrial (new) - cracked retrial", :allocation_type=>"Grad", :bill_scenario=>'TODO'},
-                20=>{ :validity=>true, :transfer_fee_full_name=>"transfer before retrial (new) - retrial", :allocation_type=>"Grad", :bill_scenario=>'TODO'}
+                40=>{ :validity=>true, :transfer_fee_full_name=>"transfer before retrial (new) - cracked retrial", :allocation_type=>"Grad", :bill_scenario=>'ST3TS4T9'},
+                20=>{ :validity=>true, :transfer_fee_full_name=>"transfer before retrial (new) - retrial", :allocation_type=>"Grad", :bill_scenario=>'ST3TS4TA'}
               },
             60=>
               {
-                20=>{ :validity=>true, :transfer_fee_full_name=>"transfer during retrial (new) - retrial", :allocation_type=>"Grad", :bill_scenario=>'TODO'}
+                20=>{ :validity=>true, :transfer_fee_full_name=>"transfer during retrial (new) - retrial", :allocation_type=>"Grad", :bill_scenario=>'ST3TS5TA'}
               },
             70=>
               {
-                "*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer after retrial and before sentence hearing (new)", :allocation_type=>"Grad", :bill_scenario=>'TODO'}
+                "*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer after retrial and before sentence hearing (new)", :allocation_type=>"Grad", :bill_scenario=>'ST3TS6TA'}
               }
           }
       },
@@ -69,22 +69,22 @@ module TransferBrainHelpers
         true=>
           {
             10=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - up to and including PCMH transfer (org)", :allocation_type=>"Fixed", :bill_scenario=>'ST4TS0T2'}},
-            20=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - before trial transfer (org)", :allocation_type=>"Fixed", :bill_scenario=>'TODO'}},
+            20=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - before trial transfer (org)", :allocation_type=>"Fixed", :bill_scenario=>'ST4TS0T4'}},
             30=>{"*"=>{ :validity=>false, :transfer_fee_full_name=>nil, :allocation_type=>nil, :bill_scenario=>nil}},
             40=>{"*"=>{ :validity=>false, :transfer_fee_full_name=>nil, :allocation_type=>nil, :bill_scenario=>nil}},
-            50=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - transfer before retrial (org)", :allocation_type=>"Fixed", :bill_scenario=>'TODO'}},
+            50=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"elected case - transfer before retrial (org)", :allocation_type=>"Fixed", :bill_scenario=>'ST4TS0T6'}},
             60=>{"*"=>{ :validity=>false, :transfer_fee_full_name=>nil, :allocation_type=>nil, :bill_scenario=>nil}},
             70=>{"*"=>{ :validity=>false, :transfer_fee_full_name=>nil, :allocation_type=>nil, :bill_scenario=>nil}}
           },
         false=>
         {
           10=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"up to and including PCMH transfer (org)", :allocation_type=>"Grad", :bill_scenario=>'ST2TS1T0'}},
-          20=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"before trial transfer (org)", :allocation_type=>"Grad", :bill_scenario=>'TODO'}},
-          30=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"during trial transfer (org) - trial", :allocation_type=>"Grad", :bill_scenario=>'TODO'}},
-          40=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer after trial and before sentence hearing (org)", :allocation_type=>"Grad", :bill_scenario=>'TODO'}},
-          50=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer before retrial (org) - retrial", :allocation_type=>"Grad", :bill_scenario=>'TODO'}},
-          60=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer during retrial (org) - retrial", :allocation_type=>"Grad", :bill_scenario=>'TODO'}},
-          70=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer after retrial and before sentence hearing (org)", :allocation_type=>"Grad", :bill_scenario=>'TODO'}}
+          20=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"before trial transfer (org)", :allocation_type=>"Grad", :bill_scenario=>'ST2TS2T0'}},
+          30=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"during trial transfer (org) - trial", :allocation_type=>"Grad", :bill_scenario=>'ST2TS3T0'}},
+          40=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer after trial and before sentence hearing (org)", :allocation_type=>"Grad", :bill_scenario=>'ST2TS7T4'}},
+          50=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer before retrial (org) - retrial", :allocation_type=>"Grad", :bill_scenario=>'ST2TS4T0'}},
+          60=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer during retrial (org) - retrial", :allocation_type=>"Grad", :bill_scenario=>'ST2TS5T0'}},
+          70=>{"*"=>{ :validity=>true, :transfer_fee_full_name=>"transfer after retrial and before sentence hearing (org)", :allocation_type=>"Grad", :bill_scenario=>'ST2TS6TA'}}
         }
       }
   }.freeze

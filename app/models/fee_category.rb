8b2aab5bb42f201b33a25976cf7a63ef50b3cadb
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-03-08 11:40:26 +0000

    Update fee_category to implement (and test) has many)

--- app/models/fee_category.rb
+++ app/models/fee_category.rb
@@ -1,2 +1,3 @@
 class FeeCategory < ActiveRecord::Base
+  has_many :fee_bands
 end
--- spec/models/fee_category_spec.rb
+++ spec/models/fee_category_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe FeeCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:fee_bands) }
 end

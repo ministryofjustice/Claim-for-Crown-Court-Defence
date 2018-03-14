45d38daa8a55979590854bd8513b37c246704923
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-03-14 13:55:37 +0000

    Add eligible interim fee feature test step

--- features/page_objects/sections/interim_fee_section.rb
+++ features/page_objects/sections/interim_fee_section.rb
@@ -1,6 +1,10 @@
+class SelectOptionSection < SitePrism::Section
+end
+
 class InterimFeeSection < SitePrism::Section
   include SelectHelper
 
+  sections :fee_type_select_options, SelectOptionSection, 'select#claim_interim_fee_attributes_fee_type_id option', visible: false
   element :select_container, 'select#claim_interim_fee_attributes_fee_type_id', visible: false
 
   element :total, '#claim_interim_fee_attributes_amount'
@@ -8,7 +12,10 @@ class InterimFeeSection < SitePrism::Section
   section :effective_pcmh_date, CommonDateSection, '.js-interim-effectivePcmh'
 
   def select_fee_type(name)
-    id = select_container[:id]
-    select name, from: id, autocomplete: false
+    select name, from: select_container[:id], autocomplete: false
+  end
+
+  def fee_type_select_names
+    fee_type_select_options.map(&:text)
   end
 end
--- features/step_definitions/interim_claim_steps.rb
+++ features/step_definitions/interim_claim_steps.rb
@@ -18,3 +18,13 @@ end
 And(/^I enter the effective PCMH date$/) do
   @interim_claim_form_page.interim_fee.effective_pcmh_date.set_date "2016-01-01"
 end
+
+Then(/^I should see interim fee types applicable to a '(.*?)'$/) do |name|
+  expect(@interim_claim_form_page.interim_fee.fee_type_select_names).to include(*expected_interim_fees_for(name))
+end
+
+def expected_interim_fees_for(case_type_name)
+  expected = ['Disbursement only', 'Warrant']
+  expected += ['Effective PCMH', 'Trial start'] if case_type_name.casecmp('trial').eql?(0)
+  expected += ['Retrial start','Retrial new solicitor'] if case_type_name.casecmp('retrial').eql?(0)
+end

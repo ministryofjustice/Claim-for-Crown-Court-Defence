33f48d6b4e84b70a255147e87759136965267453
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-03-16 10:32:13 +0000

    Update CCR injection API endpoint

--- spec/api/entities/ccr/offence_spec.rb
+++ spec/api/entities/ccr/offence_spec.rb
@@ -7,12 +7,24 @@ describe API::Entities::CCR::Offence do
     let(:scheme) { create :fee_scheme, :nine }
     let(:offence_class) { create(:offence_class, class_letter: 'C') }
     let(:offence) { build(:offence, unique_code: 'ABOCUT_C', offence_class_id: offence_class.id) }
-    let(:fee_scheme) { create(:fee_scheme, :nine) }
 
-    before {create :offence_fee_scheme, offence: offence, fee_scheme: fee_scheme }
+    before {create :offence_fee_scheme, offence: offence, fee_scheme: scheme }
 
     it 'has expected json key-value pairs' do
       expect(response).to include(unique_code: 'ABOCUT_C', offence_class: { class_letter: 'C' })
+    end
+  end
+
+  context 'scheme 10' do
+    let(:scheme) { create :fee_scheme }
+    let(:fee_band) { create :fee_band, description: 'test fee band'}
+    let(:offence) { build(:offence, unique_code: 'ABOCUT_C', fee_band: fee_band) }
+
+    before {create :offence_fee_scheme, offence: offence, fee_scheme: scheme }
+
+    # TODO: identify new codes required by CCR
+    xit 'has expected json key-value pairs' do
+      expect(response).to include(unique_code: 'ABOCUT_C', fee_band: { name: 'test fee band' })
     end
   end
 end

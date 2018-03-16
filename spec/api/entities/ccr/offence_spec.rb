23bef33cda13bac18f4beda9ea3dedbaf1ce2868
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-03-16 10:22:25 +0000

    Update the API entity for offences
    tests for scheme 9

--- spec/api/entities/ccr/offence_spec.rb
+++ spec/api/entities/ccr/offence_spec.rb
@@ -3,10 +3,16 @@ require 'rails_helper'
 describe API::Entities::CCR::Offence do
   subject(:response) { JSON.parse(described_class.represent(offence).to_json).deep_symbolize_keys }
 
-  let(:offence_class) { create(:offence_class, class_letter: 'C') }
-  let(:offence) { build(:offence, unique_code: 'ABOCUT_C', offence_class_id: offence_class.id) }
+  context 'scheme 9' do
+    let(:scheme) { create :fee_scheme, :nine }
+    let(:offence_class) { create(:offence_class, class_letter: 'C') }
+    let(:offence) { build(:offence, unique_code: 'ABOCUT_C', offence_class_id: offence_class.id) }
+    let(:fee_scheme) { create(:fee_scheme, :nine) }
 
-  it 'has expected json key-value pairs' do
-    expect(response).to include(unique_code: 'ABOCUT_C', offence_class: { class_letter: 'C' })
+    before {create :offence_fee_scheme, offence: offence, fee_scheme: fee_scheme }
+
+    it 'has expected json key-value pairs' do
+      expect(response).to include(unique_code: 'ABOCUT_C', offence_class: { class_letter: 'C' })
+    end
   end
 end

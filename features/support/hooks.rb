02ad1b953e69988435a15254527e92667ee239d2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-03-16 11:57:54 +0000

    Add scheme 10 to cucumber seeds

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -26,6 +26,7 @@ Before do
     load "#{Rails.root}/db/seeds/courts.rb"
     load "#{Rails.root}/db/seeds/offence_classes.rb"
     load "#{Rails.root}/db/seeds/offences.rb"
+    load "#{Rails.root}/db/seeds/scheme_ten.rb"
     load "#{Rails.root}/db/seeds/case_types.rb"
     load "#{Rails.root}/db/seeds/fee_types.rb"
     load "#{Rails.root}/db/seeds/certification_types.rb"

a5e1a4b076c88d4c107b16b9d28f621ee89cd213
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-03-19 17:32:21 +0000

    Introduce travel expenses step for advocate interim claims

--- app/controllers/external_users/advocates/interim_claims_controller.rb
+++ app/controllers/external_users/advocates/interim_claims_controller.rb
@@ -5,6 +5,11 @@ class ExternalUsers::Advocates::InterimClaimsController < ExternalUsers::ClaimsC
 
   def build_nested_resources
     @claim.build_warrant_fee if @claim.warrant_fee.nil?
+
+    %i[expenses].each do |association|
+      build_nested_resource(@claim, association)
+    end
+
     super
   end
 end

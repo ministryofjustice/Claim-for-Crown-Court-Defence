804d476148609d47acb7ae42bd6452891c466771
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-03-22 16:07:26 +0000

    Display only quantity for PPE on fee reform graduate fees page
    Amount is not necessary and PPE under this new fee scheme seems that it
    isn't even considered an actual fee, though current implementation makes
    it quite hard to model that without the used approach (which is to
    blacklist fee types that should not require an amount).

--- app/presenters/fee/basic_fee_presenter.rb
+++ app/presenters/fee/basic_fee_presenter.rb
@@ -1,2 +1,18 @@
 class Fee::BasicFeePresenter < Fee::BaseFeePresenter
+  def display_amount?
+    # TODO: this is not really ideal, but right now I
+    # can't see any other way to achieve this specific
+    # requirement :/
+    return true unless claim.fee_scheme == 'fee_reform'
+    return false if FEE_CODES_WITHOUT_AMOUNT.include?(fee.fee_type.code)
+    true
+  end
+
+  private
+
+  FEE_CODES_WITHOUT_AMOUNT = %w[PPE].freeze
+
+  def claim
+    fee.claim
+  end
 end

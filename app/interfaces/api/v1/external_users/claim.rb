e03038dedfad213e0c3190a7a69a143e6b0210b2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-04-03 11:43:29 +0100

    WIP: Add Advocate Interim endpoint

--- app/interfaces/api/v1/external_users/claim.rb
+++ app/interfaces/api/v1/external_users/claim.rb
@@ -8,6 +8,7 @@ module API
           end
 
           mount API::V1::ExternalUsers::Claims::AdvocateClaim
+          mount API::V1::ExternalUsers::Claims::AdvocateInterimClaim
           mount API::V1::ExternalUsers::Claims::FinalClaim
           mount API::V1::ExternalUsers::Claims::InterimClaim
           mount API::V1::ExternalUsers::Claims::TransferClaim

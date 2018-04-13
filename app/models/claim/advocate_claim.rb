3b7060a4e8b2970bbe4eea7cbb950edd031a5e23
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-04-13 14:20:44 +0100

    Add spec for scheme 10 fee api submission and fix

--- app/models/claim/advocate_claim.rb
+++ app/models/claim/advocate_claim.rb
@@ -179,8 +179,9 @@ module Claim
       # TODO: this should return a list based on the current given fee scheme
       # rather than conditionally return scheme 10 specifically
       # TBD once all the fee scheme work is integrated
-      return Fee::BasicFeeType.agfs_scheme_9s unless fee_scheme == 'fee_reform'
-      Fee::BasicFeeType.unscoped.agfs_scheme_10s.order(:position)
+      return Fee::BasicFeeType.all if from_api? || from_json_import?
+      return Fee::BasicFeeType.unscoped.agfs_scheme_10s.order(:position) if fee_scheme.eql?('fee_reform')
+      Fee::BasicFeeType.agfs_scheme_9s
     end
 
     def eligible_misc_fee_types

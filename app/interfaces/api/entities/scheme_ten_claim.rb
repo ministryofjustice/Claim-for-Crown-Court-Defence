c203073fee3fdf5c39ab32d7d61d9914b7d97d1b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-04-23 13:07:40 +0100

    Add SchemeTen entity for displaying results

---
+++ app/interfaces/api/entities/scheme_ten_claim.rb
@@ -0,0 +1,15 @@
+module API
+  module Entities
+    class SchemeTenClaim < BaseEntity
+      expose :defendant
+      expose :case_number
+      expose :case_type
+      expose :court
+      expose :offence
+      expose :offence_band
+      expose :provider_name
+      expose :user_name
+      expose :representation_order_date
+    end
+  end
+end

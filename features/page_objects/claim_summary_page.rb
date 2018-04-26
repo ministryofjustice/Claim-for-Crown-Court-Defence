fe807d70bac2a016cb60b9e5146bd424711e1497
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-04-26 11:58:38 +0100

    Fix failing features due to changes in claim summary page

--- features/page_objects/claim_summary_page.rb
+++ features/page_objects/claim_summary_page.rb
@@ -1,5 +1,5 @@
 class ClaimSummaryPage < SitePrism::Page
   set_url "/external_users/claims/{claim_id}/summary"
 
-  element :continue, "div.new-claim-sidebar > div.new-claim-hgroup > a:nth-of-type(1)"
+  element :continue, ".form-buttons > a.button-continue:nth-of-type(1)"
 end

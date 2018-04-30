eee122ad808c8c182ae32a7e705154ecc92e70b6
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-04-30 15:56:24 +0100

    Add migrator for updating/creating scheme 10 offence codes

--- lib/data_migrator/offence_code_generator.rb
+++ lib/data_migrator/offence_code_generator.rb
@@ -2,13 +2,38 @@ class OffenceCodeGenerator
   attr_reader :offence
 
   def initialize(offence)
+    Offence.send(:include, OffenceExtensions)
     @offence = offence
   end
 
   def code(modifier = nil)
-    offence.description.abbreviate +
+    offence.generated_unique_code(modifier)
+  end
+end
+
+module OffenceExtensions
+  attr_reader :modifier
+
+  def generated_unique_code(modifier = nil)
+    @modifier = modifier
+    if offence_class
+      scheme_9_offence_unique_code
+    elsif offence_band
+      scheme_10_offence_unique_code
+    end
+  end
+
+  def scheme_9_offence_unique_code
+    description.abbreviate +
       modifier.to_s +
       '_' +
-      offence.offence_class.class_letter
+      offence_class.class_letter
+  end
+
+  def scheme_10_offence_unique_code
+    description.abbreviate +
+      modifier.to_s +
+      '_' +
+      offence_band.description
   end
 end

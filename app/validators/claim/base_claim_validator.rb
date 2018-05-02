cc2d0c5a2c393826133f03cac8e61762df732e7a
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-05-02 17:39:48 +0100

    Fix validation for claim 'trial_cracked_at' field
    If the case type is not set, there's no way to know if this field is
    required or not, so it shouldn't be erroring.

--- app/validators/claim/base_claim_validator.rb
+++ app/validators/claim/base_claim_validator.rb
@@ -209,6 +209,7 @@ class Claim::BaseClaimValidator < BaseValidator
 
   def ignore_validation_for_cracked_trials?
     @record.disable_for_state_transition.eql?(:only_amount_assessed) ||
+      !@record.case_type.present? ||
       (@record.case_type && !@record.requires_cracked_dates?)
   end
 

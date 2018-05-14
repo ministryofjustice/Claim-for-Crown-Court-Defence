cbc14a7d00b377d9aefcaecf4af249f3d4c34539
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-05-14 13:22:16 +0100

    Update API POST to new `params:` method

--- spec/api/posting_to_root_spec.rb
+++ spec/api/posting_to_root_spec.rb
@@ -5,7 +5,7 @@ describe 'POSTING to root' do
   let!(:claim)    { create(:claim, source: 'api').reload }
   let(:payload)   { {  api_key: provider.api_key, claim_id: claim.uuid, first_name: "JohnAPI", last_name: "SmithAPI", date_of_birth: "1980-05-10"} }
 
-  before { post '/', payload, format: :json }
+  before { post '/', params: payload.merge(format: :json) } 
 
   it 'returns an error' do
     expect(response.status).to eq(403)

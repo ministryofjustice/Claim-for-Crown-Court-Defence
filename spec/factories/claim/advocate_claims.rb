abf8c9b0c79d6d6a6a7aa92dc428af6cf4c19cdf
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-05-15 16:46:06 +0100

    Share base factory behaviour between advocate claim types

--- spec/factories/claim/advocate_claims.rb
+++ spec/factories/claim/advocate_claims.rb
@@ -6,35 +6,8 @@ FactoryBot.define do
 
     # Alias for :claim factory that should be used since we now have a litigator claim factory
     # TODO: replace all instances for create(:claim) to create(:advocate_claim)
-    # factory :advocate_claim do
-    # end
 
-    # NOTE: this was introduced here because was the only way to get FactoryBot to set
-    # model attributes on initialize (which seems not to be the default behaviour) and
-    # was causing the factory not to assign the appropriate attributes/associations on
-    # initialize which makes after_initialize logic not to behave as expected since the
-    # expected values are not yet set.
-    # More details can be found here:
-    # https://stackoverflow.com/questions/5916162/problem-with-factory-girl-association-and-after-initialize
-    initialize_with { new(attributes) }
-    transient do
-      create_defendant_and_rep_order true
-    end
-
-    form_id SecureRandom.uuid
-    court
-    case_number { random_case_number }
-    external_user
-    source { 'web' }
-    apply_vat false
-    providers_ref { random_providers_ref }
-    case_type
-    offence
-    advocate_category 'QC'
-    sequence(:cms_number) { |n| "CMS-#{Time.now.year}-#{rand(100..199)}-#{n}" }
-
-    after(:build) { |claim| post_build_actions_for_draft_claim(claim) }
-    after(:create) { |claim, evaluator| add_defendant_and_reporder(claim) if evaluator.create_defendant_and_rep_order }
+    advocate_base_setup
 
     trait :admin_creator do
       after(:build) { |claim| make_claim_creator_advocate_admin(claim) }

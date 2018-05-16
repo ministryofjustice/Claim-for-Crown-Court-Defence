131b2a06d365f88420eca5e0b0daa72012ab6af5
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-05-16 12:59:45 +0100

    Expose all claim attributes on advocate interim claim
    This is to work through what are required in CCR
    and what are not. Most of the exposed attributes
    will be nil since they are not available for
    population in the web interface.

--- app/interfaces/api/entities/ccr/advocate_interim_claim.rb
+++ app/interfaces/api/entities/ccr/advocate_interim_claim.rb
@@ -7,22 +7,56 @@ module API
         expose :uuid
         expose :supplier_number
         expose :case_number
-
-        expose  :last_submitted_at,
+        expose  :first_day_of_trial, # will be nil
+                :trial_fixed_notice_at, # will be nil
+                :trial_fixed_at, # will be nil
+                :trial_cracked_at, # will be nil
+                :retrial_started_at, # will be nil
+                :last_submitted_at,
                 format_with: :utc
+        expose :trial_cracked_at_third
 
         expose :adapted_advocate_category, as: :advocate_category
+        # FIXME: dummy a guilty plea for injection purposes
+        # CCR requires a bill scenario in order to calculate fees
+        expose :case_type, using: API::Entities::CCR::CaseType
         expose :court, using: API::Entities::CCR::Court
         expose :offence, using: API::Entities::CCR::Offence
         expose :defendants_with_main_first, using: API::Entities::CCR::Defendant, as: :defendants
 
+        expose :retrial_reduction
+
+        with_options(format_with: :string) do
+          expose :actual_trial_length_or_one, as: :actual_trial_Length # will be nil
+          expose :estimated_trial_length_or_one, as: :estimated_trial_length # will be nil
+          expose :retrial_actual_length_or_one, as: :retrial_actual_length # will be nil
+          expose :retrial_estimated_length_or_one, as: :retrial_estimated_length # will be nil
+        end
+
         expose :additional_information
+
         expose :bills
 
         private
 
         def defendants_with_main_first
           object.defendants.order(created_at: :asc)
+        end
+
+        def estimated_trial_length_or_one
+          object.estimated_trial_length.or_one
+        end
+
+        def actual_trial_length_or_one
+          object.actual_trial_length.or_one
+        end
+
+        def retrial_actual_length_or_one
+          object.retrial_actual_length.or_one
+        end
+
+        def retrial_estimated_length_or_one
+          object.retrial_estimated_length.or_one
         end
 
         def bills

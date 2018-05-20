33b1fa6d9d6ec8396d7936ca46bf055e78ae6ccc
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-05-20 20:50:48 +0100

    Modify daily attendance adapter for scheme 10 Advocate final claims
    AGFS final claims submitted under scheme 10 have changed
    in respect of daily attendance fees in two respects:
    
    1. Only one attendance is included in the basic fee (was 2 under scheme 9)
    2. There is no banding for further days so they are simply specified
       as the number of additional days in one fee - BADAT.

--- spec/services/ccr/daily_attendance_adapter_spec.rb
+++ spec/services/ccr/daily_attendance_adapter_spec.rb
@@ -1,36 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe CCR::DailyAttendanceAdapter, type: :adapter do
-  subject { described_class.new(claim) }
-  let(:claim) { create(:authorised_claim) }
+  let(:retrial) { create(:case_type, :retrial) }
 
   describe '#attendances' do
     subject { described_class.new(claim).attendances }
 
-    context 'for trials' do
-      context 'when no daily attendance uplift fees applied' do
-        [0,1,3,nil].each do |trial_length|
-          context "and claim has an actual trial length of #{trial_length || 'nil'}" do
-            before { claim.update(actual_trial_length: trial_length) }
-            it "returns #{[trial_length,2].compact.min} - as least of actual trial length or 2 (included in basic fee)" do
-              is_expected.to eql [trial_length,2].compact.min
+    context 'scheme 9 claim' do
+      let(:claim) { create(:authorised_claim) }
+      attendances_incl_in_basic_fee = 2
+
+      context 'for trials' do
+        context 'when no daily attendance uplift fees applied' do
+          [0,1,3,nil].each do |trial_length|
+            context "and claim has an actual trial length of #{trial_length || 'nil'}" do
+              before { claim.update(actual_trial_length: trial_length) }
+              it "returns #{[trial_length, attendances_incl_in_basic_fee].compact.min} - as least of actual trial length or #{attendances_incl_in_basic_fee} (included in basic fee)" do
+                is_expected.to eql [trial_length, attendances_incl_in_basic_fee].compact.min
+              end
             end
+          end
+        end
+
+        context 'when daily attendance uplift fees applied' do
+          before do
+            claim.actual_trial_length = 51
+            create(:basic_fee, :daf_fee, claim: claim, quantity: 38, rate: 1.0)
+            create(:basic_fee, :dah_fee, claim: claim, quantity: 10, rate: 1.0)
+            create(:basic_fee, :daj_fee, claim: claim, quantity: 1, rate: 1.0)
+          end
+
+          it "returns sum of daily attendance fee types plus #{attendances_incl_in_basic_fee} (included in basic fee)" do
+            is_expected.to eql 51
           end
         end
       end
 
       context 'for retrials' do
-        let(:retrial) { create(:case_type, :retrial) }
-
-        before do
-          claim.update(case_type: retrial)
-        end
+        before { claim.update(case_type: retrial) }
 
         context 'when no daily attendance uplift fees applied' do
           [0,1,3,nil].each do |trial_length|
             context "and claim has an actual retrial length of #{trial_length || 'nil'}" do
               before { claim.update(retrial_actual_length: trial_length) }
-              it "returns #{[trial_length,2].compact.min} - as least of actual retrial length or 2 (included in basic fee)" do
+              it "returns #{[trial_length, attendances_incl_in_basic_fee].compact.min} - as least of actual retrial length or 2 (included in basic fee)" do
                 is_expected.to eql [trial_length,2].compact.min
               end
             end
@@ -39,22 +52,54 @@ RSpec.describe CCR::DailyAttendanceAdapter, type: :adapter do
       end
     end
 
-    context 'when daily attendance uplift fees applied' do
-      before do
-        claim.actual_trial_length = 51
-        create(:basic_fee, :daf_fee, claim: claim, quantity: 38, rate: 1.0)
-        create(:basic_fee, :dah_fee, claim: claim, quantity: 10, rate: 1.0)
-        create(:basic_fee, :daj_fee, claim: claim, quantity: 1, rate: 1.0)
+    context 'scheme 10 claim' do
+      let(:claim) { create(:authorised_claim, :agfs_scheme_10) }
+      attendances_incl_in_basic_fee = 1
+
+      context 'for trials' do
+        context 'when no daily attendance uplift fee (2+) applied' do
+          [0,1,2,nil].each do |trial_length|
+            context "and claim has an actual trial length of #{trial_length || 'nil'}" do
+              before { claim.update(actual_trial_length: trial_length) }
+              it "returns #{[trial_length, attendances_incl_in_basic_fee].compact.min} - as least of actual trial length or #{attendances_incl_in_basic_fee} (included in basic fee)" do
+                is_expected.to eql [trial_length, attendances_incl_in_basic_fee].compact.min
+              end
+            end
+          end
+        end
+
+        context 'when daily attendance uplift fees applied' do
+          before do
+            claim.actual_trial_length = 10
+            create(:basic_fee, :dat_fee, claim: claim, quantity: 9, rate: 1.0)
+          end
+
+          it "returns sum of daily attendance fee types plus #{attendances_incl_in_basic_fee} (included in basic fee)" do
+            is_expected.to eql 10
+          end
+        end
       end
 
-      it 'returns sum of daily attendance fee types plus 2 (included in basic fee)' do
-        is_expected.to eql 51
+      context 'for retrials' do
+        before { claim.update(case_type: retrial) }
+
+        context 'when no daily attendance uplift fee (2+) applied' do
+          [0,1,2,nil].each do |trial_length|
+            context "and claim has an actual retrial length of #{trial_length || 'nil'}" do
+              before { claim.update(retrial_actual_length: trial_length) }
+              it "returns #{[trial_length, attendances_incl_in_basic_fee].compact.min} - as least of actual retrial length or #{attendances_incl_in_basic_fee} (included in basic fee)" do
+                is_expected.to eql [trial_length, attendances_incl_in_basic_fee].compact.min
+              end
+            end
+          end
+        end
       end
     end
   end
 
   describe '.attendances_for' do
     subject { described_class.attendances_for(claim) }
+    let(:claim) { build(:authorised_claim) }
     let(:adapter) { instance_double 'DailyAttendanceAdapter' }
 
     it 'calls #attendances' do

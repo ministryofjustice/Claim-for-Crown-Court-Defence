f14e24ae1e1849d37ddcbe99995980c54e1cedb7
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-05-22 10:53:43 +0100

    Relax validations on interim claim info warrant issued date
    Since the fee has already been claim, these dates just need to be
    validated from the point of view of being a valid date, they don't
    required the same sort of validations performed when the fee is actually
    being claimed.

--- app/validators/interim_claim_info_validator.rb
+++ app/validators/interim_claim_info_validator.rb
@@ -9,9 +9,6 @@ class InterimClaimInfoValidator < BaseValidator
     validate_on_or_after(Settings.earliest_permitted_date, :warrant_issued_date, 'check_not_too_far_in_past')
     return if @record.warrant_issued_date.nil?
     validate_on_or_before(Date.today, :warrant_issued_date, 'check_not_in_future')
-    validate_on_or_before(InterimClaimInfo::MINIMUM_PERIOD_SINCE_ISSUED.ago, :warrant_issued_date, 'on_or_before')
-    check_date = @record.claim&.earliest_representation_order&.representation_order_date
-    validate_on_or_after(check_date, :warrant_issued_date, 'check_on_or_after_earliest_representation_order')
   end
 
   def validate_warrant_executed_date

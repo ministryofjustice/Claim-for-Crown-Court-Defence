923d264ec51902f29525da2a8176fc238c3477b6
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-06-20 16:44:28 +0100

    Add seed data for magistrates court establishments

--- db/seeds/establishments.rb
+++ db/seeds/establishments.rb
@@ -1,7 +1,9 @@
 require Rails.root.join('db','seeds', 'establishments', 'prison_seeder')
 require Rails.root.join('db','seeds', 'establishments', 'hospital_seeder')
+require Rails.root.join('db','seeds', 'establishments', 'magistrates_court_seeder')
 
 options = { dry_run: ENV['SEEDS_DRY_MODE'] }
 
 Seeds::Establishments::PrisonSeeder.call(options)
 Seeds::Establishments::HospitalSeeder.call(options)
+Seeds::Establishments::MagistratesCourtSeeder.call(options)

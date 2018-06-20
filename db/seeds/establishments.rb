7a6b9464e19eac45c2111a10cc669b8a74160506
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-06-20 16:30:04 +0100

    Add seeds for hospital establishments

--- db/seeds/establishments.rb
+++ db/seeds/establishments.rb
@@ -1,5 +1,7 @@
 require Rails.root.join('db','seeds', 'establishments', 'prison_seeder')
+require Rails.root.join('db','seeds', 'establishments', 'hospital_seeder')
 
 options = { dry_run: ENV['SEEDS_DRY_MODE'] }
 
 Seeds::Establishments::PrisonSeeder.call(options)
+Seeds::Establishments::HospitalSeeder.call(options)

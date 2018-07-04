c88a90bef99d03fff141fe830299225d5bfea3d7
Sergio Marques <sergio.marques@digital.justice.gov.uk>
2018-07-04 13:57:58 +0100

    Setup CircleCI 2.0 builds for the project

--- features/support/dropzone_helper.rb
+++ features/support/dropzone_helper.rb
@@ -1,19 +1,20 @@
 module DropzoneHelper
   def drag_and_drop_file(selector, file)
+    id = SecureRandom.uuid
     page.execute_script <<-JS
       tempFileInput = window.$('<input/>').attr(
-        {id: 'temp-file-input', name: 'temp-file-input', type:'file'}
+        {id: 'temp-file-input-#{id}', name: 'temp-file-input', type: 'file'}
       ).appendTo('body');
     JS
 
-    attach_file('temp-file-input', file)
+    attach_file("temp-file-input-#{id}", file)
 
     page.execute_script('fileList = [tempFileInput.get(0).files[0]];')
 
     page.execute_script <<-JS
       e = jQuery.Event('drop', { dataTransfer : { files : fileList } });
       $('.#{selector}')[0].dropzone.listeners[0].events.drop(e);
-      $('#temp-file-input').remove();
+      $('#temp-file-input-#{id}').remove();
     JS
   end
 end

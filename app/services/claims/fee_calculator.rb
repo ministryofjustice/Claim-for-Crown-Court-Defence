02ab0ee95b1b54a612ac69f085991a8b8aa888f2
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-07-09 15:33:45 +0100

    Refactor fee calculator service

--- app/services/claims/fee_calculator.rb
+++ app/services/claims/fee_calculator.rb
@@ -14,28 +14,14 @@ module Claims
 
     def call(fee_type, quantity)
       fee_scheme.calculate do |options|
-        # TODO: create select/find_by calls to list endpoints in gem
-        scenario = fee_scheme.scenarios.select {|s| s.code.eql?(CCR::CaseTypeAdapter::BILL_SCENARIOS[case_type.fee_type_code.to_sym]) }.first
-
-        # some/all fixed fees do not require offences and they has no bearing on calculated fee amount
-        offence_class = offence&.offence_class&.class_letter || offence&.offence_band&.description
-
-        advocate_type = CCR::AdvocateCategoryAdapter.code_for(advocate_category)
-
-        fee_type_code = [
-          # CCR::Fee::BasicFeeAdapter::BASIC_FEE_BILL_MAPPINGS, # TODO: all are AGFS_FEE
-          CCR::Fee::FixedFeeAdapter::FIXED_FEE_BILL_MAPPINGS,
-          CCR::Fee::MiscFeeAdapter::MISC_FEE_BILL_MAPPINGS
-        ].inject(&:merge)[fee_type.unique_code.to_sym][:bill_subtype]
-
         options[:scenario] = scenario.id
         options[:offence_class] = offence_class
         options[:advocate_type] = advocate_type
-        options[:fee_type_code] = fee_type_code
+        options[:fee_type_code] = fee_type_code_for(fee_type)
 
         # units
         # TODO: unit needs to be dynamically determined and values determined
-        # this only works assuming there is only one unit type and the quantity of
+        # Current code only works assuming there is only one unit type and the quantity of
         # the fee is for that unit type.
         units = fee_scheme.units(options).map { |u| u.id.downcase }
         units.each do |unit|
@@ -48,6 +34,7 @@ module Claims
         # modifiers
         # TODO: modifier needs to be dynamically determined and could be more than one
         # TODO: modifier values should be based on "munging" uplifts and "number of.." fee types
+        # rather than actual number of defendants/cases (based on only paying what they ask for logic)
         # options[:number_of_defendants] = 1
         # options[:number_of_cases] = 1
       end
@@ -55,10 +42,38 @@ module Claims
 
     private
 
+    def scheme_type
+      agfs? ? 'AGFS' : 'LGFS'
+    end
+
     def fee_scheme
       return @fee_scheme if @fee_scheme
-      scheme_type = agfs? ? 'AGFS' : 'LGFS'
       @fee_scheme ||= client.fee_schemes(type: scheme_type, case_date: earliest_representation_order_date.to_s(:db))
+    end
+
+    def scenario
+      # TODO: create select/find_by calls to list endpoints in client gem
+      fee_scheme.scenarios.select do |s|
+        s.code.eql?(CCR::CaseTypeAdapter::BILL_SCENARIOS[case_type.fee_type_code.to_sym])
+      end.first
+    end
+
+    def offence_class
+      # some/all fixed fees do not require offences and they have no bearing on calculated fee amount
+      # TODO: make conditional on fee scheme version
+      offence&.offence_class&.class_letter || offence&.offence_band&.description
+    end
+
+    def advocate_type
+      CCR::AdvocateCategoryAdapter.code_for(advocate_category)
+    end
+
+    def fee_type_code_for(fee_type)
+      [
+        # CCR::Fee::BasicFeeAdapter::BASIC_FEE_BILL_MAPPINGS, # TODO: all are AGFS_FEE
+        CCR::Fee::FixedFeeAdapter::FIXED_FEE_BILL_MAPPINGS,
+        CCR::Fee::MiscFeeAdapter::MISC_FEE_BILL_MAPPINGS
+      ].inject(&:merge)[fee_type.unique_code.to_sym][:bill_subtype]
     end
 
     def client

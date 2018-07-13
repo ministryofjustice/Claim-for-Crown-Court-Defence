5b846ba9ca79ba4e103040c5ebd22d251516b3f5
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-07-13 11:55:25 +0100

    Create Transform::Claim service

---
+++ app/services/transform/claim.rb
@@ -0,0 +1,59 @@
+module Transform
+  class Claim
+    DUPLICATE = %w[disk_evidence retrial_reduction case_concluded_at effective_pcmh_date first_day_of_trial
+                   legal_aid_transfer_date retrial_concluded_at retrial_started_at trial_concluded_at
+                   trial_cracked_at trial_fixed_at trial_fixed_notice_at authorised_at created_at
+                   last_submitted_at original_submission_date disbursements_total disbursements_vat
+                   expenses_total expenses_vat fees_total fees_vat total vat_amount actual_trial_length
+                   estimated_trial_length retrial_actual_length retrial_estimated_length advocate_category
+                   trial_cracked_at_third source supplier_number].freeze
+
+    COUNT = {
+      num_of_documents: { object: :documents, where: '' },
+      num_of_defendants: { object: :defendants, where: '' },
+      rejections: { object: :claim_state_transitions, where: "\"to\"='rejected'" },
+      refusals: { object: :claim_state_transitions, where: "\"to\"='refused'" }
+    }.freeze
+
+    OBJECT_VALUE = {
+      court: { object: :court, att: :name },
+      transfer_court: { object: :transfer_court, att: :name },
+      case_type: { object: :case_type, att: :name },
+      offence_name: { object: :offence, att: :description },
+      date_last_assessed: { object: :assessment, att: :created_at },
+      provider_name: { object: :provider, att: :name },
+      provider_type: { object: :provider, att: :provider_type },
+      assessment_total: { object: :assessment, att: :total },
+      assessment_vat: { object: :assessment, att: :vat_amount },
+      scheme_name: { object: :fee_scheme, att: :name },
+      scheme_number: { object: :fee_scheme, att: :version }
+    }.freeze
+
+    CALCULATIONS = {
+      amount_claimed: %i[total vat_amount],
+      amount_authorised: %i[assessment_total assessment_vat]
+    }.freeze
+
+    CLAIM_TYPE_CONVERSIONS = {
+      'Claim::AdvocateInterimClaim' =>  'Advocate interim claim',
+      'Claim::AdvocateClaim'        =>  'Advocate final claim',
+      'Claim::InterimClaim'         =>  'Litigator interim claim',
+      'Claim::LitigatorClaim'       =>  'Litigator final claim',
+      'Claim::TransferClaim'        =>  'Litigator transfer claim'
+    }.freeze
+
+    class << self
+      def call(claim)
+        hash = {}
+        DUPLICATE.each { |att| hash[att.to_sym] = claim.send(att) }
+        COUNT.each { |att, source| hash[att] = claim.send(source[:object]).where(source[:where]).count }
+        OBJECT_VALUE.each { |att, source| hash[att] = claim.send(source[:object])&.send(source[:att]) }
+        CALCULATIONS.each { |att, sum| hash[att] = (hash[sum[0]] || 0) + (hash[sum[1]] || 0) }
+        hash[:ppe] = claim.fees.find_by(fee_type_id: 11)&.quantity.to_i
+        hash[:claim_type] = CLAIM_TYPE_CONVERSIONS[claim.type.to_s]
+        hash[:offence_type] = claim.offence&.offence_band&.description || claim.offence&.offence_class&.class_letter
+        hash
+      end
+    end
+  end
+end
--- spec/models/stats/mi_data_spec.rb
+++ spec/models/stats/mi_data_spec.rb
@@ -16,40 +16,6 @@ module Stats
       let(:claim) { create :archived_pending_delete_claim }
       it { is_expected.to be true }
       it { expect { import }.to change { Stats::MIData.count }.by 1 }
-
-      context 'runs different import types' do
-        before { import }
-
-        let(:new_mi) { Stats::MIData.last }
-
-        it 'retrieves data from linked objects' do
-          expect(new_mi.case_type).to eq claim.case_type.name
-        end
-
-        it 'retrieves data from the claim directly' do
-          expect(new_mi.advocate_category).to eq claim.advocate_category
-        end
-
-        it 'calculates the totals correctly' do
-          expect(new_mi.amount_authorised).to eq claim.assessment.total + claim.assessment.vat_amount
-        end
-
-        it 'counts linked data correctly' do
-          expect(new_mi.num_of_defendants).to eq claim.defendants.count
-        end
-
-        it 'counts linked data with where clauses correctly' do
-          expect(new_mi.refusals).to eq claim.claim_state_transitions.where("\"to\"='refused'").count
-        end
-
-        it 'converts the offence type' do
-          expect(new_mi.offence_type).to eq claim.offence.offence_class.class_letter
-        end
-
-        it 'converts the claim description' do
-          expect(new_mi.claim_type).to eq 'Advocate final claim'
-        end
-      end
     end
   end
 end

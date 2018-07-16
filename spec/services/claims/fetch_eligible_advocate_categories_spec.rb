b40a224b3094a8459471b65ece3f8eb8456a4cfb
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-07-16 21:47:04 +0100

    Fix advocate categories on fixed fee page
    Found that for a new AGFS claim the advocate categegories
    on the fixed fee pages were showing the scheme 9 categories.
    
    Not sure if this came out of the recent refactor of the fee reform
    code but the fetch eligible advocate categories services was
    still relying solely on the `fee_scheme:string` of `'fee_reforms'`.

--- spec/services/claims/fetch_eligible_advocate_categories_spec.rb
+++ spec/services/claims/fetch_eligible_advocate_categories_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe Claims::FetchEligibleAdvocateCategories, type: :service do
   describe '.for' do
     subject { described_class.for(claim) }
 
-    context 'when claim is nil' do
+    context 'nil claim' do
       let(:claim) { nil }
       specify { is_expected.to eq(nil) }
     end
 
-    context 'when claim is for an LGFS' do
+    context 'LGFS claim' do
       let(:claim) { build(:litigator_claim) }
       specify { is_expected.to eq(nil) }
     end
 
-    context 'when claim is for a AGFS' do
-      shared_examples_for 'an AGFS claim' do
-        context 'and it is NOT on the AGFS fee reform scheme' do
+    context 'AGFS claim' do
+      shared_examples_for 'AGFS fee reform dependant advocate categories' do
+        context 'default scheme' do
           before do
             expect(claim).to receive(:fee_scheme).and_return('default')
           end
@@ -26,7 +26,7 @@ RSpec.describe Claims::FetchEligibleAdvocateCategories, type: :service do
           end
         end
 
-        context 'and it is on the AGFS fee reform scheme' do
+        context 'fee reform scheme' do
           before do
             expect(claim).to receive(:fee_scheme).and_return('fee_reform')
           end
@@ -35,29 +35,53 @@ RSpec.describe Claims::FetchEligibleAdvocateCategories, type: :service do
             is_expected.to match_array(["QC", "Leading junior", "Junior"])
           end
         end
+
+        context 'scheme 9 object' do
+          let(:agfs_scheme_nine) { FeeScheme.find_by(name: 'AGFS', version: 9) || create(:fee_scheme, :agfs_nine) }
+
+          before do
+            expect(claim).to receive(:fee_scheme).at_least(:once).and_return(agfs_scheme_nine)
+          end
+
+          it 'returns the list for AGFS scheme 9 advocate categories' do
+            is_expected.to match_array(["QC", "Led junior", "Leading junior", "Junior alone"])
+          end
+        end
+
+        context 'scheme 10 object' do
+          let(:agfs_scheme_ten) { FeeScheme.find_by(name: 'AGFS', version: 10) || create(:fee_scheme) }
+
+          before do
+            expect(claim).to receive(:fee_scheme).at_least(:once).and_return(agfs_scheme_ten)
+          end
+
+          it 'returns the list for AGFS scheme 10 advocate categories' do
+            is_expected.to match_array(["QC", "Leading junior", "Junior"])
+          end
+        end
       end
 
-      context 'and the claim is final' do
+      context 'when the claim is final' do
         let(:claim) { build(:advocate_claim) }
 
-        include_examples 'an AGFS claim'
+        include_examples 'AGFS fee reform dependant advocate categories'
       end
 
-      context 'and the claim is interim' do
+      context 'when the claim is interim' do
         let(:claim) { build(:advocate_interim_claim) }
 
-        include_examples 'an AGFS claim'
+        include_examples 'AGFS fee reform dependant advocate categories'
       end
 
       context 'when the claim has been submitted via the API' do
         # This will mean the offence will determine the fee_scheme, not the rep_order date
-        context 'with a scheme_ten offence' do
+        context 'with a scheme 10 offence' do
           let(:claim) { create :api_advocate_claim, :with_scheme_ten_offence }
 
           it { is_expected.to match_array(['QC', 'Leading junior', 'Junior']) }
         end
 
-        context 'with a scheme_nine offence' do
+        context 'with a scheme 9 offence' do
           let(:claim) { create :api_advocate_claim }
 
           it { is_expected.to match_array(['QC', 'Led junior', 'Leading junior', 'Junior alone']) }

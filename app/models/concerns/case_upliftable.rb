baf3e9ea3031cdc9c9d0fba2bd7671f5b1fa13dc
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-07-18 14:03:20 +0100

    Amend case upliftable
    add missing mapping and wrap
    constants with methods for use
    in other contexts.

--- app/models/concerns/case_upliftable.rb
+++ app/models/concerns/case_upliftable.rb
@@ -16,13 +16,24 @@ module CaseUpliftable
       FXASE: 'FXASU',
       FXCBR: 'FXCBU',
       FXCSE: 'FXCSU',
-      FXENP: 'FXENU'
+      FXENP: 'FXENU',
+      FXCCD: 'FXCDU'
     }.with_indifferent_access.freeze
+
+    ORPHAN_CASE_UPLIFTS = %w[BANOC FXNOC].freeze
+
+    def case_uplifts
+      where(unique_code: case_uplift_unique_codes)
+    end
+
+    def case_uplift_unique_codes
+      CASE_UPLIFT_MAPPINGS.values + ORPHAN_CASE_UPLIFTS
+    end
   end
 
   included do
     def case_uplift?
-      unique_code.in?(%w[BANOC FXNOC FXACU FXASU FXCBU FXCSU FXCDU FXENU])
+      unique_code.in?(self.class.case_uplift_unique_codes)
     end
   end
 end

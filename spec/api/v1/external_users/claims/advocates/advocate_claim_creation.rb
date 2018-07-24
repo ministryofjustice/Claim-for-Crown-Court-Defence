699d1b072d775cf9f4aa1eeaf775ede292381c63
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-07-24 17:30:55 +0100

    Add agfs scheme 9 API claim creation integration test

--- spec/api/v1/external_users/claims/advocates/advocate_claim_creation.rb
+++ spec/api/v1/external_users/claims/advocates/advocate_claim_creation.rb
@@ -16,8 +16,6 @@ RSpec.describe 'API claim creation for AGFS Scheme 10 trial' do
   let!(:other_provider) { create(:provider) }
   let!(:vendor) { create(:external_user, :admin, provider: provider) }
   let!(:advocate) { create(:external_user, :advocate, provider: provider) }
-  let!(:case_type) { create(:case_type, :trial) }
-  let!(:offence) { create(:offence, :with_fee_scheme_ten)}
   let!(:court) { create(:court)}
 
   let(:claim_params) do
@@ -33,7 +31,7 @@ RSpec.describe 'API claim creation for AGFS Scheme 10 trial' do
       estimated_trial_length: 10,
       actual_trial_length: 9,
       trial_concluded_at: "2018-04-19",
-      advocate_category: 'Junior',
+      advocate_category: advocate_category,
       offence_id: offence.id,
       court_id: court.id,
       additional_information: 'Bish bosh bash'
@@ -92,17 +90,74 @@ RSpec.describe 'API claim creation for AGFS Scheme 10 trial' do
     }
   end
 
-  # TODO:
+  around do |example|
+    result = example.run
+    ap JSON.parse(last_response.body) if result.is_a?(RSpec::Expectations::ExpectationNotMetError)
+  end
+
   context 'scheme 9' do
-    specify "Case management system creates a valid scheme 9 final graduated fee claim", skip: 'TODO: ' do
+    let(:case_type) { create(:case_type, :trial) }
+    let(:offence) { create(:offence, :with_fee_scheme) }
+    let(:representation_order_date) { Date.new(2018, 03, 31).as_json }
+    let(:advocate_category) { 'Junior alone' }
+
+    specify "Case management system creates a valid scheme 9 final graduated fee claim" do
+      post ClaimApiEndpoints.for(:advocate).create, claim_params, format: :json
+      expect(last_response.status).to eql 201
+
+      claim = Claim::BaseClaim.find_by(uuid: JSON.parse(last_response.body)['id'])
+
+      post endpoint(:defendants), defendant_params.merge(claim_id: claim.uuid), format: :json
+      expect(last_response.status).to eql 201
+
+      defendant = Defendant.find_by(uuid: JSON.parse(last_response.body)['id'] )
+
+      post endpoint(:representation_orders), representation_order_params.merge(defendant_id: defendant.uuid), format: :json
+      expect(last_response.status).to eql 201
+
+      post endpoint(:fees), base_fee_params.merge(claim_id: claim.uuid, fee_type_id: Fee::BaseFeeType.find_by(unique_code: 'BABAF').id), format: :json
+      expect(last_response.status).to eql 200
+
+      post endpoint(:fees), base_fee_params.merge(claim_id: claim.uuid, fee_type_id: Fee::BaseFeeType.find_by(unique_code: 'BADAF').id), format: :json
+      expect(last_response.status).to eql 200
+
+      fee = Fee::BaseFee.find_by(uuid: JSON.parse(last_response.body)['id'] )
+
+      post endpoint(:dates_attended), date_attended_params.merge(attended_item_id: fee.uuid, date: claim.earliest_representation_order_date.as_json), format: :json
+      expect(last_response.status).to eql 201
+
+      post endpoint(:fees), base_fee_params.merge(claim_id: claim.uuid, fee_type_id: Fee::BaseFeeType.find_by(unique_code: 'MIAPH').id), format: :json
+      expect(last_response.status).to eql 201
+
+      post endpoint(:fees), base_fee_params.merge(claim_id: claim.uuid, fee_type_id: Fee::BaseFeeType.find_by(unique_code: 'MIAHU').id), format: :json
+      expect(last_response.status).to eql 201
+
+      post endpoint(:expenses), expense_params.merge(claim_id: claim.uuid, expense_type_id: ExpenseType.find_by(unique_code: 'CAR').id, distance: 500.38, mileage_rate_id: 1), format: :json
+      expect(last_response.status).to eql 201
+
+      post endpoint(:expenses), expense_params.merge(claim_id: claim.uuid, expense_type_id: ExpenseType.find_by(unique_code: 'HOTEL').id), format: :json
+      expect(last_response.status).to eql 201
+
+      expect(claim.fee_scheme.name).to eql 'AGFS'
+      expect(claim.fee_scheme.version).to eql 9
+      expect(claim.defendants.size).to eql 1
+      expect(claim.defendants.first.representation_orders.size).to eql 1
+      expect(claim.basic_fees.where(amount: 1..Float::INFINITY).size).to eql 2
+      expect(claim.basic_fees.find_by(fee_type_id: Fee::BaseFeeType.find_by(unique_code: 'BADAF').id).dates_attended.size).to eql 1
+      expect(claim.expenses.size).to eql 2
+      expect(claim.source).to eql 'api'
+      expect(claim.state).to eql 'draft'
     end
+
     specify "Case management system creates a valid scheme 9 final fixed fee claim", skip: 'TODO: ' do
     end
   end
 
   context 'scheme 10' do
+    let(:case_type) { create(:case_type, :trial) }
     let(:offence) { create(:offence, :with_fee_scheme_ten) }
     let(:representation_order_date) { Date.new(2018, 04, 1).as_json }
+    let(:advocate_category) { 'Junior' }
 
     specify 'Case management system creates a valid scheme 10 graduated fee claim' do
       post ClaimApiEndpoints.for(:advocate).create, claim_params, format: :json
@@ -152,7 +207,9 @@ RSpec.describe 'API claim creation for AGFS Scheme 10 trial' do
       expect(claim.state).to eql 'draft'
     end
 
-    # TODO:
+    specify 'Case management system creates a valid scheme 10 fixed fee claim', skip: 'TODO: ' do
+    end
+
     specify 'Case management system creates a valid scheme 10 interim/warrant fee claim', skip: 'TODO: ' do
     end
   end
--- spec/support/seed_helpers.rb
+++ spec/support/seed_helpers.rb
@@ -3,9 +3,15 @@ require File.expand_path(Rails.root.join('db','seeds','fee_types','csv_seeder'))
 
 module SeedHelpers
   def seed_fee_schemes
-    FeeScheme.find_by(name: 'LGFS', version: 9) || create(:fee_scheme, :lgfs_nine)
-    FeeScheme.find_by(name: 'AGFS', version: 9) || create(:fee_scheme, :agfs_nine)
-    FeeScheme.find_by(name: 'AGFS', version: 10) || create(:fee_scheme, :agfs_ten)
+    # TODO: remove if not needed
+    # FeeScheme.find_by(name: 'LGFS', version: 9) || create(:fee_scheme, :lgfs_nine)
+    # FeeScheme.find_by(name: 'AGFS', version: 9) || create(:fee_scheme, :agfs_nine)
+    # FeeScheme.find_by(name: 'AGFS', version: 10) || create(:fee_scheme, :agfs_ten)
+
+    # TODO: these should be in a separate seedfile that is just loaded here - cirrently they are in scheme_ten.rb seed file
+    FeeScheme.find_or_create_by(name: 'LGFS', version: 9, start_date: Date.new(2014, 03, 20).beginning_of_day)
+    FeeScheme.find_or_create_by(name: 'AGFS', version: 9, start_date: Date.new(2012, 04, 01).beginning_of_day, end_date: Date.new(2018, 03, 31).end_of_day)
+    FeeScheme.find_or_create_by(name: 'AGFS', version: 10, start_date: Date.new(2018, 04, 01).beginning_of_day)
   end
 
   def seed_fee_types

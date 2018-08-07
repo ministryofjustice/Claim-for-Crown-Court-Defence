d5441eca01db15894f2b8a7273e4d45c178b5d3e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-08-07 16:17:45 +0100

    Stub out calculator calls in features
    This can be extended to handle other error types

---
+++ features/support/stub_calculator_requests.rb
@@ -0,0 +1,6 @@
+require 'webmock/cucumber'
+
+Before('@stub_calculator_request_and_fail') do
+  stub_request(:get, %r{\Ahttps://laa-fee-calculator(.*).k8s.integration.dsd.io/api/v1/.*\z}).
+    to_return(status: 400, body: {'error': 'delete console.log from JS if this is in features'}.to_json, headers: {})
+end

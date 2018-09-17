fdcda38e1ab22ebef37845799f61eae2a223eae8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-09-17 14:13:18 +0100

    Update vcr configuration
    This allows specs to use the inline vcr cassette name pattern, e.g.
    ```ruby
    context 'when something', vcr: { cassette_name: 'test/it' } do
    ```
    instead of having to add a
    ```ruby
    VCR.use_cassette('test/it') do
    	run_test
    end
    ```
    block each time

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -3,6 +3,7 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'vcr/cassettes'
   c.hook_into :webmock
+  c.configure_rspec_metadata!
   c.default_cassette_options = {
     erb: true,
     exclusive: true,

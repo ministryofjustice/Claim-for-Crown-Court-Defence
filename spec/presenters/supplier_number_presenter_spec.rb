159ab7ed72bcd6d0fbe6b48bc8ce984fb3da1ab8
David Plews (ClearGif) <freelance@cleargif.co.uk>
2018-10-03 18:00:49 +0100

    Updated presenter and specs
    - updated the presenter to include a supplier name if supplier

--- spec/presenters/supplier_number_presenter_spec.rb
+++ spec/presenters/supplier_number_presenter_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe SupplierNumberPresenter do
   let(:account_number) { '3N677Q' }
   let(:postcode) { 'W3A 7RG' }
-  let(:supplier_number) { build(:supplier_number, supplier_number: account_number, postcode: postcode) }
+  let(:supplier_name) { 'Some name' }
+  let(:supplier_number) { build(:supplier_number, supplier_number: account_number, postcode: postcode, name: supplier_name ) }
 
   subject(:presenter) { described_class.new(supplier_number, view) }
 
   describe '#supplier_label' do
     it 'returns a label including the supplier number and its postcode' do
-      expect(presenter.supplier_label).to eq("#{account_number} (#{postcode})")
+      expect(presenter.supplier_label).to eq("#{account_number} - #{supplier_name} (#{postcode})")
     end
 
     context 'when the supplier does not have a defined postcode' do
@@ -17,6 +18,14 @@ RSpec.describe SupplierNumberPresenter do
 
       it 'returns a label including only the supplier number' do
         expect(presenter.supplier_label).to eq(account_number)
+      end
+    end
+
+    context 'when the supplier does not have a defined postcode' do
+      let(:supplier_name) { nil }
+
+      it 'returns a label including only the supplier number' do
+        expect(presenter.supplier_label).to eq("#{account_number} - (#{postcode})")
       end
     end
   end

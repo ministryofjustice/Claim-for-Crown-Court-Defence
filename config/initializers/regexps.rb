4f62257b1d31c5c5f7b7cb934dd6dc50bdcbaf97
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-10-11 15:42:48 +0100

    Move postcode regex tosettingd.yml
    The use of an initializer to set Settings explictly
    conflicts with the Config gems methods of settings
    and caused failure in local development.

--- config/initializers/regexps.rb
+++
@@ -1,1 +0,0 @@
-Settings.postcode_regexp = /\A([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})\z/

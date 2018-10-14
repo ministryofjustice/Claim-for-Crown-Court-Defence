4314a32497f98002035187152e133c374e62e297
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-10-14 21:40:05 +0100

    Use `ENV.fetch` instead
    Make for easier test mocking

--- config/initializers/maintenance_mode.rb
+++ config/initializers/maintenance_mode.rb
@@ -1,7 +1,4 @@
 MaintenanceMode.configure do |config|
-  # route all paths to maintenance page
-  config.enabled = ENV['MAINTENANCE_MODE'].present?
-
-  # set Retry-After (seconds) header to be a good http citizen
+  config.enabled = ENV.fetch('MAINTENANCE_MODE', nil).present?
   config.retry_after = 3600
 end

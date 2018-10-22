5ab865d65cc8165852ec2698d66ab84cb7f548b6
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-10-22 20:54:27 +0100

    Fix env key not found error
    The case worker seeds for application
    users (ccr/cclf) rely on an envar api
    key that is not present on all environments.

--- db/seeds/case_workers.rb
+++ db/seeds/case_workers.rb
@@ -28,7 +28,7 @@ SeedHelper.find_or_create_caseworker!(
   password_env_var: 'CASE_WORKER_PASSWORD'
 )
 
-ccr_caseworker_api_key = ENV.fetch('CCR_CASEWORKER_API_KEY')
+ccr_caseworker_api_key = ENV.fetch('CCR_CASEWORKER_API_KEY', nil)
 if ccr_caseworker_api_key
   usr = User.find_by(email: 'ccr@example.com')
   usr.update(api_key: ccr_caseworker_api_key) if !usr.api_key.eql?(ccr_caseworker_api_key)
@@ -44,7 +44,7 @@ SeedHelper.find_or_create_caseworker!(
   password_env_var: 'CASE_WORKER_PASSWORD'
 )
 
-cclf_caseworker_api_key = ENV.fetch('CCLF_CASEWORKER_API_KEY')
+cclf_caseworker_api_key = ENV.fetch('CCLF_CASEWORKER_API_KEY', nil)
 if cclf_caseworker_api_key
   usr = User.find_by(email: 'cclf@example.com')
   usr.update(api_key: cclf_caseworker_api_key) if !usr.api_key.eql?(cclf_caseworker_api_key)

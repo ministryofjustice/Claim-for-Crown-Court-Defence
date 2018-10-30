bb0f9a7723c24355116cf2f2b071a62db6c9d79b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-10-30 16:19:49 +0000

    Extend the API offence entity
    Remove the ~00 suffix from Offence.unique_codes
    
    This allows us to store near identical codes and send identical ones
    to CCR
    
    e.g
    Scheme 10: Murder_1.1    => Murder_1.1
    Scheme 11: Murder_1.1~11 => Murder_1.1

--- app/interfaces/api/entities/ccr/offence.rb
+++ app/interfaces/api/entities/ccr/offence.rb
@@ -2,10 +2,18 @@ module API
   module Entities
     module CCR
       class Offence < API::Entities::BaseEntity
-        expose :unique_code
+        expose :unique_code_check, as: :unique_code
         expose :offence_class,
                if: ->(instance, _opts) { instance.scheme_nine? },
                using: API::Entities::CCR::OffenceClass
+
+        private
+
+        def unique_code_check
+          suffix = object.unique_code.match(/(~\d{2})/)
+          return object.unique_code unless suffix.present?
+          object.unique_code.sub(suffix[1], '')
+        end
       end
     end
   end

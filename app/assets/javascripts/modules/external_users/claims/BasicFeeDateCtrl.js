3a8a702efa712e296902c6b54dff2d1bb644520b
David Plews (ClearGif) <freelance@cleargif.co.uk>
2018-11-02 16:06:41 +0000

    Control `remove` + `add more` links

--- app/assets/javascripts/modules/external_users/claims/BasicFeeDateCtrl.js
+++ app/assets/javascripts/modules/external_users/claims/BasicFeeDateCtrl.js
@@ -2,13 +2,17 @@ moj.Modules.BasicFeeDateCtrl = {
   el: '.fx-date-controller',
   init: function () {
     this.$el = $(this.el);
+    this.loadState();
     this.bindEvents();
+
+  },
+  loadState: function () {
+    this.setAddLinkState();
   },
   bindEvents: function () {
     var self = this;
     this.$el.on('cocoon:after-insert', function (e, el) {
       self.setAddLinkState();
-
     });
 
     this.$el.on('cocoon:after-remove', function (e, el) {
@@ -16,7 +20,7 @@ moj.Modules.BasicFeeDateCtrl = {
     });
   },
   setAddLinkState: function () {
-    if(this.$el.find('.fee-dates').length){
+    if(this.$el.find('.fee-dates:visible').length){
       this.$el.find('.add_fields').hide();
       return;
     }

e781aae217c961dd7a5694a4177d29cb3d01664d
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-11-06 14:01:42 +0000

    Rename method to reflect new pattern

--- app/interfaces/api/entities/ccr/defendant.rb
+++ app/interfaces/api/entities/ccr/defendant.rb
@@ -6,7 +6,7 @@ module API
         expose :first_name
         expose :last_name
         expose :date_of_birth
-        expose :representation_orders_with_earliest_first,
+        expose :earliest_representation_order,
                using: API::Entities::CCR::RepresentationOrder,
                as: :representation_orders
 
@@ -16,7 +16,7 @@ module API
           object.claim.defendants.unscope(:order).order(created_at: :asc)&.first == object || false
         end
 
-        def representation_orders_with_earliest_first
+        def earliest_representation_order
           object.representation_orders.unscope(:order).order(representation_order_date: :asc).take(1)
         end
       end

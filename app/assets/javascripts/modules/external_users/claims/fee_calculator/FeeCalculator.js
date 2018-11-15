d90b03a3ac76a3ecaa5be5d1afb3d801af7fb2bd
David Plews (ClearGif) <freelance@cleargif.co.uk>
2018-11-15 10:44:41 +0000

    Refactor to init submodules on `Module.FeeCalculator`

--- app/assets/javascripts/modules/external_users/claims/fee_calculator/FeeCalculator.js
+++ app/assets/javascripts/modules/external_users/claims/fee_calculator/FeeCalculator.js
@@ -1,11 +1,21 @@
-$(document).ready( function() {
-  (function(){
-    'use strict';
+(function(exports, $) {
+  var Modules = exports.Modules.FeeCalculator || {};
 
-    for (var x in moj.Modules.FeeCalculator) {
-      if (typeof moj.Modules.FeeCalculator[x].init === 'function') {
-        moj.Modules.FeeCalculator[x].init();
-      }
+  // This init is called by the MOJ core js
+  Modules.init = function() {
+
+    // A bit rudimentary but here
+    // you init the submodules.
+    // We might want to look for a data-*="true"
+    // to activate it
+    //
+    if(this.UnitPrice){
+      this.UnitPrice.init();
     }
-  }());
-});
+    if(this.GraduatedPrice){
+      this.GraduatedPrice.init();
+    }
+  };
+
+  exports.Modules.FeeCalculator = Modules;
+}(moj, jQuery));

3f72985c32ce8e7ccecc51b5773343a71f5cb6db
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-11-23 12:13:37 +0000

    update deleted at to disable ddl and set concurrent

--- db/migrate/20181123101919_add_deleted_at_index_to_claims.rb
+++ db/migrate/20181123101919_add_deleted_at_index_to_claims.rb
@@ -1,5 +1,7 @@
 class AddDeletedAtIndexToClaims < ActiveRecord::Migration[5.0]
+  self.disable_ddl_transaction!
+
   def change
-    add_index :claims, :deleted_at
+    add_index :claims, :deleted_at, algorithm: :concurrently
   end
 end

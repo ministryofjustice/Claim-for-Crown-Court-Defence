622bec2539ef606d1dcc14e01296d1952a581c57
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-11-28 23:14:34 +0000

    Add advocate interim claim api smoke test
    Was missing.

--- spec/support/api/claims/base_claim_test.rb
+++ spec/support/api/claims/base_claim_test.rb
@@ -159,4 +159,17 @@ class BaseClaimTest
       "mileage_rate_id": 1
     }
   end
+
+  def scheme_date_for(text)
+    case text.downcase.strip
+      when 'scheme 11' then
+        Settings.agfs_scheme_11_release_date.strftime
+      when 'scheme 10' || 'post agfs reform' then
+        Settings.agfs_fee_reform_release_date.strftime
+      when 'lgfs' then
+        "2016-04-01"
+      else
+        "2016-01-01"
+    end
+  end
 end

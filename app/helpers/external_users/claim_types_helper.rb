3715d58ce48e453dc821ba8fc3c84549f98f4cbb
David Plews (ClearGif) <freelance@cleargif.co.uk>
2018-12-13 15:12:55 +0000

    claim_type_page_header conditions
    thanks @colinbruce

--- app/helpers/external_users/claim_types_helper.rb
+++ app/helpers/external_users/claim_types_helper.rb
@@ -13,9 +13,9 @@ module ExternalUsers
     end
 
     def claim_type_page_header(user)
-      if user.persona.has_roles?['advocate']
+      if user.persona.roles.eql?(['advocate'])
         t('.page_title_advocate')
-      elsif user.persona.has_roles?['litigator']
+      elsif user.persona.roles.eql?(['litigator'])
         t('.page_title_litigator')
       else
         t('.page_title_generic')

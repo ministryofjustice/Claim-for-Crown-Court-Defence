a545acc22732f2851fb4a47af84b742de80ba2ad
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-12-17 14:18:12 +0000

    Prevent duplicate errors being added to object

--- app/presenters/error_presenter.rb
+++ app/presenters/error_presenter.rb
@@ -40,6 +40,7 @@ class ErrorPresenter
         short_message = generate_standard_short_message(fieldname, error)
         api_message = generate_standard_api_message(fieldname, error)
       end
+      next if @error_details[fieldname] && @error_details[fieldname][0].long_message.eql?(long_message)
       @error_details[fieldname] = ErrorDetail.new(fieldname,
                                                   long_message,
                                                   short_message,

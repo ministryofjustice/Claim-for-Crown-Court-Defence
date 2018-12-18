49d136197ecf2c6b92dee55923496ca8fe7476a4
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-12-18 16:31:21 +0000

    Update fixed fee SitePrism section

--- features/page_objects/sections/fixed_fee_section.rb
+++ features/page_objects/sections/fixed_fee_section.rb
@@ -2,13 +2,14 @@ class FixedFeeSection < SitePrism::Section
   sections :checklist, '.multiple-choice' do
     element :label, 'label'
     element :input, 'input'
-
   end
-  section :adjourned_appeals_committal_breaches, FeeSection, ".fixed-fee-group"
-  section :appeals_to_the_crown_court_against_sentence, FeeSection, ".fixed-fee-group"
-  section :number_of_cases_uplift, FeeSection, ".fixed-fee-group"
-  section :number_of_defendants_uplift, FeeSection, ".fixed-fee-group"
-  section :standard_appearance_fee, FeeSection, ".fixed-fee-group"
+
+  section :adjourned_appeals_committal_breaches, FeeSection, "#adjourned-appeals-committals-and-breaches > .fixed-fee-group"
+  section :appeals_to_the_crown_court_against_sentence, FeeSection, "#appeals-to-the-crown-court-against-sentence > .fixed-fee-group"
+  section :number_of_cases_uplift, FixedFeeCaseNumbersSection, "#number-of-cases-uplift > .fixed-fee-group"
+  section :number_of_defendants_uplift, FeeSection, "#number-of-defendants-uplift > .fixed-fee-group"
+  section :standard_appearance_fee, FeeSection, "#standard-appearance-fee > .fixed-fee-group"
+  section :elected_case_not_proceeded, FeeSection, "#elected-case-not-proceeded > .fixed-fee-group"
 
   def checklist_labels
     checklist.map { |item| item.label.text if item.has_label? }

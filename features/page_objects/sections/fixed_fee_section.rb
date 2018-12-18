aeefde143a047e31498a310d4ab748a7fa8da5af
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-12-18 12:23:33 +0000

    Update the fixed_fee section of siteprism
    This reflects the new layout of the fixed fee pages in agfs

--- features/page_objects/sections/fixed_fee_section.rb
+++ features/page_objects/sections/fixed_fee_section.rb
@@ -2,16 +2,29 @@ class FixedFeeSection < SitePrism::Section
   sections :checklist, '.multiple-choice' do
     element :label, 'label'
     element :input, 'input'
+
   end
+  section :adjourned_appeals_committal_breaches, FeeSection, ".fixed-fee-group"
+  section :appeals_to_the_crown_court_against_sentence, FeeSection, ".fixed-fee-group"
+  section :number_of_cases_uplift, FeeSection, ".fixed-fee-group"
+  section :number_of_defendants_uplift, FeeSection, ".fixed-fee-group"
+  section :standard_appearance_fee, FeeSection, ".fixed-fee-group"
 
   def checklist_labels
     checklist.map { |item| item.label.text if item.has_label? }
   end
 
-
   def toggle(label)
     checklist.each do |item|
       item.label.click if item.label.text.match?(Regexp.new(label, true))
     end
+  end
+
+  def set_quantity(fee, value = 1)
+    fee_block_for(fee).quantity.set value
+  end
+
+  def fee_block_for(fee)
+    send(fee.downcase.gsub(/ /, '_').to_sym)
   end
 end

910914db2f034cfe24e3a7641e42d48c2849112e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-12-18 16:47:26 +0000

    Add a quantity hint to SitePrism pages - FeeSection - FixedFeeCaseNumber

--- features/page_objects/sections/fee_section.rb
+++ features/page_objects/sections/fee_section.rb
@@ -1,5 +1,6 @@
 class FeeSection < SitePrism::Section
   element :quantity, "input.quantity"
+  element :quantity_hint, ".quantity_wrapper span.form-hint"
   element :rate, "input.rate"
   element :add_dates, ".dates-wrapper .add_fields"
   element :total, "input.total"
--- features/page_objects/sections/fixed_fee_case_numbers_section.rb
+++ features/page_objects/sections/fixed_fee_case_numbers_section.rb
@@ -1,5 +1,6 @@
 class FixedFeeCaseNumbersSection < SitePrism::Section
   element :quantity, "input.quantity"
+  element :quantity_hint, ".quantity_wrapper span.form-hint"
   element :rate, "input.rate"
   element :case_numbers, "input.js-fixed-fee-case-numbers"
 end

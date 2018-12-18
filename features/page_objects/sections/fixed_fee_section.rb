075fef25b9ddace8e1776f96b8405bc3755ce3d4
ColinBruce <colin.bruce@digital.justice.gov.uk>
2018-12-18 16:47:57 +0000

    Add appeal against conviction to FxedFee SitePrism section

--- features/page_objects/sections/fixed_fee_section.rb
+++ features/page_objects/sections/fixed_fee_section.rb
@@ -6,6 +6,7 @@ class FixedFeeSection < SitePrism::Section
 
   section :adjourned_appeals_committal_breaches, FeeSection, "#adjourned-appeals-committals-and-breaches > .fixed-fee-group"
   section :appeals_to_the_crown_court_against_sentence, FeeSection, "#appeals-to-the-crown-court-against-sentence > .fixed-fee-group"
+  section :appeals_to_the_crown_court_against_conviction, FeeSection, "#appeals-to-the-crown-court-against-conviction > .fixed-fee-group"
   section :number_of_cases_uplift, FixedFeeCaseNumbersSection, "#number-of-cases-uplift > .fixed-fee-group"
   section :number_of_defendants_uplift, FeeSection, "#number-of-defendants-uplift > .fixed-fee-group"
   section :standard_appearance_fee, FeeSection, "#standard-appearance-fee > .fixed-fee-group"

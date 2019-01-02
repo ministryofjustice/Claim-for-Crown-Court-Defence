7ac6c344d95caf015390fee8e9e9e4f170783fd9
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-01-02 16:40:40 +0000

    Tidy up

--- spec/factories/fee_schemes.rb
+++ spec/factories/fee_schemes.rb
@@ -19,14 +19,9 @@ FactoryBot.define do
 
     trait :agfs_eleven do
       start_date Date.new(2019, 01, 01).beginning_of_day
-      # start_date "2018-10-01 00:00:00"
       end_date nil
       version 11
     end
-
-    # see doc for actual LGFS start dates
-    # https://docs.google.com/document/d/12bQF1eik5-6Avss3oFtFsvuRfwjzLmj2KpcOcSMGjik
-    # Note: currently using scheme 8 as scheme 9 was reversed
 
     # scheme 8 (default)
     # TODO: current seeds for LGFS fee schemes represent scheme 8 as 9
@@ -36,11 +31,5 @@ FactoryBot.define do
       start_date Date.new(2014, 03, 20).beginning_of_day
       version 9
     end
-
-    # trait :lgfs_nine do
-    #   name 'LGFS'
-    #   start_date '2015-07-01 00:00:00'
-    #   version 9
-    # end
   end
 end

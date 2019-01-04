b89fd3868f974ca3cfaf0bbaa47b8b46ddc543c1
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-01-04 12:57:01 +0000

    Make basic fee mappings available to fee calculator services
    In order to distinguish between different basic fee
    types while still mapping to the same API fee types
    I have extended the basic fee adapter to behave
    much like the fixed and misc fee adapters.
    
    This requires the basic fee adapter to be instantiable
    both with a claim object and without one, since for fee
    calculation we just want to reuse existing functionality
    of the fee calculation services related to fee type mappings.
    At the same time we want to preserve the functionality of
    this adapter for DI purposes.

--- app/services/ccr/fee/basic_fee_adapter.rb
+++ app/services/ccr/fee/basic_fee_adapter.rb
@@ -27,10 +27,22 @@ module CCR
     class BasicFeeAdapter < SimpleBillAdapter
       acts_as_simple_bill bill_type: 'AGFS_FEE', bill_subtype: 'AGFS_FEE'
 
+      def initialize(object = nil)
+        super(object) if object
+      end
+
+      def mappings
+        @mappings ||= fee_types.each_with_object({}) do |fee_type_unique_code, mappings|
+          mappings[fee_type_unique_code.to_sym] = { bill_type: bill_type, bill_subtype: bill_subtype }
+        end
+      end
+
       def claimed?
         filtered_fees.any? do |f|
           f.amount&.positive? || f.quantity&.positive? || f.rate&.positive?
         end
+      rescue NameError
+        raise ArgumentError, 'Instantiate with claim object to use this method'
       end
 
       private

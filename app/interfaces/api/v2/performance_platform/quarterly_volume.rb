deb1b268e14ceead6bc710b2ce64c83703e05a2f
colinbruce <colin.bruce@digital.justice.gov.uk>
2019-01-05 23:26:52 +0000

    Add placeholder API endpoint

---
+++ app/interfaces/api/v2/performance_platform/quarterly_volume.rb
@@ -0,0 +1,32 @@
+module API
+  module V2
+    module PerformancePlatform
+      class QuarterlyVolume < Grape::API
+        resource :performance_platform, desc: 'Performance Platform' do
+          resource :quarterly_volume do
+            desc 'Calculate totals for quarterly volume performance platform report'
+            params do
+              optional :api_key, type: String, desc: I18n.t('api.v2.generic.params.api_key')
+              optional :start_date,
+                       type: String,
+                       desc: I18n.t('api.v2.performance_platform.quarterly_volume.start_date')
+              optional :month_1_usd_value,
+                       type: String,
+                       desc: I18n.t('api.v2.performance_platform.quarterly_volume.value', month: 'first')
+              optional :month_2_usd_value,
+                       type: String,
+                       desc: I18n.t('api.v2.performance_platform.quarterly_volume.value', month: 'second')
+              optional :month_3_usd_value,
+                       type: String,
+                       desc: I18n.t('api.v2.performance_platform.quarterly_volume.value', month: 'third')
+            end
+            get do
+              results = { key: 'value' }
+              present JSON.parse(results.to_json, object_class: OpenStruct)
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/api/v2/performance_platform/quarterly_volume_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe API::V2::PerformancePlatform::QuarterlyVolume do
+  include Rack::Test::Methods
+  include ApiSpecHelper
+  include DatabaseHousekeeping
+  include ActiveSupport::Testing::TimeHelpers
+
+  let(:case_worker_admin) { create(:case_worker, :admin) }
+  let(:external_user) { create(:external_user) }
+  let(:params) { default_params }
+  let(:default_params) { { api_key: api_key, start_date: start_date, value_1: value_1, value_2: value_2, value_3: value_3 } }
+  let(:start_date) { Date.new(2018, 1, 1) }
+  let(:value_1) { 1000 }
+  let(:value_2) { 2000 }
+  let(:value_3) { 3000 }
+
+  def do_request
+    get '/api/performance_platform/quarterly_volume', params, format: :json
+  end
+
+  describe 'GET quarterly_volume' do
+
+    before { do_request }
+
+    context 'when accessed by a CaseWorker' do
+      let(:api_key) { case_worker_admin.user.api_key }
+
+      it 'returns success' do
+        expect(last_response).to be_ok
+      end
+    end
+
+    context 'when accessed by an user that has no permissions' do
+      let(:api_key) { external_user.user.api_key }
+
+      it 'returns unauthorised' do
+        expect(last_response).to be_unauthorized
+        expect(last_response.body).to include('Unauthorised')
+      end
+    end
+  end
+end

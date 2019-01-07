1d9c64bb4b0de37086180d365f065f31f74ff661
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-01-07 09:14:26 +0000

    TEMP: Run cron on Monday and Tuesday for testing

--- scheduled_tasks/performance_platform_channel_task.rb
+++ scheduled_tasks/performance_platform_channel_task.rb
@@ -1,11 +1,11 @@
 # https://github.com/ssoroka/scheduler_daemon for help
 class PerformancePlatformChannelTask < Scheduler::SchedulerTask
-  cron '40 3 * * 1' # 3:40 on Monday
+  cron '40 3 * * 1,2' # 3:40 on Monday
   class ReportNotActivated < RuntimeError; end
   def run
     raise ReportNotActivated unless ENV['PERF_PLAT_TBC_TOKEN']
     log('Performance Platform - Channel Task started')
-    tbc = Reports::PerformancePlatform::TransactionsByChannel.new(Date.today - 7.days)
+    tbc = Reports::PerformancePlatform::TransactionsByChannel.new(Date.today.beginning_of_week - 7.days)
     tbc.populate_data
     tbc.publish!
   rescue ReportNotActivated

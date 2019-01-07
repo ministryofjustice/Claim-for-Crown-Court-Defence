794de13269adf9e8f773dfe714357f383fe4da68
colinbruce <colin.bruce@digital.justice.gov.uk>
2019-01-07 22:01:04 +0000

    extend fake API return
    Make it clearer what could be shown and make it clearer what needs to be generated
    and where.

--- app/interfaces/api/v2/performance_platform/quarterly_volume.rb
+++ app/interfaces/api/v2/performance_platform/quarterly_volume.rb
@@ -24,8 +24,27 @@ module API
               # create stats_report record then display it
               # this can be deleted by a successful post
               # or replaced by a re-generation of this end point
-              results = { key: 'value' }
-              present JSON.parse(results.to_json, object_class: OpenStruct)
+              results = {
+                month_one: {
+                  date: params[:start_date],
+                  usd_value: params[:month_1_usd_value],
+                  gbp_value: '00.00'
+                },
+                month_two: {
+                  date: params[:start_date].to_date + 1.month,
+                  usd_value: params[:month_2_usd_value],
+                  gbp_value: '00.00'
+                },
+                month_three: {
+                  date: Date.parse(params[:start_date]) + 2.months,
+                  usd_value: params[:month_3_usd_value],
+                  gbp_value: '00.00'
+                },
+                total_quarter_cost: '00.00',
+                claim_count: '0000',
+                cost_per_transaction_quarter: '00.00'
+              }
+              present JSON.parse(results.to_json)
             end
           end
         end

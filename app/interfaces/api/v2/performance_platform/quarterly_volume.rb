a2292c09e09e9714c0da0047fb7112d8c14ebc42
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-01-08 15:48:14 +0000

    Add placeholder POST endpoint for PP QuarterlyVolume

--- app/interfaces/api/v2/performance_platform/quarterly_volume.rb
+++ app/interfaces/api/v2/performance_platform/quarterly_volume.rb
@@ -4,7 +4,6 @@ module API
       class QuarterlyVolume < Grape::API
         resource :performance_platform, desc: 'Performance Platform' do
           resource :quarterly_volume do
-            desc 'Calculate totals for quarterly volume performance platform report'
             params do
               optional :api_key, type: String, desc: I18n.t('api.v2.generic.params.api_key')
               optional :start_date,
@@ -20,6 +19,8 @@ module API
                        type: String,
                        desc: I18n.t('api.v2.performance_platform.quarterly_volume.value', month: 'third')
             end
+
+            desc 'Calculate totals for quarterly volume performance platform report'
             get do
               options = {
                 quarter_start: params[:start_date],
@@ -29,6 +30,24 @@ module API
               }
               results = Stats::QuarterlyVolumeGenerator.call(options)
               present JSON.parse(results.content.gsub(/=>/, ':').gsub(':nil,', ':null,'))
+            end
+          end
+
+          resource :quarterly_volume do
+            params do
+              optional :api_key, type: String, desc: I18n.t('api.v2.generic.params.api_key')
+              optional :start_date,
+                       type: String,
+                       desc: I18n.t('api.v2.performance_platform.quarterly_volume.start_date')
+            end
+            desc 'Submit calculated totals for quarterly volume performance platform report'
+            post do
+              # load results from tmp/s3
+              # error if previously generated files have not been saved
+              # parse results
+              # send results
+              result = { successful_upload: true }
+              present result.as_json
             end
           end
         end

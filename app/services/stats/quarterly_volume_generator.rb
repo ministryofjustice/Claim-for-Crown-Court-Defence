c316425eec942d4836bb7496de09ae9379aa4cf0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-01-08 10:54:22 +0000

    working stats generator for the Quarterly Volumes
    Still needs testing

--- app/services/stats/quarterly_volume_generator.rb
+++ app/services/stats/quarterly_volume_generator.rb
@@ -8,6 +8,8 @@ module Stats
 
     def initialize(options = {})
       @format = options.fetch(:format, DEFAULT_FORMAT)
+      @options = options.except(:format)
+      @gbp_total = 0.0
     end
 
     def call
@@ -20,17 +22,50 @@ module Stats
     attr_reader :format
 
     def generate_new_report
-      {
-        _id: 'xxxxx',
-        _timestamp: '2017-04-01T00:00:00+00:00',
-        cost_per_transaction_quarter: '0.5',
-        end_at: '2017-07-01T00:00:00+00:00',
-        period: 'quarter',
-        service: 'Crown Court Defence',
-        start_at: '2017-04-01T00:00:00+00:00',
-        total_cost_quarter: '1276.9',
-        transactions_per_quarter: '0.18'
+      result = base_hash
+      3.times do |int|
+        result.merge!(calculate_gbp_for_(int))
+      end
+      totals = {
+        total_quarter_cost: @gbp_total,
+        claim_count: count_digital_claims,
+        cost_per_transaction_quarter: @gbp_total / count_digital_claims
       }
+      result.merge!(totals)
+      result
+    end
+
+    def base_hash
+      {
+        total_quarter_cost: nil,
+        claim_count: nil,
+        cost_per_transaction_quarter: nil,
+        month_one: { date: nil, usd_value: nil, gbp_value: nil },
+        month_two: { date: nil, usd_value: nil, gbp_value: nil },
+        month_three: { date: nil, usd_value: nil, gbp_value: nil }
+      }
+    end
+
+    def calculate_gbp_for_(month_offset)
+      date = (start_date + month_offset.months).end_of_month
+      usd_value = @options[:"month_#{month_offset + 1}"]
+      gbp_value = Conversion::Currency.call(date, usd_value)
+      @gbp_total += gbp_value
+      { "month_#{number_to_text(month_offset + 1)}": { date: date, usd_value: usd_value, gbp_value: gbp_value } }
+    end
+
+    def number_to_text(value)
+      { '1' => 'one', '2' => 'two', '3' => 'three' }[value.to_s]
+    end
+
+    def start_date
+      @start_date ||= Date.parse(@options[:quarter_start])
+    end
+
+    def count_digital_claims
+      first = @start_date.beginning_of_day.to_s(:db)
+      last = @start_date.end_of_quarter.end_of_day.to_s(:db)
+      @count_digital_claims ||= Claim::BaseClaim.where(original_submission_date: first..last).count
     end
   end
 end

26facd575b92289ee80206822e2613540e722ce8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-01-08 10:54:58 +0000

    Update the quarterlyVolume endpoint
    - actually generate values
    - Better tests of the api now need a VCR cassette

--- app/interfaces/api/v2/performance_platform/quarterly_volume.rb
+++ app/interfaces/api/v2/performance_platform/quarterly_volume.rb
@@ -21,30 +21,14 @@ module API
                        desc: I18n.t('api.v2.performance_platform.quarterly_volume.value', month: 'third')
             end
             get do
-              # create stats_report record then display it
-              # this can be deleted by a successful post
-              # or replaced by a re-generation of this end point
-              results = {
-                month_one: {
-                  date: params[:start_date],
-                  usd_value: params[:month_1_usd_value],
-                  gbp_value: '00.00'
-                },
-                month_two: {
-                  date: params[:start_date].to_date + 1.month,
-                  usd_value: params[:month_2_usd_value],
-                  gbp_value: '00.00'
-                },
-                month_three: {
-                  date: Date.parse(params[:start_date]) + 2.months,
-                  usd_value: params[:month_3_usd_value],
-                  gbp_value: '00.00'
-                },
-                total_quarter_cost: '00.00',
-                claim_count: '0000',
-                cost_per_transaction_quarter: '00.00'
+              options = {
+                quarter_start: params[:start_date],
+                month_1: params[:month_1_usd_value],
+                month_2: params[:month_2_usd_value],
+                month_3: params[:month_3_usd_value]
               }
-              present JSON.parse(results.to_json)
+              results = Stats::QuarterlyVolumeGenerator.call(options)
+              present JSON.parse(results.to_json)['content']
             end
           end
         end

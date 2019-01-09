be3247514d6398c6b520434ffdf4548839eb6405
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-01-09 14:42:10 +0000

    Refactor quarterly volume
    remove some notes and reduce duplication

--- app/interfaces/api/v2/performance_platform/quarterly_volume.rb
+++ app/interfaces/api/v2/performance_platform/quarterly_volume.rb
@@ -2,6 +2,17 @@ module API
   module V2
     module PerformancePlatform
       class QuarterlyVolume < Grape::API
+        helpers do
+          def report
+            Stats::StatsReport.most_recent_by_type('quarterly_volume')
+          end
+
+          def quarterly_report
+            data = open(report.document_url).read
+            JSON.parse(data.gsub(/=>/, ':').gsub(':nil,', ':null,')).deep_symbolize_keys
+          end
+        end
+
         resource :performance_platform, desc: 'Performance Platform' do
           resource :quarterly_volume do
             params do
@@ -28,8 +39,9 @@ module API
                 month_2: params[:month_2_usd_value],
                 month_3: params[:month_3_usd_value]
               }
-              results = Stats::QuarterlyVolumeGenerator.call(options)
-              present JSON.parse(results.content.gsub(/=>/, ':').gsub(':nil,', ':null,'))
+              Stats::StatsReportGenerator.call('quarterly_volume', options)
+
+              present quarterly_report
             end
           end
 
@@ -42,11 +54,13 @@ module API
             end
             desc 'Submit calculated totals for quarterly volume performance platform report'
             post do
-              # load results from tmp/s3
-              # error if previously generated files have not been saved
-              # parse results
-              # send results
-              result = { successful_upload: true }
+              results = quarterly_report
+              start_of_quarter = Date.parse(results[:month_one][:date]).beginning_of_month
+              qvr = Reports::PerformancePlatform::QuarterlyVolume.new(start_of_quarter)
+              qvr.populate_data(results[:total_quarter_cost])
+              published = qvr.publish!
+              result = { successful_upload: JSON.parse(published)['status'] }
+              report.destroy
               present result.as_json
             end
           end

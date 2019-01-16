df9796dbcfda722e980dbbda1d6b3ec222e51037
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-01-16 22:39:53 +0000

    Need to check this with @colinbruce

--- app/validators/claim/advocate_claim_validator.rb
+++ app/validators/claim/advocate_claim_validator.rb
@@ -81,7 +81,7 @@ class Claim::AdvocateClaimValidator < Claim::BaseClaimValidator
   def validate_defendant_uplifts_fixed_fees
     return if @record.from_api? || !@record.fixed_fee_case?
     return unless defendant_uplifts_greater_than?(defendant_uplifts_fixed_fees_counts, number_of_defendants)
-    add_error(:base, 'defendant_uplifts_fixed_fees_mismatch')
+    add_error(:fixed_fee_4_quantity, 'defendant_uplifts_fixed_fees_mismatch')
   end
 
   def validate_defendant_uplifts_misc_fees

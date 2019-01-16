03043e83903f7f9230d031c138c57af305cce69d
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-01-16 22:20:43 +0000

    Instantiate eligble fixed fees for display
    Checkboxes require the objects to be built
    if thy do no already exist.

--- app/controllers/external_users/advocates/claims_controller.rb
+++ app/controllers/external_users/advocates/claims_controller.rb
@@ -6,13 +6,24 @@ class ExternalUsers::Advocates::ClaimsController < ExternalUsers::ClaimsControll
   private
 
   def build_nested_resources
-    @claim.fixed_fees.build if @claim.fixed_fees.none?
     @claim.build_interim_claim_info if @claim.interim_claim_info.nil?
+
+    build_fixed_fees if @claim.fixed_fee_case?
 
     %i[misc_fees expenses].each do |association|
       build_nested_resource(@claim, association)
     end
 
     super
+  end
+
+  def build_fixed_fees
+    # NOTE: the `reload` is !important. prevents duplicate fees being created during error flows
+    existing_fixed_fee_fee_type_ids = @claim.fixed_fees.reload.map(&:fee_type_id)
+
+    @claim.eligible_fixed_fee_types.each do |eligible_fee_type|
+      next if existing_fixed_fee_fee_type_ids.include?(eligible_fee_type.id)
+      @claim.fixed_fees.build(fee_type_id: eligible_fee_type.id)
+    end
   end
 end

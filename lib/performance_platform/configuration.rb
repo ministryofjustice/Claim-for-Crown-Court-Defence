930e69be287a446f54a9081aff3d6186a1cda517
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-01-17 10:05:36 +0000

    Update the configuration of performance platform root_url

--- lib/performance_platform/configuration.rb
+++ lib/performance_platform/configuration.rb
@@ -2,10 +2,12 @@
 
 module PerformancePlatform
   class Configuration
+    PERFORMANCE_PLATFORM_ENDPOINT = 'https://www.performance.service.gov.uk/data'
+
     attr_accessor :root_url, :service, :group
 
     def initialize
-      @root_url ||= root_url
+      @root_url ||= root_url || PERFORMANCE_PLATFORM_ENDPOINT
       @service ||= service
       @group ||= group
     end

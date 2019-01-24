f5c7425f561b06b7b6c2a23e644665a308bdd144
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-01-24 11:35:19 +0000

    Refactor custom validation matcher
    Add more useful failure messages and short success
    messages.

--- spec/support/matchers/validation_matchers.rb
+++ spec/support/matchers/validation_matchers.rb
@@ -3,31 +3,69 @@ require 'rspec/expectations'
 RSpec::Matchers.define :include_field_error_when do |options|
   options.assert_valid_keys :field, :other_field, :field_value, :other_field_value, :message, :translated_message, :translated_message_type
   match do |record|
-    record.send("#{options[:field]}=", options[:field_value])
-    record.send("#{options[:other_field]}=", options[:other_field_value])
+    set_options(options)
+    record.send("#{field}=", options[:field_value])
+    record.send("#{other_field}=", options[:other_field_value])
     record.valid?
-    result = record.errors[options[:field]].include?(options[:message]) if options.fetch(:message, nil).present?
-    result = translation_match?(options) if result && options.fetch(:translated_message, nil).present?
+    result = record.errors[field].include?(message) if message.present?
+    result = translation_match? if result && translated_message.present?
     result
   end
 
-  def translation_match?(options)
-    @translations ||= YAML.load_file("#{Rails.root}/config/locales/error_messages.en.yml") # lazy load translations
-    message_type = options[:translated_message_type] || 'short'
-    field = options[:field].to_s
+  def set_options(options)
+    @options = options
+  end
+
+  def options
+    @options
+  end
+
+  def translation_match?
     [
-      @translations.has_key?(field),
-      @translations[field].has_key?(options[:message]),
-      @translations[field][options[:message]][message_type].eql?(options[:translated_message])
+      translations.has_key?(field),
+      translations[field].has_key?(message),
+      actual_translated_message.eql?(translated_message)
     ].all?
   end
 
+  def actual_translated_message
+    translations.fetch(field, nil)&.fetch(message, nil)&.fetch(translated_message_type, nil)
+  end
+
+  def translations
+    @translations ||= YAML.load_file("#{Rails.root}/config/locales/error_messages.en.yml") # lazy load translations
+  end
+
+  def translated_message_type
+    @message_type ||= options[:translated_message_type] || 'short'
+  end
+
+  def field
+    @field ||= options[:field].to_s
+  end
+
+  def other_field
+    @other_field ||= options[:other_field].to_s
+  end
+
+  def message
+    @message ||= options.fetch(:message, nil)
+  end
+
+  def translated_message
+    @translated_message ||= options.fetch(:translated_message, nil)
+  end
+
   description do
-    "include error on #{options[:field]} when #{options[:field]}: #{options[:field_value]} and #{options[:other_field]}: #{options[:other_field_value]}"
+    "include error #{message} on #{field}"
   end
 
   failure_message do |record|
-    "expected #{record.errors.messages}} to include errors for #{options[:field]}"
+    msg = ""
+    msg += "expected valid: false\n got: true\n " if record.valid?
+    msg += "expected error: #{message} on #{field}\n got: #{record.errors[field]}\n" if message.present? && !record.errors[field].include?(message)
+    msg += "expected #{translated_message_type + ' '}translation: \"#{translated_message}\"\n got: \"#{actual_translated_message}\"" if translated_message.present? && !actual_translated_message.eql?(translated_message)
+    msg
   end
 
   failure_message_when_negated do |record|

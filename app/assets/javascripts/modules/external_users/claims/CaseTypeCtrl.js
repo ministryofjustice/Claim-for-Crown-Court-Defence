645265832b29c930228e5144c7361a0351bae45a
David Plews (ClearGif) <freelance@cleargif.co.uk>
2019-01-24 12:53:17 +0000

    Modules.CaseTypeCtrl + Styles
    A page controller to control the page interactions
    
    - Autocomplete init
    - Controlling the visibility for dates: trial, re-trial & cracked trial dates
    - Updated page to v2 stylesheets
    - Updated the nested date views + a few translations
    - Removed the FE flicker where dates are shown on load, but then hidden

---
+++ app/assets/javascripts/modules/external_users/claims/CaseTypeCtrl.js
@@ -0,0 +1,51 @@
+moj.Modules.CaseTypeCtrl = {
+  els: {
+    requiresCrackedDates: '#cracked-trial-dates',
+    requiresRetrialDates: '#retrial-dates',
+    requiresTrialDates: '#trial-dates',
+    fxAutocomplete: '.fx-autocomplete'
+  },
+
+  actions: {
+    requiresTrialDates: function(param, context) {
+      context.toggle(context.els.requiresTrialDates, param);
+    },
+    requiresRetrialDates: function(param, context) {
+      context.toggle(context.els.requiresRetrialDates, param);
+    },
+    requiresCrackedDates: function(param, context) {
+      context.toggle(context.els.requiresCrackedDates, param);
+    }
+  },
+
+  toggle: function(element, param) {
+    return $(element).css('display', param ? 'block' : 'none');
+  },
+
+  init: function() {
+    var self = this;
+    $.subscribe('/onConfirm/claim_case_type_id-select/', function(e, data) {
+      // Loop over the data object and fire the
+      // methods as required, passing in the param
+      Object.keys(data).map(function(objectKey, index) {
+        if (typeof self.actions[objectKey] == 'function') {
+          self.actions[objectKey](data[objectKey], self);
+        }
+      });
+    });
+
+    // init the autocomplete elements
+    this.initAutocomplete();
+  },
+  initAutocomplete: function() {
+    var arr = $(this.els.fxAutocomplete);
+
+    $(this.els.fxAutocomplete).is(function(idx, el) {
+      moj.Helpers.Autocomplete.new('#' + el.id, {
+        showAllValues: false,
+        autoselect: true
+      });
+    });
+
+  }
+};

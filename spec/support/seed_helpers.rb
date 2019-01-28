66b637f0dcc2fa987602ebc246264fda95eafb80
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-01-28 11:28:21 +0000

    Add explicit scheme 9 date helpers

--- spec/support/seed_helpers.rb
+++ spec/support/seed_helpers.rb
@@ -3,11 +3,11 @@ require File.expand_path(Rails.root.join('db','seeds','fee_types','csv_seeder'))
 
 module SeedHelpers
   def seed_fee_schemes
-    # TODO: these should be in a separate seedfile that is just loaded here - currently they are in scheme_ten.rb seed file
+    # TODO: these should be in a separate seedfile that is just loaded here - currently they are in scheme_10.rb/scheme_11.rb seed file
     FeeScheme.find_or_create_by(name: 'LGFS', version: 9, start_date: Date.new(2014, 03, 20).beginning_of_day)
     FeeScheme.find_or_create_by(name: 'AGFS', version: 9, start_date: Date.new(2012, 04, 01).beginning_of_day, end_date: Date.new(2018, 03, 31).end_of_day)
-    FeeScheme.find_or_create_by(name: 'AGFS', version: 10, start_date: Date.new(2018, 04, 01).beginning_of_day, end_date: Date.new(2018, 12, 31).end_of_day)
-    FeeScheme.find_or_create_by(name: 'AGFS', version: 11, start_date: Date.new(2019, 01, 01).beginning_of_day)
+    FeeScheme.find_or_create_by(name: 'AGFS', version: 10, start_date: Settings.agfs_fee_reform_release_date.beginning_of_day, end_date: Settings.agfs_scheme_11_release_date - 1.day)
+    FeeScheme.find_or_create_by(name: 'AGFS', version: 11, start_date: Settings.agfs_scheme_11_release_date.beginning_of_day)
   end
 
   def seed_case_types

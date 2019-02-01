daa6500115909ae79e2db7c1cf4542ee7632375c
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-02-01 21:55:47 +0000

    Add supplementary claim feature test

---
+++ features/step_definitions/claim_summary_steps.rb
@@ -0,0 +1,19 @@
+Then(/^the following check your claim details should exist:$/) do |table|
+  expect(@claim_summary_page).to be_displayed
+  table.hashes.each do |row|
+    within @claim_summary_page.find("##{row['section']}") do
+        expect(page).to have_content(row['prompt'])
+        expect(page).to have_content(row['value'])
+    end
+  end
+end
+
+Then(/^the following check your claim fee details should exist:$/) do |table|
+  expect(@claim_summary_page).to be_displayed
+  table.hashes.each do |row|
+    within "##{row['section']} > table > tbody > tr:nth-child(#{row['row']})" do
+      expect(page).to have_content(row['prompt'])
+      expect(page).to have_content(row['value'])
+    end
+  end
+end

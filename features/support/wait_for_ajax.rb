ac62f337f1b1e0e0a2787617b411cc07bbb75601
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-02-02 13:15:41 +0000

    Amend wait for ajax

--- features/support/wait_for_ajax.rb
+++ features/support/wait_for_ajax.rb
@@ -1,8 +1,15 @@
 module WaitForAjax
-  def wait_for_ajax(wait: Capybara.default_max_wait_time)
-    Timeout.timeout(wait) do
-      loop until finished_all_ajax_requests?
+  # Do not use Timeout
+  # https://medium.com/doctolib/hunting-flaky-tests-2-waiting-for-ajax-bd76d79d9ee9
+  # https://medium.com/@adamhooper/in-ruby-dont-use-timeout-77d9d4e5a001
+  #
+  def wait_for_ajax(wait_time: Capybara.default_max_wait_time)
+    max_time = Capybara::Helpers.monotonic_time + wait_time
+    while Capybara::Helpers.monotonic_time < max_time
+      finished = finished_all_ajax_requests?
+      finished ? break : sleep(0.1)
     end
+    raise 'wait_for_ajax timeout' unless finished
   end
 
   def finished_all_ajax_requests?

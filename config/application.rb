56af58b42e229fba45866c193126ad63b8e1b8c7
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-02-03 22:30:42 +0000

    Remove unneceesary autoload paths
    Rails documenation on autoload_paths
    "Any existing second level directories called app/*/concerns in the application and engines."
    
    [autoload_paths](https://github.com/rails/rails/blob/c03bba4f1f03bad7dc034af555b7f2b329cf76f5/guides/source/autoloading_and_reloading_constants.md)

--- config/application.rb
+++ config/application.rb
@@ -43,13 +43,8 @@ module AdvocateDefencePayments
     config.ga_id = 'not_set'
 
     config.assets.enabled = true
-    # config.paths.add File.join('app', 'interfaces'), glob: File.join('**', '*.rb')
-    # config.paths.add File.join('app', 'services'), glob: File.join('**', '*.rb')
-    # config.autoload_paths += Dir[Rails.root.join('app', 'interfaces', '*')]
-    # config.autoload_paths += Dir[Rails.root.join('app', 'services', 'stats', '*')]
+
     config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('app','presenters','concerns')
-    config.autoload_paths << Rails.root.join('app','services','concerns')
 
     config.eager_load_paths << Rails.root.join('lib')
 

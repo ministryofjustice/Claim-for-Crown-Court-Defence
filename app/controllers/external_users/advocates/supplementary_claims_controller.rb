fabb118fa4737f3f94907584d04a5218aa471638
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-02-10 04:53:46 +0000

    Change supplementary claim misc fees to checkboxes
    Design request. Since advocate claims have a lot more
    misc fees they must remain as a select list.

--- app/controllers/external_users/advocates/supplementary_claims_controller.rb
+++ app/controllers/external_users/advocates/supplementary_claims_controller.rb
@@ -4,10 +4,21 @@ class ExternalUsers::Advocates::SupplementaryClaimsController < ExternalUsers::C
   resource_klass Claim::AdvocateSupplementaryClaim
 
   def build_nested_resources
-    %i[misc_fees expenses].each do |association|
+    build_misc_fees
+
+    %i[expenses].each do |association|
       build_nested_resource(@claim, association)
     end
 
     super
+  end
+
+  def build_misc_fees
+    existing_misc_fee_fee_type_ids = @claim.misc_fees.map(&:fee_type_id)
+
+    @claim.eligible_misc_fee_types.each do |eligible_fee_type|
+      next if existing_misc_fee_fee_type_ids.include?(eligible_fee_type.id)
+      @claim.misc_fees.build(fee_type_id: eligible_fee_type.id)
+    end
   end
 end

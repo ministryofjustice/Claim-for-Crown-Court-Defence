3248ae50d20e29f9fde3cdee949a2fd284bd1613
David Plews (ClearGif) <freelance@cleargif.co.uk>
2019-02-12 15:43:15 +0000

    Autocomplete features + cocoon init event binding
    - a page controller
    - init the autocompletes
    - event binding after the cocoon insert
    - removal of disbursements form the current code with typeahead

---
+++ app/assets/javascripts/modules/external_users/claims/DisbursementsCtrl.js
@@ -0,0 +1,38 @@
+moj.Modules.DisbursementsCtrl = {
+  els:{
+    fxAutocomplete: '.fx-autocomplete'
+  },
+  initAutocomplete: function() {
+    var arr = $(this.els.fxAutocomplete);
+
+    $(this.els.fxAutocomplete).is(function(idx, el) {
+      moj.Helpers.Autocomplete.new('#' + el.id, {
+        showAllValues: true,
+        autoselect: false
+      });
+    });
+  },
+  init: function() {
+    // init the auto complete
+    this.initAutocomplete();
+
+    // check the load state
+    // binding events to the dynamic select
+    // this.checkState();
+
+    // bind general page events
+    this.bindEvents();
+  },
+  bindEvents: function() {
+    var self = this;
+
+    $('#disbursements').on('cocoon:after-insert', function(e, element){
+      var elId = $(element).find('.fx-autocomplete').attr('id');
+      moj.Helpers.Autocomplete.new('#' + elId, {
+        showAllValues: true,
+        autoselect: false
+      });
+    });
+
+  }
+};

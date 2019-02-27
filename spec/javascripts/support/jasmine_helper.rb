5cd5e18633943953d1e9a30bca822967dcd3e2bb
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-02-27 13:48:09 +0000

    Switch jasmine test suite to using headless chrome
    PhantomJS is abandoned and has been removed
    from the feature tests already.
    
    See this [jasmine frwamork related](https://github.com/jasmine/jasmine-gem/issues/293) post

--- spec/javascripts/support/jasmine_helper.rb
+++ spec/javascripts/support/jasmine_helper.rb
@@ -8,11 +8,15 @@
 #   config.boot_files = lambda { ['/absolute/path/to/boot_dir/file.js'] }
 #end
 #
-#Example: prevent PhantomJS auto install, uses PhantomJS already on your path.
-# NOTE: travis has phantomjs pre-installed and on path so auto-install
-#       not needed (and can intermittently fail with travis success
-#       if install site not available)
+require "jasmine/runners/selenium"
+
 Jasmine.configure do |config|
-  config.prevent_phantom_js_auto_install = true
+  config.runner = lambda { |formatter, jasmine_server_url|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.headless!
+
+    webdriver = Selenium::WebDriver.for(:chrome, options: options)
+    Jasmine::Runners::Selenium.new(formatter, jasmine_server_url, webdriver, 50)
+  }
 end
 

7a724a73ade29389c27c1d1ad0279b4733354911
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-02-27 11:11:52 +0000

    Split the cucumber screenshot method
    This will allow cucumber on Circle CI to save a screenshot without
    opening it, while a save_and_open_screenshot step can still be used to
    debug locally

--- features/support/screenshot_helper.rb
+++ features/support/screenshot_helper.rb
@@ -1,11 +1,17 @@
 module ScreenshotHelper
    # selenium headless chrome solution for full screenshot
   def screenshot_and_open_image
+    file_path = screenshot_image
+    Launchy.open file_path
+  end
+
+  def screenshot_image(name = 'capybara-screenshot')
     window = Capybara.current_session.driver.browser.manage.window
     window.resize_to(*dimensions)
-    file_path = Rails.root.join("tmp/capybara/capybara-screenshot-#{Time.now.utc.iso8601.gsub('-', '').gsub(':', '')}.png").to_s
+    screenshot_name = "#{name}-#{Time.now.utc.iso8601.gsub('-', '').gsub(':', '')}.png"
+    file_path = Rails.root.join("tmp/capybara/#{screenshot_name}").to_s
     save_screenshot(file_path)
-    Launchy.open file_path
+    file_path
   end
 
   def dimensions

fd4fb39dbbf64e83573f17e1249d342d69f37d89
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-03-05 11:48:06 +0000

    Add offence `unique_code` to offences entity spec

--- spec/api/entities/offence_spec.rb
+++ spec/api/entities/offence_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe API::Entities::Offence do
   context 'when scheme nine' do
     let(:offence) { create(:offence, :with_fee_scheme, offence_class: create(:offence_class, :with_lgfs_offence )) }
 
-    it { expect(JSON.parse(subject.to_json).keys).to eq %w[id description offence_class_id offence_class] }
+    it { expect(JSON.parse(subject.to_json).keys).to eq %w[id description unique_code offence_class_id offence_class] }
   end
 
   context 'when scheme ten' do
@@ -15,7 +15,7 @@ RSpec.describe API::Entities::Offence do
              offence_band: create(:offence_band,
                                   offence_category: create(:offence_category, number: 6))) }
 
-    it { expect(JSON.parse(subject.to_json).keys).to eq %w[id description act_of_law offence_band] }
+    it { expect(JSON.parse(subject.to_json).keys).to eq %w[id description unique_code act_of_law offence_band] }
   end
 
   context 'when scheme eleven' do
@@ -24,6 +24,6 @@ RSpec.describe API::Entities::Offence do
              offence_band: create(:offence_band,
                                   offence_category: create(:offence_category, number: 6))) }
 
-    it { expect(JSON.parse(subject.to_json).keys).to eq %w[id description act_of_law offence_band] }
+    it { expect(JSON.parse(subject.to_json).keys).to eq %w[id description unique_code act_of_law offence_band] }
   end
 end

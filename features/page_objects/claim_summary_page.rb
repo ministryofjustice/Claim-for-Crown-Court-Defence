44f89e0e968e756b5182fd44bb24a07387c4088b
David Plews (ClearGif) <freelance@cleargif.co.uk>
2019-03-19 10:53:07 +0000

    fix page object selector

--- features/page_objects/claim_summary_page.rb
+++ features/page_objects/claim_summary_page.rb
@@ -1,7 +1,7 @@
 class ClaimSummaryPage < SitePrism::Page
   set_url "/external_users/claims/{claim_id}/summary"
 
-  element :continue, ".form-buttons > a.button-continue:nth-of-type(1)"
+  element :continue, ".form-buttons > a.button:nth-of-type(1)"
   element :change_case_details, "#case-details-section a.link-change:nth-of-type(1)"
   element :change_defendants, "#defendants-section a.link-change:nth-of-type(1)"
   element :change_expenses, "#expenses-section a.link-change:nth-of-type(1)"

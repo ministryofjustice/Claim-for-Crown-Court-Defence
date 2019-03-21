33c7564e8e46328665be694d7b6e21c0d6017184
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-03-21 13:21:39 +0000

    Removes old feature specs
    These tests were redundant and no longer being run.
    
    Makes sense to remove them.

--- old_features/step_definitions/shared/sorting_steps.rb
+++
@@ -1,8 +0,0 @@
-Then(/^I should see "(.*?)" in top cell of column with link "(.*?)"$/) do |cell_value, link_text|
-  within('.report') do
-    link = find_link link_text
-    column = last_xpath_index(link.path)
-    top_cell = find(:xpath, "./tbody/tr[1]/td[#{column}]")
-    expect(top_cell.text).to eq cell_value
-  end
-end
--- old_features/support/stub_s3_requests.rb
+++
@@ -1,3 +0,0 @@
-Before('@stub_s3_upload') do
-  stub_request(:put, /shorter_lorem\.docx/)
-end

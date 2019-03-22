d5dfc606c494dfb298ed8ad398e3bb53265ff29b
Will McBrien <william.mcbrien@digital.justice.gov.uk>
2019-03-22 13:27:50 +0000

    Removes old feature specs (#2684)
    These tests were redundant and no longer being run.
    
    Makes sense to remove them.

--- old_features/step_definitions/advocate_archived_claim_list_steps.rb
+++
@@ -1,9 +0,0 @@
-When(/^I visit the advocate archive$/) do
-  visit archived_external_users_claims_path
-end
-
-Then(/^I should not see non\-archived claims listed$/) do
-  Claims::StateMachine.dashboard_displayable_states.each do |state|
-    expect(page).not_to have_selector("tr.#{state}")
-  end
-end
--- old_features/step_definitions/fee_section_display_steps.rb
+++
@@ -1,7 +0,0 @@
-Then(/^I should( not)? see the (basic-fees|misc-fees|fixed-fees) section$/) do |negate, section_id|
-  if negate.present?
-    expect(page).to_not have_selector("##{section_id}")
-  else
-    expect(page).to have_selector("##{section_id}")
-  end
-end

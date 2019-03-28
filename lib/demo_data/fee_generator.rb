382bd79f288e322805a73675ae6805e3cd4a96b2
Katherine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2019-03-28 17:13:32 +0000

    Fix for generating demo data for litigator misc fees
    Remove workaround for total numericality error

--- lib/demo_data/fee_generator.rb
+++ lib/demo_data/fee_generator.rb
@@ -5,7 +5,11 @@ module DemoData
     #
     def initialize(claim, fee_type_class, fee_types = nil)
       @claim          = claim
-      @fee_types      = fee_types || fee_type_class.agfs
+      if @claim.agfs?
+        @fee_types      = fee_types || fee_type_class.agfs
+      else
+        @fee_types = Fee::MiscFeeType.lgfs.where(unique_code: %w[MICJA MICJP MIEVI MISPF])
+      end
       @codes_added    = []
       @fee_type_class = fee_type_class
       @fee_class      = derive_fee_class_from_fee_type_class
@@ -18,10 +22,18 @@ module DemoData
     private
 
     def add_fee
-      fee_type = validatable_fee_types.sample
-      while @codes_added.include?(fee_type.code)
-        fee_type = validatable_fee_types.sample
+      if @claim.agfs?
+        fee_type = validatable_agfs_fee_types.sample
+        while @codes_added.include?(fee_type.code)
+          fee_type = validatable_agfs_fee_types.sample
+        end
+      else
+        fee_type = @fee_types.sample
+        while @codes_added.include?(fee_type.code)
+          fee_type = @fee_types.sample
+        end
       end
+    
       @fee_class.create(claim: @claim, fee_type: fee_type, quantity: rand(1..10), rate: rand(25..75).round(2))
       @codes_added << fee_type.code
     end
@@ -34,10 +46,14 @@ module DemoData
     # as claim must have a matching number of defendants
     # and they are uncommon
     #
-    def validatable_fee_types
+    def validatable_agfs_fee_types
       @fee_types.select do |fee_type|
-        fee_type.calculated == true && !fee_type.defendant_uplift?
+          fee_type.calculated == true && !fee_type.defendant_uplift?
       end
+    end
+
+    def validatable_lgfs_misc_fee_types
+      @fee_types.select
     end
   end
 end

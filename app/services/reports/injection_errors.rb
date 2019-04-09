eff67e0bd9b469aff60d6ab32e34cfea7aa399c1
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-04-09 16:16:59 +0100

    Rubocop fix
    Adds new methods to construction of large SQL query so as to
    reduce line length to abide by rubocop and made the query more
    ameanable to change.

--- app/services/reports/injection_errors.rb
+++ app/services/reports/injection_errors.rb
@@ -27,37 +27,37 @@ module Reports
     def aggregrate_query
       %{SELECT
       CASE
-        WHEN obj->>'error' ILIKE '%Cannot create a bill of type%' THEN 'BILL_TYPE#CREATION_ERROR'
-        WHEN obj->>'error' ILIKE '%Bill Sub Type%not found in%' THEN 'BILL_TYPE#NOT_FOUND_ERROR'
-        WHEN obj->>'error' ILIKE '%Invalid scenario%for Bill Sub Type%' THEN 'BILL_TYPE#INVALID_SCENARIO_ERROR'
-        WHEN obj->>'error' ILIKE '%At least one Bill object MUST be included on a claim%' THEN 'CLAIM#MISSING_BILL_ERROR'
-        WHEN obj->>'error' ILIKE '%Offence object MUST not be NULL for a claim.%' THEN 'CLAIM#MISSING_OFFENCE_ERROR'
-        WHEN obj->>'error' ILIKE '%is a mandatory field%' THEN 'CLAIM#MISSING_MANDATORY_FIELD_ERROR'
-        WHEN obj->>'error' ILIKE '%Required field not entered%' THEN 'CLAIM#MISSING_MANDATORY_FIELD_ERROR'
-        WHEN obj->>'error' ILIKE '%A claim already exists for these case details%' THEN 'CLAIM#ALREADY_EXISTS_ERROR'
-        WHEN obj->>'error' ILIKE '%A case already exists for these case details%' THEN 'CLAIM#ALREADY_EXISTS_ERROR'
-        WHEN obj->>'error' ILIKE '%A claim with these details already exists in the system%' THEN 'CLAIM#ALREADY_EXISTS_ERROR'
-        WHEN obj->>'error' ILIKE '%Details of the entered case already exist in%' THEN 'CLAIM#ALREADY_EXISTS_ERROR'
-        WHEN obj->>'error' ILIKE '%First day of Trial%CANNOT come before Rep Order Date%' THEN 'CLAIM#FIRST_DAY_TRIAL_VALIDATION_ERROR'
-        WHEN obj->>'error' ILIKE '%Cannot calculate the fee%' THEN 'FEE#CALCULATION_ERROR'
-        WHEN obj->>'error' ILIKE '%Wasted Preparation Fee%' THEN 'FEE#WASTED_PREPARATION_FEE_ERROR'
-        WHEN obj->>'error' ILIKE '%No defendant found for Rep Order Number%' THEN 'REP_ORDER#DEFENDANT_NOT_FOUND'
-        WHEN obj->>'error' ILIKE '%Error retrieving defendant details for Rep Order Number%' THEN 'REP_ORDER#DEFENDANT_DETAILS_NOT_FOUND'
-        WHEN obj->>'error' ILIKE '%Expense Date Incurred%' THEN 'EXPENSE#DATE_INCURRED_ERROR'
-        WHEN obj->>'error' ILIKE '%The supplier account code%' THEN 'SUPPLIER_NUMBER#INVALID_ERROR'
-        WHEN obj->>'error' ILIKE '%Supplier Account Number cannot be empty%' THEN 'SUPPLIER_NUMBER#CANNOT_BE_EMPTY_ERROR'
-        WHEN obj->>'error' ILIKE '%VAT Amount % is too high for Net Amount supplied %' THEN 'NET_AMOUNT#INVALID_VAT_AMOUNT_ERROR'
-        WHEN obj->>'error' ILIKE '%Read timed out%' THEN 'CONNECTION#TIMEOUT_ERROR'
-        WHEN obj->>'error' ILIKE '%Failed: HTTP Error Code%' THEN 'CONNECTION#HTTP_ERROR'
-        WHEN obj->>'error' ILIKE '%Claim injection failed%' THEN 'GENERIC_FAILED_INJECTION_ERROR'
-        WHEN obj->>'error' ILIKE '%Unrecognized field%' THEN 'PARSING#INVALID_FIELD_ERROR'
-        WHEN obj->>'error' ILIKE '%Text%could not be parsed%' THEN 'PARSING#PARSING_ERROR'
-        WHEN obj->>'error' ILIKE '%The supplied JSON string is EMPTY%' THEN 'PARSING#PARSING_ERROR'
-        WHEN obj->>'error' ILIKE '%no protocol:%' THEN 'INTERNAL_ERROR'
-        WHEN obj->>'error' ILIKE '%A system exception has occurred%' THEN 'INTERNAL_ERROR'
-        WHEN obj->>'error' ILIKE '%An application error has occurred%' THEN 'INTERNAL_ERROR'
-        WHEN obj->>'error' ILIKE '%java.%' THEN 'INTERNAL_ERROR'
-        WHEN ((obj->>'error' = '') IS NOT FALSE) OR (obj->>'error' = 'nil') OR (obj->>'error' = 'null') THEN 'NO_ERROR_DESCRIPTION'
+        #{when_cond('%Cannot create a bill of type%', 'BILL_TYPE#CREATION_ERROR')}
+        #{when_cond('%Bill Sub Type%not found in%', 'BILL_TYPE#NOT_FOUND_ERROR')}
+        #{when_cond('%Invalid scenario%for Bill Sub Type%', 'BILL_TYPE#INVALID_SCENARIO_ERROR')}
+        #{when_cond('%At least one Bill object MUST be included on a claim%', 'CLAIM#MISSING_BILL_ERROR')}
+        #{when_cond('%Offence object MUST not be NULL for a claim.%', 'CLAIM#MISSING_OFFENCE_ERROR')}
+        #{when_cond('%is a mandatory field%', 'CLAIM#MISSING_MANDATORY_FIELD_ERROR')}
+        #{when_cond('%Required field not entered%', 'CLAIM#MISSING_MANDATORY_FIELD_ERROR')}
+        #{when_cond('%A claim already exists for these case details%', 'CLAIM#ALREADY_EXISTS_ERROR')}
+        #{when_cond('%A case already exists for these case details%', 'CLAIM#ALREADY_EXISTS_ERROR')}
+        #{when_cond('%A claim with these details already exists in the system%', 'CLAIM#ALREADY_EXISTS_ERROR')}
+        #{when_cond('%Details of the entered case already exist in%', 'CLAIM#ALREADY_EXISTS_ERROR')}
+        #{when_cond('%First day of Trial%CANNOT come before Rep Order Date%', 'CLAIM#FIRST_DAY_TRIAL_VALIDATION_ERROR')}
+        #{when_cond('%Cannot calculate the fee%', 'FEE#CALCULATION_ERROR')}
+        #{when_cond('%Wasted Preparation Fee%', 'FEE#WASTED_PREPARATION_FEE_ERROR')}
+        #{when_cond('%No defendant found for Rep Order Number%', 'REP_ORDER#DEFENDANT_NOT_FOUND')}
+        #{when_cond('%Error retrieving defendant details for Rep Order Number%', 'REP_ORDER#DEFENDANT_DETAILS_NOT_FOUND')}
+        #{when_cond('%Expense Date Incurred%', 'EXPENSE#DATE_INCURRED_ERROR')}
+        #{when_cond('%The supplier account code%', 'SUPPLIER_NUMBER#INVALID_ERROR')}
+        #{when_cond('%Supplier Account Number cannot be empty%', 'SUPPLIER_NUMBER#CANNOT_BE_EMPTY_ERROR')}
+        #{when_cond('%VAT Amount % is too high for Net Amount supplied %', 'NET_AMOUNT#INVALID_VAT_AMOUNT_ERROR')}
+        #{when_cond('%Read timed out%', 'CONNECTION#TIMEOUT_ERROR')}
+        #{when_cond('%Failed: HTTP Error Code%', 'CONNECTION#HTTP_ERROR')}
+        #{when_cond('%Claim injection failed%', 'GENERIC_FAILED_INJECTION_ERROR')}
+        #{when_cond('%Unrecognized field%', 'PARSING#INVALID_FIELD_ERROR')}
+        #{when_cond('%Text%could not be parsed%', 'PARSING#PARSING_ERROR')}
+        #{when_cond('%The supplied JSON string is EMPTY%', 'PARSING#PARSING_ERROR')}
+        #{when_cond('%no protocol:%', 'INTERNAL_ERROR')}
+        #{when_cond('%A system exception has occurred%', 'INTERNAL_ERROR')}
+        #{when_cond('%An application error has occurred%', 'INTERNAL_ERROR')}
+        #{when_cond('%java.%', 'INTERNAL_ERROR')}
+        #{no_error_description_catch}
         ELSE 'UNCATEGORIZED_ERROR'
       END as error_category,
       COUNT(*) as total
@@ -68,6 +68,15 @@ module Reports
 
     def where_conditions
       ['ia.succeeded = false', date_range_clause].compact.join(' AND ')
+    end
+
+    def when_cond(error_string, error_type)
+      "WHEN obj->>'error' ILIKE '#{error_string}' THEN '#{error_type}'"
+    end
+
+    def no_error_description_catch
+      "WHEN ((obj->>'error' = '') IS NOT FALSE) OR (obj->>'error' = 'nil') " \
+      "OR (obj->>'error' = 'null') THEN 'NO_ERROR_DESCRIPTION'"
     end
 
     def date_range_clause

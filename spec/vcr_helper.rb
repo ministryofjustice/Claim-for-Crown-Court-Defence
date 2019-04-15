c789c81ebe2a5324bc19be2a178bb42739675234
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-04-15 12:09:57 +0100

    Amend vcr configuration
    Config was in one place for specs with addendums
    for cucumber. Simpler and more consistent to
    store in allvcr config in one place `spec/vcr_helper`
    and require this for cucumber via `env.rb`.
    
    In addition ensure we are using the custom matcher,
    which was not being applied for cucumber tests as a
    result for faulty config.

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -1,8 +1,13 @@
 require 'vcr'
 
 VCR.configure do |c|
+  # place all cassettes in this directory
   c.cassette_library_dir = 'vcr/cassettes'
+
+  # use webmock to hook into and replay requests
   c.hook_into :webmock
+
+  # enable rspec :vcr tag for autonmatically named cassettes. needed??
   c.configure_rspec_metadata!
   c.default_cassette_options = {
     erb: true,
@@ -11,33 +16,49 @@ VCR.configure do |c|
     match_requests_on: [:method, VCR.request_matchers.uri_without_param(:api_key, :key)]
   }
 
-  # Enable VCR logging using, for example,
-  #  `VCR_DEBUG=1 rspec ./spec/controllers/external_users/claims_controller_spec.rb`
+  # Enable VCR logging using, for example:
+  # `VCR_DEBUG=1 rspec ./spec/controllers/external_users/claims_controller_spec.rb`
   c.debug_logger = File.open(c.cassette_library_dir + '/vcr_debug.log', 'w') if ENV['VCR_DEBUG']
 
-  # Ignore requests other than to the API endpoints, LAA fee calculator and google maps
+  # Ignore requests to
+  # - capybara-selenium
+  # - chrome browser/selenium requests to localhost port on which it runs
+  # Do not ignore requests to:
+  # - CCCD API endpoints, LAA fee calculator and google maps
+  #
   c.ignore_request do |request|
-    URI(request.uri).path == "/__identify__" ||
-    (!URI(request.uri).path.start_with?('/api/') &&
-      !URI(request.uri).path =~ /maps.googleapis.com/ &&
-      !URI(request.uri).path =~ /apilayer.net/)
+    uri = URI(request.uri)
+    [ uri.path == "/__identify__",
+      [
+        !uri.path.start_with?('/api/'),
+        !uri.path =~ /maps.googleapis.com/,
+        !uri.path =~ /apilayer.net/
+      ].all?,
+      [
+        uri.host.eql?('127.0.0.1'),
+        (9515..9525).include?(uri.port)
+      ].all?
+    ].any?
   end
 
+  # replace sensitive data in cassettes with placeholder and apply secrets on the fly
   c.filter_sensitive_data('<GOOGLE_API_KEY>') { Rails.application.secrets.google_api_key }
   c.filter_sensitive_data('<CURRENCY_API_KEY>') { Rails.application.secrets.currency_api_key }
+
+  # custom VCR request matcher to match request based on
+  # path and query but not host because laa-fee-calculator
+  # host could change and responses are path and query specific
+  #
+  c.register_request_matcher :path_query_matcher do |request_1, request_2|
+    uri_1 = URI(request_1.uri)
+    uri_2 = URI(request_2.uri)
+    [uri_1.path == uri_2.path, uri_1.query == uri_2.query].all?
+  end
 end
 
-# use `VCR_OFF=true cucumber|rspec` too turn off vcr
+# Turn off vcr from the command line, for example:
+# `VCR_OFF=true cucumber|rspec`
 VCR.turn_off!(:ignore_cassettes => true) if ENV['VCR_OFF']
-
-# custom VCR request matcher to match request based on
-# path and query but not host because laa-fee-calculator
-# host could change and responses are path and query specific
-path_query_matcher = lambda do |request_1, request_2|
-  uri_1 = URI(request_1.uri)
-  uri_2 = URI(request_2.uri)
-  [uri_1.path == uri_2.path, uri_1.query == uri_2.query].all?
-end
 
 # Create VCR cassettes for any specs with a :fee_calc_vcr tag
 # in the cassette library under a directory structure
@@ -46,7 +67,7 @@ RSpec.configure do |config|
   config.around(:each, [:fee_calc_vcr, :currency_vcr]) do |example|
     if VCR.turned_on?
       cassette = Pathname.new(example.metadata[:file_path]).cleanpath.sub_ext('').to_s
-      VCR.use_cassette(cassette, :record => :new_episodes, :match_requests_on => [:method, path_query_matcher]) do
+      VCR.use_cassette(cassette, :record => :new_episodes, :match_requests_on => [:method, :path_query_matcher]) do
         example.run
       end
     else

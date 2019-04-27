f01fdc81d9890b9c828e931400c29791eaf9c6ef
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-04-27 23:49:09 +0100

    Remove LAC1 doc type
    The LAC1 memo of conviction is no longer
    required from a business point of view. PM
    confirms!
    
    It was also causing havoc with feature
    tests and had conditional JS and ruby
    to prevent it from appearing unless it
    had a persisted value.

--- spec/factories/claim/deterministic_claim.rb
+++ spec/factories/claim/deterministic_claim.rb
@@ -39,7 +39,7 @@ FactoryBot.define do
     advocate_category { 'QC' }
     cms_number { 'CMS-12345' }
     additional_information { 'This is some important additional information.' }
-    evidence_checklist_ids { [1,2] }
+    evidence_checklist_ids { [1,3] }
     apply_vat { true }
 
     case_number { 'T20161234' }

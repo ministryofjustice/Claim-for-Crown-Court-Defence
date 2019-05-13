a3fda2b2630b742452e66ed13223750cf0288b13
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-05-13 10:51:42 +0100

    Amend Zendesk Stub inline with User-Agent change
    Move from 1.17.0 to 1.19.0 changes the User-Agent
    header and this broke the existing stub.
    
    I have amended the spec to make this more obvious
    when failing. Previously the stubbing mechanism
    resulted in an actual request being made and returning
    401 (unauthorized) because it was stubbing a request
    with specific headers (which then changed).
    
    Now the spec stubs all request to the Zendesk URI
    and the spec tests that a specific request was made.

--- spec/services/zendesk_sender_spec.rb
+++ spec/services/zendesk_sender_spec.rb
@@ -1,39 +1,42 @@
-require 'rails_helper'
-
 RSpec.describe ZendeskSender do
+  subject(:sender) { ZendeskSender.new(ticket_payload) }
+  let(:ticket_payload) { double('ticket_payload', subject: 'Bug report', description: 'event - outcome - email address', referrer: '/claims', user_agent: 'chrome') }
 
-  let(:ticket_payload) { double 'ticket_payload', subject: 'Bug report', description: 'event - outcome - email address', referrer: '/claims', user_agent: 'chrome' }
-  subject { ZendeskSender.new(ticket_payload) }
-
-  before(:each) do
+  before do
     allow(Rails).to receive(:host).and_return(double(env: 'test'))
-    stub_request(:post, %r{\Ahttps://.*ministryofjustice.zendesk.com/api/v2/tickets\z}).
-            with(
-              :body => "{\"ticket\":{\"subject\":\"Bug report\",\"description\":\"event - outcome - email address\",\"custom_fields\":[{\"id\":\"26047167\",\"value\":\"/claims\"},{\"id\":\"23757677\",\"value\":\"advocate_defence_payments\"},{\"id\":\"23791776\",\"value\":\"chrome\"},{\"id\":\"32342378\",\"value\":\"test\"}]}}",
-              :headers => {'Accept'=>'application/json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/json', 'User-Agent'=>'ZendeskAPI Ruby 1.17.0'}).
-            to_return(:status => 201, :body => "", :headers => {})
+    stub_request(:post, %r{\Ahttps://.*ministryofjustice.zendesk.com/api/v2/tickets\z})
   end
 
   describe '.send!' do
-    it 'instantiates a new ZendeskSender object, with a payload and calls the instance method #send!' do
-      expect(ZendeskSender).to receive(:new).with(ticket_payload).and_return(subject)
-      expect(subject).to receive(:send!)
-      ZendeskSender.send!(ticket_payload)
+    it 'calls #send!' do
+      expect(described_class).to receive(:new).with(ticket_payload).and_return(sender)
+      expect(sender).to receive(:send!)
+      described_class.send!(ticket_payload)
+    end
+  end
+
+  describe '#send!' do
+    let(:stubbed_request) do
+      stub_request(:post, 'https://ministryofjustice.zendesk.com/api/v2/tickets').
+        with(
+          :body => "{\"ticket\":{\"subject\":\"Bug report\",\"description\":\"event - outcome - email address\",\"custom_fields\":[{\"id\":\"26047167\",\"value\":\"/claims\"},{\"id\":\"23757677\",\"value\":\"advocate_defence_payments\"},{\"id\":\"23791776\",\"value\":\"chrome\"},{\"id\":\"32342378\",\"value\":\"test\"}]}}",
+          :headers => {
+            'Accept'=>'application/json',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Content-Type'=>'application/json',
+            'User-Agent'=> 'ZendeskAPI Ruby 1.19.0'
+          }
+        )
     end
 
-    describe '#send!' do
-      it 'creates a new Zendesk Ticket' do
-        expect(ZendeskAPI::Ticket).to receive(:create!)
-        subject.send!
-      end
+    it 'calls ZendeskAPI::Ticket.create!' do
+      expect(ZendeskAPI::Ticket).to receive(:create!)
+      sender.send!
+    end
 
-      it 'and a duck-typed ticket_payload is used via a common interface: #subject, #description, #referrer, #user_agent' do
-        expect(ticket_payload).to receive(:subject)
-        expect(ticket_payload).to receive(:description)
-        expect(ticket_payload).to receive(:referrer)
-        expect(ticket_payload).to receive(:user_agent)
-        subject.send!
-      end
+    it 'makes the expected request, with common and custom fields' do
+      sender.send!
+      expect(stubbed_request).to have_been_requested
     end
   end
 end

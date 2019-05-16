ffb1428335687a6d3e0c3340f2042161cae346a4
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-05-16 16:51:19 +0100

    Update Dockerfile inline with platforms k8s requirements
    Changelog:
    
      * Add tzinfo gem to handle timezones info in alpine
    
      * Use latest functional image.
        § Note using alpine 3.8 as pdftk package
        dependences not available in 3.9
        see https://bugs.alpinelinux.org/issues/10136
    
      * Add maintainer and fail fast instrutions.
        Good practice to specify maintainer although
        this could be changed to something more CCCD
        specific.
    
      * Fail early and print all commands to stdout
        useful for debugging and prevent swallowing errors
    
      * Add `file` runtime dependency.
        For paperclip file type spoof checking
    
      * Change app dir.
        Follow linux convention `/usr/src/app`
    
      * COPY Gemfile and bundle install separately
        For speed and efficient docker layering.
    
      * Precompile assets silently
        Suppress verbose output from asset precompilation
        as they are adding a lot of noise to Jenkins console
        output.
    
      * Add non-root, appuser
    
      * Give ownership of remaining app files to non-root user
    
      * Switch to appuser prior to docker run entrypoint call
        Use UID to change user, not name. See https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#how-to-adapt-to-the-pod-security-policies
        for reasons.
    
      * rename entrypoint script following conventions
    
      * Use apk --no-cache flag
        prevent needs to apk update or tidy up
    
      * use sh not bash in docker entrypoint as alpine does not
        come with bash preinstalled.
    
      * remove travis block as not longer used
    
      * Extend .dockeringore
        - implement recursive lookup for simplicity
        - exlclude features and vcr cassettes as not required
        - NOTE: spec folder needed for smoke test unless the
          smoke test client class is moved out
        - exclude common unrequired config and dev setup files

--- runtests.sh
+++ runtests.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
-# set variables. It's presumed the absence of these is causing
-# TRAVIS to fail
-if [ "$TRAVIS" = "true" ]; then
-  printf '\e[32mInfo: Loading Schema\e[0m\n'
-  bundle exec rake db:schema:load
-  bundle exec rake ci:test_suite
-  exit 0
-else
-  printf '\e[33mInfo: Executing smoke test\e[0m\n'
-  bundle exec rake db:migrate
-  bundle exec rake db:seed
-  bundle exec rake api:smoke_test
-fi
+
+printf '\e[33mExecuting smoke test\e[0m\n'
+bundle exec rake db:migrate
+bundle exec rake db:seed
+bundle exec rake api:smoke_test

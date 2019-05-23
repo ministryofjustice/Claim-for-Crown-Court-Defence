b06fe8fa90175a2ff977f4303e99f436e6677225
Katherine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2019-05-23 15:16:12 +0100

    re-add SNS code
    This got omitte from a re-base

--- app/services/notification_queue/aws_client.rb
+++ app/services/notification_queue/aws_client.rb
@@ -1,8 +1,12 @@
 module NotificationQueue
   class AwsClient
     def send!(claim)
-      client = Aws::SNS::Client.new(access_key_id: Settings.aws.access, secret_access_key: Settings.aws.secret)
-      arn = Settings.aws.submitted_topic_arn
+      client = Aws::SNS::Client.new(
+        access_key_id: Settings.aws.sns.access,
+        secret_access_key: Settings.aws.sns.secret,
+        region: Settings.aws.region
+      )
+      arn = Settings.aws.sns.submitted_topic_arn
       message = NotificationQueue::MessageTemplate.for_claim(arn, claim)
       client.publish(message)
       true

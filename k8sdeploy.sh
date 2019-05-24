1891846608c3d22c70b971f0bad6eb5d42cba8b3
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-05-24 14:13:54 +0100

    Add build and deploy scripts
    ```
    -# build, tag and push image (tagged with last commit shah)
    ./k8sbuild.sh
    
    -# deploy the image (tagged with last commit shah) to dev
    ./k8sdeploy.sh dev
    
    ```
    
    Amend kubernetes build and deploy script
    
     - Apply the service and ingress yaml
       files too. This ensures any changes
       to these files are also applied
       and avoids confusion when creating
       a new environment since they need
       applying in order to "spin-up" a fully
       functional/accessible environment.
    
    - Add and use `latest` tag to build and deploy
      Add additional tag to builds
      and have deploy use the latest
      branch tag rather than the shah.
    
      This is too avoid deploying unbuilt
      branches and to make the registry
      iamges tags more human readable.

---
+++ k8sdeploy.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+function _k8sdeploy() {
+  usage="k8sdeploy -- deploy image from current commit to an environment
+  Usage: ./k8sdeploy.sh environment
+  Where:
+    environment [dev|staging|api-sandbox]"
+
+  if [ $# -gt 1 ]
+  then
+    echo "$usage"
+    return 0
+  fi
+
+  case "$1" in
+    dev | staging | api-sandbox)
+      environment=$1
+      ;;
+    *)
+      echo "$usage"
+      return 0
+      ;;
+  esac
+
+  context='live-1'
+  component=app
+  current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+  current_version=$(git rev-parse $current_branch)
+  docker_registry=754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-get-paid/cccd
+  docker_image_tag=${docker_registry}:${component}-${current_version}
+
+  # get latest tag for branch
+  # DOES NOT WORK FOR JUST A DEPLOY WITH NO CHANGES TO deployment.yaml
+  # case $current_branch in
+  #   master)
+  #     docker_image_tag=${docker_registry}:${component}-latest
+  #     ;;
+  #   *)
+  #     docker_image_tag=${docker_registry}:${component}-${current_branch}-latest
+  #     ;;
+  # esac
+
+  kubectl config set-context ${context} --namespace=cccd-${environment}
+  kubectl config use-context ${context}
+
+  echo "--------------------------------------------------"
+  echo "Context: $context"
+  echo "Environment: $environment"
+  echo "Current branch: $current_branch"
+  echo "Docker image: $docker_image_tag"
+  echo "--------------------------------------------------"
+
+  kubectl set image -f kubectl_deploy/${environment}/deployment.yaml cccd-app=${docker_image_tag} --local -o yaml | kubectl apply -f -
+  kubectl apply \
+    -f kubectl_deploy/${environment}/service.yaml \
+    -f kubectl_deploy/${environment}/ingress.yaml
+
+}
+
+_k8sdeploy $@

0da989160e5110ed6dd7bfe2a88f9055e31d9543
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2019-05-29 15:08:55 +0100

    Non root docker v2 (#2787)
    * Update Dockerfile inline with platforms k8s requirements
    
    Changelog:
    
      * Add tzinfo gem to handle timezones info in alpine
    
      * Use latest functional image.
        § Note using alpine 3.8 as pdftk package
        dependencies not available in 3.9
        see https://bugs.alpinelinux.org/issues/10136
    
      * Add maintainer and fail fast instructions.
        Good practice to specify maintainer although
        this could be changed to something more CCCD
        specific.
    
      * Fail early and print all commands to stdout
        useful for debugging and prevent swallowing errors
    
      * Add `file` runtime dependency.
        For paperclip file type spoof checking
    
      * Change app dir.
        Follow linux convention `/usr/src/app`
    
      * COPY Gemfile and bundle install separately
        For speed and efficient docker layering.
    
      * Precompile assets silently
        Suppress verbose output from asset precompilation
        as they are adding a lot of noise to Jenkins console
        output.
    
      * Add non-root, appuser
    
      * Give ownership of remaining app files to non-root user
    
      * Switch to appuser prior to docker run entrypoint call
        Use UID to change user, not name. See https://user-guide.cloud-platform.service.justice.gov.uk/tasks.html#how-to-adapt-to-the-pod-security-policies
        for reasons.
    
      * rename entry-point script following conventions
    
      * Use apk --no-cache flag
        prevents needs to apk update or tidy up cache
    
      * use sh not bash in docker entry-point as alpine does not
        come with bash preinstalled.
    
      * remove travis block as not longer used
    
      * Extend .dockeringore
        - implement recursive lookup for simplicity
        - exclude features and VCR cassettes as not required
        - NOTE: spec folder needed for smoke test unless the
          smoke test client class is moved out
        - exclude common un-required config and dev setup files

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# last modified 17-05-2019
+set +ex
+
+# output gets written to /var/log/upstart/... with template-deploy
+#
+case ${DOCKER_STATE} in
+migrate)
+    printf '\e[33mINFO: executing rake db:migrate\e[0m\n'
+    bundle exec rake db:migrate
+    ;;
+seed)
+    printf '\e[33mINFO: executing rake db:seed\e[0m\n'
+    bundle exec rake db:seed
+    ;;
+reload)
+    printf '\e[33mINFO: executing rake db:create + db:reload\e[0m\n'
+    bundle exec rake db:create
+    bundle exec rake db:reload
+    ;;
+reseed)
+    printf '\e[33mINFO: executing rake db:reseed\e[0m\n'
+    bundle exec rake db:reseed
+    ;;
+esac
+
+set -ex
+
+# if REDIS_URL is not set then we start redis-server locally
+if [ -z ${REDIS_URL+x} ]; then
+  printf '\e[33mINFO: Starting redis-server daemon\e[0m\n'
+  redis-server --daemonize yes
+else
+  printf '\e[33mINFO: Using remote redis-server\e[0m\n'
+fi
+
+printf '\e[33mINFO: Starting sidekiq daemon\e[0m\n'
+bundle exec sidekiq -d
+
+printf '\e[33mINFO: Starting scheduler_daemon daemon\e[0m\n'
+bundle exec scheduler_daemon start
+
+printf '\e[33mINFO: Launching unicorn\e[0m\n'
+bundle exec unicorn -p 3000 -c config/unicorn.rb

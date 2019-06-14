2bb4aa713f13f930fd9e3a06e94427275abf1c42
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-06-14 09:57:53 +0100

    Update the stats_report model
    Include the new concern and replace the call with the new method

--- app/models/concerns/s3_headers.rb
+++ app/models/concerns/s3_headers.rb
@@ -4,8 +4,8 @@ module S3Headers
   module ClassMethods
     def s3_headers
       { s3_headers: {
-          'x-amz-meta-Cache-Control' => 'no-cache',
-          'Expires' => 3.months.from_now.httpdate
+        'x-amz-meta-Cache-Control' => 'no-cache',
+        'Expires' => 3.months.from_now.httpdate
       },
         s3_permissions: :private,
         s3_region: Settings.aws.s3.region }

b8665f6d946ee1c7a9268c8afe48de34caef20fb
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-06-14 09:57:07 +0100

    Extract the s3 header hash
    This is referenced in 4 models and should therefore be centralised

---
+++ app/models/concerns/s3_headers.rb
@@ -0,0 +1,14 @@
+module S3Headers
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def s3_headers
+      { s3_headers: {
+          'x-amz-meta-Cache-Control' => 'no-cache',
+          'Expires' => 3.months.from_now.httpdate
+      },
+        s3_permissions: :private,
+        s3_region: Settings.aws.s3.region }
+    end
+  end
+end

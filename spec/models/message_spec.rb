f68761f94cd1c601d3b701697ed6ee537752c845
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-06-14 11:33:38 +0100

    Test the headers use in message_spec

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -124,4 +124,23 @@ RSpec.describe Message, type: :model do
       expect(claim.state).to eq 'part_authorised'
     end
   end
+
+  describe '.s3_headers' do
+    subject { described_class.s3_headers[:s3_region] }
+
+    context 'when an aws region has been explicitly recorded in the settings' do
+      let(:fake_aws_region) { 'eu-west-49'}
+
+      before { allow(Settings.aws).to receive(:region).and_return(fake_aws_region) }
+
+      it { is_expected.to eql fake_aws_region }
+    end
+
+    context 'when an aws region has not been set' do
+
+      before { allow(Settings.aws).to receive(:region).and_return(nil) }
+
+      it { is_expected.to eql 'eu-west-1' }
+    end
+  end
 end

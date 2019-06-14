2deafb89924a3c02c7b0d2b8bd4b1eecddda98ec
ColinBruce <colin.bruce@digital.justice.gov.uk>
2019-06-14 18:57:19 +0100

    Temporarily change the slack channel
    While we are testing Kubernetes, output slack messages to a new,
    temporary channel

--- app/services/claims/injection_channel.rb
+++ app/services/claims/injection_channel.rb
@@ -2,6 +2,7 @@ module Claims
   class InjectionChannel
     def self.for(claim)
       return 'cccd_development' if claim.nil?
+      return 'cccd-k8s-injection' if Settings.aws&.sqs&.response_queue_url
       claim.agfs? ? 'cccd_ccr_injection' : 'cccd_cclf_injection'
     end
   end

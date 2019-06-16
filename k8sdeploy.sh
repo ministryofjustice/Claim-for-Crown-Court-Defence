8c8bc324f1756294c0b3828ce570c9f75c955d47
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-06-16 10:22:19 +0100

    Amend kubernetes deploy script
    Apply the service and ingress yaml
    files too. This ensures any changes
    to these files are also applied
    and avoids confusion when creating
    a new environment since they need
    applying in order to "spin-up" a fully
    functional/accessible environment.

--- k8sdeploy.sh
+++ k8sdeploy.sh
@@ -5,7 +5,7 @@ function _k8sdeploy() {
   Where:
     environment [dev|staging]"
 
-  if [ $# -gt 1 ]
+  if [ $# -gt 2 ]
   then
     echo "$usage"
     return 0
@@ -23,12 +23,17 @@ function _k8sdeploy() {
 
   context='live-1'
   version=$(git rev-parse $(git branch | grep \* | cut -d ' ' -f2))
-  docker_registry='754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-get-paid/cccd'
+  docker_registry=754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-get-paid/cccd
   docker_tag=app-${version}
-  docker_image=${docker_registry}:${docker_tag}
+  docker_image_tag=${docker_registry}:${docker_tag}
 
-  echo "Deploying $docker_image to $environment on cluster $context"
-  kubectl set image -f kubectl_deploy/${environment}/deployment.yaml cccd-app=${docker_image} --local -o yaml | kubectl apply -n cccd-${environment} --context ${context} -f -
+  echo "Deploying $docker_image_tag to $environment on cluster $context"
+  kubectl set image -f kubectl_deploy/${environment}/deployment.yaml cccd-app=${docker_image_tag} --local -o yaml | kubectl apply -n cccd-${environment} --context ${context} -f -
+  echo "Applying service..."
+  kubectl apply -n cccd-${environment} --context ${context} -f kubectl_deploy/${environment}/service.yaml
+  echo "Applying ingress..."
+  kubectl apply -n cccd-${environment} --context ${context} -f kubectl_deploy/${environment}/ingress.yaml
+
 }
 
 _k8sdeploy $@

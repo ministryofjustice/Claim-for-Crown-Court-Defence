d0d35096145173521fa38db20009edc6e0a35166
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-06-19 11:55:02 +0100

    Extend s3 headers test
    Test other s3 headers and spec the
    use of an env var for region in particular
    as we replaced the settings hard-coded default
    with an ENV var call.

--- spec/support/shared_examples/models/s3_headers.rb
+++ spec/support/shared_examples/models/s3_headers.rb
@@ -1,20 +1,39 @@
 RSpec.shared_examples 'an s3 bucket' do
   describe '.s3_headers' do
-    subject { described_class.s3_headers[:s3_region] }
+    subject { described_class.s3_headers }
 
-    context 'when an aws region has been explicitly recorded in the settings' do
-      let(:fake_aws_region) { 'eu-west-49'}
+    context ':s3_headers' do
+      subject(:s3_headers) { described_class.s3_headers[:s3_headers] }
 
-      before { allow(Settings.aws).to receive(:region).and_return(fake_aws_region) }
+      it 'includes no-cache directive' do
+        freeze_time do
+          expect(s3_headers.values).to include('no-cache')
+        end
+      end
 
-      it { is_expected.to eql fake_aws_region }
+      it 'includes 3 month expiry directive' do
+        freeze_time do
+          is_expected.to include('Expires' => 3.months.from_now.httpdate)
+        end
+      end
     end
 
-    context 'when an aws region has not been set' do
+    context ':s3_permissions' do
+      subject(:s3_permissions) { described_class.s3_headers[:s3_permissions] }
 
-      before { allow(Settings.aws).to receive(:region).and_return(nil) }
+      it 'includes private directive for s3' do
+        is_expected.to eql(:private)
+      end
+    end
 
-      it { is_expected.to eql 'eu-west-1' }
+    context ':s3_region' do
+      subject(:s3_region) { described_class.s3_headers[:s3_region] }
+      let(:fake_aws_region) { 'eu-west-49' }
+
+      it 'includes region value from settings' do
+        expect(Settings.aws).to receive(:region).and_return(fake_aws_region)
+        is_expected.to eql fake_aws_region
+      end
     end
   end
 end

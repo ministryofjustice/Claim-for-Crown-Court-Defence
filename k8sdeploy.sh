50ec5e881229eddb3cd2906efbcf89df1adccafd
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-06-20 22:37:07 +0100

    Add and use `latest` tag to build and deploy
    Add additional tag to builds
    and have deploy use the latest
    branch tag rather than the shah.
    
    This is too avoid deploying unbuilt
    branches and to make the registry
    iamges tags more human readable.

--- k8sdeploy.sh
+++ k8sdeploy.sh
@@ -22,17 +22,34 @@ function _k8sdeploy() {
   esac
 
   context='live-1'
-  version=$(git rev-parse $(git branch | grep \* | cut -d ' ' -f2))
+  component=app
+  current_branch=$(git branch | grep \* | cut -d ' ' -f2)
   docker_registry=754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-get-paid/cccd
-  docker_tag=app-${version}
-  docker_image_tag=${docker_registry}:${docker_tag}
 
-  echo "Deploying $docker_image_tag to $environment on cluster $context"
-  kubectl set image -f kubectl_deploy/${environment}/deployment.yaml cccd-app=${docker_image_tag} --local -o yaml | kubectl apply -n cccd-${environment} --context ${context} -f -
-  echo "Applying service..."
-  kubectl apply -n cccd-${environment} --context ${context} -f kubectl_deploy/${environment}/service.yaml
-  echo "Applying ingress..."
-  kubectl apply -n cccd-${environment} --context ${context} -f kubectl_deploy/${environment}/ingress.yaml
+  # get latest tag for branch
+  case $current_branch in
+    master)
+      docker_image_tag=${docker_registry}:${component}-latest
+      ;;
+    *)
+      docker_image_tag=${docker_registry}:${component}-${current_branch}-latest
+      ;;
+  esac
+
+  kubectl config set-context ${context} --namespace=cccd-${environment}
+  kubectl config use-context ${context}
+
+  echo "--------------------------------------------------"
+  echo "Context: $context"
+  echo "Environment: $environment"
+  echo "Current branch: $current_branch"
+  echo "Docker image: $docker_image_tag"
+  echo "--------------------------------------------------"
+
+  kubectl set image -f kubectl_deploy/${environment}/deployment.yaml cccd-app=${docker_image_tag} --local -o yaml | kubectl apply -f -
+  kubectl apply \
+    -f kubectl_deploy/${environment}/service.yaml \
+    -f kubectl_deploy/${environment}/ingress.yaml
 
 }
 

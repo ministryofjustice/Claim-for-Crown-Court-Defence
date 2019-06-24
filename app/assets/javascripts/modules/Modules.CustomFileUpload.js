9c0eccb140568d372e65ea7a1b980b18903dedd2
Peter Hunt <naseberry@users.noreply.github.com>
2019-06-24 09:15:29 +0100

    [CBO-771] Fix DAC report issues

--- app/assets/javascripts/modules/Modules.CustomFileUpload.js
+++ app/assets/javascripts/modules/Modules.CustomFileUpload.js
@@ -2,34 +2,34 @@ moj.Modules.CustomFileUpload = {
   el: 'input:file.custom-uploader',
   tpml: {
     button: ['<button class="button button-tertiary external-user-json-export" type="button">Choose file</button>'].join(''),
-    text: ['<p tabindex="0" class="file-upload-name" aria-hidden="true" ><span class="visuallyhidden">File name:</span></p>'].join('')
+    text: ['<p tabindex="-1" class="file-upload-name" aria-hidden="true" ><span class="visuallyhidden">File name:</span></p>'].join('')
   },
 
-  init: function() {
+  init: function () {
     $('.btn-import-file').hide().attr('aria-hidden', 'true');
     this.cacheEls();
     this.bindEvents();
     this.setUp();
   },
 
-  setUp: function() {
+  setUp: function () {
     this.$fileInputs
       .after(this.$chooseFileButtonTmpl.clone())
       .after(this.$chooseFileNameTmpl.clone());
   },
 
-  bindEvents: function() {
+  bindEvents: function () {
     this.$forms
-      .on('click', function(e) {
+      .on('click', function (e) {
         var $target = $(e.target);
         if ($target.attr('type') === 'button') {
           $target.siblings(':file').click();
         }
       })
-      .on('change', ':file', function(e) {
+      .on('change', ':file', function (e) {
         var fileName = this.value.replace('C:\\fakepath\\', '');
         $(e.target).closest('form')
-          .find('.file-upload-name').append(fileName).attr('aria-hidden','')
+          .find('.file-upload-name').append(fileName).attr('aria-hidden', '')
           .parent()
           .find('.errors').empty();
 
@@ -37,12 +37,12 @@ moj.Modules.CustomFileUpload = {
         $('.btn-import-file').show().attr('aria-hidden', '');
 
       })
-      .on('submit', function() {
+      .on('submit', function () {
         $('input.btn-import-file').prop('disabled', true).val('Please wait...');
       });
   },
 
-  cacheEls: function() {
+  cacheEls: function () {
     this.$fileInputs = $(this.el);
     this.$forms = this.$fileInputs.closest('form');
     this.$chooseFileNameTmpl = $(this.tpml.text);
--- app/form_builders/adp_text_field.rb
+++ app/form_builders/adp_text_field.rb
@@ -51,7 +51,8 @@ class AdpTextField
 
   def to_html
     result = div_start
-    result += anchor
+    # this breaks the label & input association
+    # result += anchor
     result += label
     result += hint
     result += error_message

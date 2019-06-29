ac7901f1fc904740ac4db818d6bc23c3adf0b9ca
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-06-29 21:26:24 +0100

    Amend k8sdeploy.sh
    - allow specifying image to deploy
    - use rolling restart
      New as of kubectl 1.15, this forces
      a restart of all the pods irrespective
      of whether deployment.yaml was changed
      or not.
    
      This is useful a) because secret changes
      will be applied regardless and b) because its rolling
      (although basic tests show it not falling rolling
      back as expected with one instance).
    
    - annotate deployment to facilitate rollout undo etc
      e.g.
      `kubectl rollout history deployments`
        -> identifiy <n>th rollout
      `kubectl rollout undo deployment/claim-for-cornw-court-defence --to-revision <n>`

--- k8sdeploy.sh
+++ k8sdeploy.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 function _k8sdeploy() {
   usage="k8sdeploy -- deploy image from current commit to an environment
-  Usage: ./k8sdeploy.sh environment
+  Usage: ./k8sdeploy.sh environment [image-tag]
   Where:
-    environment [dev|staging|api-sandbox]"
+    environment [dev|staging|api-sandbox]
+    [image_tag] any valid ECR image tag for app
+  Example:
+    # deploy image for current commit to dev
+    ./k8sdeploy.sh dev
 
-  if [ $# -gt 1 ]
+    # deploy latest image of master to dev
+    ./k8sdeploy.sh dev latest
+
+    # deploy latest branch image to dev
+    ./k8sdeploy.sh dev <branch-name>-latest
+
+    # deploy specific image (based on commit sha)
+    ./k8sdeploy.sh dev <commit-sha>
+    "
+
+  if [ $# -gt 2 ]
   then
     echo "$usage"
     return 0
@@ -21,10 +35,16 @@ function _k8sdeploy() {
       ;;
   esac
 
+  if [ -z "$2" ]
+  then
+    current_branch=$(git branch | grep \* | cut -d ' ' -f2)
+    current_version=$(git rev-parse $current_branch)
+  else
+    current_version=$2
+  fi
+
   context='live-1'
   component=app
-  current_branch=$(git branch | grep \* | cut -d ' ' -f2)
-  current_version=$(git rev-parse $current_branch)
   docker_registry=754256621582.dkr.ecr.eu-west-2.amazonaws.com/laa-get-paid/cccd
   docker_image_tag=${docker_registry}:${component}-${current_version}
 
@@ -42,17 +62,26 @@ function _k8sdeploy() {
   kubectl config set-context ${context} --namespace=cccd-${environment}
   kubectl config use-context ${context}
 
-  echo "--------------------------------------------------"
-  echo "Context: $context"
-  echo "Environment: $environment"
-  echo "Current branch: $current_branch"
-  echo "Docker image: $docker_image_tag"
-  echo "--------------------------------------------------"
+  printf "\e[33m--------------------------------------------------\e[0m\n"
+  printf "\e[33mContext: $context\e[0m\n"
+  printf "\e[33mEnvironment: $environment\e[0m\n"
+  printf "\e[33mDocker image: $docker_image_tag\e[0m\n"
+  printf "\e[33m--------------------------------------------------\e[0m\n"
 
-  kubectl set image -f kubernetes_deploy/${environment}/deployment.yaml cccd-app=${docker_image_tag} --local -o yaml | kubectl apply -f -
+  # TODO: check if image exists and if not offer to build or abort
+
+  kubectl set image -f kubernetes_deploy/${environment}/deployment.yaml cccd-app=${docker_image_tag} --local --output yaml | kubectl apply -f -
   kubectl apply \
     -f kubernetes_deploy/${environment}/service.yaml \
-    -f kubernetes_deploy/${environment}/ingress.yaml
+    -f kubernetes_deploy/${environment}/ingress.yaml \
+    -f kubernetes_deploy/${environment}/secrets.yaml
+
+  # Forcibly restart the app regardless of whether
+  # there are changes to apply new secrets, at least.
+  # - requires kubectl verion 1.15+
+  #
+  kubectl annotate deployments/claim-for-crown-court-defence kubernetes.io/change-cause="$(date) - deploying: $docker_image_tag"
+  kubectl rollout restart deployments/claim-for-crown-court-defence
 
 }
 

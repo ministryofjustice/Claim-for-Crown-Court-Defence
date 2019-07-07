1c5ef11410efc76c711ab1066f1c9403e8138634
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-07-07 21:55:07 +0100

    Refactor batch transitioner
    - extract methods
    - add ENV to log output
    - use logstuff
      for compatability with kibana

--- app/models/timed_transitions/batch_transitioner.rb
+++ app/models/timed_transitions/batch_transitioner.rb
@@ -1,10 +1,10 @@
 module TimedTransitions
   class BatchTransitioner
-    attr_accessor :transitions_counter
+    attr_reader :dummy, :limit, :transitions_counter
 
     def initialize(options = {})
       @dummy = options.fetch(:dummy, false)
-      @limit = options[:limit].to_i
+      @limit = options.fetch(:limit, '10000').to_i
       @transitions_counter = 0
     end
 
@@ -13,15 +13,21 @@ module TimedTransitions
       softly_deleted_ids = Transitioner.softly_deleted_ids
       found_ids = (claims_ids | softly_deleted_ids)
 
+      log_start
       found_ids.each do |claim_id|
-        claim = Claim::BaseClaim.find claim_id
-
-        transitioner = Transitioner.new(claim, @dummy)
-        transitioner.run
-        increment_counter if transitioner.success?
-
+        transition(claim_id)
         break if limit_reached?
       end
+      log_end
+    end
+
+    private
+
+    def transition(claim_id)
+      claim = Claim::BaseClaim.find(claim_id)
+      transitioner = Transitioner.new(claim, dummy)
+      transitioner.run
+      increment_counter if transitioner.success?
     end
 
     def increment_counter
@@ -29,8 +35,46 @@ module TimedTransitions
     end
 
     def limit_reached?
-      return false if @limit.zero?
-      @transitions_counter >= @limit
+      return false if limit.zero?
+      transitions_counter >= limit
+    end
+
+    def started_at
+      @started_at ||= DateTime.current
+    end
+
+    def finished_at
+      @finished_at ||= DateTime.current
+    end
+
+    def log_start
+      log(:info,
+          'Starting processing of stale claims',
+          started_at: started_at)
+    end
+
+    def log_end
+      log(:info,
+          'Finished processing of stale claims',
+          started_at: started_at,
+          finished_at: finished_at,
+          claims_processed: transitions_counter,
+          seconds_taken: finished_at.to_i - started_at.to_i)
+    end
+
+    def log(level, message, options)
+      LogStuff.send(
+        level.to_sym,
+        'TimedTransitions::BatchTransitioner',
+        environment: ENV.fetch('ENV', nil),
+        limit: limit,
+        started_at: options[:started_at],
+        claims_processed: options[:claims_processed],
+        finished_at: options[:finished_at],
+        seconds_taken: options[:seconds_taken]
+      ) do
+        message
+      end
     end
   end
 end

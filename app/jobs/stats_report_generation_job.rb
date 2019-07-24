68e56faffdc0eebb0ffe8e38103e6a91323db9d4
willmcb <william.mcbrien@digital.justice.gov.uk>
2019-07-24 12:04:59 +0100

    Add error catching to report generation Job class

--- app/jobs/stats_report_generation_job.rb
+++ app/jobs/stats_report_generation_job.rb
@@ -2,6 +2,14 @@ class StatsReportGenerationJob < ApplicationJob
   queue_as :stats_reports
 
   def perform(report_type)
-    Stats::StatsReportGenerator.call(report_type)
+    begin
+      LogStuff.call(:info, "Stats report job starting for report type: #{report_type}")
+      Stats::StatsReportGenerator.call(report_type)
+      LogStuff.call(:info, "Stats report job finished for report type: #{report_type}")
+    rescue StandardError => e
+      LogStuff.send(:error, "Report generation error has occured:")
+      LogStuff.send(:error, "#{e.class} - #{e.message}")
+      LogStuff.send(:error, "#{e.backtrace.inspect}")
+    end
   end
 end

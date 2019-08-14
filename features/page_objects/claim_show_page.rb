3b37703bdb7a2317aaf24a0d03d9cc04172075bb
Peter Hunt <naseberry@users.noreply.github.com>
2019-08-14 12:20:38 +0100

    [CBO-783] Advocates updates

--- features/page_objects/claim_show_page.rb
+++ features/page_objects/claim_show_page.rb
@@ -1,5 +1,5 @@
 class ClaimShowPage < BasePage
-  section :nav, "div.breadcrumbs > ol" do
+  section :nav, "nav.breadcrumbs > ul" do
     element :your_claims, "li:nth-of-type(1) > a"
     element :archive, "li:nth-of-type(2) > a"
   end

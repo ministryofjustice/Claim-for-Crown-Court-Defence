b07f2f33f9c4f77577bb4e78ea6af597812a378c
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-09-09 17:09:30 +0100

    Add supervisor for app startup
    Sidekiq updates require use of init system
    to start sidekiq. This also is best practice
    for interdependent services.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -34,11 +34,14 @@ else
   printf '\e[33mINFO: Using remote redis-server\e[0m\n'
 fi
 
-printf '\e[33mINFO: Starting sidekiq daemon\e[0m\n'
-bundle exec sidekiq -d
+# printf '\e[33mINFO: Starting sidekiq daemon\e[0m\n'
+# bundle exec sidekiq -d
 
-printf '\e[33mINFO: Starting scheduler_daemon daemon\e[0m\n'
-bundle exec scheduler_daemon start
+# printf '\e[33mINFO: Starting scheduler_daemon daemon\e[0m\n'
+# bundle exec scheduler_daemon start
 
-printf '\e[33mINFO: Launching unicorn\e[0m\n'
-bundle exec unicorn -p 3000 -c config/unicorn.rb
+# printf '\e[33mINFO: Launching unicorn\e[0m\n'
+# bundle exec unicorn -p 3000 -c config/unicorn.rb
+
+printf '\e[33mINFO: Starting services \e[0m\n'
+supervisord --nodaemon -c /usr/src/app/docker/supervisord.conf

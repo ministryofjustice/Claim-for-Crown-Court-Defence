8d92bf3593c5a617171eafddfaf2047e57ebd994
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-11-15 12:21:31 +0000

    Move build and push steps to single build script and call

---
+++ .circleci/build.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+function _circleci_build() {
+  usage="build -- build, tag and push app image for current CircleCI commit to ecr
+  Usage: build"
+
+  if [[ -z "${ECR_ENDPOINT}" ]] || \
+      [[ -z "${AWS_DEFAULT_REGION}" ]] || \
+      [[ -z "${GITHUB_TEAM_NAME_SLUG}" ]] || \
+      [[ -z "${REPO_NAME}" ]] || \
+      [[ -z "${CIRCLE_BRANCH}" ]] || \
+      [[ -z "${CIRCLE_SHA1}" ]]
+  then
+    echo "Missing environment vars: only run this via circleCI with all relevant environment variables"
+    return 1
+  fi
+
+  # build
+  docker_registry_tag="${ECR_ENDPOINT}/${GITHUB_TEAM_NAME_SLUG}/${REPO_NAME}:app-${CIRCLE_SHA1}"
+
+  printf "\e[33m------------------------------------------------------------------------\e[0m\n"
+  printf "\e[33mBranch: $CIRCLE_BRANCH\e[0m\n"
+  printf "\e[33mRegistry tag: $docker_registry_tag\e[0m\n"
+  printf "\e[33m------------------------------------------------------------------------\e[0m\n"
+
+  $(aws ecr get-login --region ${AWS_DEFAULT_REGION} --no-include-email)
+
+  docker build \
+    --build-arg VERSION_NUMBER="NOT USED ANYMORE" \
+    --build-arg BUILD_DATE=$(date +%Y-%m-%dT%H:%M:%S%z) \
+    --build-arg COMMIT_ID=${CIRCLE_SHA1} \
+    --build-arg BUILD_TAG="app-${CIRCLE_SHA1}" \
+    --build-arg APP_BRANCH=${CIRCLE_BRANCH} \
+    --build-arg LIVE1_DB_TASK=migrate \
+    --pull \
+    --tag $docker_registry_tag \
+    --file docker/Dockerfile .
+
+  # push
+  docker push $docker_registry_tag
+
+  if [ "${CIRCLE_BRANCH}" == "master" ]; then
+    docker_registry_latest_tag="${ECR_ENDPOINT}/${GITHUB_TEAM_NAME_SLUG}/${REPO_NAME}:app-latest"
+    docker tag $docker_registry_tag $docker_registry_latest_tag
+    docker push $docker_registry_latest_tag
+  fi
+}
+
+_circleci_build $@

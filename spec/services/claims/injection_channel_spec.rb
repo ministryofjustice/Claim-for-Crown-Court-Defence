2a5c8a96d6afdf8729755e994278cd34d3cc0ca4
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-11-22 12:06:42 +0000

    Redirect DI slakc messages in live-1 to normal channels

--- spec/services/claims/injection_channel_spec.rb
+++ spec/services/claims/injection_channel_spec.rb
@@ -9,13 +9,6 @@ RSpec.describe Claims::InjectionChannel, type: :service do
     it { is_expected.to eql('cccd_development') }
   end
 
-  context 'when a response_queue name matches live-1 SQS name' do
-    before { allow(Settings.aws).to receive(:response_queue).and_return('laa-get-paid-test-responses-for-cccd') }
-    let(:claim) { create(:litigator_claim) }
-
-    it { is_expected.to eql('cccd-k8s-injection') }
-  end
-
   context 'when claim is for LGFS' do
     let(:claim) { create(:litigator_claim) }
 

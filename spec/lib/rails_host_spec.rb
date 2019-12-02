7cb4afb29d04eab38ca6cd2b55384eeb8f48e496
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-12-02 17:30:21 +0000

    Fix an refactor RailsHost spec
    Inline with removal of demo and
    to increase coverage.

--- spec/lib/rails_host_spec.rb
+++ spec/lib/rails_host_spec.rb
@@ -1,58 +1,105 @@
 require 'rails_helper'
 
 RSpec.describe RailsHost do
+  around do |example|
+    with_env(environment) { example.run }
+  end
+
   describe '.env' do
     subject { described_class.env }
 
-    around do |example|
-      with_env('staging') { example.run }
+    context 'staging' do
+      let(:environment) { 'staging' }
+
+      it 'returns value of environment variable ENV' do
+        is_expected.to eq('staging')
+      end
     end
 
-    it 'returns value of environment variable ENV' do
-      is_expected.to eq('staging')
+    context 'api-sandbox' do
+      let(:environment) { 'api-sandbox' }
+
+      it 'returns value of environment variable ENV' do
+        is_expected.to eq('api-sandbox')
+      end
+    end
+
+    context 'production' do
+      let(:environment) { 'production' }
+
+      it 'returns value of environment variable ENV' do
+        is_expected.to eq('production')
+      end
+    end
+
+    context 'gibberish' do
+      let(:environment) { 'gibberish' }
+
+      it 'returns value of environment variable ENV' do
+        is_expected.to eq('gibberish')
+      end
     end
   end
 
-  context 'api_sandbox' do
-    around do |example|
-      with_env('api-sandbox') { example.run }
+  describe '.host' do
+    context 'valid environments' do
+      context 'api-sandbox' do
+        let(:environment) { 'api-sandbox' }
+
+        it 'returns the rails host envirobment name' do
+          expect(Rails.host.env).to eq 'api-sandbox'
+        end
+
+        it '#api_sandbox?' do
+          expect(Rails.host.api_sandbox?).to be true
+        end
+
+        it 'returns false for dev' do
+          expect(Rails.host.dev?).to be false
+        end
+      end
+
+      context 'staging' do
+        let(:environment) { 'staging' }
+
+        it 'returns the rails environement host name' do
+          expect(Rails.host.env).to eq 'staging'
+        end
+
+        it 'returns true for staging?' do
+          expect(Rails.host.staging?).to be true
+        end
+
+        it 'returns false for dev' do
+          expect(Rails.host.dev?).to be false
+        end
+      end
+
+      context 'production' do
+        let(:environment) { 'production' }
+
+        it 'returns the rails environement host name' do
+          expect(Rails.host.env).to eq 'production'
+        end
+
+        it 'returns true for production?' do
+          expect(Rails.host.production?).to be true
+        end
+
+        it 'returns false for dev' do
+          expect(Rails.host.dev?).to be false
+        end
+      end
     end
 
-    it 'should return the rails host envirobment name' do
-      expect(Rails.host.env).to eq 'api-sandbox'
-    end
+    context 'invalid environments' do
+      let(:environment) { 'gibberish' }
 
-    it 'should return true for api_sandbox?' do
-      expect(Rails.host.api_sandbox?).to be true
-    end
-
-    it 'should return false for demo' do
-      expect(Rails.host.demo?).to be false
-    end
-
-    it 'should raise method missing if invalid method name' do
-      expect {
-        Rails.host.gibberish?
-
-      }.to raise_error NoMethodError, /undefined method .gibberish\?/
-    end
-  end
-
-  context 'staging' do
-    around do |example|
-      with_env('staging') { example.run }
-    end
-
-    it 'should return the rails environement host name' do
-      expect(Rails.host.env).to eq 'staging'
-    end
-
-    it 'should return true for staging?' do
-      expect(Rails.host.staging?).to be true
-    end
-
-    it 'should return false for demo' do
-      expect(Rails.host.demo?).to be false
+      it 'raises method missing if invalid method name' do
+        expect {
+          Rails.host.gibberish?
+        }.to raise_error NoMethodError, /undefined method .gibberish\?/
+      end
     end
   end
 end

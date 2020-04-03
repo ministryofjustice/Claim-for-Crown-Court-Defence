29fe48e8c28cd912eaee7f1f0f7db1b95bdf6be6
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-04-03 23:51:32 +0100

    [WIP] add hardship claim/bill type
    currently have flow going trhough to the fee page.
    
    - add new claim type `Claim::LitigatorHardshipClaim`
    - add new fee_type `Fee::HardshipFeeType`
    - add new fee `Fee::HardshipFee`
    - add hardship claim controller
    - add hardship claim presenter
    - add hardship claim validator
    - add hardship fee validator
    - add page flow config (TBC*)
    - add views for page flow
    - add views for summary
    - add views/js for sidebar
    - add misc fee page
    - add disbursements page
    - add expenses page
    
    * TBC:
     - what case types are applicable
     - what other fees can be claimed
     - can expenses be claimed
     - can disbursements be claimed

--- spec/controllers/external_users/claim_types_controller_spec.rb
+++ spec/controllers/external_users/claim_types_controller_spec.rb
@@ -45,7 +45,7 @@ RSpec.describe ExternalUsers::ClaimTypesController, type: :controller, focus: tr
 
       it "assigns bill types based on provider roles" do
         get :selection
-        expect(assigns(:available_claim_types)).to match_array(%w(agfs agfs_interim agfs_supplementary lgfs_final lgfs_interim lgfs_transfer))
+        expect(assigns(:available_claim_types)).to match_array(%w(agfs agfs_interim agfs_supplementary lgfs_final lgfs_interim lgfs_transfer lgfs_hardship))
       end
 
       it "renders the bill type options page" do
@@ -73,7 +73,7 @@ RSpec.describe ExternalUsers::ClaimTypesController, type: :controller, focus: tr
 
       it "assigns bill types based on provider roles" do
         get :selection
-        expect(assigns(:available_claim_types)).to match_array(%w(lgfs_final lgfs_interim lgfs_transfer))
+        expect(assigns(:available_claim_types)).to match_array(%w(lgfs_final lgfs_interim lgfs_transfer lgfs_hardship))
       end
 
       it "renders bill type selection page" do
@@ -87,7 +87,7 @@ RSpec.describe ExternalUsers::ClaimTypesController, type: :controller, focus: tr
 
       it "assigns bill types based on external_user roles" do
         get :selection
-        expect(assigns(:available_claim_types)).to match_array(%w(lgfs_final lgfs_interim lgfs_transfer))
+        expect(assigns(:available_claim_types)).to match_array(%w(lgfs_final lgfs_interim lgfs_transfer lgfs_hardship))
       end
 
       it 'renders the bill type selection page' do

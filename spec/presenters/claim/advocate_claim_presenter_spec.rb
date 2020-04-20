2b59f6fa404d93920f01eb49ffb92b8846984435
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-04-20 12:56:43 +0100

    Remove duplicate advocate claim presenter spec
    One spec existing correctly a `spec/presenters/claims/advocate_claim_presenter_spec.rb`. The other at a level
    up. Tests were moved from the duplicate as necessary.

--- spec/presenters/claim/advocate_claim_presenter_spec.rb
+++ spec/presenters/claim/advocate_claim_presenter_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Claim::AdvocateClaimPresenter, type: :presenter do
+  subject(:presenter) { described_class.new(claim, view) }
+
   let(:claim_9) { create(:advocate_claim, :agfs_scheme_9) }
   let(:claim_10) { create(:advocate_claim, :agfs_scheme_10) }
   let!(:lgfs_scheme_nine) { FeeScheme.find_by(name: 'LGFS', version: 9) || create(:fee_scheme, :lgfs) }
@@ -11,7 +13,7 @@ RSpec.describe Claim::AdvocateClaimPresenter, type: :presenter do
   let(:claim_discontinuance_9) { create(:advocate_claim, :agfs_scheme_9, case_type: discontinuance, prosecution_evidence: true) }
   let(:claim_discontinuance_10) { create(:advocate_claim, :agfs_scheme_10, case_type: discontinuance, prosecution_evidence: true) }
 
-  subject(:presenter) { described_class.new(claim, view) }
+  it { is_expected.to be_kind_of(Claim::BaseClaimPresenter) }
 
   describe '#pretty_type' do
     specify { expect(presenter.pretty_type).to eq('AGFS Final') }
@@ -25,16 +27,68 @@ RSpec.describe Claim::AdvocateClaimPresenter, type: :presenter do
     specify { expect(presenter.can_have_disbursements?).to be_falsey }
   end
 
-  describe '#requires_interim_claim_info?' do
-    context 'when claim is not for the AGFS fee reform scheme' do
+  describe '#can_have_expenses?' do
+    specify { expect(presenter.can_have_expenses?).to be_truthy }
+  end
 
-      specify { expect(presenter.requires_interim_claim_info?).to be_falsey }
+  describe '#requires_interim_claim_info?' do
+    subject { presenter.requires_interim_claim_info? }
+
+    before { seed_fee_schemes }
+
+    context 'when claim is pre agfs reform' do
+      let(:claim) { create(:advocate_claim, :agfs_scheme_9) }
+      it { is_expected.to be_falsey }
     end
 
-    context 'when claim is for the AGFS fee reform scheme' do
-      let(:claim) { claim_10 }
+    context 'when claim is post agfs reform' do
+      let(:claim) { create(:advocate_claim, :agfs_scheme_10) }
+      it { is_expected.to be_truthy }
+    end
+  end
 
-      specify { expect(presenter.requires_interim_claim_info?).to be_truthy }
+  describe '#raw_basic_fees_total' do
+    it 'sends message to claim' do
+      expect(claim).to receive(:calculate_fees_total).with(:basic_fees)
+      presenter.raw_basic_fees_total
+    end
+  end
+
+  describe '#raw_fixed_fees_total' do
+    it 'sends message to claim' do
+      expect(claim).to receive(:calculate_fees_total).with(:fixed_fees)
+      presenter.raw_fixed_fees_total
+    end
+  end
+
+  describe '#raw_fixed_fees_combined_total' do
+    it 'sends messages to self' do
+      expect(presenter.raw_fixed_fees_combined_total). to be_kind_of(BigDecimal)
+    end
+  end
+
+  describe 'calculate #fixed_fees' do
+    before do
+      allow(presenter).to receive(:raw_fixed_fees_total).and_return 10.0
+      allow(claim).to receive(:created_at).and_return Date.today
+      allow(claim).to receive(:apply_vat?).and_return true
+    end
+
+    it '#raw_fixed_fees_vat' do
+      expect(presenter.raw_fixed_fees_vat).to eq(2.0)
+    end
+
+    it 'returns #raw_fixed_fees_gross' do
+      allow(presenter).to receive(:raw_fixed_fees_vat).and_return 2.0
+      expect(presenter.raw_fixed_fees_gross).to eq(12.0)
+    end
+
+    it 'returns #fixed_fees_vat with the associated currency' do
+      expect(presenter.fixed_fees_vat).to eq('£2.00')
+    end
+
+    it 'returns #fixed_fees_gross with the associated currency' do
+      expect(presenter.fixed_fees_gross).to eq('£12.00')
     end
   end
 

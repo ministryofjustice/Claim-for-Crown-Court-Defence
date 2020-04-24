6508fbf09596ea2feb8c5a1ec6541af04e5e5309
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-04-24 17:25:40 +0100

    Modify seed loading mechanism to handle FKs
    Certain seed data relies on others via
    FKs. IN particular the case_stages must be
    seeded after case_types since they have a
    an FK to them.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,38 @@
-Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
-  puts "Seeding '#{seed}'..."
-  load seed
+# Seed all lookup data
+# - order is important to handle foreign key dependencies
+#
+
+# Use this to generate a list of seed files in alphabtical order
+# which you can then tweak in load list below
+#
+# Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
+#   file_name = File.basename(seed, '.*')
+#   puts file_name
+#   # puts "load 'db/seeds/#{file_name}'"
+# end
+
+SEED_FILES = %w[
+  case_types
+  case_stages
+  case_workers
+  certification_types
+  courts
+  disbursement_types
+  establishments
+  expense_types
+  fee_types
+  locations
+  offence_classes
+  offences
+  scheme_10
+  scheme_11
+  super_admins
+  supplier_numbers
+  vat_rates
+]
+
+SEED_FILES.each do |file|
+  file_path = "db/seeds/#{file}.rb"
+  puts "loading #{file_path}..."
+  load file_path
 end

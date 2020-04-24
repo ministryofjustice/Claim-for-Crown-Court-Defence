ae2e06a9a94677b151c4e91205d12672054a1308
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-04-24 17:35:38 +0100

    Prefix shared_context with RSpec with class

--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -12,13 +12,13 @@ module TestHelpers
 
   include DatabaseHousekeeping
 
-  shared_context 'claim-types helpers' do
+  RSpec.shared_context 'claim-types helpers' do
     let(:agfs_claim_types) { %w[agfs agfs_interim agfs_supplementary agfs_hardship] }
     let(:lgfs_claim_types) { %w[lgfs_final lgfs_interim lgfs_transfer] }
     let(:all_claim_types) { agfs_claim_types | lgfs_claim_types }
   end
 
-  shared_context 'claim-types object helpers' do
+  RSpec.shared_context 'claim-types object helpers' do
     let(:agfs_claim_object_types) { %w[Claim::AdvocateClaim Claim::AdvocateInterimClaim Claim::AdvocateSupplementaryClaim Claim::AdvocateHardshipClaim] }
     let(:lgfs_claim_object_types) { %w[Claim::LitigatorClaim Claim::InterimClaim Claim::TransferClaim] }
     let(:all_claim_object_types) { agfs_claim_object_types | lgfs_claim_object_types }

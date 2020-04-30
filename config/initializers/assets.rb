44f7b3de9d4206c75f9aed4f9448875082a98f2a
Peter Hunt <naseberry@users.noreply.github.com>
2020-04-30 16:43:11 +0100

    Resolve broken specs

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,8 @@
 Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
-Rails.application.config.assets.precompile += %w( adp_swagger_application.js *.png)
-Rails.application.config.assets.precompile += %w( application.test.js ) if Rails.env.test?
-Rails.application.config.assets.precompile += %w( pdf.css )
+# Migrated to Webpacker
+# Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+# Rails.application.config.assets.precompile += %w( adp_swagger_application.js *.png)
+# Rails.application.config.assets.precompile += %w( application.test.js ) if Rails.env.test?
+# Rails.application.config.assets.precompile += %w( pdf.css )

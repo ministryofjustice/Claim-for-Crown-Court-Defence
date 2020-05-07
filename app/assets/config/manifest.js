dff85db041f39b8ec4768318257aa3827804f679
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-05-07 13:46:07 +0100

    Add manifest.js to keep sprocket-rails happy
    See https://github.com/rails/sprockets-rails/issues/444
    and https://github.com/rails/sprockets-rails/blob/v3.2.1/lib/sprockets/railtie.rb#L103-L110
    
    can be removed if/when we move to webpacker

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+// just here to keep sprocket-rails happy for rails 6.0.3
+// can be removed if/when we move to webpacker
+// see https://github.com/rails/sprockets-rails/issues/444
+// and https://github.com/rails/sprockets-rails/blob/v3.2.1/lib/sprockets/railtie.rb#L103-L110
+//

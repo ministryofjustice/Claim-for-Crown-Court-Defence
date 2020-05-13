0e86d0c3e0b80d567aaaf48011a11424846cf263
Peter Hunt <naseberry@users.noreply.github.com>
2020-05-13 09:37:30 +0100

    Add assets to manifest.js
    needed by sprockets now for some reason - to
    be investigated.

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,14 @@
+// needed by sprocket-rails with rails 6.0.3
+// can be removed if/when we move to webpacker
+// see https://github.com/rails/sprockets-rails/issues/444
+// and https://github.com/rails/sprockets-rails/blob/v3.2.1/lib/sprockets/railtie.rb#L103-L110
+//
+
+//= link application.css
+//= link govuk-template.css
+//= link govuk-template-ie6.css
+//= link govuk-template-ie7.css
+//= link govuk-template-ie8.css
+//= link govuk-template-print.css
+//= link fonts.css
+//= link application.js

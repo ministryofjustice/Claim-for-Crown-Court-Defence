be2ddfccba590ef98ba4f39b65258e48b8dc1245
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-05-19 13:15:31 +0100

    Add calculated_fee_adapter_spec

---
+++ spec/services/ccr/calculated_fee_adapter_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe CCR::CalculatedFeeAdapter, type: :adapter do
+  let(:claim) { create(:advocate_hardship_claim, case_stage: build(:case_stage, :agfs_pre_ptph_no_evidence)) }
+
+  describe '#ex_vat' do
+  subject { described_class.new(claim).ex_vat }
+    it "returns sum of basic fees" do
+      is_expected.to eql 25
+    end
+  end
+
+  describe '.ex_vat_for' do
+    subject { described_class.ex_vat_for(claim) }
+    let(:adapter) { instance_double 'CalculatedFeeAdapter' }
+    it 'calls #ex_vat' do
+      expect(described_class).to receive(:new).with(claim).and_return adapter
+      expect(adapter).to receive(:ex_vat)
+      subject
+    end
+  end
+end

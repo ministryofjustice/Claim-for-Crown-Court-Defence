25ca2daf226bd67f5c316820f7e3842b3a9277a6
Peter Hunt <naseberry@users.noreply.github.com>
2020-06-19 13:28:32 +0100

    Add link components
    - Extend rails helpers `link_to` and `mail_to`
    - add govuk-header__link
    - add govuk-skip-link
    - add govuk-back-link
    - add govuk-footer__link
    - add govuk-link

---
+++ lib/govuk_component/link_helpers.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module GovukComponent
+  module LinkHelpers
+    def govuk_back_link_to(body = nil, url = nil, **tag_options)
+      tag_options = prepend_classes('govuk-back-link', tag_options)
+      link_to body, url, **tag_options
+    end
+
+    def govuk_footer_link_to(body = nil, url = nil, **tag_options)
+      tag_options = prepend_classes('govuk-footer__link', tag_options)
+      link_to body, url, **tag_options
+    end
+
+    def govuk_header_link_to(body = nil, url = nil, **tag_options)
+      tag_options = prepend_classes('govuk-header__link', tag_options)
+      link_to body, url, **tag_options
+    end
+
+    def govuk_link_to(body = nil, url = nil, **tag_options)
+      tag_options = prepend_classes('govuk-link', tag_options)
+      link_to body, url, **tag_options
+    end
+
+    def govuk_mail_to(body = nil, url = nil, **tag_options)
+      tag_options = prepend_classes('govuk-link', tag_options)
+      mail_to body, url, **tag_options
+    end
+
+    def govuk_skip_link_to(body = nil, url = nil, **tag_options)
+      tag_options = prepend_classes('govuk-skip-link', tag_options)
+      link_to body, url, **tag_options
+    end
+  end
+end

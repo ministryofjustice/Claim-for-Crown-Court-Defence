4f2e31aa8841957439cedc653d0c0b87795c626f
Peter Hunt <naseberry@users.noreply.github.com>
2020-06-29 09:50:36 +0100

    Update panel spec

--- spec/lib/govuk_component/panel_helpers_spec.rb
+++ spec/lib/govuk_component/panel_helpers_spec.rb
@@ -1,15 +1,35 @@
 # frozen_string_literal: true
 
 RSpec.describe GovukComponent::PanelHelpers, type: :helper do
-  describe '#govuk_panel' do
-    it 'renders govuk-panel component' do
-      rendered = helper.govuk_panel('Application complete', 'Your reference number<br><strong>HDJ2123F</strong>')
-      expected_markup = '<div class="govuk-panel govuk-panel--confirmation">'\
-                        '<h1 class="govuk-panel__title">Application complete</h1>'\
-                        '<div class="govuk-panel__body">Your reference number<br><strong>HDJ2123F</strong></div>'\
-                        '</div>'
+  include RSpecHtmlMatchers
 
-      expect(rendered).to eql expected_markup
+  describe '#govuk_panel' do
+    subject(:markup) { helper.govuk_panel('My panel heading', 'My content') }
+
+    it 'adds panel with govuk class' do
+      is_expected.to have_tag(:div, with: { class: 'govuk-panel govuk-panel--confirmation' })
+    end
+
+    it 'adds nested h1 tag with govuk class' do
+      is_expected.to have_tag(:div) do
+        with_tag(:h1, with: { class: 'govuk-panel__title' }, text: 'My panel heading')
+      end
+    end
+
+    it 'adds nested div tag with govuk class' do
+      is_expected.to have_tag(:div) do
+        with_tag(:div, with: { class: 'govuk-panel__body' }, text: 'My content')
+      end
+    end
+
+    context 'with custom classes' do
+      subject(:markup) do
+        helper.govuk_panel('My panel heading', 'My content', class: 'my-custom-class1 my-custom-class2')
+      end
+
+      it 'adds panel with custom classes, prepended by govuk class' do
+        is_expected.to have_tag(:div, with: { class: 'govuk-panel govuk-panel--confirmation my-custom-class1 my-custom-class2' })
+      end
     end
   end
 end

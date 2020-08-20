12b405cda3e36e165865df963d9fb0220b6c2261
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-08-20 12:59:17 +0100

    Revert "Log api requests in Kibana"
    This reverts commit b3ba63d07a16cd64422713fc82c8a57f7c378106.

--- app/interfaces/api/logger.rb
+++ app/interfaces/api/logger.rb
@@ -26,6 +26,6 @@ class API::Logger < Grape::Middleware::Base
 
   def log_api(api_type, data)
     api_request = { log_type: api_type, timestamp: Time.now }.merge!(data)
-    LogStuff.send(:info, api_request)
+    Rails.logger.info api_request.to_json
   end
 end

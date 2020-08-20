77de0cfc42de1f10d31ebad00eb6f0abfae17b66
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-08-20 13:07:23 +0100

    Sentence case all endpoint descriptions
    Applies the standard set for other content
    by content designers for GDS.

--- app/interfaces/api/v1/dropdown_data.rb
+++ app/interfaces/api/v1/dropdown_data.rb
@@ -41,7 +41,7 @@ module API
 
       group do
         resource :case_types do
-          desc 'Return all Case Types'
+          desc 'Return all case types'
           params { use :role_filter }
           get { present CaseType.__send__(role), with: API::Entities::CaseType }
         end
@@ -52,7 +52,7 @@ module API
         end
 
         resource :advocate_categories do
-          desc 'Return all Advocate Categories'
+          desc 'Return all advocate categories'
           params { use :scheme_role_filter }
           get do
             if scheme_role.in?(%i[agfs_scheme_10s agfs_scheme_11s agfs_scheme_12s])
@@ -64,17 +64,17 @@ module API
         end
 
         resource :trial_cracked_at_thirds do
-          desc 'Return all Trial Cracked at Third values (i.e. first, second, final)'
+          desc 'Return all trial cracked at third values (i.e. first, second, final)'
           get { Settings.trial_cracked_at_third }
         end
 
         resource :offence_classes do
-          desc 'Return all Offence Class Types, with the matching offence_id for LGFS claims.'
+          desc 'Return all offence class types, with the matching offence_id for LGFS claims.'
           get { present OffenceClass.all, with: API::Entities::OffenceClass }
         end
 
         resource :offences do
-          desc 'Return all Offence-ids to be used in advocate claims (see OffenceClasses for Litigator claims).'
+          desc 'Return all offence-ids to be used in advocate claims (see OffenceClasses for Litigator claims).'
           params do
             optional :offence_description,
                      type: String,
@@ -114,7 +114,7 @@ module API
                      desc: 'OPTIONAL: The unique identifier of the fee type'
           end
 
-          desc 'Return all AGFS Fee Types (optional category and unique_code filter).'
+          desc 'Return all AGFS fee types (optional category and unique_code filter).'
           get do
             fee_types = Fee::BaseFeeType
             fee_types = category.blank? || category.eql?('all') ? fee_types : fee_types.send(category)
@@ -125,33 +125,33 @@ module API
         end
 
         resource :expense_types do
-          desc 'Return all Expense Types.'
+          desc 'Return all expense types.'
           params { use :role_filter }
           get { present ExpenseType.__send__(role), with: API::Entities::ExpenseType }
         end
 
         resource :expense_reasons do
-          desc 'Return all Expense Reasons by reason set.'
+          desc 'Return all expense reasons by reason set.'
           get { present ExpenseType.reason_sets, with: API::Entities::ExpenseReasonSet }
         end
 
         resource :disbursement_types do
-          desc 'Return all Disbursement Types.'
+          desc 'Return all disbursement types.'
           get { present DisbursementType.active, with: API::Entities::DisbursementType }
         end
 
         resource :transfer_stages do
-          desc 'Return all Transfer Stages'
+          desc 'Return all transfer stages'
           get { present ::Claim::TransferBrain::TRANSFER_STAGES.to_a, with: API::Entities::SimpleKeyValueList }
         end
 
         resource :transfer_case_conclusions do
-          desc 'Return all Transfer Case Conclusions'
+          desc 'Return all transfer case conclusions'
           get { present ::Claim::TransferBrain::CASE_CONCLUSIONS.to_a, with: API::Entities::SimpleKeyValueList }
         end
 
         resource :case_stages do
-          desc 'Return all Case Stages'
+          desc 'Return all case stages'
           params { use :role_filter }
           get { present CaseStage.active.__send__(role), with: API::Entities::CaseStage }
         end

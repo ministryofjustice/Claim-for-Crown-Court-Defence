6d077c673b147da577ff940445d63230bca641cf
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-08-21 13:50:49 +0100

    Syntax tweaks

--- spec/validators/claim/litigator_claim_validator_spec.rb
+++ spec/validators/claim/litigator_claim_validator_spec.rb
@@ -3,13 +3,13 @@ require_relative 'shared_examples_for_advocate_litigator'
 require_relative 'shared_examples_for_step_validators'
 
 RSpec.describe Claim::LitigatorClaimValidator, type: :validator do
-  include_context "force-validation"
+  include_context 'force-validation'
 
   let(:litigator) { build(:external_user, :litigator) }
   let(:claim) { create(:litigator_claim) }
 
   include_examples 'common advocate litigator validations', :litigator
-  include_examples "common litigator validations"
+  include_examples 'common litigator validations'
 
   include_examples 'common partial validations', {
     case_details: %i[

3eb48226755c7686aec9c28e8f4522f52ed7a62d
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-08-28 14:44:32 +0100

    remove pointless model schema annotation

--- app/models/fee/misc_fee_type.rb
+++ app/models/fee/misc_fee_type.rb
@@ -1,21 +1,3 @@
-# == Schema Information
-#
-# Table name: fee_types
-#
-#  id                  :integer          not null, primary key
-#  description         :string
-#  code                :string
-#  created_at          :datetime
-#  updated_at          :datetime
-#  max_amount          :decimal(, )
-#  calculated          :boolean          default(TRUE)
-#  type                :string
-#  roles               :string
-#  parent_id           :integer
-#  quantity_is_decimal :boolean          default(FALSE)
-#  unique_code         :string
-#
-
 class Fee::MiscFeeType < Fee::BaseFeeType
   AGFS_SUPPLEMENTARY_ONLY_TYPES = %w[MISAF MIPCM].freeze
   AGFS_SUPPLEMENTARY_SHARED_TYPES = %w[MISAU MISPF MIWPF MIDTH MIDTW MIDHU MIDWU MIDSE MIDSU MIPHC MIUMU MIUMO].freeze

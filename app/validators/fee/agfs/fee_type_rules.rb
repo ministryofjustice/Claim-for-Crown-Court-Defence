8840fd10cf01e8e0f120b89748c65480ac069e1d
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-09-09 08:57:56 +0100

    Rename namespaces for fee type rules
    receiving unintialized constant warnings
    on circleci (only), possibly related to
    use of capitals for AGFS/LGFS for current
    namespacing of fee_type_rules.

--- app/validators/fee/agfs/fee_type_rules.rb
+++ app/validators/fee/agfs/fee_type_rules.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Fee
-  module AGFS
+  module Agfs
     class FeeTypeRules
       include Fee::Concerns::FeeTypeRulesCreator
 
--- app/validators/fee/lgfs/fee_type_rules.rb
+++ app/validators/fee/lgfs/fee_type_rules.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Fee
-  module LGFS
+  module Lgfs
     class FeeTypeRules
       include Fee::Concerns::FeeTypeRulesCreator
 

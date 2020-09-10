7a3da603bc4667faa20c6f36acc664445e3b4b7a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-09-10 11:36:12 +0100

    Remove `agfs_scheme_11` fee_type dropdown data filter
    There is no agfs_scheme_11 role and therefore we cannot
    filter by this.
    
    We may want to map role filter behavior differently so
    that, but am asking vendors:
    
     - agfs_scheme_11 returns agfs_scheme_10 fee types
     - agfs_scheme_12 returns agfs_scheme_10 + agfs_scheme_12 fee types

--- spec/api/v1/dropdown_data_spec.rb
+++ spec/api/v1/dropdown_data_spec.rb
@@ -360,12 +360,6 @@ RSpec.describe API::V1::DropdownData do
       include_examples 'returns agfs scheme 10+ advocate categories'
     end
 
-    context 'when role is agfs_scheme_11' do
-      let(:role) { 'agfs_scheme_11' }
-
-      include_examples 'returns agfs scheme 10+ advocate categories'
-    end
-
     context 'when role is agfs_scheme_12' do
       let(:role) { 'agfs_scheme_12' }
 

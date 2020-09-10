20a0174cda8eb5ede8552141e7ab6390da8ed773
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2020-09-10 17:13:32 +0100

    Remove Unused material (over 3 hours) quantity validation (#3509)
    Remove Unused material (over 3 hours) quantity validation
    
    We have become aware of a logic issue with the current
    implementation of the CLAR “Unused material (over 3 hours)”
    fee type for AGFS. The regulations stipulate that the first 3
    hours are claimable at the fixed rate in addition to any hours
    over 3 at an hourly rate. This means we will be allowing any
    quantity to be claimed for this fee type where each unit/hour
    should be in excess of the first 3 hours.
    
    example:
      3 and half hours of work for “Unused material” would be claimable
      as two fees: `quantity: 1` of type “Unused material (upto 3 hours)”
      and `quantity: 0.5` of type “Unused material (over 3 hours)”.

--- app/validators/fee/agfs/fee_type_rules.rb
+++ app/validators/fee/agfs/fee_type_rules.rb
@@ -12,7 +12,6 @@ module Fee
         end
 
         with_set_for_fee_type('MIUMO') do |set|
-          set << add_rule(:quantity, :min, 3.01, message: 'miumo_numericality')
           set << add_rule(*graduated_fee_type_only_rule)
         end
 

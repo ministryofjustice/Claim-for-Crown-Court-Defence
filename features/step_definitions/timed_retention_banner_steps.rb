086a1c85e684f3a15237087d44e8a5cae5f1502e
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-09-15 11:09:22 +0100

    Remove timeout retention enabled flag
    Fix settings.yml

--- features/step_definitions/timed_retention_banner_steps.rb
+++ features/step_definitions/timed_retention_banner_steps.rb
@@ -1,7 +1,3 @@
-Given(/^The timed retention banner feature flag is enabled$/) do
-  allow(Settings).to receive(:timed_retention_banner_enabled?).and_return(true)
-end
-
 And(/^The timed retention banner (is|is not) visible$/) do |visibility|
   selector = 'div.js-callout-banner[data-setting=timed_retention_banner_seen]'
 

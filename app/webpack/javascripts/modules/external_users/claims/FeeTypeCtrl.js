f9df78b358041b50d77968aa439d86d77ad644ac
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-09-15 13:12:38 +0100

    remove console.logs from js

--- app/webpack/javascripts/modules/external_users/claims/FeeTypeCtrl.js
+++ app/webpack/javascripts/modules/external_users/claims/FeeTypeCtrl.js
@@ -1,31 +1,26 @@
 moj.Modules.FeeTypeCtrl = {
   init: function () {
-    console.log('init');
     if ($('#misc-fees').exists()) {
       this.bindEvents();
     }
   },
 
   bindEvents: function () {
-    console.log('bindEvents');
     this.miscFeeTypesSelectChange();
     this.miscFeeTypesRadioChange();
     this.pageLoad();
   },
 
   getFeeTypeSelectUniqueCode: function (context) {
-    console.log('getFeeTypeSelectUniqueCode');
     return $(context).closest('.fx-fee-group').find('option:selected').data('unique-code');
   },
 
   getFeeTypeRadioUniqueCode: function (context) {
-    console.log('getFeeTypeRadioUniqueCode');
     return $(context).closest('.fx-fee-group').find(':checked').data('unique-code');
   },
 
   // needs to be usable by cocoon:after-insert so can bind to one or many elements
   miscFeeTypesSelectChange: function ($el) {
-    console.log('miscFeeTypesSelectChange');
     var self = this;
     var $els = $el || $('.fx-fee-group');
 
@@ -38,7 +33,6 @@ moj.Modules.FeeTypeCtrl = {
 
   // needs to be usable by cocoon:after-insert so can bind to one or many elements
   miscFeeTypesRadioChange: function ($el) {
-    console.log('miscFeeTypesRadioChange');
     var self = this;
     var $els = $el || $('.fx-fee-group');
 
@@ -50,14 +44,12 @@ moj.Modules.FeeTypeCtrl = {
   },
 
   showHideUnusedMaterialWarning: function (context, unique_code) {
-    console.log('showHideUnusedMaterialWarning');
     show = (unique_code == 'MIUMO');
     var $warning = $(context).closest('.fx-fee-group').find('.fx-unused-materials-warning');
     show ? $warning.removeClass('js-hidden') : $warning.addClass('js-hidden');
   },
 
   pageLoad: function () {
-    console.log('pageLoad');
     var $radios = $('.fee-type input[type=radio]');
     var self = this;
 

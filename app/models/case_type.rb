4c7f459a492c91276db6b601b84fb7903ffa7ed4
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-09-16 11:31:02 +0100

    Add scope for `.trail_fees`
    Return case types that are kinds of trial fees.
    This is essnetial graduated fee case types but
    excluding discontinuance and guilty plea.
    
    This is for use in validatin gcertain fee types,
    specifically the unused material fee type.

--- app/models/case_type.rb
+++ app/models/case_type.rb
@@ -30,6 +30,7 @@ class CaseType < ApplicationRecord
   scope :fixed_fee,               -> { where(is_fixed_fee: true) }
   scope :not_fixed_fee,           -> { where(is_fixed_fee: false) }
   scope :graduated_fees,          -> { where(fee_type_code: Fee::GraduatedFeeType.pluck(:unique_code)) }
+  scope :trial_fees,              -> { where(fee_type_code: %w[GRCBR GRRAK GRRTR GRTRL]) }
   scope :requires_cracked_dates,  -> { where(requires_cracked_dates: true) }
   scope :requires_trial_dates,    -> { where(requires_trial_dates: true) }
   scope :requires_retrial_dates,  -> { where(requires_retrial_dates: true) }

e009ddacb90284daa679e47113b7c453c658c0e5
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-09-16 15:09:52 +0100

    Add scope to return misc fees excluding those for trial fees
    Needed to not display fee types for "trial" case type
    claims that are not applicable/claimable/valid
    
    The unused materials fee types can only be claimed on "trial"
    type claims.

--- app/models/fee/misc_fee_type.rb
+++ app/models/fee/misc_fee_type.rb
@@ -2,11 +2,13 @@ class Fee::MiscFeeType < Fee::BaseFeeType
   AGFS_SUPPLEMENTARY_ONLY_TYPES = %w[MISAF MIPCM].freeze
   AGFS_SUPPLEMENTARY_SHARED_TYPES = %w[MISAU MISPF MIWPF MIDTH MIDTW MIDHU MIDWU MIDSE MIDSU MIPHC MIUMU MIUMO].freeze
   AGFS_SUPPLEMENTARY_TYPES = (AGFS_SUPPLEMENTARY_ONLY_TYPES + AGFS_SUPPLEMENTARY_SHARED_TYPES).freeze
+  TRIAL_ONLY_TYPES = %w[MIUMU MIUMO].freeze
 
   default_scope { order(description: :asc) }
 
   scope :without_supplementary_only, -> { where.not(unique_code: AGFS_SUPPLEMENTARY_ONLY_TYPES) }
   scope :supplementary, -> { where(unique_code: AGFS_SUPPLEMENTARY_TYPES) }
+  scope :without_trial_fee_only, -> { where.not(unique_code: TRIAL_ONLY_TYPES) }
 
   def fee_category_name
     'Miscellaneous Fees'

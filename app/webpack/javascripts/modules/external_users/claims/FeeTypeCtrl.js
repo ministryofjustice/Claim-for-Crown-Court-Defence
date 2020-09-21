e3ad36933a2e3d701083b679b6b31ebed2b372b3
Peter Hunt <naseberry@users.noreply.github.com>
2020-09-21 16:56:37 +0100

    Add unused fee quantity logic

--- app/webpack/javascripts/modules/external_users/claims/FeeTypeCtrl.js
+++ app/webpack/javascripts/modules/external_users/claims/FeeTypeCtrl.js
@@ -12,6 +12,7 @@ moj.Modules.FeeTypeCtrl = {
   bindEvents: function () {
     this.miscFeeTypesSelectChange();
     this.miscFeeTypesRadioChange();
+    this.miscFeeTypesCheckboxChange();
     this.pageLoad();
   },
 
@@ -23,6 +24,10 @@ moj.Modules.FeeTypeCtrl = {
     return $(context).closest('.fx-fee-group').find(':checked').data('unique-code');
   },
 
+  getFeeTypeCheckboxUniqueCode: function (context) {
+    return $(context).closest('.fx-fee-group').find(':checked').data('unique-code');
+  },
+
   // needs to be usable by cocoon:after-insert so can bind to one or many elements
   miscFeeTypesSelectChange: function ($el) {
     var self = this;
@@ -31,6 +36,11 @@ moj.Modules.FeeTypeCtrl = {
     if ($('.fx-unused-materials-warning').exists()) {
       $els.change(function () {
         self.showHideUnusedMaterialWarning(this, self.getFeeTypeSelectUniqueCode(this));
+      });
+    }
+    if ($('.fee-quantity').exists()) {
+      $els.change(function () {
+        self.readOnlyQuantity(this, self.getFeeTypeSelectUniqueCode(this));
       });
     }
   },
@@ -47,10 +57,34 @@ moj.Modules.FeeTypeCtrl = {
     }
   },
 
+  miscFeeTypesCheckboxChange: function ($el) {
+    var self = this;
+    var $els = $el || $('.fx-fee-group');
+
+    if ($('input.fee-quantity').exists()) {
+      $els.change(function () {
+        self.readOnlyQuantity(this, self.getFeeTypeCheckboxUniqueCode(this));
+      });
+    }
+  },
+
   showHideUnusedMaterialWarning: function (context, unique_code) {
     show = (unique_code == 'MIUMO');
     var $warning = $(context).closest('.fx-fee-group').find('.fx-unused-materials-warning');
     show ? $warning.removeClass('js-hidden') : $warning.addClass('js-hidden');
+  },
+
+  readOnlyQuantity: function (context, unique_code) {
+    readOnly = (unique_code == 'MIUMU');
+    var defaultQuantity = 1;
+    var $quantity = $(context).closest('.fx-fee-group').find('input.fee-quantity');
+    if(readOnly){
+      $quantity.val(defaultQuantity);
+      $quantity.attr('readonly', true);
+    } else {
+      $quantity.val();
+      $quantity.removeAttr('readonly');
+    }
   },
 
   pageLoad: function () {
@@ -59,11 +93,17 @@ moj.Modules.FeeTypeCtrl = {
     $(document).ready(function () {
       $('.js-fee-type:visible').each(function () {
         self.showHideUnusedMaterialWarning(this, self.getFeeTypeSelectUniqueCode(this));
+        self.readOnlyQuantity(this, self.getFeeTypeSelectUniqueCode(this));
       });
 
       $('.fee-type input[type=radio]:checked').each(function() {
         self.showHideUnusedMaterialWarning(this, self.getFeeTypeRadioUniqueCode(this));
       });
+
+      $('.multiple-choice input[type=checkbox]:checked').each(function() {
+        self.showHideUnusedMaterialWarning(this, self.getFeeTypeCheckboxUniqueCode(this));
+      });
+
     });
   }
 };

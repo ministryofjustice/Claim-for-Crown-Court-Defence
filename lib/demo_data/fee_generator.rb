2700ff8d505d2d2e3181dc27e27857ec54bb0dc3
Katharine Ahern <katharine.ahern@legalaid.gsi.gov.uk>
2020-09-28 16:34:27 +0100

    Fix demo data creation

--- lib/demo_data/fee_generator.rb
+++ lib/demo_data/fee_generator.rb
@@ -5,7 +5,11 @@ module DemoData
     #
     def initialize(claim, fee_type_class, fee_types = nil)
       @claim          = claim
-      @fee_types      = fee_types || fee_type_class.agfs
+      if @claim.agfs?
+        @fee_types      = fee_types || fee_type_class.agfs
+      else
+        @fee_types = Fee::MiscFeeType.lgfs.where(unique_code: %w[MICJA MICJP MIEVI MISPF])
+      end
       @codes_added    = []
       @fee_type_class = fee_type_class
       @fee_class      = derive_fee_class_from_fee_type_class
@@ -18,8 +22,19 @@ module DemoData
     private
 
     def add_fee
-      fee_type = validatable_fee_types.sample
-      @fee_class.create(claim: @claim, fee_type: fee_type, quantity: rand(1..10), rate: rand(25..75).round(2))
+      if @claim.agfs?
+        fee_type = validatable_agfs_fee_types.sample
+        while @codes_added.include?(fee_type.code)
+          fee_type = validatable_agfs_fee_types.sample
+        end
+        @fee_class.create(claim: @claim, fee_type: fee_type, quantity: rand(1..10), rate: rand(25..75).round(2))
+      else
+        fee_type = @fee_types.sample
+        while @codes_added.include?(fee_type.code)
+          fee_type = validatable_lgfs_misc_fee_types.sample
+        end
+        @fee_class.create(claim: @claim, fee_type: fee_type, amount: rand(25..75).round(2))
+      end
       @codes_added << fee_type.code
     end
 
@@ -31,14 +46,14 @@ module DemoData
     # as claim must have a matching number of defendants
     # and they are uncommon
     #
-    def validatable_fee_types
+    def validatable_agfs_fee_types
       @fee_types.select do |fee_type|
-        fee_type.calculated == true && !fee_type.defendant_uplift? && !fee_type.case_uplift?
+        fee_type.calculated == true && !fee_type.defendant_uplift?
       end
     end
 
     def validatable_lgfs_misc_fee_types
-      @fee_types.select
+      @fee_types
     end
   end
 end

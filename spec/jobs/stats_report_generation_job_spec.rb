badd1c1819f235a9498a938f783c682b10a4f7ec
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-10-07 11:05:28 +0100

    Remove spec of returned result from job
    The jobs returned result is not important
    and prevents adding logging for job completion.

--- spec/jobs/stats_report_generation_job_spec.rb
+++ spec/jobs/stats_report_generation_job_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe StatsReportGenerationJob, type: :job do
-  describe '#perform' do
-    let(:report_type) { 'provisional_assessment' }
-    let(:result) { double(:generator_result) }
+  subject(:job) { described_class.new }
 
-    subject(:job) { described_class.new }
+  describe '#perform' do
+    subject (:perform) { job.perform(report_type) }
+
+    let(:report_type) { 'provisional_assessment' }
 
     it 'calls the stats report generator with the provided report type' do
-      expect(Stats::StatsReportGenerator).to receive(:call).with(report_type).and_return(result)
-      expect(job.perform(report_type)).to eq(result)
+      expect(Stats::StatsReportGenerator).to receive(:call).with(report_type)
+      perform
     end
   end
 end

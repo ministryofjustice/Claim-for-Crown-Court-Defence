c0aa3b0cc787755cc1205af7371859a7d48d251e
Joe Haig <joseph.haig@digital.justice.gov.uk>
2020-12-11 15:40:46 +0000

    Remove unused methods

--- app/models/claims/user_messages.rb
+++ app/models/claims/user_messages.rb
@@ -1,20 +1,4 @@
 module Claims::UserMessages
-  def read_messages
-    user_messages_relation.where('user_message_statuses.read = ?', true)
-  end
-
-  def unread_messages
-    user_messages_relation.where('user_message_statuses.read = ?', false)
-  end
-
-  def has_read_messages?
-    read_messages.any?
-  end
-
-  def has_unread_messages?
-    unread_messages.any?
-  end
-
   def has_unread_messages_for?(user)
     unread_messages_for(user).any?
   end

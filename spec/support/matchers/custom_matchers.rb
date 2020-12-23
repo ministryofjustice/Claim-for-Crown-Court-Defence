2779f1f5ace3d0287ee71961cc99f530650ec2e3
Joe Haig <joseph.haig@digital.justice.gov.uk>
2020-12-23 11:39:09 +0000

    Remove `contain_claims` custom matcher
    Does not appear to be used anywhere anymore.

--- spec/support/matchers/custom_matchers.rb
+++ spec/support/matchers/custom_matchers.rb
@@ -1,21 +1,5 @@
 require 'rspec/expectations'
 
-RSpec::Matchers.define :contain_claims do |*expected|
-  match do |actual|
-    result = expected.size == actual.size
-    expected.each do |e|
-      unless actual.include?(e)
-        result = false
-        break
-      end
-    end
-  end
-
-  failure_message do |actual|
-    "expected that records:\n\t #{actual.inspect} \n\nwould be equal to records\n\t #{expected.inspect}"
-  end
-end
-
 # Given a claim, returns true if specified attributes on claim
 # plus derived totals match the values specified.
 # e.g.

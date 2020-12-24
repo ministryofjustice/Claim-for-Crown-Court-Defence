010cd64a64f3694bf7052951b2c2791bf69f3901
Joe Haig <joseph.haig@digital.justice.gov.uk>
2020-12-24 09:40:06 +0000

    Enable Active Storage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
     url: "tmp/dev/reports/:filename"
   }
 
+  config.active_storage.service = :local
+  # config.active_storage.service = :amazon_dev
+
   # logging
   jsonlogger = LogStuff.new_logger("#{Rails.root}/log/logstash_development.log", Logger::INFO)
   config.logstasher.enabled = true

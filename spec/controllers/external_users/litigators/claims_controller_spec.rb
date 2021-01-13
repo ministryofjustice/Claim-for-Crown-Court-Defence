70493fef23a466d67203cac368c7072f68fb86b5
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-01-13 09:43:55 +0000

    Rubocop: Layout/MultilineHashBraceLayout

--- spec/controllers/external_users/litigators/claims_controller_spec.rb
+++ spec/controllers/external_users/litigators/claims_controller_spec.rb
@@ -73,7 +73,8 @@ RSpec.describe ExternalUsers::Litigators::ClaimsController, type: :controller do
               expense_params
             ],
             defendants_attributes: [
-              { first_name: 'John',
+              {
+                first_name: 'John',
                 last_name: 'Smith',
                 date_of_birth_dd: '4',
                 date_of_birth_mm: '10',
@@ -425,18 +426,28 @@ RSpec.describe ExternalUsers::Litigators::ClaimsController, type: :controller do
 
     context 'Date Parameter handling' do
       it 'should transform dates with named months into dates' do
-        put :update, params: { id: subject, claim: {
-          'first_day_of_trial_yyyy' => '2015',
-          'first_day_of_trial_mm' => 'jan',
-          'first_day_of_trial_dd' => '4' }, commit_submit_claim: 'Submit to LAA' }
+        put :update, params: {
+          id: subject,
+          claim: {
+            'first_day_of_trial_yyyy' => '2015',
+            'first_day_of_trial_mm' => 'jan',
+            'first_day_of_trial_dd' => '4'
+          },
+          commit_submit_claim: 'Submit to LAA'
+        }
         expect(assigns(:claim).first_day_of_trial).to eq Date.new(2015, 1, 4)
       end
 
       it 'should transform dates with numbered months into dates' do
-        put :update, params: { id: subject, claim: {
-          'first_day_of_trial_yyyy' => '2015',
-          'first_day_of_trial_mm' => '11',
-          'first_day_of_trial_dd' => '4' }, commit_submit_claim: 'Submit to LAA' }
+        put :update, params: {
+          id: subject,
+          claim: {
+            'first_day_of_trial_yyyy' => '2015',
+            'first_day_of_trial_mm' => '11',
+            'first_day_of_trial_dd' => '4'
+          },
+          commit_submit_claim: 'Submit to LAA'
+        }
         expect(assigns(:claim).first_day_of_trial).to eq Date.new(2015, 11, 4)
       end
     end

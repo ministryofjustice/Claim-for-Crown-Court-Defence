d306a23873c21a04eaaaf8a0c570a77a96cafcf4
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-01-18 15:02:52 +0000

    Use `claim` as subject instead of `@claim`

--- spec/models/timed_transitions/transitioner_spec.rb
+++ spec/models/timed_transitions/transitioner_spec.rb
@@ -263,22 +263,26 @@ RSpec.describe TimedTransitions::Transitioner do
         end
 
         context 'when last state transition more than 16 weeks ago' do
-          before do
+          subject(:claim) do
             travel_to(17.weeks.ago) do
-              @claim = create :litigator_claim, :archived_pending_delete, case_number: 'A20164444'
+              create :litigator_claim, :archived_pending_delete, case_number: 'A20164444'
             end
           end
 
           after(:all) { clean_database }
 
           it 'destroys the claims' do
-            described_class.new(@claim).run
-            expect(Claim::BaseClaim.where(id: @claim.id)).to be_empty
+            described_class.new(claim).run
+            expect(Claim::BaseClaim.where(id: claim.id)).to be_empty
+          end
+
+          context 'with test associations' do
+
           end
 
           it 'destroys all associated records', delete: true do
             setup_associations
-            described_class.new(@claim).run
+            described_class.new(claim).run
             expect_claim_and_all_associations_to_be_gone
           end
 
@@ -286,67 +290,67 @@ RSpec.describe TimedTransitions::Transitioner do
             expect(LogStuff).to receive(:info)
               .with('TimedTransitions::Transitioner',
                     action: 'destroy',
-                    claim_id: @claim.id,
+                    claim_id: claim.id,
                     claim_state: 'archived_pending_delete',
-                    softly_deleted_on: @claim.deleted_at,
-                    valid_until: @claim.valid_until,
+                    softly_deleted_on: claim.deleted_at,
+                    valid_until: claim.valid_until,
                     dummy_run: false,
                     error: nil,
                     succeeded: true)
-            described_class.new(@claim).run
+            described_class.new(claim).run
           end
 
           def setup_associations
-            @claim.defendants.first.representation_orders << RepresentationOrder.new
-            2.times { @claim.expenses << Expense.new }
-            2.times { @claim.disbursements << create(:disbursement, claim: @claim) }
-            2.times { @claim.messages << create(:message, claim: @claim) }
-            @claim.injection_attempts << create(:injection_attempt, claim: @claim)
-            @claim.expenses.first.dates_attended << DateAttended.new
-            @claim.documents << create(:document, claim: @claim, verified: true)
-            @claim.certification = create(:certification, claim: @claim)
-            @claim.save!
-            @claim.reload
+            claim.defendants.first.representation_orders << RepresentationOrder.new
+            2.times { claim.expenses << Expense.new }
+            2.times { claim.disbursements << create(:disbursement, claim: claim) }
+            2.times { claim.messages << create(:message, claim: claim) }
+            claim.injection_attempts << create(:injection_attempt, claim: claim)
+            claim.expenses.first.dates_attended << DateAttended.new
+            claim.documents << create(:document, claim: claim, verified: true)
+            claim.certification = create(:certification, claim: claim)
+            claim.save!
+            claim.reload
 
-            @expense = @claim.expenses.first
-            @defendant = @claim.defendants.first
-            @document = @claim.documents.first
+            @expense = claim.expenses.first
+            @defendant = claim.defendants.first
+            @document = claim.documents.first
 
-            expect(@claim.case_worker_claims).not_to be_empty
-            expect(@claim.case_workers).not_to be_empty
-            expect(@claim.fees).not_to be_empty
-            expect(@claim.expenses).not_to be_empty
-            expect(@claim.expenses.first.dates_attended).not_to be_empty
-            expect(@claim.disbursements).not_to be_empty
-            expect(@claim.defendants).not_to be_empty
-            expect(@claim.defendants.first.representation_orders).not_to be_empty
-            expect(@claim.documents).not_to be_empty
-            expect(File.exist?(@claim.documents.first.document.path)).to be true
-            expect(File.exist?(@claim.documents.first.converted_preview_document.path)).to be true
-            expect(@claim.messages).not_to be_empty
-            expect(@claim.claim_state_transitions).not_to be_empty
-            expect(@claim.determinations).not_to be_empty
-            expect(@claim.certification).not_to be_nil
-            expect(@claim.injection_attempts).not_to be_empty
+            expect(claim.case_worker_claims).not_to be_empty
+            expect(claim.case_workers).not_to be_empty
+            expect(claim.fees).not_to be_empty
+            expect(claim.expenses).not_to be_empty
+            expect(claim.expenses.first.dates_attended).not_to be_empty
+            expect(claim.disbursements).not_to be_empty
+            expect(claim.defendants).not_to be_empty
+            expect(claim.defendants.first.representation_orders).not_to be_empty
+            expect(claim.documents).not_to be_empty
+            expect(File.exist?(claim.documents.first.document.path)).to be true
+            expect(File.exist?(claim.documents.first.converted_preview_document.path)).to be true
+            expect(claim.messages).not_to be_empty
+            expect(claim.claim_state_transitions).not_to be_empty
+            expect(claim.determinations).not_to be_empty
+            expect(claim.certification).not_to be_nil
+            expect(claim.injection_attempts).not_to be_empty
           end
 
           def expect_claim_and_all_associations_to_be_gone
-            expect { Claim::BaseClaim.find(@claim.id) }.to raise_error ActiveRecord::RecordNotFound, "Couldn't find Claim::BaseClaim with 'id'=#{@claim.id}"
-            expect(CaseWorkerClaim.where(claim_id: @claim.id)).to be_empty
-            expect(Fee::BaseFee.where(claim_id: @claim_id)).to be_empty
-            expect(Expense.where(claim_id: @claim_id)).to be_empty
+            expect { Claim::BaseClaim.find(claim.id) }.to raise_error ActiveRecord::RecordNotFound, "Couldn't find Claim::BaseClaim with 'id'=#{claim.id}"
+            expect(CaseWorkerClaim.where(claim_id: claim.id)).to be_empty
+            expect(Fee::BaseFee.where(claim_id: claim_id)).to be_empty
+            expect(Expense.where(claim_id: claim_id)).to be_empty
             expect(DateAttended.where(attended_item_id: @expense_id, attended_item_type: 'Expense')).to be_empty
-            expect(Disbursement.where(claim_id: @claim_id)).to be_empty
-            expect(Defendant.where(claim_id: @claim_id)).to be_empty
+            expect(Disbursement.where(claim_id: claim_id)).to be_empty
+            expect(Defendant.where(claim_id: claim_id)).to be_empty
             expect(RepresentationOrder.where(defendant_id: @defendant.id)).to be_empty
-            expect(Document.where(claim_id: @claim.id)).to be_empty
+            expect(Document.where(claim_id: claim.id)).to be_empty
             expect(File.exist?(@document.document.path)).to be false
             expect(File.exist?(@document.converted_preview_document.path)).to be false
-            expect(Message.where(claim_id: @claim_id)).to be_empty
-            expect(ClaimStateTransition.where(claim_id: @claim.id)).to be_empty
-            expect(Determination.where(claim_id: @claim.id)).to be_empty
-            expect(Certification.where(claim_id: @claim.id)).to be_empty
-            expect(InjectionAttempt.where(claim_id: @claim.id)).to be_empty
+            expect(Message.where(claim_id: claim_id)).to be_empty
+            expect(ClaimStateTransition.where(claim_id: claim.id)).to be_empty
+            expect(Determination.where(claim_id: claim.id)).to be_empty
+            expect(Certification.where(claim_id: claim.id)).to be_empty
+            expect(InjectionAttempt.where(claim_id: claim.id)).to be_empty
           end
         end
       end
@@ -425,20 +429,20 @@ RSpec.describe TimedTransitions::Transitioner do
         end
 
         context 'when last state transition more than 16 weeks ago' do
-          before do
+          subject(:claim) do
             travel_to(17.weeks.ago) do
-              @claim = create :litigator_claim, :archived_pending_delete, case_number: 'A20164444'
+              create :litigator_claim, :archived_pending_delete, case_number: 'A20164444'
             end
           end
 
           it 'does not destroy the claim' do
-            described_class.new(@claim, true).run
-            expect(Claim::BaseClaim.where(id: @claim.id)).not_to be_empty
+            described_class.new(claim, true).run
+            expect(Claim::BaseClaim.where(id: claim.id)).not_to be_empty
           end
 
           it 'does not destroy all associated records' do
             setup_associations
-            described_class.new(@claim, true).run
+            described_class.new(claim, true).run
             expect_claim_and_all_associations_to_be_present
           end
 
@@ -447,62 +451,62 @@ RSpec.describe TimedTransitions::Transitioner do
               .with(
                 'TimedTransitions::Transitioner',
                 action: 'destroy',
-                claim_id: @claim.id,
-                claim_state: @claim.state,
-                softly_deleted_on: @claim.deleted_at,
-                valid_until: @claim.valid_until,
+                claim_id: claim.id,
+                claim_state: claim.state,
+                softly_deleted_on: claim.deleted_at,
+                valid_until: claim.valid_until,
                 dummy_run: true,
                 error: nil,
                 succeeded: false
               )
-            described_class.new(@claim, true).run
+            described_class.new(claim, true).run
           end
 
           def setup_associations
-            @claim.defendants.first.representation_orders << RepresentationOrder.new
-            2.times { @claim.expenses << Expense.new }
-            2.times { @claim.disbursements << create(:disbursement, claim: @claim) }
-            2.times { @claim.messages << create(:message, claim: @claim) }
-            @claim.expenses.first.dates_attended << DateAttended.new
-            @claim.documents << create(:document, claim: @claim, verified: true)
-            @claim.certification = create(:certification, claim: @claim)
-            @claim.save!
-            @claim.reload
+            claim.defendants.first.representation_orders << RepresentationOrder.new
+            2.times { claim.expenses << Expense.new }
+            2.times { claim.disbursements << create(:disbursement, claim: claim) }
+            2.times { claim.messages << create(:message, claim: claim) }
+            claim.expenses.first.dates_attended << DateAttended.new
+            claim.documents << create(:document, claim: claim, verified: true)
+            claim.certification = create(:certification, claim: claim)
+            claim.save!
+            claim.reload
 
-            @expense = @claim.expenses.first
-            @defendant = @claim.defendants.first
-            @document = @claim.documents.first
+            @expense = claim.expenses.first
+            @defendant = claim.defendants.first
+            @document = claim.documents.first
 
-            expect(@claim.case_worker_claims).not_to be_empty
-            expect(@claim.case_workers).not_to be_empty
-            expect(@claim.fees).not_to be_empty
-            expect(@claim.expenses).not_to be_empty
-            expect(@claim.expenses.first.dates_attended).not_to be_empty
-            expect(@claim.disbursements).not_to be_empty
-            expect(@claim.defendants).not_to be_empty
-            expect(@claim.defendants.first.representation_orders).not_to be_empty
-            expect(@claim.documents).not_to be_empty
-            expect(File.exist?(@claim.documents.first.document.path)).to be true
-            expect(@claim.messages).not_to be_empty
-            expect(@claim.claim_state_transitions).not_to be_empty
-            expect(@claim.determinations).not_to be_empty
-            expect(@claim.certification).not_to be_nil
+            expect(claim.case_worker_claims).not_to be_empty
+            expect(claim.case_workers).not_to be_empty
+            expect(claim.fees).not_to be_empty
+            expect(claim.expenses).not_to be_empty
+            expect(claim.expenses.first.dates_attended).not_to be_empty
+            expect(claim.disbursements).not_to be_empty
+            expect(claim.defendants).not_to be_empty
+            expect(claim.defendants.first.representation_orders).not_to be_empty
+            expect(claim.documents).not_to be_empty
+            expect(File.exist?(claim.documents.first.document.path)).to be true
+            expect(claim.messages).not_to be_empty
+            expect(claim.claim_state_transitions).not_to be_empty
+            expect(claim.determinations).not_to be_empty
+            expect(claim.certification).not_to be_nil
           end
 
           def expect_claim_and_all_associations_to_be_present
-            expect(Claim::BaseClaim.find(@claim.id)).to eq @claim
-            expect(CaseWorkerClaim.where(claim_id: @claim.id)).not_to be_empty
-            expect(Fee::BaseFee.where(claim_id: @claim.id)).not_to be_empty
-            expect(Expense.where(claim_id: @claim.id)).not_to be_empty
+            expect(Claim::BaseClaim.find(claim.id)).to eq claim
+            expect(CaseWorkerClaim.where(claim_id: claim.id)).not_to be_empty
+            expect(Fee::BaseFee.where(claim_id: claim.id)).not_to be_empty
+            expect(Expense.where(claim_id: claim.id)).not_to be_empty
             expect(DateAttended.where(attended_item_id: @expense.id, attended_item_type: 'Expense')).not_to be_empty
-            expect(Disbursement.where(claim_id: @claim.id)).not_to be_empty
-            expect(Defendant.where(claim_id: @claim.id)).not_to be_empty
+            expect(Disbursement.where(claim_id: claim.id)).not_to be_empty
+            expect(Defendant.where(claim_id: claim.id)).not_to be_empty
             expect(RepresentationOrder.where(defendant_id: @defendant.id)).not_to be_empty
-            expect(Document.where(claim_id: @claim.id)).not_to be_empty
-            expect(Message.where(claim_id: @claim.id)).not_to be_empty
-            expect(ClaimStateTransition.where(claim_id: @claim.id)).not_to be_empty
-            expect(Determination.where(claim_id: @claim.id)).not_to be_empty
-            expect(Certification.where(claim_id: @claim.id)).not_to be_empty
+            expect(Document.where(claim_id: claim.id)).not_to be_empty
+            expect(Message.where(claim_id: claim.id)).not_to be_empty
+            expect(ClaimStateTransition.where(claim_id: claim.id)).not_to be_empty
+            expect(Determination.where(claim_id: claim.id)).not_to be_empty
+            expect(Certification.where(claim_id: claim.id)).not_to be_empty
           end
         end
       end

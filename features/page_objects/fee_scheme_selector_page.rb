49bf28a3d7bc1bc8cfe5d031523e3933b2bba7a8
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-01-25 20:35:01 +0000

    Fix site prism selectors inline with form changes
    The DfE formbuilder has a particular naming
    convention for geneating radio inputs and their labels
    as well as submit buttons.
    
    The selectors used by siteprism for the elements
    needs changing inline with these.

--- features/page_objects/fee_scheme_selector_page.rb
+++ features/page_objects/fee_scheme_selector_page.rb
@@ -1,14 +1,14 @@
 class FeeSchemeSelectorPage < BasePage
   set_url "/external_users/claims/types"
 
-  element :advocate_final_fee, "label[for='claim_type_agfs']"
-  element :advocate_hardship_fee, "label[for='claim_type_agfs_hardship']"
-  element :advocate_interim_fee, "label[for='claim_type_agfs_interim']"
-  element :advocate_supplementary_fee, "label[for='claim_type_agfs_supplementary']"
-  element :litigator_final_fee, "label[for='claim_type_lgfs_final']"
-  element :litigator_interim_fee, "label[for='claim_type_lgfs_interim']"
-  element :litigator_transfer_fee, "label[for='claim_type_lgfs_transfer']"
-  element :litigator_hardship_fee, "label[for='claim_type_lgfs_hardship']"
+  element :advocate_final_fee, "label[for='claim-type-agfs-field']"
+  element :advocate_hardship_fee, "label[for='claim-type-agfs-hardship-field']"
+  element :advocate_interim_fee, "label[for='claim-type-agfs-interim-field']"
+  element :advocate_supplementary_fee, "label[for='claim-type-agfs-supplementary-field']"
+  element :litigator_final_fee, "label[for='claim-type-lgfs-final-field']"
+  element :litigator_interim_fee, "label[for='claim-type-lgfs-interim-field']"
+  element :litigator_transfer_fee, "label[for='claim-type-lgfs-transfer-field']"
+  element :litigator_hardship_fee, "label[for='claim-type-lgfs-hardship-field']"
 
-  element :continue, "div.form-group > button:nth-of-type(1)"
+  element :continue, "input[type='submit']"
 end

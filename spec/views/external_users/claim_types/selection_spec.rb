24ed0f7a9bb44e4aab69454ffa252f69909bc415
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-01-25 20:47:08 +0000

    Fix and refactor selection view spec inline with changes
    There is no default now (GDS good practice), and
    refactor to only have one test/expectation per
    example.

--- spec/views/external_users/claim_types/selection_spec.rb
+++ spec/views/external_users/claim_types/selection_spec.rb
@@ -1,78 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe 'external_users/claim_types/selection.html.haml', type: :view do
+  let(:external_user) { create(:external_user) }
+
   before(:each) do
     initialize_view_helpers(view)
+    sign_in(external_user.user)
   end
 
   include_context 'claim-types helpers'
 
   context 'claim type options' do
-    context 'when logged in as AGFS/LGFS admin' do
-      let(:external_user) { create(:external_user, :agfs_lgfs_admin) }
-
+    context 'with all available_claim_types' do
       before do
-        sign_in(external_user.user)
         assign(:available_claim_types, all_claim_types)
         render
       end
 
-      it 'should include advocate fee, litigator final, interim and transfer fee options' do
-        expect(response.body).to include('Advocate final fee')
-        expect(response.body).to include('Advocate warrant fee')
-        expect(response.body).to include('Advocate supplementary fee')
-        expect(response.body).to include('Advocate hardship fee')
-        expect(response.body).to include('Litigator final fee')
-        expect(response.body).to include('Litigator interim fee')
-        expect(response.body).to include('Litigator transfer fee')
-      end
-
-      it 'should default to selecting Advocate final fee' do
-        expect(response.body).to have_checked_field(:claim_type_agfs)
+      it 'should include advocate and litigator claim types' do
+        expect(response.body).to include('Advocate final fee',
+                                         'Advocate warrant fee',
+                                         'Advocate supplementary fee',
+                                         'Advocate hardship fee',
+                                         'Litigator final fee',
+                                         'Litigator interim fee',
+                                         'Litigator transfer fee',
+                                         'Litigator hardship fee')
       end
     end
 
-    context 'when logged in as litigator' do
-      let(:external_user) { create(:external_user, :litigator) }
-
+    context 'with lgfs available_claim_types' do
       before do
-        sign_in(external_user.user)
         assign(:available_claim_types, lgfs_claim_types)
         render
       end
 
-      it 'should include litigator final, interim and transfer fee options only' do
-        expect(response.body).not_to include('Advocate final fee')
-        expect(response.body).not_to include('Advocate warrant fee')
-        expect(response.body).not_to include('Advocate supplementary fee')
-        expect(response.body).not_to include('Advocate hardship fee')
-        expect(response.body).to include('Litigator final fee')
-        expect(response.body).to include('Litigator interim fee')
-        expect(response.body).to include('Litigator transfer fee')
+      it 'should include litigator claim types' do
+        expect(response.body).to include('Litigator final fee',
+                                         'Litigator interim fee',
+                                         'Litigator transfer fee',
+                                         'Litigator hardship fee')
       end
 
-      it 'should default to selecting Litigator final fee' do
-        expect(response.body).to have_checked_field(:claim_type_lgfs_final)
+      it 'should not include advocate claim types' do
+        expect(response.body).not_to include('Advocate final fee',
+                                             'Advocate warrant fee',
+                                             'Advocate supplementary fee',
+                                             'Advocate hardship fee')
       end
     end
 
-    context 'when logged in as advocate' do
-      let(:external_user) { create(:external_user, :advocate) }
-
+    context 'with agfs available_claim_types' do
       before do
-        sign_in(external_user.user)
         assign(:available_claim_types, agfs_claim_types)
         render
       end
 
-      it 'should include advocate fee options only' do
-        expect(response.body).to include('Advocate final fee')
-        expect(response.body).to include('Advocate warrant fee')
-        expect(response.body).to include('Advocate supplementary fee')
-        expect(response.body).to include('Advocate hardship fee')
-        expect(response.body).not_to include('Litigator final fee')
-        expect(response.body).not_to include('Litigator interim fee')
-        expect(response.body).not_to include('Litigator transfer fee')
+      it 'should include advocate claim types' do
+        expect(response.body).to include('Advocate final fee',
+                                         'Advocate warrant fee',
+                                         'Advocate supplementary fee',
+                                         'Advocate hardship fee')
+      end
+
+      it 'should not include litigator claim types' do
+        expect(response.body).not_to include('Litigator final fee',
+                                             'Litigator interim fee',
+                                             'Litigator transfer fee',
+                                             'Litigator hardship fee')
       end
     end
   end

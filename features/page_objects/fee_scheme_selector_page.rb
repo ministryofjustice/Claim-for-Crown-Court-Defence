5e2eb51a5edaa72e34dcf6b7335c89e91a9cf532
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-01-26 15:30:08 +0000

    Fix fee scheme/claim-type selectors

--- features/page_objects/fee_scheme_selector_page.rb
+++ features/page_objects/fee_scheme_selector_page.rb
@@ -1,14 +1,14 @@
 class FeeSchemeSelectorPage < BasePage
   set_url "/external_users/claims/types"
 
-  element :advocate_final_fee, "label[for='claim-type-agfs-field']"
-  element :advocate_hardship_fee, "label[for='claim-type-agfs-hardship-field']"
-  element :advocate_interim_fee, "label[for='claim-type-agfs-interim-field']"
-  element :advocate_supplementary_fee, "label[for='claim-type-agfs-supplementary-field']"
-  element :litigator_final_fee, "label[for='claim-type-lgfs-final-field']"
-  element :litigator_interim_fee, "label[for='claim-type-lgfs-interim-field']"
-  element :litigator_transfer_fee, "label[for='claim-type-lgfs-transfer-field']"
-  element :litigator_hardship_fee, "label[for='claim-type-lgfs-hardship-field']"
+  element :advocate_final_fee, "label[for='claim-type-id-agfs-field']"
+  element :advocate_hardship_fee, "label[for='claim-type-id-agfs-hardship-field']"
+  element :advocate_interim_fee, "label[for='claim-type-id-agfs-interim-field']"
+  element :advocate_supplementary_fee, "label[for='claim-type-id-agfs-supplementary-field']"
+  element :litigator_final_fee, "label[for='claim-type-id-lgfs-final-field']"
+  element :litigator_interim_fee, "label[for='claim-type-id-lgfs-interim-field']"
+  element :litigator_transfer_fee, "label[for='claim-type-id-lgfs-transfer-field']"
+  element :litigator_hardship_fee, "label[for='claim-type-id-lgfs-hardship-field']"
 
   element :continue, "input[value='Continue']"
 end

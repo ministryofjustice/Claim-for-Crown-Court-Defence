e322bc23ba7a02a7bf2ff6957b05148305bf44dd
Peter Hunt <naseberry@users.noreply.github.com>
2021-02-15 15:14:35 +0000

    [CBO-1695] Remove govuk_template gem
    Remove the govuk_template gem and all traces of it within the codebase

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,14 +3,6 @@
 // see https://github.com/rails/sprockets-rails/issues/444
 // and https://github.com/rails/sprockets-rails/blob/v3.2.1/lib/sprockets/railtie.rb#L103-L110
 //
-
-//= link govuk-template.css
-//= link govuk-template-ie6.css
-//= link govuk-template-ie7.css
-//= link govuk-template-ie8.css
-//= link govuk-template-print.css
-//= link fonts.css
-
 // swagger docs
 //= link grape_swagger_rails/application.css
 //= link grape_swagger_rails/application.js

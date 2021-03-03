f688a5df7b9e73e810513ac744f9dda036e3e9da
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-03-03 12:59:29 +0000

    Remove check that attachment is a Paperclip
    This check is unecessary and, with the move to Active Storage, it is no longer
    valid.
    
    I have also done some refactoring on this file:
    
    * Using a subject (`call_report_generator`) for all tests
    * Move some `let!` statements into `begin`
    * Only one `expect` in each test

--- spec/services/stats/stats_report_generator_spec.rb
+++ spec/services/stats/stats_report_generator_spec.rb
@@ -50,6 +50,7 @@ RSpec.describe Stats::StatsReportGenerator, type: :service do
       before do
         allow(Stats::ManagementInformationGenerator).to receive(:call).and_raise(StandardError)
       end
+    end
 
       it 'raises the error' do
         expect { call_report_generator }.to raise_error(StandardError)

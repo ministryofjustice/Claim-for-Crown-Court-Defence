fe8f33d70257c0476a8d321cb5aee03526749b23
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-03-20 23:18:42 +0000

    Handle panel_helpers keyword parameter deprecation
    Fix the deprecation, that will break in ruby 3.0
    ``
    lib/govuk_component/panel_helpers.rb:8: warning: Using the last argument as keyword parameters is deprecated; maybe ** should be added to the call
    ```

--- lib/govuk_component/panel_helpers.rb
+++ lib/govuk_component/panel_helpers.rb
@@ -5,7 +5,7 @@ module GovukComponent
     def govuk_panel(title = nil, body = nil, tag_options = {})
       tag_options = prepend_classes('govuk-panel govuk-panel--confirmation', tag_options)
 
-      tag.div(tag_options) do
+      tag.div(**tag_options) do
         concat tag.h1(sanitize(title), class: 'govuk-panel__title')
         concat tag.div(sanitize(body), class: 'govuk-panel__body')
       end

7fbe03c3563c7de4b5dff21d3c10b9041ea96633
Mabel Li <mabel.li@digital.justice.gov.uk>
2021-03-25 16:49:16 +0000

    Replace cookies controller specs with request specs

--- spec/requests/cookies_request_spec.rb
+++ spec/requests/cookies_request_spec.rb
@@ -2,9 +2,60 @@ require 'rails_helper'
 
 RSpec.describe 'Cookies', type: :request do
   describe 'GET /help/cookies' do
+    before { get '/help/cookies' }
+
     it 'returns http success' do
-      get '/help/cookies'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'assigns @cookies' do
+      expect(assigns(:cookies)).to be_instance_of Cookies
+    end
+
+    it 'renders the template' do
+      expect(response).to render_template(:new)
+    end
+
+    it 'sets @cookies.analytics based on value of cookies' do
+      expect(assigns(:cookies).analytics.to_s).to eq cookies[:usage_opt_in]
+    end
+  end
+
+  describe 'POST /help/cookies' do
+    context 'when cookies are valid' do
+      before { post '/help/cookies', params: { cookies: { analytics: true } } }
+
+      it 'sets the flash message' do
+        expect(flash[:success]).to eq I18n.t('cookies.new.cookie_notification')
+      end
+
+      it 'sets the usage_opt_in cookie' do
+        expect(cookies[:usage_opt_in]).to eq 'true'
+      end
+
+      it 'sets the cookies_preference cookie' do
+        expect(cookies[:cookies_preference]).to eq 'true'
+      end
+
+      it 'assigns the value of @cookies_preferences_set' do
+        expect(assigns(:cookies_preferences_set)).to eq true
+      end
+
+      it 'redirects to cookies path' do
+        expect(response).to redirect_to cookies_path
+      end
+    end
+
+    context 'when cookies are invalid' do
+      before { post '/help/cookies', params: { cookies: { analytics: nil } } }
+
+      it 'renders the new template' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'sets an error message' do
+        expect(assigns(:cookies).errors[:analytics]).to include 'Choose analytics cookie setting'
+      end
     end
   end
 

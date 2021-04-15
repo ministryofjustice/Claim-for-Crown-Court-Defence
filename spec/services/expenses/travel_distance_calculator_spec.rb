27f9ec7d905fe9ada640bc6882a51558563d14ce
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-04-15 16:37:06 +0100

    Rubocop cleanup

--- spec/services/expenses/travel_distance_calculator_spec.rb
+++ spec/services/expenses/travel_distance_calculator_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Expenses::TravelDistanceCalculator, type: :service do
+  subject(:service) { described_class.new(claim, params) }
+
   let(:supplier_number) { '9A999I' }
   let(:supplier_postcode) { 'MK40 3TN' }
-  let!(:supplier) { create(:supplier_number, supplier_number: supplier_number, postcode: supplier_postcode) }
   let(:claim) { create(:litigator_claim, supplier_number: supplier_number) }
   let(:destination) { 'MK40 1HG' }
   let(:params) { { destination: destination } }
 
-  subject(:service) { described_class.new(claim, params) }
+  before { create(:supplier_number, supplier_number: supplier_number, postcode: supplier_postcode) }
 
-  context 'but the associated claim does not exist' do
+  context 'when the associated claim does not exist' do
     let(:claim) { nil }
 
     it 'returns a failure result with the appropriate error code' do
@@ -21,7 +22,7 @@ RSpec.describe Expenses::TravelDistanceCalculator, type: :service do
     end
   end
 
-  context 'but the associated claim is not for LGFS' do
+  context 'when the associated claim is not for LGFS' do
     let(:claim) { create(:advocate_claim) }
 
     it 'returns a failure result with the appropriate error code' do
@@ -32,8 +33,8 @@ RSpec.describe Expenses::TravelDistanceCalculator, type: :service do
     end
   end
 
-  context 'but the supplier associated with the claim does not have a postcode set' do
-    let!(:supplier) { create(:supplier_number, supplier_number: supplier_number, postcode: nil) }
+  context 'when the supplier associated with the claim does not have a postcode set' do
+    let(:supplier_postcode) { nil }
 
     it 'returns a failure result with the appropriate error code' do
       result = service.call
@@ -43,7 +44,7 @@ RSpec.describe Expenses::TravelDistanceCalculator, type: :service do
     end
   end
 
-  context 'but the destination was not provided' do
+  context 'when the destination was not provided' do
     let(:params) { { foo: 'bar' } }
 
     it 'returns a failure result with the appropriate error code' do
@@ -54,7 +55,7 @@ RSpec.describe Expenses::TravelDistanceCalculator, type: :service do
     end
   end
 
-  context 'but the destination is not a postcode' do
+  context 'when the destination is not a postcode' do
     let(:params) { { destination: 'London' } }
 
     it 'returns a failure result with the appropriate error code' do
@@ -65,7 +66,7 @@ RSpec.describe Expenses::TravelDistanceCalculator, type: :service do
     end
   end
 
-  context 'but the distance cannot be calculated' do
+  context 'when the distance cannot be calculated' do
     it 'returns nil as the calculated distance' do
       expect(Maps::DistanceCalculator).to receive(:call).with(supplier_postcode, destination).and_return(nil)
 

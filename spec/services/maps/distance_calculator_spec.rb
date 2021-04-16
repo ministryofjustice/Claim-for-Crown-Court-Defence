eeec5202f7efc3a8f4614a8cf701c504936dfb48
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-04-16 09:40:05 +0100

    Reorganize and amend distance calculator spec
    1. Use `.class_method` block description for clarity
    and perform the class method call as the subject.
    
    2. remove misleading example description and
    add additional concrete test for response
    (because this black box tests the max distance
    being returned).

--- spec/services/maps/distance_calculator_spec.rb
+++ spec/services/maps/distance_calculator_spec.rb
@@ -3,29 +3,31 @@ require 'rails_helper'
 RSpec.describe Maps::DistanceCalculator, type: :service do
   let(:options) { {} }
 
-  subject(:service) { -> { described_class.call(origin, destination, options) } }
+  describe '.call' do
+    subject(:call) { described_class.call(origin, destination, options) }
 
-  context 'when a valid origin and destination are provided', vcr: { cassette_name: 'maps/valid_result' } do
-    let(:origin) { 'SW1A 2BJ' }
-    let(:destination) { 'MK40 1HG' }
+    let(:options) { {} }
 
-    it 'returns the maximum distance from the returned results' do
-      result = service.call
-      expect(result).to be_kind_of(Numeric)
+    context 'when a valid origin and destination are provided', vcr: { cassette_name: 'maps/valid_result' } do
+      let(:origin) { 'SW1A 2BJ' }
+      let(:destination) { 'MK40 1HG' }
+
+      it { is_expected.to be_kind_of(Numeric) }
+      it { is_expected.to be(118_943) }
     end
-  end
 
-  context 'when an invalid origin is provided', vcr: { cassette_name: 'maps/invalid_origin_result' } do
-    let(:origin) { 'A galaxy far far away' }
-    let(:destination) { 'MK40 1HG' }
+    context 'when an invalid origin is provided', vcr: { cassette_name: 'maps/invalid_origin_result' } do
+      let(:origin) { 'A galaxy far far away' }
+      let(:destination) { 'MK40 1HG' }
 
-    it { expect(service.call).to be_nil }
-  end
+      it { is_expected.to be_nil }
+    end
 
-  context 'when an invalid destination is provided', vcr: { cassette_name: 'maps/invalid_destination_result' } do
-    let(:origin) { 'SW1A 2BJ' }
-    let(:destination) { 'A galaxy far far away' }
+    context 'when an invalid destination is provided', vcr: { cassette_name: 'maps/invalid_destination_result' } do
+      let(:origin) { 'SW1A 2BJ' }
+      let(:destination) { 'A galaxy far far away' }
 
-    it { expect(service.call).to be_nil }
+      it { is_expected.to be_nil }
+    end
   end
 end

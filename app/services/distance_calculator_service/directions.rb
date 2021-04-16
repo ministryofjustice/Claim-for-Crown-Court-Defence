4448735e7d7c9b8756d4c1fd2835e9f0b77cb531
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-04-16 09:46:28 +0100

    Refactor

--- app/services/distance_calculator_service/directions.rb
+++ app/services/distance_calculator_service/directions.rb
@@ -2,35 +2,36 @@ class DistanceCalculatorService
   class Directions
     GOOGLE_DIRECTIONS_API = 'https://maps.google.com/maps/api/directions/json'.freeze
 
-    attr_reader :origin, :destination, :options
+    delegate :max_distance, to: :results
 
     def initialize(origin, destination, options = {})
       @origin = origin
       @destination = destination
-      @options = options || {}
-    end
-
-    def max_distance
-      result = DistanceCalculatorService::DirectionsResult.new(routes)
-      result.max_distance
-    rescue StandardError => e
-      log("Failed to calculating distance form #{origin} to #{destination}", error: e, level: :error)
-      nil
+      @options = options
     end
 
     private
 
+    def results
+      @results ||= DistanceCalculatorService::DirectionsResult.new(routes)
+    end
+
     def routes
-      JSON.parse(directions.body)['routes']
+      @routes ||= begin
+        JSON.parse(directions.body)['routes']
+      rescue StandardError => e
+        log("Failed to calculating distance form #{@origin} to #{@destination}", error: e, level: :error)
+        []
+      end
     end
 
     def directions
-      params = query_options.merge(origin: origin, destination: destination)
+      params = query_options.merge(origin: @origin, destination: @destination)
       RestClient.get GOOGLE_DIRECTIONS_API, params: params
     end
 
     def query_options
-      default_options.merge(options)
+      default_options.merge(@options)
     end
 
     def default_options
@@ -46,8 +47,8 @@ class DistanceCalculatorService
         level,
         class: 'DistanceCalculatorService::Directions',
         action: 'call',
-        origin: origin,
-        destination: destination,
+        origin: @origin,
+        destination: @destination,
         error: error ? "#{error.class} - #{error.message}" : 'false'
       ) do
         message

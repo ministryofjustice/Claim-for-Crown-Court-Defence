df005964ae74d07c2bd149bfb02127e87661b959
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-05-03 18:42:40 +0100

    Enforce consistent ordering of strings across postgres distibutions
    Differences in postgres distributions and hosts OS appear to cause
    different handling of braces when ordering by strings. For example
    postgres 9.6 on alpine and postgres 9.6 on darwin/apple order strings
    containing braces consistently but differently from debian and Amazon
    linux hosted versions.
    
    ```on darwin and alpine
    Confiscation hearings (half day uplift)
    Confiscation hearings (half day)
    ```
    
    ```on debian and amazon linux
    Confiscation hearings (half day)
    Confiscation hearings (half day uplift)
    ```

--- app/models/fee/misc_fee_type.rb
+++ app/models/fee/misc_fee_type.rb
@@ -5,7 +5,8 @@ class Fee::MiscFeeType < Fee::BaseFeeType
   AGFS_SUPPLEMENTARY_TYPES = (AGFS_SUPPLEMENTARY_ONLY_TYPES + AGFS_SUPPLEMENTARY_SHARED_TYPES).freeze
   TRIAL_ONLY_TYPES = %w[MIUMU MIUMO].freeze
 
-  default_scope { order(description: :asc) }
+  # default_scope { order(description: :asc) }
+  default_scope { order(Arel.sql("regexp_replace(\"fee_types\".\"description\", '[()]', '', 'g')")) }
 
   scope :without_supplementary_only, -> { where.not(unique_code: AGFS_SUPPLEMENTARY_ONLY_TYPES) }
   scope :supplementary, -> { where(unique_code: AGFS_SUPPLEMENTARY_TYPES) }

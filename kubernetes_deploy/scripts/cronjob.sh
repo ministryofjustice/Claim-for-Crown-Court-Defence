a99a6f7f1008aa786bbd8faedff00e17afdb689a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-05-05 18:32:13 +0100

    Add vacuum DB cronjob
    This can scheduled regularly to reclaim
    postgres DB storage (deleting dead tuples).
    
    It is most useful prior to an upgrade to
    minimize snapshot creation time.

--- kubernetes_deploy/scripts/cronjob.sh
+++ kubernetes_deploy/scripts/cronjob.sh
@@ -3,7 +3,7 @@ function _cronjob() {
   usage="cronjob -- apply job in the specified environment
   Usage: cronjob job environment [branch]
   Where:
-    job [archive_stale|clean_ecr]
+    job [archive_stale|clean_ecr|vacuum_db]
     environment [dev|dev-lgfs|staging|api-sandbox|production]
     branch [<branchname>-latest|commit-sha]
 
@@ -28,7 +28,7 @@ function _cronjob() {
   fi
 
   case "$1" in
-    archive_stale)
+    archive_stale | vacuum_db)
       job=$1
       ;;
     clean_ecr)

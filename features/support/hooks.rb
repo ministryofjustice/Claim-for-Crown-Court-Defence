52a1592a9cc97c10cdb990fd435aff16d107f3fa
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-05-17 21:45:24 +0100

    Add alternative name for @caseworker-seed-requirements hook
    This Before hook just adds vat seeds which is useful in other
    scenarios not requiring full seed, not just for caseworkers.
    
    Specifically when creating a claim using factories there
    is a dependency on VAT rates #2:
    
    Also, rename the OS variable used to store the hooks status
    to match, for clarity.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -48,10 +48,10 @@ Before('not @no-seed') do
     load "#{Rails.root}/db/seeds/certification_types.rb"
     load "#{Rails.root}/db/seeds/disbursement_types.rb"
     load "#{Rails.root}/db/seeds/expense_types.rb"
-    load "#{Rails.root}/db/seeds/vat_rates.rb" unless @caseworker_seed_done
+    load "#{Rails.root}/db/seeds/vat_rates.rb" unless @vat_seed_done
     load "#{Rails.root}/db/seeds/establishments.rb"
 
-    @caseworker_seed_done = true
+    @vat_seed_done = true
     $seed_done = true
   end
 end
@@ -59,10 +59,10 @@ end
 # minimum seeding necessary for case worker functionality
 # to avoid long start up time for basic case worker features
 #
-Before('@caseworker-seed-requirements') do
-  unless (@caseworker_seed_done ||= false)
+Before('@caseworker-seed-requirements or @vat-seeds') do
+  unless (@vat_seed_done ||= false)
     load "#{Rails.root}/db/seeds/vat_rates.rb"
-    @caseworker_seed_done = true
+    @vat_seed_done = true
   end
 end
 

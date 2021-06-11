f9add28293eab0d13fc5eaf1fc5587a9c2d1d551
Joseph Haig <jrmhaig@users.noreply.github.com>
2021-06-11 09:59:47 +0100

    Update app/models/concerns/criminal_legal_aid_remuneration2020.rb
    Co-authored-by: Joel Sugarman <joel.sugarman@digital.justice.gov.uk>

--- app/models/concerns/criminal_legal_aid_remuneration2020.rb
+++ app/models/concerns/criminal_legal_aid_remuneration2020.rb
@@ -2,6 +2,6 @@ module CriminalLegalAidRemuneration2020
   extend ActiveSupport::Concern
 
   def unused_materials_applicable?
-    case_type && ['Trial', 'Cracked Trial'].include?(case_type.name) && clar?
+    %w[GRTRL GRRAK].include?(case_type&.fee_type_code) && clar?
   end
 end

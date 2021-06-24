03743e247cfc36f07d6b4032a8bbef7ee809cb1d
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-06-24 14:37:29 +0100

    Fix Rubocop warnings
    After converting the new caseworker creation controller tests to request
    tests the Rubocop exceptions no longer mask out some warnings. Rather than
    updating the exceptions this commit fixes the warnings.

--- spec/requests/case_workers/admin/case_workers_spec.rb
+++ spec/requests/case_workers/admin/case_workers_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Caseworker admin', type: :request do
     let(:create_case_workers_request) { post(case_workers_admin_case_workers_path, params: case_worker_params) }
 
     context 'when valid' do
-      let(:case_worker_params) {
+      let(:case_worker_params) do
         {
           case_worker: {
             user_attributes: {
@@ -21,7 +21,7 @@ RSpec.describe 'Caseworker admin', type: :request do
             location_id: create(:location).id
           }
         }
-      }
+      end
 
       it 'creates a case_worker' do
         expect { create_case_workers_request }.to change(User, :count).by(1)
@@ -33,29 +33,34 @@ RSpec.describe 'Caseworker admin', type: :request do
       end
 
       it 'attempts to deliver an email' do
-        expect(DeviseMailer).to receive(:reset_password_instructions)
+        allow(DeviseMailer).to receive(:reset_password_instructions)
         create_case_workers_request
+        expect(DeviseMailer).to have_received(:reset_password_instructions)
       end
 
       describe 'if there is an issue with delivering the email' do
-        let(:mailer) { double DeviseMailer }
+        let(:mailer) { instance_double DeviseMailer }
 
         before do
           allow(DeviseMailer).to receive(:reset_password_instructions).and_raise(NoMethodError)
         end
 
         it 'raises an error' do
-          expect(Rails.logger).to receive(:error).with(/DEVISE MAILER ERROR: 'NoMethodError' while sending reset password mail/)
+          allow(Rails.logger).to receive(:error)
           create_case_workers_request
+          expect(Rails.logger).to have_received(:error)
+            .with(/DEVISE MAILER ERROR: 'NoMethodError' while sending reset password mail/)
         end
       end
     end
 
     context 'when invalid' do
-      let(:case_worker_params) { { case_worker: { roles: ['case_worker'], user_attributes: { email: 'invalidemail' } } } }
+      let(:case_worker_params) do
+        { case_worker: { roles: ['case_worker'], user_attributes: { email: 'invalidemail' } } }
+      end
 
       it 'does not create a case worker' do
-        expect { create_case_workers_request }.to_not change(User, :count)
+        expect { create_case_workers_request }.not_to change(User, :count)
       end
 
       it 'renders the new template' do

8603b27aec8ece52f60a0e2ad76764a2ee2b8835
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-06-29 09:10:38 +0100

    Search based on Claim::BaseClaim#original_submission_date

--- lib/tasks/rake_helpers/submitted_claims.rb
+++ lib/tasks/rake_helpers/submitted_claims.rb
@@ -1,19 +1,18 @@
 module RakeHelpers
   module SubmittedClaims
     def self.write(filename)
-      truncate = Arel.sql("date_trunc('week', created_at)")
+      truncate = Arel.sql("date_trunc('week', original_submission_date::date)")
 
       end_date = Time.zone.now.monday
       start_date = end_date - 12.weeks
 
       CSV.open(filename, 'wb') do |csv|
         csv << ['Week starting', 'Submitted claims']
-        ClaimStateTransition
-          .where(ClaimStateTransition.arel_table[:created_at].between(start_date..end_date))
-          .where(to: 'submitted')
-          .group(truncate).count
-          .sort_by { |row| row.first }.reverse
-          .each { |row| csv << [row[0].strftime('%d/%m/%Y'), row[1]] }
+        claims = Claim::BaseClaim
+          .where(original_submission_date: start_date..end_date)
+          .group(truncate).order(truncate).count
+          .transform_keys { |key| key.strftime('%d/%m/%Y') }
+          .to_a.each { |row| csv << row }
       end
     end
   end

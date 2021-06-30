b3ed4815b6c9028709b62ce1283a10470b119bfa
Peter Hunt <naseberry@users.noreply.github.com>
2021-06-30 08:41:38 +0100

    Add babel env preset 'loose' option
    Silence webpacker warning by explictly adding `loose: true` option to @babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -32,7 +32,8 @@ module.exports = function (api) {
           useBuiltIns: 'entry',
           corejs: 3,
           modules: false,
-          exclude: ['transform-typeof-symbol']
+          exclude: ['transform-typeof-symbol'],
+          loose: true
         }
       ]
     ].filter(Boolean),

4342fea9ce3fdef882e4b8a99fb3f2ec51bab8fb
Peter Hunt <naseberry@users.noreply.github.com>
2021-07-20 15:13:12 +0100

    Refactor re-allocation JavaScript
    ReAllocation.js
    - Replace Show&Hide functionality with the GOV.UK Design System conditionally revealing function
    - Replace jQuery with vanilla JS
    
    Modules.AllocationDataTable.js
    - Fix to target only the allocation page scheme filter radio button
    - Apply Design System markup to input
    
    Modules.AllocationFilterSubmit.js
    - Replace jQuery with vanilla JS
    - Set checked state of selected scheme
    
    Allocation.js
    - Refactor JS and apply Design System markup to input
    - Toggle Select/De-select claim text

--- app/webpack/javascripts/modules/Modules.AllocationFilterSubmit.js
+++ app/webpack/javascripts/modules/Modules.AllocationFilterSubmit.js
@@ -1,8 +1,50 @@
 moj.Modules.AllocationFilterSubmit = {
   init: function () {
-    $('.allocation-filter-form')
-      .on('change', ':radio', function () {
-        $(this).closest('form').submit()
+    const reAllocationForm = document.querySelector('.allocation-filter-form')
+
+    if (reAllocationForm) {
+      this.autoAllocationFilterSubmit()
+      this.setSelectedScheme()
+    }
+  },
+
+  autoAllocationFilterSubmit: function () {
+    const schemeFilters = document.querySelectorAll('.govuk-radios__input[name="scheme"]')
+
+    schemeFilters.forEach(function (scheme) {
+      scheme.onchange = function () {
+        this.closest('form').submit()
+      }
+    })
+  },
+
+  // circumvent GOVUK radio rule "Do not pre-select radio options"
+  // this feature requires a selected state
+  setSelectedScheme: function () {
+    const selectedScheme = this.getUrlParam('scheme') ? this.getUrlParam('scheme') : 'agfs'
+    const selectedSchemeInput = document.querySelector('#scheme-' + selectedScheme + '-field')
+
+    selectedSchemeInput.setAttribute('checked', true)
+  },
+
+  // change to Web API: URLSearchParams (https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams)
+  // once IE usage fall below 2%
+  getUrlParam: function (parameterName) {
+    let result = null
+    let arr = []
+    const queryString = window.location.search.slice(1)
+
+    if (queryString) {
+      const parameters = queryString.split('&')
+
+      parameters.forEach(function (parameter) {
+        arr = parameter.split('=')
+        if (arr[0] === parameterName) {
+          result = decodeURIComponent(arr[1])
+        }
       })
+
+      return result
+    }
   }
 }

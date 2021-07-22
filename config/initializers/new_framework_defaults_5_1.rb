e06a8f0c0910906ef79c5ce700c0f09301e1d93a
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-07-22 16:59:59 +0100

    Remove individual framework default files
    No longer needed as defaults are now set using
    'config.load_defaults 6.1' and non-defaults set
    in `config/application.rb`.

--- config/initializers/new_framework_defaults_5_1.rb
+++
@@ -1,6 +0,0 @@
-# # From https://edgeguides.rubyonrails.org/configuring.html#configuring-assets
-# # config.assets.unknown_asset_fallback allows you to modify the behavior of
-# # the asset pipeline when an asset is not in the pipeline, if you use
-# # sprockets-rails 3.2.0 or newer.
-
-# Rails.application.config.assets.unknown_asset_fallback = false

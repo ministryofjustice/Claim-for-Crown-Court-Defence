374aa3b4f8eb856001b02d5a99d037fd595327f6
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-08-03 11:58:17 +0100

    Refactor `spec/models/supplier_number_spec.rb`
    Apply better spec styling
     - move setup to before blocks
     - use named subjects
     - use `with` and `when` context descriptions

--- spec/models/supplier_number_spec.rb
+++ spec/models/supplier_number_spec.rb
@@ -1,34 +1,23 @@
-# == Schema Information
-#
-# Table name: supplier_numbers
-#
-#  id              :integer          not null, primary key
-#  provider_id     :integer
-#  supplier_number :string
-#
-
-require 'rails_helper'
-
 RSpec.describe SupplierNumber, type: :model do
   subject(:supplier) { build(:supplier_number) }
 
-  context 'uniqueness' do
-    it 'fails if two records with the same suppplier number are created' do
-      create :supplier_number, supplier_number: '9X999X'
+  context 'when validating uniqueness' do
+    before { create(:supplier_number, supplier_number: '9X999X') }
+
+    specify do
       expect {
         create :supplier_number, supplier_number: '9X999X'
       }.to raise_error ActiveRecord::RecordInvalid, 'Validation failed: Supplier number has already been taken'
     end
 
-    it 'fails if the supplier number after upcasing is the same as an existing record' do
-      create :supplier_number, supplier_number: '9X999X'
+    specify do
       expect {
         create :supplier_number, supplier_number: '9x999x'
       }.to raise_error ActiveRecord::RecordInvalid, 'Validation failed: Supplier number has already been taken'
     end
   end
 
-  context 'postcode validation' do
+  context 'when validating postcode' do
     it 'is valid if postcode is nil' do
       supplier.postcode = nil
       expect(supplier).to be_valid
@@ -44,37 +33,49 @@ RSpec.describe SupplierNumber, type: :model do
       expect(supplier).to be_valid
     end
 
-    it 'is invalid if postcode is filled and has the wrong format' do
-      supplier.postcode = 'not-a-valid-postcode'
-      expect(supplier).not_to be_valid
-      expect(supplier.errors[:postcode]).to include('Enter a valid postcode')
+    context 'with wrong format' do
+      before do
+        supplier.postcode = 'not-a-valid-postcode'
+        supplier.validate
+      end
+
+      it { expect(supplier).to be_invalid }
+      it { expect(supplier.errors[:postcode]).to include('Enter a valid postcode') }
     end
   end
 
-  context 'validates supplier number format' do
+  context 'when validating supplier number' do
     let(:format_error) { ['Enter a valid LGFS supplier number'] }
 
-    it 'fails for incorrect format' do
-      allow(subject).to receive(:supplier_number).and_return('ABC123')
-      expect(subject).not_to be_valid
-      expect(subject.errors[:supplier_number]).to eq(format_error)
+    before do
+      allow(supplier).to receive(:supplier_number).and_return(supplier_number)
+      supplier.validate
     end
 
-    it 'succeeds for correct format but lowercase' do
-      allow(subject).to receive(:supplier_number).and_return('1b222z')
-      expect(subject).to be_valid
-      expect(subject.supplier_number).to eq '1B222Z'
+    context 'with invalid format' do
+      let(:supplier_number) { 'ABC123' }
+
+      it { expect(supplier.errors[:supplier_number]).to eq(format_error) }
     end
 
-    it 'pass for correct format' do
-      allow(subject).to receive(:supplier_number).and_return('1B222Z')
-      expect(subject).to be_valid
+    context 'with valid lowercase format' do
+      let(:supplier_number) { '1b222z' }
+
+      it { expect(supplier).to be_valid }
+      it { expect(supplier.supplier_number).to eq '1B222Z' }
+    end
+
+    context 'with valid format' do
+      let(:supplier_number) { '1B222Z' }
+
+      it { expect(supplier).to be_valid }
     end
   end
 
   describe '#has_non_archived_claims?' do
+    subject { described_class.new(supplier_number: '6X666X').has_non_archived_claims? }
+
     let(:relation) { double(ActiveRecord::Relation) }
-    subject { described_class.new(supplier_number: '6X666X') }
 
     before do
       expect(Claim::BaseClaim).to receive(:non_archived_pending_delete).and_return(relation)
@@ -84,17 +85,13 @@ RSpec.describe SupplierNumber, type: :model do
     context 'when there are claims' do
       let(:claims) { [double('Claim')] }
 
-      it 'returns true' do
-        expect(subject.has_non_archived_claims?).to be true
-      end
+      it { is_expected.to be_truthy }
     end
 
     context 'when there are no claims' do
       let(:claims) { [] }
 
-      it 'returns false' do
-        expect(subject.has_non_archived_claims?).to be false
-      end
+      it { is_expected.to be_falsey }
     end
   end
 end

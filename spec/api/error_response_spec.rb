561a215b5532cfe33d80fd5287ff61c080de6331
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-08-08 22:01:09 +0100

    Refactor and extend `spec/api/error_response_spec.rb`
    For clarirty brevity and adhering to betterspecs
    practices:
     - use named subjects
     - put set up in before blocks
     - break examples up to only have a single expectation
     - use contexts to group logic and ensure `let` scoping
     - use shared examples for block iterations
     - rely on default/implecit rspec example output
    
    Adds a missing context for testing custom error handling
    on model instances.

--- spec/api/error_response_spec.rb
+++ spec/api/error_response_spec.rb
@@ -1,76 +1,114 @@
-require 'rails_helper'
+RSpec.shared_examples 'model error handler' do |model_klass|
+  context "with #{model_klass} object" do
+    subject(:error_response) { described_class.new(model_instance) }
 
-describe API::ErrorResponse do
-  VALID_MODEL_KLASSES = [::Fee, ::Expense, ::Disbursement, ::Claim, ::Defendant, ::DateAttended, ::RepresentationOrder]
+    let(:model_instance) { build(model_name.to_sym) }
+    let(:model_name) { model_klass.name.demodulize.underscore }
+
+    context 'with no errors' do
+      before { allow(model_instance).to receive(:errors).and_return([]) }
+
+      it { expect { error_response }.to raise_error(RuntimeError, 'unable to build error response as no errors were found') }
+    end
+
+    context 'with errors on nested models' do
+      before do
+        error = instance_double(ActiveModel::Error, attribute: 'foo', message: 'bar')
+        allow(model_instance).to receive(:errors) { [error] }
+      end
+
+      it { expect(error_response.status).to eq(400) }
+      it { expect(error_response.body).to include({ error: "#{model_name.humanize} 1 foo bar" }) }
+    end
+  end
+end
+
+RSpec.shared_examples 'claim model error handler' do |model_klass|
+  context "with #{model_klass} object" do
+    subject(:error_response) { described_class.new(model_instance) }
+
+    let(:model_instance) { build(model_name.to_sym) }
+    let(:model_name) { model_klass.name.demodulize.underscore }
+
+    context 'with no errors' do
+      before { allow(model_instance).to receive(:errors).and_return([]) }
+
+      it { expect { error_response }.to raise_error(RuntimeError, 'unable to build error response as no errors were found') }
+    end
+
+    context 'with errors on claims' do
+      before do
+        error = instance_double(ActiveModel::Error, attribute: 'foo', message: 'bar')
+        allow(model_instance).to receive(:errors) { [error] }
+      end
+
+      it { expect(error_response.status).to eq(400) }
+      it { expect(error_response.body).to include({ error: 'Foo bar' }) }
+    end
+  end
+end
+
+RSpec.shared_examples 'api exception handler' do |exception_klass|
+  context "with #{exception_klass} exception object" do
+    subject(:error_response) { described_class.new(ex) }
+
+    let(:ex) { exception_klass.new('my exception message') }
+
+    it { expect { error_response }.not_to raise_error }
+    it { expect(error_response.status).to eq 400 }
+    it { expect(error_response.body).to include(error: 'my exception message') }
+  end
+end
+
+RSpec.describe API::ErrorResponse do
+  subject(:error_response) { described_class.new(claim) }
+
+  NESTED_MODEL_KLASSES = [::Expense, ::Disbursement, ::Defendant, ::DateAttended, ::RepresentationOrder,
+                          Fee::GraduatedFee, Fee::InterimFee, Fee::TransferFee, Fee::BasicFee, Fee::MiscFee, Fee::FixedFee]
+  CLAIM_MODEL_KLASSES =  [Claim::AdvocateClaim, Claim::AdvocateInterimClaim, Claim::AdvocateSupplementaryClaim,
+                          Claim::LitigatorClaim, Claim::InterimClaim, Claim::TransferClaim, Claim::AdvocateHardshipClaim,
+                          Claim::LitigatorHardshipClaim]
   EXCEPTION_KLASSES = [RuntimeError, ArgumentError]
 
-  let(:claim) { FactoryBot.build :claim, case_number: 'A123456' }
-  let(:er) { described_class.new(claim) }
+  let(:claim) { build(:claim, case_number: 'A123456') }
 
   before do
     claim.force_validation = true
     claim.valid?
   end
 
-  context 'accepts specific model objects' do
-    VALID_MODEL_KLASSES.each do |model_klass|
-      let(:model_instance) { FactoryBot.build(model_klass.name.underscore.to_sym) }
-      it "accepts #{model_klass.name}" do
-        expect { er = described_class.new(model_instance) }.to raise_error(RuntimeError, 'unable to build error response as no errors were found')
-      end
-    end
+  it { is_expected.to respond_to(:status, :body) }
+  it { expect { error_response.body.to_json }.not_to raise_error }
+
+  NESTED_MODEL_KLASSES.each do |model_klass|
+    it_behaves_like 'model error handler', model_klass
   end
 
-  context 'accepts exception objects' do
-    EXCEPTION_KLASSES.each do |exception_klass|
-      let(:ex) { exception_klass.new('my exception message') }
-
-      it "does not raise a runtime error for #{exception_klass}" do
-        expect { er = described_class.new(ex) }.not_to raise_error
-      end
-
-      it 'populates status with 400 and body with a JSON error format message' do
-        er = described_class.new(ex)
-        expect(er.status).to eq 400
-        json = JSON.parse(er.body.to_json)
-        expect(json[0]['error']).to include('my exception message')
-      end
-    end
-
-    it 'populates status with 401 if exception message is "Unauthorized"' do
-      ex = ArgumentError.new('Unauthorised')
-      er = described_class.new(ex)
-      expect(er.status).to eq 401
-    end
+  CLAIM_MODEL_KLASSES.each do |model_klass|
+    it_behaves_like 'claim model error handler', model_klass
   end
 
-  context 'accepts other objects without breaking' do
+  EXCEPTION_KLASSES.each do |exception_klass|
+    it_behaves_like 'api exception handler', exception_klass
+  end
+
+  context 'with Unauthorized exception' do
+    subject(:error_response) { described_class.new(ex) }
+
+    let(:ex) { ArgumentError.new('Unauthorised') }
+
+    it { expect(error_response.status).to eq 401 }
+  end
+
+  context 'with other objects' do
     [1, '1', [1, 2]].each do |other_object|
-      it "accepts #{other_object.class.name} without raising an error" do
-        expect { described_class.new(other_object) }.not_to raise_error
-      end
+      context "when #{other_object.class}" do
+        subject(:error_response) { described_class.new(other_object) }
 
-      it 'populates statuswith 400 and body with general error message' do
-        er = described_class.new(other_object)
-        expect(er.status).to eq 400
-        json = JSON.parse(er.body.to_json)
-        expect(json[0]['error']).to include("No message provided by object #{other_object.class.name}")
+        it { expect { error_response }.not_to raise_error }
+        it { expect(error_response.status).to eq 400 }
+        it { expect(error_response.body).to include(error: "No message provided by object #{other_object.class}") }
       end
     end
-  end
-
-  it 'returns status and body' do
-    expect(er).to respond_to :status
-    expect(er).to respond_to :body
-  end
-
-  it 'returned body is in JSON format' do
-    expect { er.body.to_json }.not_to raise_error
-  end
-
-  it 'raises an error if model is valid' do  # pending because claim.errors contains an empty array for external_users
-    claim.update_attribute(:case_number, 'A20161234')
-    expect(claim).to be_valid
-    expect { described_class.new(claim) }.to raise_error('unable to build error response as no errors were found')
   end
 end

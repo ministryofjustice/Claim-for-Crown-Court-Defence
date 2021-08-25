c33428325687e19266b3368f9f475ff96ae6b40d
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-08-25 12:59:15 +0100

    Reduce the Sentry sampling to 5%
    See https://ministryofjustice.github.io/operations-engineering/documentation/services/sentry.html#sentry-io

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,7 +5,7 @@ if Rails.env.eql?('production') && ENV['SENTRY_DSN'].present?
     config.dsn = ENV['SENTRY_DSN']
     config.breadcrumbs_logger = [:active_support_logger]
 
-    # Send 10% of transactions for performance monitoring
-    config.traces_sample_rate = 0.1
+    # Send 5% of transactions for performance monitoring
+    config.traces_sample_rate = 0.05
   end
 end

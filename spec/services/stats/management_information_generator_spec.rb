d036c8a43960a454847477b1c7a4c2de9622c298
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-09-20 17:55:51 +0100

    Tweak MI generator spec examples

--- spec/services/stats/management_information_generator_spec.rb
+++ spec/services/stats/management_information_generator_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Stats::ManagementInformationGenerator do
-  subject(:result) { described_class.call }
-
   let(:frozen_time) { Time.new(2015, 3, 10, 11, 44, 55) }
   let(:report_columns) do
     [
@@ -33,11 +31,14 @@ RSpec.describe Stats::ManagementInformationGenerator do
     ]
   end
 
-  context 'when generating data' do
-    subject(:contents) { result.content.split("\n") }
+  describe '#call' do
+    subject(:call) { described_class.new.call }
+
+    let(:rows) { call.content.split("\n") }
 
     let!(:valid_claims) {
       [
+        create(:submitted_claim),
         create(:allocated_claim),
         create(:authorised_claim),
         create(:part_authorised_claim)
@@ -46,34 +47,30 @@ RSpec.describe Stats::ManagementInformationGenerator do
     let!(:draft_claim) { create(:draft_claim) }
     let!(:non_active_claim) { travel_to(frozen_time) { create(:allocated_claim) } }
 
-    it 'returns CSV content with a header and a row for all active non-draft claims' do
-      expect(contents.size).to eq(valid_claims.size + 1)
+    it 'returns a row for all active non-draft claims' do
+      expect(rows.size).to eq(valid_claims.size + 1)
     end
 
-    it 'has expected columns' do
-      expect(contents.first.split(',')).to match_array(report_columns)
+    it 'has expected headers' do
+      expect(rows.first.split(',')).to match_array(report_columns)
     end
   end
 
-  context 'For logging' do
-    let(:error) { StandardError.new('test error') }
+  context 'when logging without errors' do
+    it 'log start and end' do
+      expect(LogStuff).to receive(:info).twice
+      described_class.call
+    end
+  end
 
-    context 'when successful' do
-      it 'uses LogStuff to log start and end' do
-        expect(LogStuff).to receive(:info).twice
-        described_class.call
-      end
+  context 'when logging errors' do
+    before do
+      allow(CSV).to receive(:generate).and_raise(StandardError)
     end
 
-    context 'when error raised' do
-      before do
-        allow(CSV).to receive(:generate).and_raise error
-      end
-
-      it 'uses LogStuff to log error' do
-        expect(LogStuff).to receive(:error).once
-        described_class.call
-      end
+    it 'uses LogStuff to log error' do
+      expect(LogStuff).to receive(:error).once
+      described_class.call
     end
   end
 end

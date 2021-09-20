96dae302e516b65dd47c6a0d5010334c152bc3f9
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-09-20 16:39:23 +0100

    Tweak wording and setup of examples

--- spec/services/stats/stats_report_generator_spec.rb
+++ spec/services/stats/stats_report_generator_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Stats::StatsReportGenerator, type: :service do
 
     let(:report_type) { 'management_information' }
 
-    context 'when the report type is not valid' do
+    context 'with an invalid report type' do
       let(:report_type) { 'some-report-type' }
 
       before { allow(Settings).to receive(:notify_report_errors).and_return(false) }
@@ -20,7 +20,9 @@ RSpec.describe Stats::StatsReportGenerator, type: :service do
       end
     end
 
-    context 'when there is already a report of that type in progress' do
+    context 'with a valid report type that is already in progress' do
+      let(:report_type) { 'management_information' }
+
       before { Stats::StatsReport.create(report_name: report_type, status: 'started') }
 
       it 'does not create a new report' do
@@ -28,54 +30,55 @@ RSpec.describe Stats::StatsReportGenerator, type: :service do
       end
     end
 
-    context 'when there is no report of that type in progress' do
+    context 'with a valid report type that is not already in progress' do
+      let(:report_type) { 'management_information' }
       let(:mocked_result) { Stats::Result.new('some new content', 'csv') }
 
       before { allow(Stats::ManagementInformationGenerator).to receive(:call).and_return(mocked_result) }
 
-      it 'adds a new completed report' do
+      it 'marks report as completed' do
         expect { call_report_generator }
           .to change(Stats::StatsReport.where(report_name: report_type).completed, :count).by 1
       end
 
-      it 'puts data into the new report' do
+      it 'generates report content' do
         call_report_generator
-        new_record = Stats::StatsReport.where(report_name: report_type).completed.last
-        file_path = ActiveStorage::Blob.service.path_for(new_record.document.blob.key)
+        record = Stats::StatsReport.where(report_name: report_type).completed.last
+        file_path = ActiveStorage::Blob.service.path_for(record.document.blob.key)
         expect(File.open(file_path).read).to eq('some new content')
       end
     end
 
-    context 'when an error happens during the generation of the report' do
+    context 'with an unexpected error' do
       before do
         allow(Stats::ManagementInformationGenerator).to receive(:call).and_raise(StandardError)
+      end
+
+      it 'marks report as errored' do
+        expect {
+          call_report_generator rescue nil
+        }.to change { Stats::StatsReport.where(report_name: report_type).errored.count }.from(0).to(1)
       end
 
       it 'raises the error' do
         expect { call_report_generator }.to raise_error(StandardError)
       end
 
-      it 'creates a new report marked as errored' do
-        expect {
-          call_report_generator rescue nil
-        }.to change { Stats::StatsReport.where(report_name: report_type).errored.count }.from(0).to(1)
-      end
-
-      context 'when the error notifications are enabled' do
+      context 'with notifications enabled' do
         before do
           allow(Settings).to receive(:notify_report_errors).and_return(true)
-        end
-
-        it 'sends an error notification' do
           allow(ActiveSupport::Notifications).to receive(:instrument)
           call_report_generator rescue nil
+        end
+
+        it 'sends an error notification with expected args' do
           record = Stats::StatsReport.where(report_name: report_type).errored.first
           args = ['call_failed.stats_report', id: record.id, name: report_type, error: instance_of(StandardError)]
           expect(ActiveSupport::Notifications).to have_received(:instrument).with(*args)
         end
       end
 
-      context 'when the error notifications are disabled' do
+      context 'with notifications disabled' do
         before do
           allow(Settings).to receive(:notify_report_errors).and_return(false)
           allow(ActiveSupport::Notifications).to receive(:instrument)

1363c5cd786b79b02a45273e63b85d41989ea2c1
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-09-23 17:29:54 +0100

    Rename AUTHORIZATION_BEARER to BEARER_TOKEN

--- config/initializers/survey_monkey.rb
+++ config/initializers/survey_monkey.rb
@@ -1,6 +1,6 @@
 SurveyMonkey.configure do |config|
   config.root_url = 'https://api.eu.surveymonkey.com/v3/'
-  config.bearer = ENV['SURVEY_MONKEY_AUTHORIZATION_BEARER']
+  config.bearer = ENV['SURVEY_MONKEY_BEARER_TOKEN']
   config.collector_id = ENV['SURVEY_MONKEY_COLLECTOR_ID']
   config.logger = Rails.logger
   config.verbose_logging = true

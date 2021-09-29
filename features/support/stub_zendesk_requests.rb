d49fed452d9bc22fe7031c37e76dbd7f86b505b7
jsugarman <joel.sugarman@digital.justice.gov.uk>
2021-09-29 17:15:14 +0100

    Example stub that could be used instead

--- features/support/stub_zendesk_requests.rb
+++ features/support/stub_zendesk_requests.rb
@@ -3,3 +3,8 @@ require 'webmock/cucumber'
 Before('@stub_zendesk_request') do
   @called_zendesk = stub_request(:post, %r{\Ahttps://.*ministryofjustice.zendesk.com/api/v2/tickets\z} )
 end
+
+Before('@stub_survey_monkey_request') do
+  stub_request(:post, %r{\Ahttps://api.eu.surveymonkey.com/v3/collectors/.*/responses\z} ).
+    and_returns(status: 201, body: { id: 123 }.to_json)
+end

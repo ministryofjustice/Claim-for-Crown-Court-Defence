e2f320535615cab190d0abbf65629044e1a49c6a
Joseph Haig <jrmhaig@users.noreply.github.com>
2021-10-01 09:21:20 +0100

    Update features/support/stub_survey_monkey_requests.rb
    Co-authored-by: Peter Hunt <naseberry@users.noreply.github.com>

--- features/support/stub_survey_monkey_requests.rb
+++ features/support/stub_survey_monkey_requests.rb
@@ -1,5 +1,6 @@
 require 'webmock/cucumber'
 
 Before('@stub_survey_monkey_success') do
-  @called_zendesk = stub_request(:post, %r{\Ahttps://api.eu.surveymonkey.com/v3/.*} ).and_return(status: 201, body: { id: 123 }.to_json )
+  stub_request(:post, %r{\Ahttps://api.eu.surveymonkey.com/v3/.*} )
+    .and_return(status: 201, body: { id: 123 }.to_json )
 end

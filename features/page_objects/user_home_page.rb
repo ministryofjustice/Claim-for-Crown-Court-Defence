7683d6e253ffe216974d5e7658bbd397b3361d39
Peter Hunt <naseberry@users.noreply.github.com>
2021-11-01 08:33:27 +0000

    Migrate main_claims_list table to GOVUK table component

--- features/page_objects/user_home_page.rb
+++ features/page_objects/user_home_page.rb
@@ -2,11 +2,11 @@ class UserHomePage < BasePage
   element :start_a_claim, "a.govuk-button--start"
   element :your_claims_link, "nav.breadcrumbs > ul > li:nth-of-type(1) > a"
 
-  sections :claims, "table.report > tbody > tr" do
+  sections :claims, "table.govuk-table > tbody > tr" do
     element :case_number, "a.js-test-case-number-link"
     element :state, "strong.govuk-tag"
-    element :claimed, "td.claimed-amount"
-    element :view_messages, "td.messages > a"
+    element :claimed, "td[data-label='Claimed']"
+    element :view_messages, "td[data-label='Messages'] > a"
     element :error_message, '.error-message'
   end
 

9a71473f7ab97c7f2f982b7db014f6de08c9fd2e
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-11-09 12:43:20 +0000

    Remove unnecessary `describe`
    Tests still work without replacing the `claim` and `presenter` variables.

--- spec/presenters/claim/transfer_claim_presenter_spec.rb
+++ spec/presenters/claim/transfer_claim_presenter_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Claim::TransferClaimPresenter, type: :presenter do
 
   subject(:presenter) { described_class.new(claim, view) }
 
+  it { expect(presenter).to be_instance_of(described_class) }
+  it { expect(presenter).to be_kind_of(Claim::BaseClaimPresenter) }
+
   specify { expect(presenter.pretty_type).to eq('LGFS Transfer') }
   specify { expect(presenter.type_identifier).to eq('lgfs_transfer') }
 
@@ -102,80 +105,72 @@ RSpec.describe Claim::TransferClaimPresenter, type: :presenter do
     end
   end
   
-  describe 'copied from spec/presenters/transfer_claim_presenter_spec.rb' do
-    let(:claim) { instance_double(Claim::TransferClaim, transfer_detail: double) }
-    let(:presenter) { Claim::TransferClaimPresenter.new(claim, view) }
-  
-    it { expect(presenter).to be_instance_of(Claim::TransferClaimPresenter) }
-    it { expect(presenter).to be_kind_of(Claim::BaseClaimPresenter) }
-  
-    it 'has disbursements' do
-      expect(presenter.can_have_disbursements?).to eq(true)
+  it 'has disbursements' do
+    expect(presenter.can_have_disbursements?).to eq(true)
+  end
+
+  let(:case_conclusions) do
+    {
+      '10' => 'Trial',
+      '20' => 'Retrial',
+      '30' => 'Cracked',
+      '40' => 'Cracked before retrial',
+      '50' => 'Guilty plea'
+    }
+  end
+
+  context '#case_conclusions' do
+    it 'returns a has of case conclusion descriptions and ids' do
+      expect(presenter.case_conclusions).to match case_conclusions
     end
-  
-    let(:case_conclusions) do
-      {
-        '10' => 'Trial',
-        '20' => 'Retrial',
-        '30' => 'Cracked',
-        '40' => 'Cracked before retrial',
-        '50' => 'Guilty plea'
-      }
-    end
-  
-    context '#case_conclusions' do
-      it 'returns a has of case conclusion descriptions and ids' do
-        expect(presenter.case_conclusions).to match case_conclusions
-      end
-    end
-  
-    context '#transfer_detail_summary' do
-      context 'for transfer details NOT requiring a conclusion' do
-        let(:claim) { create :transfer_claim, litigator_type: 'new', elected_case: true, transfer_stage_id: 10, case_conclusion_id: nil }
-        it 'returns a string of expected values' do
-          expect(presenter.transfer_detail_summary).to eql 'Elected case - up to and including PCMH transfer (new)'
-        end
-      end
-  
-      context 'for transfer details NOT requiring a conclusion and from original litigator' do
-        let(:claim) { create :transfer_claim, litigator_type: 'original', elected_case: false, transfer_stage_id: 40, case_conclusion_id: nil }
-        it 'returns a string of expected values' do
-          expect(presenter.transfer_detail_summary).to eql 'Transfer after trial and before sentence hearing (org)'
-        end
-      end
-  
-      context 'for transfer details requiring a conclusion' do
-        let(:claim) { create :transfer_claim, litigator_type: 'new', elected_case: false, transfer_stage_id: 20, case_conclusion_id: 30 }
-        it 'returns a string of expected values' do
-          expect(presenter.transfer_detail_summary).to eql 'Before trial transfer (new) - cracked'
-        end
-      end
-  
-      context 'for incomplete transfer details' do
-        let(:claim) { create :transfer_claim, litigator_type: nil, elected_case: nil, transfer_stage_id: nil, case_conclusion_id: nil }
-        it 'returns blank string' do
-          expect(presenter.transfer_detail_summary).to be_blank
-        end
+  end
+
+  context '#transfer_detail_summary' do
+    context 'for transfer details NOT requiring a conclusion' do
+      let(:claim) { create :transfer_claim, litigator_type: 'new', elected_case: true, transfer_stage_id: 10, case_conclusion_id: nil }
+      it 'returns a string of expected values' do
+        expect(presenter.transfer_detail_summary).to eql 'Elected case - up to and including PCMH transfer (new)'
       end
     end
-  
-    context 'descriptor methods' do
-      let(:claim) { create :transfer_claim, litigator_type: 'new', elected_case: false, transfer_stage_id: 20, transfer_date: Date.parse('2015-05-21'), case_conclusion_id: 30 }
-      it '#litigator_type_description' do
-        expect(presenter.litigator_type_description).to eql 'New'
+
+    context 'for transfer details NOT requiring a conclusion and from original litigator' do
+      let(:claim) { create :transfer_claim, litigator_type: 'original', elected_case: false, transfer_stage_id: 40, case_conclusion_id: nil }
+      it 'returns a string of expected values' do
+        expect(presenter.transfer_detail_summary).to eql 'Transfer after trial and before sentence hearing (org)'
       end
-      it '#elected_case_description' do
-        expect(presenter.elected_case_description).to eql 'No'
+    end
+
+    context 'for transfer details requiring a conclusion' do
+      let(:claim) { create :transfer_claim, litigator_type: 'new', elected_case: false, transfer_stage_id: 20, case_conclusion_id: 30 }
+      it 'returns a string of expected values' do
+        expect(presenter.transfer_detail_summary).to eql 'Before trial transfer (new) - cracked'
       end
-      it '#transfer_stage_description' do
-        expect(presenter.transfer_stage_description).to eql 'Before trial transfer'
+    end
+
+    context 'for incomplete transfer details' do
+      let(:claim) { create :transfer_claim, litigator_type: nil, elected_case: nil, transfer_stage_id: nil, case_conclusion_id: nil }
+      it 'returns blank string' do
+        expect(presenter.transfer_detail_summary).to be_blank
       end
-      it '#transfer_date' do
-        expect(presenter.transfer_date).to eql '21/05/2015'
-      end
-      it '#case_conclusion_description' do
-        expect(presenter.case_conclusion_description).to eql 'Cracked'
-      end
+    end
+  end
+
+  context 'descriptor methods' do
+    let(:claim) { create :transfer_claim, litigator_type: 'new', elected_case: false, transfer_stage_id: 20, transfer_date: Date.parse('2015-05-21'), case_conclusion_id: 30 }
+    it '#litigator_type_description' do
+      expect(presenter.litigator_type_description).to eql 'New'
+    end
+    it '#elected_case_description' do
+      expect(presenter.elected_case_description).to eql 'No'
+    end
+    it '#transfer_stage_description' do
+      expect(presenter.transfer_stage_description).to eql 'Before trial transfer'
+    end
+    it '#transfer_date' do
+      expect(presenter.transfer_date).to eql '21/05/2015'
+    end
+    it '#case_conclusion_description' do
+      expect(presenter.case_conclusion_description).to eql 'Cracked'
     end
   end
 end

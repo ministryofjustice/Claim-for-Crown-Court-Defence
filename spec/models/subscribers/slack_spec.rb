f9797700c77c8cf74331c1339b61f931f012654a
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-11-16 09:28:01 +0000

    Split expects into separate tests

--- spec/models/subscribers/slack_spec.rb
+++ spec/models/subscribers/slack_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Subscribers::Slack, type: :subscriber do
   describe '#process' do
+    subject(:process) { described_class.new(event_name, start, ending, transaction_id, payload) }
+
     let(:event_name) { 'call_failed.stats_report' }
     let(:start) { 2.minutes.ago }
     let(:ending) { 1.minutes.ago }
@@ -10,21 +12,23 @@ RSpec.describe Subscribers::Slack, type: :subscriber do
     let(:notifier) { instance_double(SlackNotifier) }
     let(:send_result) { double(:send_result) }
 
-    subject(:process) { described_class.new(event_name, start, ending, transaction_id, payload) }
-
     before do
       allow(SlackNotifier).to receive(:new).and_return(notifier)
       allow(notifier).to receive(:build_payload)
       allow(notifier).to receive(:send_message).and_return(send_result)
+
+      process
     end
 
-    it 'sends a message to slack channel with the error content' do
-      process
+    it { is_expected.to be_kind_of(Subscribers::Base) }
 
+    it 'creates a new SlackNotifier' do
       expect(SlackNotifier)
         .to have_received(:new)
         .with('cccd_development', formatter: an_instance_of(SlackNotifier::Formatter::Generic))
+    end
 
+    it 'builds the payload with the notifier arguments' do
       notifier_args = {
         icon: ':robot_face:',
         title: 'provisional_assessment failed on test',
@@ -33,8 +37,10 @@ RSpec.describe Subscribers::Slack, type: :subscriber do
       }
 
       expect(notifier).to have_received(:build_payload).with(**notifier_args)
+    end
+
+    it 'sends the message with the notifier' do
       expect(notifier).to have_received(:send_message)
-      expect(process).to be_kind_of(Subscribers::Base)
     end
   end
 end

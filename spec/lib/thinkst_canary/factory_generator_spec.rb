9167c4b2bb20b09bafa6fe5a324a027024893f96
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-11-26 12:22:01 +0000

    Make test expectations more explicit

--- spec/lib/thinkst_canary/factory_generator_spec.rb
+++ spec/lib/thinkst_canary/factory_generator_spec.rb
@@ -14,16 +14,16 @@ RSpec.describe ThinkstCanary::FactoryGenerator do
     end
 
     it { is_expected.to be_a ThinkstCanary::Factory }
-    it { expect(create_factory.factory_auth).to eq(factory_response['factory_auth']) }
-    it { expect(create_factory.flock_id).to eq(factory_options[:flock_id]) }
-    it { expect(create_factory.memo).to eq(factory_options[:memo]) }
+    it { expect(create_factory.factory_auth).to eq('factory_auth_token') }
+    it { expect(create_factory.flock_id).to eq('test_flock') }
+    it { expect(create_factory.memo).to eq('test_memo') }
 
     it 'makes a POST request for a factory' do
       create_factory
 
       expect(ThinkstCanary.configuration).to have_received(:query).with(
         :post, '/api/v1/canarytoken/create_factory',
-        params: { flock_id: factory_options[:flock_id], memo: factory_options[:memo] }
+        params: { flock_id: 'test_flock', memo: 'test_memo' }
       )
     end
   end

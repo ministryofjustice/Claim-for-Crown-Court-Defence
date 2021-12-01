5416121076cea280c63a95d310aa29b916a44879
Joe Haig <joseph.haig@digital.justice.gov.uk>
2021-12-01 16:56:40 +0000

    PDF Canarytoken
    The Canary token factory can create PDF tokens using the `pdf-acrobat-reader`
    kind as follows;
    
    ```ruby
    token = factory.create_token(
      kind: 'pdf_acrobat-reader',
      memo: 'A new PDF Canarytoken',
      file: File.open('pdf_base.pdf')
    )

---
+++ lib/thinkst_canary/token/file_based.rb
@@ -0,0 +1,21 @@
+module ThinkstCanary
+  module Token
+    class FileBased < Base
+      def initialize(**kwargs)
+        @file = kwargs[:file]
+
+        super
+      end
+
+      private
+
+      def create_options
+        super.merge(@file_key => doc)
+      end
+
+      def doc
+        @doc ||= Faraday::UploadIO.new(@file, @mime)
+      end
+    end
+  end
+end

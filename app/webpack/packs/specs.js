052fd531491fee5ef9121b7c2d30cd56af3842ce
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2021-12-02 11:04:03 +0000

    npm jasmine runs successfully without errors

--- app/webpack/packs/specs.js
+++ app/webpack/packs/specs.js
@@ -5,6 +5,6 @@
     context.keys().forEach(context)
   }
 
-  requireAll(require.context('../../../spec/javascripts/support/', true, /\.js/))
+  requireAll(require.context('../../../spec/javascripts/helpers/', true, /\.js/))
   requireAll(require.context('../../../spec/javascripts/', true, /[sS]pec\.js/))
 })()

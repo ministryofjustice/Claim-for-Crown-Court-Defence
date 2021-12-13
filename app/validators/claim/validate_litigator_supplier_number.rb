ecc83692b42f743898f9fa9f3a7e0b54cab1a13e
Peter Hunt <naseberry@users.noreply.github.com>
2021-12-13 11:32:54 +0000

    Update case_details - supplier number validator

--- app/validators/claim/validate_litigator_supplier_number.rb
+++ app/validators/claim/validate_litigator_supplier_number.rb
@@ -1,13 +1,13 @@
 module Claim
   module ValidateLitigatorSupplierNumber
     def validate_supplier_number
-      validate_presence(:supplier_number, 'blank')
+      validate_presence(:supplier_number, :blank)
 
       return if @record.supplier_number.blank?
 
-      validate_pattern(:supplier_number, supplier_number_regex, 'invalid')
+      validate_pattern(:supplier_number, supplier_number_regex, :invalid)
       return if @record.errors.key?(:supplier_number)
-      validate_inclusion(:supplier_number, provider_supplier_numbers, 'unknown')
+      validate_inclusion(:supplier_number, provider_supplier_numbers, :unknown)
     end
   end
 end

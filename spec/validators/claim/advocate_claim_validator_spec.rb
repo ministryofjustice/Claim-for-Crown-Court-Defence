c4eb96474c830219a69dc44ddf3e6dd7d301ec70
joel.sugarman <joel.sugarman@digital.justice.gov.uk>
2021-12-23 13:52:02 +0000

    Migrate transfer_court and transfer_case_number
    ...to govuk-formbuilder compatible validations and errors

--- spec/validators/claim/advocate_claim_validator_spec.rb
+++ spec/validators/claim/advocate_claim_validator_spec.rb
@@ -120,7 +120,7 @@ RSpec.describe Claim::AdvocateClaimValidator, type: :validator do
       court_id
       case_number
       case_transferred_from_another_court
-      transfer_court
+      transfer_court_id
       transfer_case_number
       estimated_trial_length
       actual_trial_length
--- spec/validators/claim/transfer_claim_validator_spec.rb
+++ spec/validators/claim/transfer_claim_validator_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Claim::TransferClaimValidator, type: :validator do
       court_id
       case_number
       case_transferred_from_another_court
-      transfer_court
+      transfer_court_id
       transfer_case_number
       case_concluded_at
       supplier_number

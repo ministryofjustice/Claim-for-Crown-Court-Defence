294be55264e191fe90d6b0ec492683b52fa706b3
Joe Haig <joseph.haig@digital.justice.gov.uk>
2022-01-31 14:13:17 +0000

    Convert to request specs

--- spec/requests/offences_spec.rb
+++ spec/requests/offences_spec.rb
@@ -1,17 +1,6 @@
-# == Schema Information
-#
-# Table name: offences
-#
-#  id               :integer          not null, primary key
-#  description      :string
-#  offence_class_id :integer
-#  created_at       :datetime
-#  updated_at       :datetime
-#
-
 require 'rails_helper'
 
-RSpec.describe OffencesController, type: :controller do
+RSpec.describe 'offences details', type: :request do
   let!(:scheme_9_offences) {
     [
       create(:offence, :with_fee_scheme, description: 'Offence 1'),
@@ -29,13 +18,13 @@ RSpec.describe OffencesController, type: :controller do
 
   describe 'GET index' do
     it 'returns all offences if no description present' do
-      get :index, xhr: true
+      get offences_url, xhr: true
       expect(assigns(:offences).size).to eq 3
       expect(assigns(:offences).map(&:description)).to eq(['Offence 1', 'Offence 2', 'Offence 3'])
     end
 
     it 'just gets the matching offence' do
-      get :index, params: { description: 'Offence 3' }, xhr: true
+      get offences_url, params: { description: 'Offence 3' }, xhr: true
       expect(assigns(:offences).map(&:description)).to eq(['Offence 3'])
     end
 
@@ -43,7 +32,7 @@ RSpec.describe OffencesController, type: :controller do
       let(:params) { { fee_scheme: 'AGFS 10' } }
 
       it 'returns offences only for fee scheme 10' do
-        get :index, params: params, xhr: true
+        get offences_url, params: params, xhr: true
         expect(assigns(:offences).size).to eq 3
         expect(assigns(:offences).map(&:description)).to match_array(['Offence 10-1', 'Offence 10-3', 'Offence 10-2'])
       end
@@ -51,7 +40,7 @@ RSpec.describe OffencesController, type: :controller do
       it 'calls the fee reform search offences service with the provided filters' do
         expected_args = strong_params(fee_scheme: 'AGFS 10')
         expect(FeeReform::SearchOffences).to receive(:call).with(expected_args).and_call_original
-        get :index, params: params, xhr: true
+        get offences_url, params: params, xhr: true
       end
     end
   end

ff5232bb8477c7e9be1247135f6036df3cf51ccb
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2022-01-31 11:11:28 +0000

    Update controller to retire `index.js.erb`

--- app/webpack/javascripts/modules/external_users/claims/OffenceCtrl.js
+++ app/webpack/javascripts/modules/external_users/claims/OffenceCtrl.js
@@ -1,7 +1,8 @@
+const selectedOffenceClass = []
 moj.Modules.OffenceCtrl = {
   els: {
-    offenceClassSelectWrapper: '.offence-class-select',
-    offenceClassSelect: '#offence_class_description',
+    offenceClassSelectWrapper: '.js-offence-class-select-wrapper',
+    offenceClassSelect: '.js-offence-class-select select',
     offenceID: '#claim_offence_id',
     offenceCategoryDesc: '#claim_offence_category_description'
   },
@@ -34,22 +35,57 @@ moj.Modules.OffenceCtrl = {
   },
 
   bindEvents: function () {
-    $.subscribe('/onConfirm/claim_offence_category_description-select/', function (e, data) {
-      const param = $.param({
-        description: data.query
+    $('.fx-autocomplete-wrapper select').each(function () {
+      $.subscribe('/onConfirm/' + this.id + '/', function (e, data) {
+        const param = $.param({
+          description: data.query
+        })
+        $.getScript('/offences?' + param).done(function (result) {
+          console.log(param) // description=VAT%20offences
+          moj.Modules.OffenceCtrl.test(result)
+        })
       })
-      $.getScript('/offences?' + param)
     })
+  },
+
+  initialiseOffenceID: function () {
+    $(this.els.offenceClassSelectWrapper).hide()
+    $(this.els.offenceID).val('')
   },
 
   attachToOffenceClassSelect: function () {
     const self = this
     $(this.els.offenceClassSelect).on('change', function () {
-      $(self.els.offenceID).val($(this).val())
-      if (!$(this).val()) {
-        $(self.els.offenceClassSelectWrapper).hide()
-        $(self.els.offenceID).val('')
-      }
+      console.log(selectedOffenceClass)
+      self.setValueToOffenceID($(this).val())
     })
+  },
+
+  setValueToOffenceID: function (data) {
+    if (data) {
+      const value = parseInt(data)
+      const selectedValue = selectedOffenceClass[0].filter(function (object) {
+        return object.id === value
+      })
+      console.log(selectedValue)
+      $(this.els.offenceID).val(selectedValue[0].offence_id)
+    } else {
+      this.initialiseOffenceID()
+    }
+  },
+
+  test: function (result) {
+    const array = JSON.parse('[' + result + ']')
+    console.log(array)
+    selectedOffenceClass.push(array[0])
+    if (array.length > 0) {
+      $(this.els.offenceClassSelectWrapper).show()
+      $(this.els.offenceClassSelect).empty()
+      $.each(array[0], function (e, data) {
+        $(moj.Modules.OffenceCtrl.els.offenceClassSelect).append(
+          '<option value=' + data.id + '>' + data.description + '</option>'
+        )
+      })
+    }
   }
 }

bb049e3ad5d4e1eedb1e5cdc15202c0b1269138e
Joel Sugarman <joel.sugarman@digital.justice.gov.uk>
2022-02-08 15:53:29 +0000

    clear selection feature test (#4655)
    * Add feature test for offence errors and clearing
    
    * Handle kwarg deprecation warning
    
    * Rename file inline with intended purpose
    
    putting offence related steps in it generally.

--- features/step_definitions/offence_class_steps.rb
+++
@@ -1,7 +0,0 @@
-Then(/^the offence class list is set to '(.*?)'$/) do |text|
-  expect(page).to have_xpath("//option[text()='#{text}']")
-end
-
-Then(/^the offence class list has (.*?) options$/) do |count|
-  expect(page).to have_xpath('//select[@id="claim-offence-class-field"]/option', count: count)
-end

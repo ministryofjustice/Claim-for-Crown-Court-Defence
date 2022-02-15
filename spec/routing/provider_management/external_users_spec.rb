f429c92d0d9c8aacef18b51c97c448b8b4f70baa
Joe Haig <joseph.haig@digital.justice.gov.uk>
2022-02-15 17:42:29 +0000

    Routing specs for existing endpoints

---
+++ spec/routing/provider_management/external_users_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe ProviderManagement::ExternalUsersController, type: :routing do
+  it 'routes GET /provider_management/providers/:provider_id/external_users to #index' do
+    is_expected.to route(:get, '/provider_management/providers/1/external_users').to(action: :index, provider_id: 1)
+  end
+
+  it 'routes POST /provider_management/providers/:provider_id/external_users to #create' do
+    is_expected.to route(:post, '/provider_management/providers/1/external_users').to(action: :create, provider_id: 1)
+  end
+
+  it 'routes GET /provider_management/providers/:provider_id/external_users/new to #new' do
+    is_expected.to route(:get, '/provider_management/providers/1/external_users/new').to(action: :new, provider_id: 1)
+  end
+
+  it 'routes GET /provider_management/providers/:provider_id/external_users/:id/edit to #edit' do
+    is_expected
+      .to route(:get, '/provider_management/providers/1/external_users/2/edit')
+      .to(action: :edit, provider_id: 1, id: 2)
+  end
+
+  it 'routes GET /provider_management/providers/:provider_id/external_users/:id to #show' do
+    is_expected
+      .to route(:get, '/provider_management/providers/1/external_users/2')
+      .to(action: :show, provider_id: 1, id: 2)
+  end
+
+  it 'routes PATCH /provider_management/providers/:provider_id/external_users/:id to #update' do
+    is_expected
+      .to route(:patch, '/provider_management/providers/1/external_users/2')
+      .to(action: :update, provider_id: 1, id: 2)
+  end
+
+  it 'routes PUT /provider_management/providers/:provider_id/external_users/:id to #update' do
+    is_expected
+      .to route(:put, '/provider_management/providers/1/external_users/2')
+      .to(action: :update, provider_id: 1, id: 2)
+  end
+
+  it 'routes GET /provider_management/external_users/find to #find' do
+    is_expected.to route(:get, '/provider_management/external_users/find').to(action: :find)
+  end
+
+  it 'routes POST /provider_management/external_users/find to #search' do
+    is_expected.to route(:post, '/provider_management/external_users/find').to(action: :search)
+  end
+
+  it 'routes GET /provider_management/providers/1/external_users/2/change_password to #change_password' do
+    is_expected
+      .to route(:get, '/provider_management/providers/1/external_users/2/change_password')
+      .to(action: :change_password, provider_id: 1, id: 2)
+  end
+
+  it 'routes PATCH /provider_management/providers/1/external_users/2/update_password to #update_password' do
+    is_expected
+      .to route(:patch, '/provider_management/providers/1/external_users/2/update_password')
+      .to(action: :update_password, provider_id: 1, id: 2)
+  end
+end

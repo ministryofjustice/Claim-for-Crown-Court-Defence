967bd1f7873804812c9f9186f354735753f4f0d5
Joe Haig <joseph.haig@digital.justice.gov.uk>
2022-02-16 11:02:09 +0000

    Enable redis_logger in breadcrumbs_logger

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@
 if Rails.env.eql?('production') && ENV['SENTRY_DSN'].present?
    Sentry.init do |config|
     config.dsn = ENV['SENTRY_DSN']
-    config.breadcrumbs_logger = [:active_support_logger]
+    config.breadcrumbs_logger = [:active_support_logger, :redis_logger]
 
     # Send 5% of transactions for performance monitoring
     config.traces_sample_rate = 0.05

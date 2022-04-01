038ea139b397121168626f8cce66bba36f379d25
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2022-04-01 15:28:55 +0100

    Migrate misc_fees - LGFS
    - Migrate `misc_fees` for litigator view to the GOVUK Design System FormBuilder
    
    - Update validator message from a string to a symbol
    
    - Update relevant localization (error message and model)
    
    - Update relevant tests for misc_fees LGFS view

--- features/page_objects/sections/lgfs_misc_fee_section.rb
+++ features/page_objects/sections/lgfs_misc_fee_section.rb
@@ -1,7 +1,7 @@
 require_relative 'radio_section'
 
 class FeeTypeSection < SitePrism::Section
-  sections :radios, RadioSection, '.multiple-choice'
+  sections :radios, RadioSection, '.govuk-radios__item'
 
   def radio_labels
     radios.map { |radio| radio.label.text.gsub(/\n(.*)/, '') }

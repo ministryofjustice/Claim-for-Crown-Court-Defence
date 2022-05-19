c83a2c8f73e2c3ce6da0cc50bb5726af2c84a6d1
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2022-05-19 17:28:24 +0100

    Fix failing feature tests

--- features/page_objects/sections/expense_section.rb
+++ features/page_objects/sections/expense_section.rb
@@ -1,26 +1,15 @@
 class ExpenseSection < SitePrism::Section
-  #
-  # TODO: Fix this.
-  # This will only work for 1 expense. If there are more than 1 expense,
-  # it will always populate the first expense, because the way we are referencing
-  # the elements by ID pointing to the first (zero-index) one.
-  #
   element :expense_type_dropdown, ".fx-travel-expense-type select"
   element :destination_label, ".fx-establishment-select label"
-  element :destination, "#expense_1_location"
-  element :distance, "#expense_1_distance"
+  element :destination, ".fx-travel-location .fx-location-model"
+  element :distance, ".fx-travel-distance input"
   element :reason_for_travel_dropdown, ".fx-travel-reason select"
-  element :other_reason_input, "#expense_1_reason_text"
-  element :amount, "#expense_1_amount"
-  element :vat_amount, "#expense_1_vat_amount"
-  element :mileage_20, ".fx-travel-mileage .fx-travel-mileage-bike label"
-  element :mileage_25, ".fx-travel-mileage .fx-travel-mileage-car label:first"
-  element :mileage_45, ".fx-travel-mileage .fx-travel-mileage-bike label:last"
+  element :other_reason_input, ".fx-travel-reason-other input"
+  element :amount, ".fx-travel-net-amount input"
+  element :vat_amount, ".fx-travel-vat-amount input"
+  element :mileage_20, ".fx-travel-mileage.fx-travel-mileage-bike label"
+  element :mileage_25, ".fx-travel-mileage.fx-travel-mileage-car label:first"
+  element :mileage_45, ".fx-travel-mileage.fx-travel-mileage-bike label:last"
 
-  section :expense_date, "#expense_1_date" do
-    include DateHelper
-    element :day, "input#claim_expenses_attributes_0_date_dd"
-    element :month, "input#claim_expenses_attributes_0_date_mm"
-    element :year, "input#claim_expenses_attributes_0_date_yyyy"
-  end
+  section :expense_date, GovukDateSection, ".fx-travel-date"
 end

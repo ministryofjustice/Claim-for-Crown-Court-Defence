bad81066edd71a75f3ad2bc08660b69002bf41b8
Peter Hunt <naseberry@users.noreply.github.com>
2022-06-17 15:20:46 +0100

    Migrate (interim|disbursement|warrant) fees - Resolve autocompletes
    Having migrated the existing autocomplete field (`:disbursement_type_id`) to use the GDS form builder, there are new issues when working in conjunction with cocoon.
    
    The form builder gem automatically generates an inputs `id`, whereas before we manaually controlled an inputs `id`
    When using cocoon to 'add another' once, it automatically generates a new id, this works, however, choosing to 'add another' more than once, all new id are the same value
    
    This causes two issues:
    
    - Label + Input association becomes incorrect (Accessibility & Usability)
    
    - This breaks the Autocomplete search inputs as these require unique identifiers
    
    The solution:
    
    - Rewrite `Modules.AutocompleteWrapper.js` to allow a `select` to become an autocomplete field on page load or when called
    
    - As the disbursement partial (`_disbursement_fields.html.haml`) is used by both interm and disbursement form_steps, update `DisbursementsCtrl.js` and `InterimFeeFieldsDisplay.js` to call autocomplete when cocoon inserts a new section
    
    - Update `CocoonHelper.js` to update an inputs `id` and labels `for` value, this resolves the accessibility issue related to label & input association and fixes the broken autocomplete fields

--- app/webpack/javascripts/modules/external_users/claims/DisbursementsCtrl.js
+++ app/webpack/javascripts/modules/external_users/claims/DisbursementsCtrl.js
@@ -2,9 +2,11 @@ moj.Modules.DisbursementsCtrl = {
   els: {
     fxAutocomplete: '.fx-autocomplete'
   },
+
   activate: function () {
     return $('#claim_form_step').val() === 'disbursements'
   },
+
   initAutocomplete: function () {
     $(this.els.fxAutocomplete).is(function (idx, el) {
       moj.Helpers.Autocomplete.new('#' + el.id, {
@@ -13,6 +15,7 @@ moj.Modules.DisbursementsCtrl = {
       })
     })
   },
+
   init: function () {
     if (this.activate()) {
       // init the auto complete
@@ -21,13 +24,11 @@ moj.Modules.DisbursementsCtrl = {
       this.bindEvents()
     }
   },
+
   bindEvents: function () {
     $('#disbursements').on('cocoon:after-insert', function (e, element) {
-      const elId = $(element).find('.fx-autocomplete').attr('id')
-      moj.Helpers.Autocomplete.new('#' + elId, {
-        showAllValues: true,
-        autoselect: false
-      })
+      const elId = $(element).find('.fx-autocomplete-wrapper select').attr('id')
+      moj.Modules.AutocompleteWrapper.Autocomplete(elId)
     })
   }
 }

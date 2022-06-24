7ab9b0f232fa6ca439ad35d395caa6f99c20115c
Joe Haig <joseph.haig@digital.justice.gov.uk>
2022-06-24 11:16:43 +0100

    Memoize collection_hash

--- app/models/claim/transfer_brain_data_item_collection.rb
+++ app/models/claim/transfer_brain_data_item_collection.rb
@@ -17,17 +17,16 @@ module Claim
 
     def initialize
       load_data_items
-      @collection_hash = construct_collection_hash
     end
 
     data_item_delegate :transfer_fee_full_name, :allocation_type, :bill_scenario, :ppe_required, :days_claimable
 
     def to_h
-      @collection_hash
+      collection_hash
     end
 
     def to_json(opts = nil)
-      @collection_hash.to_json(opts)
+      collection_hash.to_json(opts)
     end
 
     def detail_valid?(detail)
@@ -38,7 +37,7 @@ module Claim
     end
 
     def valid_transfer_stage_ids(litigator_type, elected_case)
-      transfer_stages = @collection_hash.fetch(litigator_type).fetch(elected_case)
+      transfer_stages = collection_hash.fetch(litigator_type).fetch(elected_case)
       ids = []
       transfer_stages.each do |transfer_stage_id, result_hash|
         result_hash.each_value do |result|
@@ -49,7 +48,7 @@ module Claim
     end
 
     def valid_case_conclusion_ids(litigator_type, elected_case, transfer_stage_id)
-      result = @collection_hash.fetch(litigator_type).fetch(elected_case).fetch(transfer_stage_id).keys
+      result = collection_hash.fetch(litigator_type).fetch(elected_case).fetch(transfer_stage_id).keys
       result = TransferBrain.case_conclusion_ids if result == ['*']
       result.sort
     end
@@ -61,7 +60,7 @@ module Claim
     private
 
     def specific_mapping_for(detail)
-      @collection_hash.dig(
+      collection_hash.dig(
         detail.litigator_type,
         detail.elected_case,
         detail.transfer_stage_id,
@@ -70,7 +69,7 @@ module Claim
     end
 
     def wildcard_mapping_for(detail)
-      @collection_hash.dig(
+      collection_hash.dig(
         detail.litigator_type,
         detail.elected_case,
         detail.transfer_stage_id,
@@ -102,8 +101,8 @@ module Claim
       @data_items = parse_data_items(data_items)
     end
 
-    def construct_collection_hash
-      @data_items.each_with_object({}) do |item, collection_hash|
+    def collection_hash
+      @collection_hash ||= @data_items.each_with_object({}) do |item, collection_hash|
         collection_hash.deep_merge!(item.to_h)
       end
     end

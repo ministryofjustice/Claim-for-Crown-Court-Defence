a7dce5c6a8c6857778ca800858908f4a9059439c
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2022-07-14 16:48:28 +0100

    :bug: Migrate misc_fees - Sup - Update feature tests

--- features/page_objects/sections/miscellaneous_fee_section.rb
+++ features/page_objects/sections/miscellaneous_fee_section.rb
@@ -1,15 +1,15 @@
 require_relative 'checkbox_fee_section'
 
 class MiscellaneousFeeSection < CheckboxFeeSection
-  section :confiscation_hearings_half_day_uplift, FixedFeeCaseNumbersSection, "#confiscation-hearings-half-day-uplift > .misc-fee-group"
-  section :confiscation_hearings_half_day, FeeSection, "#confiscation-hearings-half-day > .misc-fee-group"
-  section :confiscation_hearings_whole_day_uplift, FeeSection, "#confiscation-hearings-whole-day-uplift > .misc-fee-group"
-  section :confiscation_hearings_whole_day, FeeSection, "#confiscation-hearings-whole-day > .misc-fee-group"
-  section :deferred_sentence_hearings_uplift, FeeSection, "#deferred-sentence-hearings-uplift > .misc-fee-group"
-  section :deferred_sentence_hearings, FeeSection, "#deferred-sentence-hearings > .misc-fee-group"
-  section :plea_and_trial_preparation_hearing, FeeSection, "#plea-and-trial-preparation-hearing > .misc-fee-group"
-  section :special_preparation_fee, FeeSection, "#special-preparation-fee > .misc-fee-group"
-  section :standard_appearance_fee_uplift, FeeSection, "#standard-appearance-fee-uplift > .misc-fee-group"
-  section :standard_appearance_fee, FeeSection, "#standard-appearance-fee > .misc-fee-group"
-  section :wasted_preparation_fee, FeeSection, "#wasted-preparation-fee > .misc-fee-group"
+  section :confiscation_hearings_half_day_uplift, FixedFeeCaseNumbersSection, "[data-target = 'confiscation-hearings-half-day-uplift'] .misc-fee-group"
+  section :confiscation_hearings_half_day, FeeSection, "[data-target = 'confiscation-hearings-half-day'] .misc-fee-group"
+  section :confiscation_hearings_whole_day_uplift, FeeSection, "[data-target = 'confiscation-hearings-whole-day-uplift'] .misc-fee-group"
+  section :confiscation_hearings_whole_day, FeeSection, "[data-target = 'confiscation-hearings-whole-day'] .misc-fee-group"
+  section :deferred_sentence_hearings_uplift, FeeSection, "[data-target = 'deferred-sentence-hearings-uplift'] .misc-fee-group"
+  section :deferred_sentence_hearings, FeeSection, "[data-target = 'deferred-sentence-hearings > .misc-fee-group"
+  section :plea_and_trial_preparation_hearing, FeeSection, "[data-target = 'plea-and-trial-preparation-hearing'] .misc-fee-group"
+  section :special_preparation_fee, FeeSection, "[data-target = 'special-preparation-fee'] .misc-fee-group"
+  section :standard_appearance_fee_uplift, FeeSection, "[data-target = 'standard-appearance-fee-uplift'] .misc-fee-group"
+  section :standard_appearance_fee, FeeSection, "[data-target = 'standard-appearance-fee'] .misc-fee-group"
+  section :wasted_preparation_fee, FeeSection, "[data-target = 'wasted-preparation-fee'] .misc-fee-group"
 end

51245d7b7db67517eb34daa31ada158256a175ac
imtiazAhmd <imtiaz.ahmed@digital.justice.gov.uk>
2022-07-14 12:34:27 +0100

    :fire: Migrate misc_fees - Fix error presenter

--- app/validators/base_sub_model_validator.rb
+++ app/validators/base_sub_model_validator.rb
@@ -47,7 +47,7 @@ class BaseSubModelValidator < BaseValidator
     associated_record = record.__send__(association_name)
     return if associated_record_has_no_errors?(associated_record)
     @result = false
-    copy_errors_to_base_record(record, association_name, associated_record, nil) if %i[fixed_fee graduated_fee hardship_fee interim_fee transfer_fee warrant_fee].include? association_name # add the name of the association being migrated to this array, eg %i[graduated_fee]. remove this guard when govuk migration is complete
+    copy_errors_to_base_record(record, association_name, associated_record, nil) if %i[fixed_fee graduated_fee hardship_fee interim_fee interim_claim_info misc_fees transfer_fee warrant_fee].include? association_name # add the name of the association being migrated to this array, eg %i[graduated_fee]. remove this guard when govuk migration is complete
   end
 
   def associated_record_has_no_errors?(associated_record)
@@ -73,7 +73,8 @@ class BaseSubModelValidator < BaseValidator
     #
     # rubocop:disable Layout/LineLength
     if %i[basic_fees dates_attended defendants disbursements expenses fixed_fees fixed_fee
-          graduated_fee hardship_fee interim_fee representation_orders transfer_fee warrant_fee].include? association_name
+          graduated_fee hardship_fee interim_fee interim_claim_info misc_fees
+          representation_orders transfer_fee warrant_fee].include? association_name
       [association_name.to_s, 'attributes', record_num.to_s, error.attribute.to_s].compact_blank.join('_')
     else
       "#{association_name.to_s.singularize}_#{record_num + 1}_#{error.attribute}"

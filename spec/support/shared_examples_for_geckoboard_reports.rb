84f96c8e5a02886097b684013ea4cd35eeb477c6
Joe Haig <joseph.haig@digital.justice.gov.uk>
2022-09-15 12:17:27 +0100

    Rubocop: Fix RSpec/NoExpectationExample
    Fix RSpec/NoExpectationExample in `spec/support/shared_examples_for_geckoboard_reports.rb`.
    Also move a setup method into a `before` block and avoid an implicit subject.
    
    .

--- spec/support/shared_examples_for_geckoboard_reports.rb
+++ spec/support/shared_examples_for_geckoboard_reports.rb
@@ -5,7 +5,7 @@ RSpec.shared_examples 'geckoboard publishable report' do
   let(:logger) { double Rails.logger }
   let(:null_object) { double('null object').as_null_object }
 
-  subject { described_class.new }
+  subject(:report) { described_class.new }
 
   it { expect(described_class).to have_constant name: :ITEMS_CHUNK_SIZE, value: 500 }
   it { expect(described_class).to have_constant name: :MAX_STRING_LENGTH, value: 100 }
@@ -16,69 +16,58 @@ RSpec.shared_examples 'geckoboard publishable report' do
   it { is_expected.to respond_to :force? }
   it { is_expected.to respond_to :published? }
 
-  def mock_expectations(exit_without_ping = nil)
+  before do
+    allow(ENV).to receive(:fetch).and_call_original
     allow(ENV).to receive(:fetch).with('ENV', nil).and_return 'staging'
     allow(ENV).to receive(:fetch).with('GECKOBOARD_API_KEY', nil).and_return 'fake-API-key'
-    expect(Geckoboard).to receive(:client).with('fake-API-key').and_return client
-    return client if exit_without_ping
-    expect(client).to receive(:ping).and_return true
-    yield client if block_given?
+    allow(client).to receive(:ping).and_return true
+    allow(Geckoboard).to receive(:client).with('fake-API-key').and_return client
   end
 
   describe '#new' do
     it 'connects to geckoboard using API key stored in ENV variable' do
-      mock_expectations
-      subject
+      expect { report }.not_to raise_error
     end
 
     it 'returns report instance on success' do
-      mock_expectations
-      expect(subject).to be_instance_of described_class
+      expect(report).to be_instance_of described_class
     end
 
     it 'raises and logs error on failure' do
-      client = mock_expectations true
       allow(client).to receive(:ping).and_raise Geckoboard::UnauthorizedError
       expect(Rails).to receive(:logger).and_return logger
       expect(logger).to receive(:warn).with(/.*Geckoboard API key.*/)
-      expect { subject }.to raise_error Geckoboard::UnauthorizedError
+      expect { report }.to raise_error Geckoboard::UnauthorizedError
     end
   end
 
   describe '#id' do
-    before do
-      mock_expectations
-    end
-
     it 'is specific to app, environment and report name' do
-      expect(subject.id).to eql "advocate_defence_payments-staging.#{described_class.name.demodulize.underscore}"
+      expect(report.id).to eql "advocate_defence_payments-staging.#{described_class.name.demodulize.underscore}"
     end
   end
 
   describe '#publish!' do
     it 'creates (or finds) geckoboard dataset and replaces its data' do
-      mock_expectations
-      expect(subject).to receive(:create_dataset!)
-      expect(subject).to receive(:add_to_dataset!).and_return true
-      expect(subject.publish!).to be true
+      expect(report).to receive(:create_dataset!)
+      expect(report).to receive(:add_to_dataset!).and_return true
+      expect(report.publish!).to be true
     end
 
     context 'handles conflict errors' do
       before do
-        mock_expectations do |client|
-          allow(client).to receive(:datasets).and_return datasets_client
-          allow(datasets_client).to receive(:find_or_create).with(any_args).and_raise Geckoboard::ConflictError
-        end
+        allow(client).to receive(:datasets).and_return datasets_client
+        allow(datasets_client).to receive(:find_or_create).with(any_args).and_raise Geckoboard::ConflictError
       end
 
       it 'by overwriting existing dataset when force specified' do
-        expect(subject).to receive(:overwrite!)
-        subject.publish! true
+        expect(report).to receive(:overwrite!)
+        report.publish! true
       end
 
       it 'by raising errors when force not specified' do
-        expect(subject).not_to receive(:overwrite!)
-        expect { subject.publish! }.to raise_error Geckoboard::ConflictError
+        expect(report).not_to receive(:overwrite!)
+        expect { report.publish! }.to raise_error Geckoboard::ConflictError
       end
     end
   end
@@ -86,21 +75,17 @@ RSpec.shared_examples 'geckoboard publishable report' do
   describe '#unpublish!' do
     context 'when dataset exists' do
       it 'deletes the dataset and returns true' do
-        mock_expectations do |client|
-          expect(client).to receive(:datasets).and_return dataset
-          expect(dataset).to receive(:delete).and_return true
-          expect(subject.unpublish!).to be true
-        end
+        expect(client).to receive(:datasets).and_return dataset
+        expect(dataset).to receive(:delete).and_return true
+        expect(report.unpublish!).to be true
       end
     end
 
     context 'when dataset does not exist' do
       it 'returns false' do
-        mock_expectations do |client|
-          expect(client).to receive(:datasets).and_return dataset
-          expect(dataset).to receive(:delete).and_raise Geckoboard::UnexpectedStatusError
-          expect(subject.unpublish!).to be false
-        end
+        expect(client).to receive(:datasets).and_return dataset
+        expect(dataset).to receive(:delete).and_raise Geckoboard::UnexpectedStatusError
+        expect(report.unpublish!).to be false
       end
     end
   end

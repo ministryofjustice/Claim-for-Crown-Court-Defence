3c6c8c58c4d74cc0c20fbeabe6538d6261b36f2e
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2022-12-19 13:01:59 +0000

    Add lgfs main hearing date feature flag to lgfs api endpoints

--- app/interfaces/api/v1/external_users/claims/final_claim.rb
+++ app/interfaces/api/v1/external_users/claims/final_claim.rb
@@ -12,7 +12,7 @@ module API::V1::ExternalUsers
         optional :actual_trial_length,
                  type: Integer,
                  desc: 'REQUIRED/UNREQUIRED: The actual trial length in days, required for graduated fees.'
-        if Settings.main_hearing_date_enabled?
+        if Settings.main_hearing_date_enabled_for_lgfs?
           optional :main_hearing_date,
                    type: String,
                    desc: 'OPTIONAL: The date of the main hearing of the case (YYYY-MM-DD)',

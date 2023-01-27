c6c4d383e8b864cc5455bc9965cedeea8fd189f6
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-01-27 13:50:18 +0000

    Get webpack assets working correctly
    * Add some directories to the assets load paths
    * Add the sassc gem
    * Add yarn build to docker file
    * Configure babel following https://github.com/rails/jsbundling-rails/blob/main/docs/switch_from_webpacker.md#optional-babel

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,3 @@
-// needed by sprocket-rails with rails 6.0.3
-// can be removed if/when we move to webpacker
-// see https://github.com/rails/sprockets-rails/issues/444
-// and https://github.com/rails/sprockets-rails/blob/v3.2.1/lib/sprockets/railtie.rb#L103-L110
-//
 // swagger docs
 //= link grape_swagger_rails/application.css
 //= link grape_swagger_rails/application.js
@@ -14,6 +9,5 @@
 //= link govuk-apple-touch-icon-167x167.png
 //= link govuk-apple-touch-icon-152x152.png
 //= link govuk-apple-touch-icon.png
-//= link application.css
 //= link govuk-opengraph-image.png
 //= link application.js

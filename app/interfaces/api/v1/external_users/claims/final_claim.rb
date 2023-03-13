d2125bb0d96ea33477002cdb444953637448d697
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-03-13 17:52:22 +0000

    Fix Style/ClassAndModuleChildren offences in API claim classes

--- app/interfaces/api/v1/external_users/claims/final_claim.rb
+++ app/interfaces/api/v1/external_users/claims/final_claim.rb
@@ -1,36 +1,40 @@
-module API::V1::ExternalUsers
-  module Claims
-    class FinalClaim < Grape::API
-      helpers API::V1::ClaimParamsHelper
+module API
+  module V1
+    module ExternalUsers
+      module Claims
+        class FinalClaim < Grape::API
+          helpers API::V1::ClaimParamsHelper
 
-      params do
-        use :common_params
-        use :common_lgfs_params
-        optional :case_concluded_at,
-                 type: String,
-                 desc: 'REQUIRED: The date the case concluded (YYYY-MM-DD)', standard_json_format: true
-        optional :actual_trial_length,
-                 type: Integer,
-                 desc: 'REQUIRED/UNREQUIRED: The actual trial length in days, required for graduated fees.'
-        optional :main_hearing_date,
-                 type: String,
-                 desc: 'OPTIONAL: The date of the main hearing of the case (YYYY-MM-DD)',
-                 standard_json_format: true
-      end
+          params do
+            use :common_params
+            use :common_lgfs_params
+            optional :case_concluded_at,
+                     type: String,
+                     desc: 'REQUIRED: The date the case concluded (YYYY-MM-DD)', standard_json_format: true
+            optional :actual_trial_length,
+                     type: Integer,
+                     desc: 'REQUIRED/UNREQUIRED: The actual trial length in days, required for graduated fees.'
+            optional :main_hearing_date,
+                     type: String,
+                     desc: 'OPTIONAL: The date of the main hearing of the case (YYYY-MM-DD)',
+                     standard_json_format: true
+          end
 
-      namespace :final do
-        desc 'Create a Litigator final claim.'
-        post do
-          create_resource(::Claim::LitigatorClaim)
-          status api_response.status
-          api_response.body
-        end
+          namespace :final do
+            desc 'Create a Litigator final claim.'
+            post do
+              create_resource(::Claim::LitigatorClaim)
+              status api_response.status
+              api_response.body
+            end
 
-        desc 'Validate a Litigator final claim.'
-        post '/validate' do
-          validate_resource(::Claim::LitigatorClaim)
-          status api_response.status
-          api_response.body
+            desc 'Validate a Litigator final claim.'
+            post '/validate' do
+              validate_resource(::Claim::LitigatorClaim)
+              status api_response.status
+              api_response.body
+            end
+          end
         end
       end
     end

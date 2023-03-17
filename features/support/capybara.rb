f2aa1101049e2d6a4201609da258a259f1a36dec
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-03-17 11:15:47 +0000

    Fix Selenium deprecation warning
    The following warning is displayed when running feature tests:
    
    `WARN Selenium [DEPRECATION] [:capabilities] The :capabilities parameter
    for Selenium::WebDriver::Chrome::Driver is deprecated. Use :options
    argument with an instance of Selenium::WebDriver::Chrome::Driver instead.`
    
    This commit replaces the use of `capabilities` with `options` as
    recommended.

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -17,10 +17,10 @@ Capybara.register_server :puma do |app, port, host|
 end
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Chrome::Options.new(
+  options = Selenium::WebDriver::Chrome::Options.new(
     args: %w[headless disable-gpu window-size=1366,768]
   )
-  Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: capabilities)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
 
 # use headless chrome for javascript

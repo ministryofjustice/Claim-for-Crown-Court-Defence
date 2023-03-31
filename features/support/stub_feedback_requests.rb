10c4c9f2b88608994ce98ebe7f2513a406a5e7f8
Obsiye <abdirizak.obsiye@digital.justice.gov.uk>
2023-03-31 10:29:39 +0100

    Fix broken cucumber test for bug report
    Fix the broken cucumber test because bug reports will always be sent to zendesk and thus
    shouldn't use the feedback_service_url method

--- features/support/stub_feedback_requests.rb
+++ features/support/stub_feedback_requests.rb
@@ -10,6 +10,16 @@ Before('@stub_feedback_failure') do
     .and_return(status: 500, body: unsuccessful_zendesk_body.to_json)
 end
 
+Before('@stub_bug_report_success') do
+  stub_request(:post, %r{\Ahttps://ministryofjustice.zendesk.com/api/v2/.*} )
+  .and_return(status: 201, body: successful_zendesk_body.to_json)
+end
+
+Before('@stub_bug_report_failure') do
+  stub_request(:post, %r{\Ahttps://ministryofjustice.zendesk.com/api/v2/.*} )
+    .and_return(status: 500, body: unsuccessful_zendesk_body.to_json)
+end
+
 def successful_zendesk_body
   { item: {} }.to_json
 end

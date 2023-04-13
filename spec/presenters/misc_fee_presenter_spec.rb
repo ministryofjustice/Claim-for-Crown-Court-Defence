7d3678bd8785cea7575d626ac8a2f4e1f97a6f5e
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-04-13 16:52:31 +0100

    Present rate of s28 hearing fee as n/a

--- spec/presenters/misc_fee_presenter_spec.rb
+++ spec/presenters/misc_fee_presenter_spec.rb
@@ -1,28 +1,41 @@
 require 'rails_helper'
 
 describe Fee::MiscFeePresenter do
-  let(:misc_fee) { instance_double(Fee::MiscFee, claim: double, quantity_is_decimal?: false, errors: { quantity: [] }) }
-  let(:presenter) { Fee::MiscFeePresenter.new(misc_fee, view) }
+  subject(:presenter) { described_class.new(misc_fee, view) }
 
-  context '#rate' do
-    it 'calls not_applicable_html when belonging to an LGFS claim' do
-      allow(presenter).to receive(:agfs?).and_return false
-      expect(presenter).to receive(:not_applicable_html)
-      presenter.rate
+  describe '#rate' do
+    subject { presenter.rate }
+
+    context 'with an AGFS claim and calculated fee type' do
+      let(:misc_fee) { create(:misc_fee, rate: 12.01, claim: build(:advocate_claim)) }
+
+      before { allow(misc_fee).to receive(:calculated?).and_return(true) }
+
+      it { is_expected.to eq('£12.01') }
     end
 
-    it 'returns number as currency for calculated fees belonging to an AGFS claim' do
-      allow(presenter).to receive(:agfs?).and_return true
-      allow(misc_fee).to receive(:calculated?).and_return true
-      expect(misc_fee).to receive(:rate).and_return 12.01
-      expect(presenter.rate).to eq '£12.01'
+    context 'with an AGFS claim and uncalculated fee type' do
+      let(:misc_fee) { create(:misc_fee, rate: 12.01, claim: build(:advocate_claim)) }
+
+      before { allow(misc_fee).to receive(:calculated?).and_return(false) }
+
+      it { is_expected.to match(%r{n/a}) }
     end
 
-    it 'returns not_applicable for uncalculated fees belonging to an AGFS claim' do
-      allow(presenter).to receive(:agfs?).and_return true
-      allow(misc_fee).to receive(:calculated?).and_return false
-      expect(presenter).to receive(:not_applicable)
-      presenter.rate
+    context 'with a section 28 fee (MISTE)' do
+      let(:misc_fee) do
+        create(:misc_fee, rate: 12.01, claim: build(:advocate_claim), fee_type: build(:misc_fee_type, :miste))
+      end
+
+      it { is_expected.to match(%r{n/a}) }
+    end
+
+    context 'with an LGFS claim' do
+      let(:misc_fee) { create(:misc_fee, rate: 12.01, claim: build(:litigator_claim)) }
+
+      before { allow(misc_fee).to receive(:calculated?).and_return(true) }
+
+      it { is_expected.to match(%r{n/a}) }
     end
   end
 

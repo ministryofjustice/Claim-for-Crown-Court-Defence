b100521dd380f597c3c77c204adb5fd8e36522cb
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-04-17 10:47:00 +0100

    Allow for nil fee_type

--- app/presenters/fee/misc_fee_presenter.rb
+++ app/presenters/fee/misc_fee_presenter.rb
@@ -2,14 +2,14 @@ module Fee
   class MiscFeePresenter < Fee::BaseFeePresenter
     def quantity
       return not_applicable_html unless agfs?
-      return not_applicable_html if fee_type.unique_code == 'MISTE'
+      return not_applicable_html if fee_type&.unique_code == 'MISTE'
 
       super
     end
 
     def rate
       return not_applicable_html unless agfs?
-      return not_applicable_html if fee_type.unique_code == 'MISTE'
+      return not_applicable_html if fee_type&.unique_code == 'MISTE'
 
       super
     end

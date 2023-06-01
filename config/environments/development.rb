ministryofjustice/Claim-for-Crown-Court-Defence

commit bb9ccebc5b513f6029bf7fd2322ed440af89509f

Update the GRAPE_SWAGGER_ROOT_URL in the config to CASEWORKER_API_URL
Replace the GRAPE_SWAGGER_ROOT_URL with CASEWORKER_API_URL in the config files, where appropriate (where those calls are the caseworker api ones not the swagger doc ones)

config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: ENV["GRAPE_SWAGGER_ROOT_URL"] || 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV["CASEWORKER_API_URL"] || 'http://localhost:3000' }
   config.action_mailer.asset_host = config.action_mailer.default_url_options[:host]
 
   # Print deprecation notices to the Rails logger.

config/environments/devunicorn.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: ENV["GRAPE_SWAGGER_ROOT_URL"] || 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV["CASEWORKER_API_URL"] || 'localhost:3000' }
   config.action_mailer.asset_host = config.action_mailer.default_url_options[:host]
 
   # enable the ability to preview devise emails

config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "advocate_defence_payments_production"
 
-  config.action_mailer.default_url_options = { host: ENV['GRAPE_SWAGGER_ROOT_URL'] }
+  config.action_mailer.default_url_options = { host: ENV['CASEWORKER_API_URL'] }
   config.action_mailer.asset_host = config.action_mailer.default_url_options[:host]
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp

config/environments/test.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV["GRAPE_SWAGGER_ROOT_URL"] || 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV["CASEWORKER_API_URL"] || 'http://localhost:3000' }
   config.action_mailer.asset_host = config.action_mailer.default_url_options[:host]
 
   # Raise exceptions instead of rendering exception templates.

config/settings.yml
@@ -136,7 +136,7 @@ timed_transition_soft_delete_weeks: 16
 # Override them in your settings.local.yml if needed, but DO NOT change these here
 #
 remote_api_key: 'not-provided'
-remote_api_url: <%= (ENV['GRAPE_SWAGGER_ROOT_URL'] || 'http://localhost:3001') + '/api' %>
+remote_api_url: <%= (ENV['CASEWORKER_API_URL'] || 'http://localhost:3001') + '/api' %>
 
 # Postgres DB `journeys` function flag to allow for its "recreation".
 # This is used by MI reports.

commit 19b792eae716af8c328fe7e8c8f61145afe3bf26

Fix password reset url in emails
bb9ccebc5b513f6029b accidentally changed the url used for password reset
links in emails sent following the use of the forgotten password feature.

This commit puts the url back to the correct value.

config/environments/development.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: ENV["CASEWORKER_API_URL"] || 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV["GRAPE_SWAGGER_ROOT_URL"] || 'http://localhost:3000' }
   config.action_mailer.asset_host = config.action_mailer.default_url_options[:host]
 
   # Print deprecation notices to the Rails logger.

config/environments/devunicorn.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: ENV["CASEWORKER_API_URL"] || 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV["GRAPE_SWAGGER_ROOT_URL"] || 'localhost:3000' }
   config.action_mailer.asset_host = config.action_mailer.default_url_options[:host]
 
   # enable the ability to preview devise emails

config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "advocate_defence_payments_production"
 
-  config.action_mailer.default_url_options = { host: ENV['CASEWORKER_API_URL'] }
+  config.action_mailer.default_url_options = { host: ENV['GRAPE_SWAGGER_ROOT_URL'] }
   config.action_mailer.asset_host = config.action_mailer.default_url_options[:host]
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp

config/environments/test.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV["CASEWORKER_API_URL"] || 'http://localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV["GRAPE_SWAGGER_ROOT_URL"] || 'http://localhost:3000' }
   config.action_mailer.asset_host = config.action_mailer.default_url_options[:host]
 
   # Raise exceptions instead of rendering exception templates.
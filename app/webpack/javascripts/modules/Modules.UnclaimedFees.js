1e4b3f6b4269d3bc195583e7510f82c476e3795e
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-07-04 17:13:11 +0100

    Javascript linting

--- app/webpack/javascripts/modules/Modules.UnclaimedFees.js
+++ app/webpack/javascripts/modules/Modules.UnclaimedFees.js
@@ -3,11 +3,11 @@ moj.Modules.UnclaimedFees = {
     this.checkListWrapper = document.getElementById('mod-unclaimed-fees')
     if (this.checkListWrapper) {
       this.button = document.getElementById('add-unclaimed-fees')
-      const form = this.button.closest("form")
-      this.url = form.getAttribute("action")
-      this.token = Array.from(form.getElementsByTagName("input")).filter(i => i.getAttribute("name") === "authenticity_token")[0].value
+      const form = this.button.closest('form')
+      this.url = form.getAttribute('action')
+      this.token = Array.from(form.getElementsByTagName('input')).filter(i => i.getAttribute('name') === 'authenticity_token')[0].value
       this.checkBoxes = Array.from(this.checkListWrapper.getElementsByClassName('govuk-checkboxes__input'))
-      this.checkBoxes.forEach(box => box.onchange = () => this.setSubmitButton())
+      this.checkBoxes.forEach(box => { box.onchange = () => this.setSubmitButton() })
       this.setSubmitButton()
       this.button.addEventListener('click', (event) => this.submit(event))
     }
@@ -24,16 +24,16 @@ moj.Modules.UnclaimedFees = {
 
     const feesToAdd = this.checkBoxes.filter(box => box.checked)
 
-    const response = await fetch(this.url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    await fetch(this.url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         authenticity_token: this.token,
         fees: feesToAdd.map(fee => fee.value)
       })
     }).then(response => {
       if (response.ok) {
-        feesToAdd.forEach(box => box.disabled = true)
+        feesToAdd.forEach(box => { box.disabled = true })
         this.setSubmitButton()
       } else {
         console.log('There was a problem')

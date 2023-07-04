dd37985c54ddba7a08d773c7b216d70ca0ab7242
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-07-04 16:39:45 +0100

    Add unclaimed fees to claim from checklist

--- app/webpack/javascripts/modules/Modules.UnclaimedFees.js
+++ app/webpack/javascripts/modules/Modules.UnclaimedFees.js
@@ -3,6 +3,9 @@ moj.Modules.UnclaimedFees = {
     this.checkListWrapper = document.getElementById('mod-unclaimed-fees')
     if (this.checkListWrapper) {
       this.button = document.getElementById('add-unclaimed-fees')
+      const form = this.button.closest("form")
+      this.url = form.getAttribute("action")
+      this.token = Array.from(form.getElementsByTagName("input")).filter(i => i.getAttribute("name") === "authenticity_token")[0].value
       this.checkBoxes = Array.from(this.checkListWrapper.getElementsByClassName('govuk-checkboxes__input'))
       this.checkBoxes.forEach(box => box.onchange = () => this.setSubmitButton())
       this.setSubmitButton()
@@ -16,12 +19,25 @@ moj.Modules.UnclaimedFees = {
     this.button.disabled = !this.checkBoxes.reduce((selected, box) => selected || (box.checked && !box.disabled), false)
   },
 
-  submit: function (event) {
+  submit: async function (event) {
     event.preventDefault()
 
     const feesToAdd = this.checkBoxes.filter(box => box.checked)
 
-    feesToAdd.forEach(box => box.disabled = true)
-    this.setSubmitButton()
+    const response = await fetch(this.url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        authenticity_token: this.token,
+        fees: feesToAdd.map(fee => fee.value)
+      })
+    }).then(response => {
+      if (response.ok) {
+        feesToAdd.forEach(box => box.disabled = true)
+        this.setSubmitButton()
+      } else {
+        console.log('There was a problem')
+      }
+    })
   }
 }

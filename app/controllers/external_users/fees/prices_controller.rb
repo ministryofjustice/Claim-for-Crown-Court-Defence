315197f7b5d0262ad89ce0601b911d7f85514e1d
Alex Sheppard <alexander.sheppard@digital.justice.gov.uk>
2023-07-20 16:56:58 +0100

    Rubocop fixes/cleanup to prices_controller.rb and prices_controller_spec.rb

--- app/controllers/external_users/fees/prices_controller.rb
+++ app/controllers/external_users/fees/prices_controller.rb
@@ -1,48 +1,52 @@
-class ExternalUsers::Fees::PricesController < ExternalUsers::ApplicationController
-  skip_load_and_authorize_resource
-  before_action :set_claim, only: %i[calculate]
+module ExternalUsers
+  module Fees
+    class PricesController < ExternalUsers::ApplicationController
+      skip_load_and_authorize_resource
+      before_action :set_claim, only: %i[calculate]
 
-  attr_reader :claim
+      attr_reader :claim
 
-  ALLOWABLE_PRICER_TYPES = {
-    'UnitPrice' => Claims::FeeCalculator::UnitPrice,
-    'GraduatedPrice' => Claims::FeeCalculator::GraduatedPrice
-  }.freeze
+      ALLOWABLE_PRICER_TYPES = {
+        'UnitPrice' => Claims::FeeCalculator::UnitPrice,
+        'GraduatedPrice' => Claims::FeeCalculator::GraduatedPrice
+      }.freeze
 
-  def calculate
-    calculator = pricer.new(claim, calculator_params.except(:id))
-    response = calculator.call
-    respond_to do |format|
-      format.html
-      format.json do
-        render json: response, status: response.success? ? :ok : :unprocessable_entity
+      def calculate
+        calculator = pricer.new(claim, calculator_params.except(:id))
+        response = calculator.call
+        respond_to do |format|
+          format.html
+          format.json do
+            render json: response, status: response.success? ? :ok : :unprocessable_entity
+          end
+        end
+      end
+
+      private
+
+      def pricer
+        ALLOWABLE_PRICER_TYPES.fetch(calculator_params[:price_type], Claims::FeeCalculator::NullPrice)
+      end
+
+      def set_claim
+        @claim = Claim::BaseClaim.active.find_by(id: calculator_params[:claim_id])
+      end
+
+      def calculator_params
+        params.permit(
+          :format,
+          :id,
+          :price_type,
+          :claim_id,
+          :fee_type_id,
+          :advocate_category,
+          :ppe,
+          :pw,
+          :days,
+          :pages_of_prosecuting_evidence,
+          fees: %i[fee_type_id quantity]
+        )
       end
     end
-  end
-
-  private
-
-  def pricer
-    ALLOWABLE_PRICER_TYPES.fetch(calculator_params[:price_type], Claims::FeeCalculator::NullPrice)
-  end
-
-  def set_claim
-    @claim = Claim::BaseClaim.active.find_by(id: calculator_params[:claim_id])
-  end
-
-  def calculator_params
-    params.permit(
-      :format,
-      :id,
-      :price_type,
-      :claim_id,
-      :fee_type_id,
-      :advocate_category,
-      :ppe,
-      :pw,
-      :days,
-      :pages_of_prosecuting_evidence,
-      fees: %i[fee_type_id quantity]
-    )
   end
 end
--- spec/controllers/external_users/fees/prices_controller_spec.rb
+++ spec/controllers/external_users/fees/prices_controller_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe ExternalUsers::Fees::PricesController do
   before { sign_in advocate.user }
 
   describe 'POST #calculate.json' do
-    context 'message sending' do
+    context 'when sending message' do
       subject(:calculate) { post :calculate, params: }
 
       let(:claim) { create(:draft_claim) }
@@ -57,23 +57,29 @@ RSpec.describe ExternalUsers::Fees::PricesController do
         let(:calculate_price) { instance_double(calculate_price_service) }
         let(:params) { { format: 'json', claim_id: claim.id.to_s, price_type: 'UnitPrice' } }
 
-        it 'sends message to UnitPrice service' do
-          expect(calculate_price_service).to receive(:new).with(claim, strong_params).and_return(calculate_price)
-          expect(calculate_price).to receive(:call).and_return(calc_response)
+        before do
+          allow(calculate_price_service).to receive(:new).with(claim, strong_params).and_return(calculate_price)
+          allow(calculate_price).to receive(:call).and_return(calc_response)
+
           calculate
+        end
+
+        it 'sends message to UnitPrice service' do
+          expect(calculate_price_service).to have_received(:new).with(claim, strong_params)
+        end
+
+        it 'calls the UnitPrice service' do
+          expect(calculate_price).to have_received(:call)
         end
       end
 
-      context 'Incorrect price type specified', :fee_calc_vcr do
+      context 'with Incorrect price type specified', :fee_calc_vcr do
         let(:params) { { format: 'json', claim_id: claim.id.to_s, price_type: 'BillScenario' } }
 
-        before do
-          calculate
-        end
+        before { calculate }
 
         it_returns 'a failed price calculation response'
       end
-
 
       context 'when GraduatedPrice price type specified' do
         let(:calculate_price_service) { Claims::FeeCalculator::GraduatedPrice }
@@ -91,15 +97,24 @@ RSpec.describe ExternalUsers::Fees::PricesController do
           }
         end
 
-        it 'sends message to GraduatedPrice service' do
-          expect(calculate_price_service).to receive(:new).with(claim, strong_params).and_return(calculate_price)
-          expect(calculate_price).to receive(:call).and_return(calc_response)
+        before do
+          allow(calculate_price_service).to receive(:new).with(claim, strong_params).and_return(calculate_price)
+          allow(calculate_price).to receive(:call).and_return(calc_response)
+
           calculate
+        end
+
+        it 'sends message to GraduatedPrice service' do
+          expect(calculate_price_service).to have_received(:new).with(claim, strong_params)
+        end
+
+        it 'calls the GraduatedPrice service' do
+          expect(calculate_price).to have_received(:call)
         end
       end
     end
 
-    context 'AGFS' do
+    context 'with AGFS' do
       # IMPORTANT: use specific case type, offence class, fee types and reporder
       # date in order to reduce and afix VCR cassettes required (that have to match
       # on query values), prevent flickering specs (from random offence classes,
@@ -130,7 +145,7 @@ RSpec.describe ExternalUsers::Fees::PricesController do
         }
       end
 
-      context 'success', :fee_calc_vcr do
+      context 'with success', :fee_calc_vcr do
         before do
           post :calculate, params: calculator_params
         end
@@ -138,7 +153,7 @@ RSpec.describe ExternalUsers::Fees::PricesController do
         it_returns 'a successful price calculation response'
       end
 
-      context 'failure', :fee_calc_vcr do
+      context 'with failure', :fee_calc_vcr do
         before do
           calculator_params['advocate_category'] = 'Rubbish'
           post :calculate, params: calculator_params
@@ -148,7 +163,7 @@ RSpec.describe ExternalUsers::Fees::PricesController do
       end
     end
 
-    context 'LGFS' do
+    context 'with LGFS' do
       # IMPORTANT: use specific case type, offence class, fee types and reporder
       # date in order to reduce and afix VCR cassettes required (that have to match
       # on query values), prevent flickering specs (from random offence classes,
@@ -187,7 +202,7 @@ RSpec.describe ExternalUsers::Fees::PricesController do
         }
       end
 
-      context 'success', :fee_calc_vcr do
+      context 'with success', :fee_calc_vcr do
         before do
           post :calculate, params: calculator_params
         end
@@ -195,7 +210,7 @@ RSpec.describe ExternalUsers::Fees::PricesController do
         it_returns 'a successful price calculation response'
       end
 
-      context 'failure', :fee_calc_vcr do
+      context 'with failure', :fee_calc_vcr do
         before do
           calculator_params['fee_type_id'] = 'Rubbish'
           post :calculate, params: calculator_params

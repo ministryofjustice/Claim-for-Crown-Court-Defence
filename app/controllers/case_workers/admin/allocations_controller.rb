b56add2ca61f11abb71c661c3018833dbfde697a
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-07-21 09:51:32 +0100

    Fix Rubocop Style/ClassAndModuleChildren offences ...in app/controllers/case_workers

--- app/controllers/case_workers/admin/allocations_controller.rb
+++ app/controllers/case_workers/admin/allocations_controller.rb
@@ -1,130 +1,134 @@
-class CaseWorkers::Admin::AllocationsController < CaseWorkers::Admin::ApplicationController
-  include PaginationHelpers
+module CaseWorkers
+  module Admin
+    class AllocationsController < CaseWorkers::Admin::ApplicationController
+      include PaginationHelpers
 
-  before_action :set_case_workers, only: %i[new create]
-  before_action :set_claims, only: %i[new create]
-  before_action :set_summary_values, only: [:new], if: :summary_from_previous_request?
-  before_action :process_claim_ids, only: [:create], if: :quantity_allocation?
+      before_action :set_case_workers, only: %i[new create]
+      before_action :set_claims, only: %i[new create]
+      before_action :set_summary_values, only: [:new], if: :summary_from_previous_request?
+      before_action :process_claim_ids, only: [:create], if: :quantity_allocation?
 
-  def new
-    @allocation = Allocation.new
-  end
+      def new
+        @allocation = Allocation.new
+      end
 
-  def create
-    @allocation = Allocation.new(allocation_params.merge(current_user:))
-    if @allocation.save
-      redirect_with_feedback(@allocation)
-    else
-      render :new
+      def create
+        @allocation = Allocation.new(allocation_params.merge(current_user:))
+        if @allocation.save
+          redirect_with_feedback(@allocation)
+        else
+          render :new
+        end
+      end
+
+      private
+
+      def redirect_with_feedback(allocation)
+        flash[:notice] = notification(allocation)
+        redirect_to case_workers_admin_allocations_path(tab:, scheme:)
+      end
+
+      def summary_from_previous_request?
+        params[:claim_ids].present? && (params[:case_worker_id].present? || params[:deallocate])
+      end
+
+      # TODO: these will also need to be remote API calls, eventually
+      # I don't think this is needed any more.
+      # def set_summary_values
+      #   @case_worker = CaseWorker.active.find(params[:case_worker_id]) rescue nil
+      #   @allocated_claims = Claim::BaseClaim.active.find(params[:claim_ids].reject(&:blank?))
+      #   params.delete(:case_worker_id)
+      #   params.delete(:claim_ids)
+      # end
+
+      def quantity_allocation?
+        quantity_to_allocate.positive?
+      end
+
+      def quantity_to_allocate
+        params[:quantity_to_allocate].to_i
+      end
+
+      def process_claim_ids
+        params[:allocation][:claim_ids] = @claims.first(quantity_to_allocate).map(&:id).map(&:to_s)
+      end
+
+      def set_case_workers
+        @case_workers = CaseWorkerService.new(current_user:).active
+      end
+
+      def set_claims
+        @claims = Claims::CaseWorkerClaims.new(current_user:, action: tab, criteria: criteria_params).claims
+        add_claim_carousel_info
+      end
+
+      def scheme
+        %w[agfs lgfs].include?(params[:scheme]) ? params[:scheme] : 'agfs'
+      end
+
+      def tab
+        %w[allocated unallocated].include?(params[:tab]) ? params[:tab] : 'unallocated'
+      end
+
+      def filter
+        params[:filter] || 'all'
+      end
+
+      def value_band_id
+        params[:value_band_id] || 0
+      end
+
+      def search_terms
+        params[:search]
+      end
+
+      def search_claims(states = nil)
+        return if search_terms.blank?
+        @claims = @claims.search(search_terms, states, :case_worker_name_or_email)
+      end
+
+      def sort_and_paginate
+        @claims = @claims.sort_using(sort_column, sort_direction).page(current_page).per(page_size)
+      end
+
+      def allocation_params
+        allocator_params = params.require(:allocation).permit(:case_worker_id, :deallocate, claim_ids: [])
+        allocator_params.merge(allocating: is_allocating?)
+      end
+
+      def notification(allocation)
+        claims = allocation.successful_claims
+        case_worker = allocation.case_worker
+        message = "#{claims.size} #{'claim'.pluralize(claims.size)}"
+
+        if case_worker
+          "#{message} allocated to #{case_worker.name}"
+        else
+          "#{message} returned to allocation pool"
+        end
+      end
+
+      def is_allocating?
+        params[:commit] == 'Allocate'
+      end
+
+      def default_page_size
+        25
+      end
+
+      def sort_column
+        'last_submitted_at'
+      end
+
+      def sort_direction
+        'asc'
+      end
+
+      def criteria_params
+        limit = quantity_allocation? ? quantity_to_allocate : page_size
+        { sorting: sort_column, direction: sort_direction, scheme:, filter:,
+          page: current_page, limit:, search: search_terms, value_band_id: }
+      end
     end
-  end
-
-  private
-
-  def redirect_with_feedback(allocation)
-    flash[:notice] = notification(allocation)
-    redirect_to case_workers_admin_allocations_path(tab:, scheme:)
-  end
-
-  def summary_from_previous_request?
-    params[:claim_ids].present? && (params[:case_worker_id].present? || params[:deallocate])
-  end
-
-  # TODO: these will also need to be remote API calls, eventually
-  # I don't think this is needed any more.
-  # def set_summary_values
-  #   @case_worker = CaseWorker.active.find(params[:case_worker_id]) rescue nil
-  #   @allocated_claims = Claim::BaseClaim.active.find(params[:claim_ids].reject(&:blank?))
-  #   params.delete(:case_worker_id)
-  #   params.delete(:claim_ids)
-  # end
-
-  def quantity_allocation?
-    quantity_to_allocate.positive?
-  end
-
-  def quantity_to_allocate
-    params[:quantity_to_allocate].to_i
-  end
-
-  def process_claim_ids
-    params[:allocation][:claim_ids] = @claims.first(quantity_to_allocate).map(&:id).map(&:to_s)
-  end
-
-  def set_case_workers
-    @case_workers = CaseWorkerService.new(current_user:).active
-  end
-
-  def set_claims
-    @claims = Claims::CaseWorkerClaims.new(current_user:, action: tab, criteria: criteria_params).claims
-    add_claim_carousel_info
-  end
-
-  def scheme
-    %w[agfs lgfs].include?(params[:scheme]) ? params[:scheme] : 'agfs'
-  end
-
-  def tab
-    %w[allocated unallocated].include?(params[:tab]) ? params[:tab] : 'unallocated'
-  end
-
-  def filter
-    params[:filter] || 'all'
-  end
-
-  def value_band_id
-    params[:value_band_id] || 0
-  end
-
-  def search_terms
-    params[:search]
-  end
-
-  def search_claims(states = nil)
-    return if search_terms.blank?
-    @claims = @claims.search(search_terms, states, :case_worker_name_or_email)
-  end
-
-  def sort_and_paginate
-    @claims = @claims.sort_using(sort_column, sort_direction).page(current_page).per(page_size)
-  end
-
-  def allocation_params
-    allocator_params = params.require(:allocation).permit(:case_worker_id, :deallocate, claim_ids: [])
-    allocator_params.merge(allocating: is_allocating?)
-  end
-
-  def notification(allocation)
-    claims = allocation.successful_claims
-    case_worker = allocation.case_worker
-    message = "#{claims.size} #{'claim'.pluralize(claims.size)}"
-
-    if case_worker
-      "#{message} allocated to #{case_worker.name}"
-    else
-      "#{message} returned to allocation pool"
-    end
-  end
-
-  def is_allocating?
-    params[:commit] == 'Allocate'
-  end
-
-  def default_page_size
-    25
-  end
-
-  def sort_column
-    'last_submitted_at'
-  end
-
-  def sort_direction
-    'asc'
-  end
-
-  def criteria_params
-    limit = quantity_allocation? ? quantity_to_allocate : page_size
-    { sorting: sort_column, direction: sort_direction, scheme:, filter:,
-      page: current_page, limit:, search: search_terms, value_band_id: }
   end
 end
--- app/controllers/case_workers/claims_controller.rb
+++ app/controllers/case_workers/claims_controller.rb
@@ -1,143 +1,145 @@
-class CaseWorkers::ClaimsController < CaseWorkers::ApplicationController
-  include PaginationHelpers
+module CaseWorkers
+  class ClaimsController < CaseWorkers::ApplicationController
+    include PaginationHelpers
 
-  skip_load_and_authorize_resource
-  authorize_resource class: Claim::BaseClaim
+    skip_load_and_authorize_resource
+    authorize_resource class: Claim::BaseClaim
 
-  helper_method :sort_column, :sort_direction, :search_terms
+    helper_method :sort_column, :sort_direction, :search_terms
 
-  respond_to :html
+    respond_to :html
 
-  # callback order is important (must set claims before filtering and sorting)
-  before_action :set_claims,              only: %i[index archived]
-  before_action :filter_current_claims,   only: [:index]
-  before_action :filter_archived_claims,  only: [:archived]
-  before_action :sort_claims,             only: %i[index archived]
-  before_action :set_claim, only: %i[show messages publish enquire download_zip]
+    # callback order is important (must set claims before filtering and sorting)
+    before_action :set_claims,              only: %i[index archived]
+    before_action :filter_current_claims,   only: [:index]
+    before_action :filter_archived_claims,  only: [:archived]
+    before_action :sort_claims,             only: %i[index archived]
+    before_action :set_claim, only: %i[show messages publish enquire download_zip]
 
-  include ReadMessages
-  include MessageControlsDisplay
+    include ReadMessages
+    include MessageControlsDisplay
 
-  def index; end
+    def index; end
 
-  def archived; end
+    def archived; end
 
-  def show
-    prepare_show_action
-  end
-
-  def download_zip
-    zip_file = S3ZipDownloader.new(@claim).generate!
-
-    send_file zip_file,
-              filename: "#{@claim.case_number}-documents.zip",
-              type: 'application/zip',
-              disposition: 'attachment'
-  end
-
-  def messages
-    render template: 'messages/claim_messages'
-  end
-
-  def update
-    updater = Claims::CaseWorkerClaimUpdater.new(params[:id], claim_params.merge(current_user:)).update!
-    @claim = updater.claim
-    if updater.result == :ok
-      redirect_to case_workers_claim_path(permitted_params)
-    else
-      @error_presenter = ErrorMessage::Presenter.new(@claim)
+    def show
       prepare_show_action
-      render :show
     end
-  end
 
-  private
+    def download_zip
+      zip_file = S3ZipDownloader.new(@claim).generate!
 
-  def permitted_params
-    params.permit(:messages)
-  end
+      send_file zip_file,
+                filename: "#{@claim.case_number}-documents.zip",
+                type: 'application/zip',
+                disposition: 'attachment'
+    end
 
-  def prepare_show_action
-    @claim.assessment = Assessment.new if @claim.assessment.nil?
-    @messages = @claim.messages.most_recent_last
-    @message = @claim.messages.build
-  end
+    def messages
+      render template: 'messages/claim_messages'
+    end
 
-  def search(states = nil)
-    @claims = @claims.search(search_terms, states, *search_options) unless @claims.remote?
-  end
+    def update
+      updater = Claims::CaseWorkerClaimUpdater.new(params[:id], claim_params.merge(current_user:)).update!
+      @claim = updater.claim
+      if updater.result == :ok
+        redirect_to case_workers_claim_path(permitted_params)
+      else
+        @error_presenter = ErrorMessage::Presenter.new(@claim)
+        prepare_show_action
+        render :show
+      end
+    end
 
-  def claim_params
-    params.require(:claim).permit(
-      :state,
-      :refuse_reason_text,
-      :reject_reason_text,
-      :additional_information,
-      assessment_attributes: %i[
-        id
-        fees
-        expenses
-        disbursements
-        vat_amount
-      ],
-      redeterminations_attributes: %i[
-        id
-        fees
-        expenses
-        disbursements
-        vat_amount
-      ]
-    ).merge(params.permit(state_reason: []))
-    # the state_reason needs to be merged because the collection_check_boxes control on the view requires
-    # a claim object to render.  Because state_reason does not belong to claim, it refuses to render and
-    # therefore nil is passed to the object.  This then comes back outside of the claim namespace and has
-    # to be manually merged.
-  end
+    private
 
-  def set_claims
-    @claims = Claims::CaseWorkerClaims.new(current_user:, action: tab, criteria: criteria_params).claims
-  end
+    def permitted_params
+      params.permit(:messages)
+    end
 
-  # Only these 2 actions are handle in this controller. Rest of actions in the admin-namespaced controller.
-  def tab
-    %w[current archived].include?(params[:tab]) ? params[:tab] : 'current'
-  end
+    def prepare_show_action
+      @claim.assessment = Assessment.new if @claim.assessment.nil?
+      @messages = @claim.messages.most_recent_last
+      @message = @claim.messages.build
+    end
 
-  def set_claim
-    @claim = Claim::BaseClaim.active.find(params[:id])
-  end
+    def search(states = nil)
+      @claims = @claims.search(search_terms, states, *search_options) unless @claims.remote?
+    end
 
-  def filter_current_claims
-    search(Claims::StateMachine::CASEWORKER_DASHBOARD_UNDER_ASSESSMENT_STATES) if search_terms.present?
-  end
+    def claim_params
+      params.require(:claim).permit(
+        :state,
+        :refuse_reason_text,
+        :reject_reason_text,
+        :additional_information,
+        assessment_attributes: %i[
+          id
+          fees
+          expenses
+          disbursements
+          vat_amount
+        ],
+        redeterminations_attributes: %i[
+          id
+          fees
+          expenses
+          disbursements
+          vat_amount
+        ]
+      ).merge(params.permit(state_reason: []))
+      # the state_reason needs to be merged because the collection_check_boxes control on the view requires
+      # a claim object to render.  Because state_reason does not belong to claim, it refuses to render and
+      # therefore nil is passed to the object.  This then comes back outside of the claim namespace and has
+      # to be manually merged.
+    end
 
-  def filter_archived_claims
-    search(Claims::StateMachine::CASEWORKER_DASHBOARD_ARCHIVED_STATES) if search_terms.present?
-  end
+    def set_claims
+      @claims = Claims::CaseWorkerClaims.new(current_user:, action: tab, criteria: criteria_params).claims
+    end
 
-  def sort_column
-    (params[:sort].presence || 'last_submitted_at')
-  end
+    # Only these 2 actions are handle in this controller. Rest of actions in the admin-namespaced controller.
+    def tab
+      %w[current archived].include?(params[:tab]) ? params[:tab] : 'current'
+    end
 
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
-  end
+    def set_claim
+      @claim = Claim::BaseClaim.active.find(params[:id])
+    end
 
-  def search_terms
-    params[:search]
-  end
+    def filter_current_claims
+      search(Claims::StateMachine::CASEWORKER_DASHBOARD_UNDER_ASSESSMENT_STATES) if search_terms.present?
+    end
 
-  def sort_and_paginate
-    # GOTCHA: must paginate in same call that sorts/orders
-    @claims = @claims.sort_using(sort_column, sort_direction).page(current_page).per(page_size) unless @claims.remote?
-  end
+    def filter_archived_claims
+      search(Claims::StateMachine::CASEWORKER_DASHBOARD_ARCHIVED_STATES) if search_terms.present?
+    end
 
-  def sort_claims
-    sort_and_paginate
-    add_claim_carousel_info
-  end
+    def sort_column
+      (params[:sort].presence || 'last_submitted_at')
+    end
 
-  def criteria_params
-    { sorting: sort_column, direction: sort_direction, page: current_page, limit: page_size, search: search_terms }
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
+    end
+
+    def search_terms
+      params[:search]
+    end
+
+    def sort_and_paginate
+      # GOTCHA: must paginate in same call that sorts/orders
+      @claims = @claims.sort_using(sort_column, sort_direction).page(current_page).per(page_size) unless @claims.remote?
+    end
+
+    def sort_claims
+      sort_and_paginate
+      add_claim_carousel_info
+    end
+
+    def criteria_params
+      { sorting: sort_column, direction: sort_direction, page: current_page, limit: page_size, search: search_terms }
+    end
   end
 end

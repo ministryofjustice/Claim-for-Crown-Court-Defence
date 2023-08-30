bbe37c9d4637584a37faa89655c52dde2ab26040
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-08-30 15:59:46 +0100

    Add service account

--- lib/tasks/rake_helpers/s3_bucket.rb
+++ lib/tasks/rake_helpers/s3_bucket.rb
@@ -2,7 +2,7 @@
 
 require 'aws-sdk-s3'
 
-Credentials = Struct.new(:access_key_id, :secret_access_key, :bucket)
+Credentials = Struct.new(:bucket)
 
 class S3Bucket
   def initialize(host)
@@ -36,10 +36,7 @@ class S3Bucket
   end
 
   def client
-    @client ||= Aws::S3::Client.new(
-      access_key_id: credentials.access_key_id,
-      secret_access_key: credentials.secret_access_key
-    )
+    @client ||= Aws::S3::Client.new
   end
 
   private
@@ -54,8 +51,6 @@ class S3Bucket
 
   def load_from_settings
     @credentials ||= Credentials.new(
-                   Settings.aws.s3.access,
-                   Settings.aws.s3.secret,
                    Settings.aws.s3.bucket
                  )
   end
@@ -63,11 +58,7 @@ class S3Bucket
   def load_from_secrets
     secrets = YAML.safe_load(`#{s3_secrets_cmd}`.chomp)
     secrets['data'].transform_values! { |v| Base64.decode64(v) unless v.nil? }
-    @credentials ||= Credentials.new(
-                       secrets['data']['access_key_id'],
-                       secrets['data']['secret_access_key'],
-                       secrets['data']['bucket_name']
-                     )
+    @credentials ||= Credentials.new(secrets['data']['bucket_name'])
   rescue StandardError => e
     raise StandardError, "error retrieving secrets. do you have access?: #{e.message}"
   end

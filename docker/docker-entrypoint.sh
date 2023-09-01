1679fcc5b7fbb345948151bf45e8e7efe123b171
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-09-01 17:55:26 +0100

    Remove change of storage.yml from docker entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -19,14 +19,6 @@ else
   printf '\e[33mINFO: Using remote redis-server specified in REDIS_URL\e[0m\n'
 fi
 
-if [ ! $SETTINGS__AWS__S3__ACCESS ]
-then
-  printf '\e[33mINFO: Removing AWS credentials from config/storage.yml\e[0m\n'
-  cat config/storage.yml | grep -v access_key_id | grep -v secret_access_key > config/new_storage.yml && mv config/new_storage.yml config/storage.yml
-else
-  printf '\e[33mINFO: Not removing AWS credentials from config/storage.yml\e[0m\n'
-fi
-
 printf '\e[33mINFO: Starting scheduler_daemon daemon\e[0m\n'
 bundle exec scheduler_daemon start
 

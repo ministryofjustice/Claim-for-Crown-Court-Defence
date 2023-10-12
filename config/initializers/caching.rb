52fd799570016684044fd7dd191965efb87e3597
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-10-12 15:36:27 +0100

    CTSKF-241 Zeitwerk: Fix Caching namespace
    Based on file structure, the Caching class belongs in the Caching
    namespace. This hasn't been a problem until now but Zeitwerk is
    particular about this.
    
    Instead of nesting the namespace, this commit moves
    `lib/caching/caching.rb` to `lib/caching.rb`.

--- config/initializers/caching.rb
+++ config/initializers/caching.rb
@@ -3,8 +3,8 @@ Dir[File.join(Rails.root, 'lib', 'caching', '*.rb')].each { |file| require file 
 # REDIS_URL env variable will be used automatically if set,
 # otherwise, default will be 'redis://127.0.0.1:6379'
 #
-Caching.backend = if Rails.env.production?
-                    Caching::RedisStore
-                  else
-                    Caching::MemoryStore
-                  end
+Caching::Caching.backend = if Rails.env.production?
+                             Caching::RedisStore
+                           else
+                             Caching::MemoryStore
+                           end

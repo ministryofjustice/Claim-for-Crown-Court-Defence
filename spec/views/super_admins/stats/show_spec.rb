0fdb31358ec5c23b14908beb23103ed40d20f347
Alex Sheppard <alexander.sheppard@digital.justice.gov.uk>
2023-10-16 16:02:41 +0100

    Basic responsive grid structure added to stats page

--- spec/views/super_admins/stats/show_spec.rb
+++ spec/views/super_admins/stats/show_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'layouts/_primary_navigation.html.haml' do
+describe 'layouts/_primary_navigation.html.haml' do
 
   before do
-    @super_admin = create(:super_admin)
+    super_admin = create(:super_admin)
     initialize_view_helpers(view)
-    sign_in(@super_admin.user, scope: :user)
+    sign_in(super_admin.user, scope: :user)
     allow(view).to receive(:current_user_persona_is?).and_return(false)
 
     render
@@ -18,21 +18,24 @@ RSpec.describe 'layouts/_primary_navigation.html.haml' do
   end
 end
 
-RSpec.describe 'super_admins/stats/show.html.haml' do
+describe 'super_admins/stats/show.html.haml' do
 
   before do
-    @super_admin = create(:super_admin)
+    super_admin = create(:super_admin)
     initialize_view_helpers(view)
-    sign_in(@super_admin.user, scope: :user)
+    sign_in(super_admin.user, scope: :user)
     allow(view).to receive(:current_user_persona_is?).and_return(false)
 
     assign(:test, 'Test variable')
     render
   end
 
-  it 'exists, and pulls a test variable from the controller' do
+  it 'includes two half-width columns' do
+    expect(rendered).to have_css('div', class: 'govuk-grid-column-one-half').twice
+  end
 
-    expect(rendered).to include('Test variable')
+  it 'includes one full-width column' do
+    expect(rendered).to have_css('div', class: 'govuk-grid-column-full').once
   end
 
 end

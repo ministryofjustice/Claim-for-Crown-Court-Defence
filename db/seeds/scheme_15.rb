956ad1d955a532ef9fe34d60c4c643fa7ab90fa8
Joe Haig <joseph.haig@digital.justice.gov.uk>
2023-10-19 10:14:45 +0100

    Add offences cleanup to dev setup
    Run the AGFS offences cleanup when the database is set up for dev.
    
    This is commented out until the cleanup has been done in the production
    environment and has run without issues for a period of time.

--- db/seeds/scheme_15.rb
+++ db/seeds/scheme_15.rb
@@ -1,4 +1,11 @@
 require Rails.root.join('db','seeds', 'schemas', 'add_agfs_fee_scheme_15')
+# require Rails.root.join('db','seeds', 'schemas', 'clean_agfs_offences')
 
 adder = Seeds::Schemas::AddAGFSFeeScheme15.new(pretend: false)
 adder.up
+# TODO: Update setup script to clean up offences after fee scheme 15 has been created
+# fixer = Seeds::Schemas::CleanAgfsOffences.new(pretend: false, quiet: true)
+# fixer.merge_12
+# fixer.merge_13
+# fixer.merge_11
+# fixer.remove_redundant

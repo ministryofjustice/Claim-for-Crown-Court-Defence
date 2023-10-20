cb4003867f02c06b91e2b6cddb8cd30cdb15e08f
Alex Sheppard <alexander.sheppard@digital.justice.gov.uk>
2023-10-20 12:05:32 +0100

    Some refactoring to the handling of time.
    Added implementation and testing for six month breakdown.

--- app/controllers/super_admins/stats_controller.rb
+++ app/controllers/super_admins/stats_controller.rb
@@ -1,45 +1,57 @@
 module SuperAdmins
   class StatsController < ApplicationController
     skip_load_and_authorize_resource only: :show
+
     def show
       @date_err = false
+      set_colours
+      process_dates
       set_times
-      @dummy_data_1 = { 'AGFS 9': 2,
-                        'AGFS 10': 10,
-                        'AGFS 11': 12,
-                        'AGFS 13': 8,
-                        'AGFS 14': 15,
-                        'AGFS 15': 22,
-                        'LGFS 9': 8,
-                        'LGFS 10': 33 }
-      @dummy_data_2 = { 'AGFS 9': 34.56,
+      retrieve_data
+      @dummy_data_1 = { 'AGFS 9': 34.56,
                         'AGFS 10': 1000.34,
                         'AGFS 11': 120.43,
+                        'AGFS 12': 342,
                         'AGFS 13': 86.78,
                         'AGFS 14': 153.29,
                         'AGFS 15': 2030.45,
                         'LGFS 9': 135.65,
                         'LGFS 10': 2582.56 }
-      @dummy_data_3 = Claim::BaseClaim.group_by_day(:last_submitted_at).count
+      @dummy_data_2 = { 'AGFS 9': 34.56,
+                        'AGFS 10': 1000.34,
+                        'AGFS 11': 120.43,
+                        'AGFS 12': 342,
+                        'AGFS 13': 86.78,
+                        'AGFS 14': 153.29,
+                        'AGFS 15': 2030.45,
+                        'LGFS 9': 135.65,
+                        'LGFS 10': 2582.56 }
+
+    end
+
+    def set_colours
+      @chart_colours = %w[#ffdd00	#00703c #5694ca #912b88 #f47738 #85994b #003078 #f499be #b52c17 #ea7361 #66ff66].freeze
     end
 
     def set_times
+      @current_month_end = Time.current.at_end_of_month
+      @six_months_ago = 5.months.ago.at_beginning_of_month
       if params['date_from(3i)'].nil?
         set_default_dates
+      elsif invalid_dates?
+        @date_err = true
+        set_default_dates
       else
-        process_dates
-        if invalid_dates?
-          @date_err = true
-          set_default_dates
-        else
-          set_provided_dates
-        end
+        set_provided_dates
       end
     end
 
     def set_default_dates
-      @from = Time.zone.today.at_beginning_of_month.to_formatted_s(:short)
-      @to = Time.zone.today.to_formatted_s(:short)
+      @from = Time.zone.now.at_beginning_of_month
+      @from_str = @from.strftime('%d %b')
+      @to = Time.zone.now
+      @to_str = @to.strftime('%d %b')
+
     end
 
     def process_dates
@@ -48,19 +60,63 @@ module SuperAdmins
     end
 
     def set_provided_dates
-      @from = Date.parse(@from_input).to_formatted_s(:short)
-      @to = Date.parse(@to_input).to_formatted_s(:short)
+      @from = ActiveSupport::TimeZone['UTC'].parse(@from_input)
+      @from_str = @from.strftime('%d %b')
+
+      @to = ActiveSupport::TimeZone['UTC'].parse(@to_input)
+      @to_str = @to.strftime('%d %b')
+
     end
 
     def invalid_dates?
       return true if @from_input > @to_input
+      return true if ActiveSupport::TimeZone['UTC'].parse(@from_input).nil?
+      return true if ActiveSupport::TimeZone['UTC'].parse(@to_input).nil?
+      false
+    end
 
-      begin
-        from_test = Date.parse(@from_input)
-        to_test = Date.parse(@to_input)
-        false
-      rescue Date::Error
-        true
+    def claims
+      @claims ||= Claim::BaseClaim.active.non_draft
+    end
+
+    def ordered_fee_schemes
+      @ordered_fee_schemes ||= FeeScheme.where(name: %w[AGFS LGFS]).order(:name, :version)
+                                        .map { |scheme| "#{scheme.name} #{scheme.version}" }
+    end
+
+    def empty_results_hash
+      output = {}
+      ordered_fee_schemes.each do |scheme|
+        # Hash default set to 0 to simplify building the results csv
+        output[scheme] = Hash.new(0)
+      end
+      output
+    end
+
+    def empty_linechart_array
+      output = []
+      ordered_fee_schemes.each do |scheme|
+        output.append({name: scheme, data: Hash.new(0)})
+      end
+      output
+    end
+
+    def retrieve_data
+      @total_claims = empty_results_hash
+      @total_values = empty_results_hash
+      @six_month_breakdown = empty_linechart_array
+
+      Claim::BaseClaim.active.non_draft.where(last_submitted_at:
+                                                @six_months_ago..@current_month_end).find_each do |claim|
+        six_month_breakdown(claim)
+      end
+    end
+
+    def six_month_breakdown(claim)
+      @six_month_breakdown.each do |hash|
+        if hash[:name] == "#{claim.fee_scheme.name} #{claim.fee_scheme.version}"
+          hash[:data][claim.last_submitted_at.strftime('%b')] += 1
+        end
       end
     end
   end
--- spec/controllers/super_admins/stats_controller_spec.rb
+++ spec/controllers/super_admins/stats_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
+def sign_in_sa
+  super_admin = create(:super_admin)
+
+  sign_in(super_admin.user)
+end
+
 RSpec.describe SuperAdmins::StatsController do
   context 'when not logged in as Super Admin' do
     it 'redirects the user' do
@@ -9,10 +15,8 @@ RSpec.describe SuperAdmins::StatsController do
   end
 
   context 'when logged in as Super Admin' do
-    let(:super_admin) { create(:super_admin) }
-
     before do
-      sign_in(super_admin.user)
+      sign_in_sa
       get :show
     end
 
@@ -23,38 +27,118 @@ RSpec.describe SuperAdmins::StatsController do
     it 'returns a 200' do
       expect(response).to have_http_status(:ok)
     end
+  end
+
+  context 'when processing dates' do
+    before { sign_in_sa }
 
     it 'provides default dates when not provided prams' do
-      expect([assigns(:from), assigns(:to)]).to eq([
-                                                     Time.zone.today.at_beginning_of_month.to_formatted_s(:short),
-                                                     Time.zone.today.to_formatted_s(:short)
-                                                   ])
+      get :show
+      expect([assigns(:from_str), assigns(:to_str)]).to eq([Time.zone.now.at_beginning_of_month.strftime('%d %b'),
+                                                            Time.zone.now.strftime('%d %b')])
     end
 
     it 'provides default dates when not provided empty params' do
       get :show, params: { 'date_from(3i)': '', 'date_from(2i)': '', 'date_from(1i)': '',
                            'date_to(3i)': '', 'date_to(2i)': '', 'date_to(1i)': '' }
-      expect([assigns(:from), assigns(:to)]).to eq([Time.zone.today.at_beginning_of_month.to_formatted_s(:short),
-                                                    Time.zone.today.to_formatted_s(:short)])
+      expect([assigns(:from_str), assigns(:to_str)]).to eq([Time.zone.now.at_beginning_of_month.strftime('%d %b'),
+                                                            Time.zone.now.strftime('%d %b')])
     end
 
     it 'processes dates when provided params' do
       get :show, params: { 'date_from(3i)': '01', 'date_from(2i)': '08', 'date_from(1i)': '2023',
                            'date_to(3i)': '31', 'date_to(2i)': '08', 'date_to(1i)': '2023' }
-      expect([assigns(:from), assigns(:to)]).to eq(['01 Aug', '31 Aug'])
+      expect([assigns(:from_str), assigns(:to_str)]).to eq(['01 Aug', '31 Aug'])
     end
 
     it 'provides default dates when given invalid params' do
       get :show, params: { 'date_from(3i)': '01', 'date_from(2i)': '02', 'date_from(1i)': '2023',
                            'date_to(3i)': '01', 'date_to(2i)': '01', 'date_to(1i)': '2023' }
-      expect([assigns(:from), assigns(:to)]).to eq([Time.zone.today.at_beginning_of_month.to_formatted_s(:short),
-                                                    Time.zone.today.to_formatted_s(:short)])
+      expect([assigns(:from_str), assigns(:to_str)]).to eq([Time.zone.now.at_beginning_of_month.strftime('%d %b'),
+                                                            Time.zone.now.strftime('%d %b')])
     end
 
     it 'displays an error when given invalid params' do
       get :show, params: { 'date_from(3i)': '01', 'date_from(2i)': '02', 'date_from(1i)': '2023',
                            'date_to(3i)': '01', 'date_to(2i)': '01', 'date_to(1i)': '2023' }
       expect(assigns(:date_err)).to be(true)
+    end
+  end
+
+  context 'when generating stats for user inputted date' do
+    before do
+      sign_in_sa
+    end
+
+    it 'has more potential colours than Fee Schemes' do
+      # This will only fail if more fee schemes are added than colours in the palette, to prevent re-use. As written,
+      # there are some extra colours added to account for changes in the future. If this fails, add more colour hex
+      # codes to @chart_colours
+      get :show
+
+      expect(assigns(:chart_colours).count).to be > assigns(:ordered_fee_schemes).count
+    end
+  end
+
+  context 'when generating stats for previous six months' do
+    let(:agfs_nine_hash) { { name: 'AGFS 9', data: { Jan: 1, Feb: 1, Mar: 1, Apr: 1, May: 1, Jun: 1 } } }
+    let(:lgfs_nine_hash) { { name: 'AGFS 9', data: { Jan: 1, Feb: 1, Mar: 1, Apr: 1, May: 1, Jun: 1 } } }
+
+    before do
+      sign_in_sa
+
+      travel_to Time.zone.local(2023, 1, 1) do
+        create(:advocate_final_claim, :submitted)
+        create(:advocate_final_claim, :submitted)
+        create(:advocate_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+      end
+      travel_to Time.zone.local(2023, 2, 1) do
+        create(:advocate_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+      end
+      travel_to Time.zone.local(2023, 3, 1) do
+        create(:advocate_final_claim, :submitted)
+        create(:advocate_final_claim, :submitted)
+        create(:advocate_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+      end
+      travel_to Time.zone.local(2023, 4, 1) do
+        create(:advocate_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+      end
+      travel_to Time.zone.local(2023, 5, 1) do
+        create(:advocate_final_claim, :submitted)
+        create(:advocate_final_claim, :submitted)
+        create(:advocate_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+      end
+      travel_to Time.zone.local(2023, 6, 1) do
+        create(:advocate_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+        create(:litigator_final_claim, :submitted)
+      end
+    end
+
+    it 'generates correct AGFS 9 data for a six month graph' do
+      travel_to Time.zone.local(2023, 6, 30) do
+        get :show
+      end
+
+      expect(assigns(:six_month_breakdown).to_s).to match(/"Jan"=>3, "Feb"=>1, "Mar"=>3, "Apr"=>1, "May"=>3, "Jun"=>1/)
+    end
+
+    it 'generates correct LGFS 9 data for a six month graph' do
+      travel_to Time.zone.local(2023, 6, 30) do
+        get :show
+      end
+
+      expect(assigns(:six_month_breakdown).to_s).to match(/"Jan"=>1, "Feb"=>3, "Mar"=>1, "Apr"=>3, "May"=>1, "Jun"=>3/)
     end
   end
 end

85338ca6418bfa1f90758535f3c73c216ef970af
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2023-10-20 09:34:57 +0100

    Fix acronym inflection in migration classes
    Previous commits changed the inflection of certain acronyms. The rails
    zeitwerk check did not pick up the fact that those acronyms are used in
    migration classes, however the fact that they are present in camel case
    form (instead of upper case) is now causing database migrations to fail.
    
    This commit updates the acronyms in those classes.

--- db/migrate/20180822130536_add_assessment_break_down_to_mi_data.rb
+++ db/migrate/20180822130536_add_assessment_break_down_to_mi_data.rb
@@ -1,4 +1,4 @@
-class AddAssessmentBreakDownToMiData < ActiveRecord::Migration[5.0]
+class AddAssessmentBreakDownToMIData < ActiveRecord::Migration[5.0]
   def change
     add_column :mi_data, :assessment_fees, :decimal
     add_column :mi_data, :assessment_expenses, :decimal

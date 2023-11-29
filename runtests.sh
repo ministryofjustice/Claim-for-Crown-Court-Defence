07985d9c9d95cab0649fefe8f22fd66d096180f2
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2023-11-29 15:05:00 +0000

    testing

--- runtests.sh
+++ runtests.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -ex
 
-printf '\e[33mbundle show\e[0m\n'
-which -a bundle
-bundle show
-bundle exec ruby -v
-bundle exec rails -v
+# printf '\e[33mbundle show\e[0m\n'
+# which -a bundle
+# bundle show
+# bundle exec ruby -v
+# bundle exec rails -v
 
-printf '\e[33mExecuting smoke test\e[0m\n'
-bundle exec rails db:migrate:status
-bundle exec rake db:migrate
-bundle exec rake db:seed
-bundle exec rake api:smoke_test
+# printf '\e[33mExecuting smoke test\e[0m\n'
+# bundle exec rails db:migrate:status
+# bundle exec rake db:migrate
+# bundle exec rake db:seed
+# bundle exec rake api:smoke_test

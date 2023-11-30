dde5b4aab06b9a617df2cebbea1ce4020757cb13
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2023-11-30 13:10:19 +0000

    testing docker entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,29 +2,29 @@
 # last modified 17-05-2019
 set +ex
 
-case ${LIVE1_DB_TASK} in
-migrate)
-    printf '\e[33mINFO: executing rake db:migrate\e[0m\n'
-    bundle exec rake db:migrate
-    ;;
-esac
+# case ${LIVE1_DB_TASK} in
+# migrate)
+#     printf '\e[33mINFO: executing rake db:migrate\e[0m\n'
+#     bundle exec rake db:migrate
+#     ;;
+# esac
 
-set -ex
+# set -ex
 
-# if REDIS_URL is not set then we start redis-server locally
-if [ -z ${REDIS_URL+x} ]; then
-  printf '\e[33mINFO: Starting redis-server daemon\e[0m\n'
-  redis-server --daemonize yes
-else
-  printf '\e[33mINFO: Using remote redis-server specified in REDIS_URL\e[0m\n'
-fi
+# # if REDIS_URL is not set then we start redis-server locally
+# if [ -z ${REDIS_URL+x} ]; then
+#   printf '\e[33mINFO: Starting redis-server daemon\e[0m\n'
+#   redis-server --daemonize yes
+# else
+#   printf '\e[33mINFO: Using remote redis-server specified in REDIS_URL\e[0m\n'
+# fi
 
-printf '\e[33mINFO: Starting scheduler_daemon daemon\e[0m\n'
-bundle exec scheduler_daemon start
+# printf '\e[33mINFO: Starting scheduler_daemon daemon\e[0m\n'
+# bundle exec scheduler_daemon start
 
-echo 'IRB.conf[:USE_AUTOCOMPLETE] = false' >> ~/.irbrc # Disable IRB autocompletion in rails console
-# NOTE: "RUBYOPT=-W:no-deprecated" removes verbose
-# warnings raised by rails using ruby 2.7
-#
-printf '\e[33mINFO: Launching puma\e[0m\n'
-RUBYOPT=-W:no-deprecated bundle exec puma
+# echo 'IRB.conf[:USE_AUTOCOMPLETE] = false' >> ~/.irbrc # Disable IRB autocompletion in rails console
+# # NOTE: "RUBYOPT=-W:no-deprecated" removes verbose
+# # warnings raised by rails using ruby 2.7
+# #
+# printf '\e[33mINFO: Launching puma\e[0m\n'
+# RUBYOPT=-W:no-deprecated bundle exec puma

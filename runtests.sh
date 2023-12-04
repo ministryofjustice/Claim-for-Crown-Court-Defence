9d4123c45984b0c083e75e644d2ef20819b108a7
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2023-12-04 15:49:30 +0000

    final

--- runtests.sh
+++ runtests.sh
@@ -3,8 +3,5 @@ set -ex
 
 printf '\e[33mExecuting smoke test\e[0m\n'
 
-# sleep 10 # waits for the postgres database to setup first
-
-# bundle exec rails db:migrate
 bundle exec rails db:seed
 bundle exec rails api:smoke_test

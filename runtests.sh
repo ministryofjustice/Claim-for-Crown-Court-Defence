c891972c4d555d17690c26088dd311ede7bd537d
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2023-12-04 12:41:42 +0000

    remove fluff

--- runtests.sh
+++ runtests.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -ex
 
-printf '\e[33mbundle show\e[0m\n'
-which -a bundle
-bundle show
-bundle exec ruby -v
-bundle exec rails -v
-
 printf '\e[33mExecuting smoke test\e[0m\n'
 bundle exec rails db:migrate
 bundle exec rails db:seed

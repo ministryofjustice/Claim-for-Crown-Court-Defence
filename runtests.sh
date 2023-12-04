4685cd4cd84d0097236bfb84065561033b1c388a
Abdirizak Obsiye <Obsiye@users.noreply.github.com>
2023-12-04 10:32:30 +0000

    comment out migration

--- runtests.sh
+++ runtests.sh
@@ -8,7 +8,7 @@ bundle exec ruby -v
 bundle exec rails -v
 
 printf '\e[33mExecuting smoke test\e[0m\n'
-bundle exec rails db:migrate:status
-bundle exec rake db:migrate
+# bundle exec rails db:migrate:status
+# bundle exec rake db:migrate
 bundle exec rake db:seed
 bundle exec rake api:smoke_test

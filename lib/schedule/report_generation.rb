562547ebe18e008d3d14bea07117315f6ffc6075
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2024-01-11 15:50:13 +0000

    Test sidekiq monitor error

--- lib/schedule/report_generation.rb
+++ lib/schedule/report_generation.rb
@@ -6,11 +6,12 @@ module Schedule
     sentry_monitor_check_ins
 
     def perform(report_type)
-      LogStuff.info { "#{report_type.to_s.humanize} generation started" }
-      Stats::StatsReportGenerator.call(report_type:)
-      LogStuff.info { "#{report_type.to_s.humanize} generation finished" }
-    rescue StandardError => e
-      LogStuff.error { "#{report_type.to_s.humanize} generation error: #{e.message}" }
+      raise StandardError, "This is a fake error"
+    #   LogStuff.info { "#{report_type.to_s.humanize} generation started" }
+    #   Stats::StatsReportGenerator.call(report_type:)
+    #   LogStuff.info { "#{report_type.to_s.humanize} generation finished" }
+    # rescue StandardError => e
+    #   LogStuff.error { "#{report_type.to_s.humanize} generation error: #{e.message}" }
     end
   end
 end

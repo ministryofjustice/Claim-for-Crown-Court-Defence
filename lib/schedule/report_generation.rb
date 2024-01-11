7d67edc3b702dac861320e6c9964c6df2b2b541e
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2024-01-11 15:12:00 +0000

    Test scheduled job is monitored

--- lib/schedule/report_generation.rb
+++ lib/schedule/report_generation.rb
@@ -6,12 +6,11 @@ module Schedule
     sentry_monitor_check_ins
 
     def perform(report_type)
-      raise(StandardError, 'This is a fake error')
-    #   LogStuff.info { "#{report_type.to_s.humanize} generation started" }
-    #   Stats::StatsReportGenerator.call(report_type:)
-    #   LogStuff.info { "#{report_type.to_s.humanize} generation finished" }
-    # rescue StandardError => e
-    #   LogStuff.error { "#{report_type.to_s.humanize} generation error: #{e.message}" }
+      LogStuff.info { "#{report_type.to_s.humanize} generation started" }
+      Stats::StatsReportGenerator.call(report_type:)
+      LogStuff.info { "#{report_type.to_s.humanize} generation finished" }
+    rescue StandardError => e
+      LogStuff.error { "#{report_type.to_s.humanize} generation error: #{e.message}" }
     end
   end
 end

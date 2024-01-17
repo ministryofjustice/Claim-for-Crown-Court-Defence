2e23cba6d02a3fcb3e7d14d886e499fade6cc9cf
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2024-01-17 09:17:39 +0000

    Modify slug to make it more specific

--- lib/schedule/poll_injection_responses.rb
+++ lib/schedule/poll_injection_responses.rb
@@ -3,7 +3,7 @@ module Schedule
     include Sidekiq::Job
     include Sentry::Cron::MonitorCheckIns
 
-    sentry_monitor_check_ins
+    sentry_monitor_check_ins slug: '<Schedule::PollInjectionResponses>'
 
     def perform
       queue = Settings.aws.response_queue

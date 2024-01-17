47c0ce2a26e96228cb8de823547dc82b4cc25732
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2024-01-17 09:17:39 +0000

    Modify slug to make it more specific

--- lib/schedule/report_generation.rb
+++ lib/schedule/report_generation.rb
@@ -3,7 +3,7 @@ module Schedule
     include Sidekiq::Job
     include Sentry::Cron::MonitorCheckIns
 
-    sentry_monitor_check_ins
+    sentry_monitor_check_ins slug: '<Schedule::ReportGeneration>'
 
 
     def perform(report_type)

a80871df5066e43a8e83337b3cda3b6ccdc6e227
Joe Haig <joseph.haig@digital.justice.gov.uk>
2024-01-25 14:24:12 +0000

    Update initializers to avoid autoloading constants
    From Ruby 6.1 the autoloading of constants in initializers is deprecated. see:
    
    * https://guides.rubyonrails.org/autoloading_and_reloading_constants.html#autoloading-when-the-application-boots
    
    To avoid this the blocks that use these constants are wrapped in `to_prepare`
    to ensure that they run when the application is reloaded.

--- config/initializers/survey_monkey.rb
+++ config/initializers/survey_monkey.rb
@@ -1,10 +1,12 @@
-SurveyMonkey.configure do |config|
-  config.root_url = 'https://api.eu.surveymonkey.com/v3/'
-  config.bearer = Rails.application.secrets.survey_monkey_bearer_token.to_s
-  config.collector_id = ENV['SURVEY_MONKEY_COLLECTOR_ID']
-  config.logger = Rails.logger
-  config.verbose_logging = true
+Rails.application.reloader.to_prepare do
+  SurveyMonkey.configure do |config|
+    config.root_url = 'https://api.eu.surveymonkey.com/v3/'
+    config.bearer = Rails.application.secrets.survey_monkey_bearer_token.to_s
+    config.collector_id = ENV['SURVEY_MONKEY_COLLECTOR_ID']
+    config.logger = Rails.logger
+    config.verbose_logging = true
 
-  page = FeedbackForm.new
-  config.register_page(page.name, page.id, **page.template)
+    page = FeedbackForm.new
+    config.register_page(page.name, page.id, **page.template)
+  end
 end

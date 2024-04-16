51fec7ff7af56f27a81c0c6114a500b32d066b16
Joe Haig <joseph.haig@digital.justice.gov.uk>
2024-04-16 14:56:53 +0100

    Remove unused anchored_label helper method

--- app/form_builders/adp_form_builder.rb
+++ app/form_builders/adp_form_builder.rb
@@ -1,11 +1,6 @@
 class AdpFormBuilder < ActionView::Helpers::FormBuilder
   include ActionView::Helpers::FormTagHelper
 
-  def anchored_label(label, anchor_name = nil, options = {})
-    anchor_name ||= label.tr(' ', '_').downcase
-    anchor_and_label_markup(anchor_name, label, options)
-  end
-
   def anchored_without_label(label, anchor_name = nil, options = {})
     anchor_name ||= label.tr(' ', '_').downcase
     anchor_and_label_markup(anchor_name, nil, options)
--- spec/form_builders/adp_form_builder_spec.rb
+++ spec/form_builders/adp_form_builder_spec.rb
@@ -4,34 +4,6 @@ RSpec.describe AdpFormBuilder do
   let(:resource)  { create(:claim) }
   let(:builder)   { AdpFormBuilder.new(:claim, resource, self, {}) }
 
-  describe 'anchored_label' do
-    context 'no anchor name supplied' do
-      it 'takes the label as the anchor name' do
-        expected_html = '<a id="advocate_category"></a><label for="claim_advocate_category">Advocate category</label>'
-        expect(builder.anchored_label('Advocate category')).to eq expected_html
-      end
-    end
-
-    context 'anchor name supplied' do
-      it 'uses anchor name supplied' do
-        expected_html = '<a id="ad_cat"></a><label for="claim_ad_cat">Advocate category</label>'
-        expect(builder.anchored_label('Advocate category', 'ad_cat')).to eq expected_html
-      end
-    end
-
-    context 'extra html attributes supplied' do
-      it 'uses the attributes when anchor name supplied' do
-        expected_html = '<a id="ad_cat" class="red"></a><label class="blue" for="claim_ad_cat">Advocate category</label>'
-        expect(builder.anchored_label('Advocate category', 'ad_cat', { anchor_attributes: { class: 'red' }, label_attributes: { class: 'blue' } })).to eq expected_html
-      end
-
-      it 'uses the attributes when no anchor name provided' do
-        expected_html = '<a id="advocate_category" class="red"></a><label class="blue" for="claim_advocate_category">Advocate category</label>'
-        expect(builder.anchored_label('Advocate category', nil, { anchor_attributes: { class: 'red' }, label_attributes: { class: 'blue' } })).to eq expected_html
-      end
-    end
-  end
-
   describe 'anchored_without_label' do
     context 'no anchor name supplied' do
       it 'takes the label as the anchor name' do

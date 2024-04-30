1e45924360c4c8e7ddc905bb84c10276291364ce
Joe Haig <joseph.haig@digital.justice.gov.uk>
2024-04-30 16:13:34 +0100

    Catch CSP alerts

---
+++ app/controllers/csp_reports_controller.rb
@@ -0,0 +1,34 @@
+class CspReportsController < ApplicationController
+  skip_load_and_authorize_resource only: %i[create]
+  skip_forgery_protection
+
+  def create
+    slack_notifier.build_payload(
+      icon: ':security:',
+      title: 'Content Security Policy violation',
+      message: report.map { |key, value| "#{key}: #{value}" }.join("\n"),
+      status: :fail
+    )
+    slack_notifier.send_message
+
+    head :ok
+  end
+
+  private
+
+  def slack_notifier
+    @slack_notifier ||= SlackNotifier.new(
+      'laa-cccd-alerts',
+      formatter: SlackNotifier::Formatter::Generic.new,
+      slack_bot_name: 'CSP'
+    )
+  end
+
+  def report
+    @report ||= JSON.parse(body)['csp-report']
+  rescue JSON::ParserError
+    @report ||= { error: 'Unable to parse data' }
+  end
+
+  def body = @body ||= request.body.read
+end

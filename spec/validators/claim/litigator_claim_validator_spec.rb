426162da8de79465200c9e89babba9e71583b92b
Alex Sheppard <alexander.sheppard@digital.justice.gov.uk>
2024-08-02 15:27:51 +0100

    Step validation unit tests
    ** Expanded step validation specs to include the new attribute

--- spec/validators/claim/litigator_claim_validator_spec.rb
+++ spec/validators/claim/litigator_claim_validator_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe Claim::LitigatorClaimValidator, type: :validator do
       case_type_id
       court_id
       case_number
+      london_rates_apply
       case_transferred_from_another_court
       transfer_court_id
       transfer_case_number

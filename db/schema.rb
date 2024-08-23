d1e50aae159f1a23a4b856a4da649820b7ef375f
Joe Haig <joseph.haig@digital.justice.gov.uk>
2024-08-23 18:19:18 +0100

    Upgrade to Rails 7.1
    `config.load_defaults` is set to 7.0 and upgrading tasks have not been done yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_19_173039) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_23_171419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_19_173039) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum"
     t.datetime "created_at", null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true

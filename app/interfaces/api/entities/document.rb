dcb588c176915e6d1a8b96342175478cf5ed4dc4
VinceChiuMOJ <vince.chiu@digital.justice.gov.uk>
2024-09-24 02:55:56 +0100

    remove redundant code for new action
    remove redundant code for new action

--- app/interfaces/api/entities/document.rb
+++ app/interfaces/api/entities/document.rb
@@ -2,26 +2,11 @@ module API
   module Entities
     class Document < BaseEntity
       expose :uuid, if: ->(instance, _opts) { instance.respond_to?(:uuid) }
-      expose :url
-      expose :file_name
-      expose :size
 
       private
 
       def attachment
         object.is_a?(ActiveStorage::Attachment) ? object : object.attachment
-      end
-
-      def url
-        attachment.blob.url(disposition: 'attachment') if attachment.attached?
-      end
-
-      def file_name
-        attachment.filename if attachment.attached?
-      end
-
-      def size
-        attachment.byte_size if attachment.attached?
       end
     end
   end

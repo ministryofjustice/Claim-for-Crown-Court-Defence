23ffa473dbb5af29db3282ec5c7dea8d94065bfc
VinceChiuMOJ <vince.chiu@digital.justice.gov.uk>
2024-12-17 12:37:20 +0000

    Added error handling for multi-file-upload.
    Added error handling for multi-file-upload.

--- app/webpack/javascripts/modules/Modules.MultiFileUpload.js
+++ app/webpack/javascripts/modules/Modules.MultiFileUpload.js
@@ -1,14 +1,14 @@
 moj.Modules.MultiFileUpload = {
   init: function () {
     let container = document.querySelector('.moj-multi-file-upload');
+    let fields = container.querySelector('.moj-multi-file__uploaded-fields');
 
     new MOJFrontend.MultiFileUpload({
       container: container,
       uploadUrl: '/documents/upload',
       deleteUrl: '/documents/delete',
       uploadFileExitHook: function (uploader, file, response) {
-        let fields = container.querySelector('.moj-multi-file__uploaded-fields');
-        console.log('fields');
+        console.log('Success fields');
         console.log(fields);
         let input = document.createElement('input');
         input.type = 'hidden';
@@ -16,8 +16,24 @@ moj.Modules.MultiFileUpload = {
         input.value = response.file.filename;
         fields.appendChild(input);
       },
+      uploadFileErrorHook: function (uploader, file, jqXHR, textStatus, errorThrown) {
+        console.log('Error fields');
+        console.log(fields);
+        let input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = 'message[document_ids][]';
+        input.value =  errorThrown;
+        fields.appendChild(input);
+        
+        let errorContainer = document.querySelector('.govuk-error-summary');
+        let errors = errorContainer.querySelector('.govuk-list.govuk-error-summary__list');
+        errorContainer.style.display = '';
+        let error = document.createElement('span');
+        error.style = 'color:red;font-weight:bold';
+        error.innerHTML = file.name + ' is ' + errorThrown + '.<br/>';
+        errors.appendChild(error);
+      },
       fileDeleteHook: function (uploader, response) {
-        let fields = container.querySelector('.moj-multi-file__uploaded-fields');
         let input = fields.querySelector('input[value="' + response.file.filename + '"]');
         console.log('remove input');
         console.log(input);

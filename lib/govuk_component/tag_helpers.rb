2abe240ac6ce5349629e94c758efeea948152969
Katy McCann <katherine.mccann@digital.justice.gov.uk>
2025-02-11 14:13:08 +0000

    Fix failing test by including SharedHelpers in TagHelpersclass

--- lib/govuk_component/tag_helpers.rb
+++ lib/govuk_component/tag_helpers.rb
@@ -2,6 +2,7 @@
 
 module GovukComponent
   module TagHelpers
+    include SharedHelpers
     def govuk_tag(body = nil, color = nil, **tag_options)
       tag_options = prepend_classes('govuk-tag--' + color, tag_options) if color.present?
       tag_options = prepend_classes('govuk-tag', tag_options)

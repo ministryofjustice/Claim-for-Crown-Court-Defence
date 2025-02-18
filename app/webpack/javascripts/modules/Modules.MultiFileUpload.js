a5faca0631b2addaea3413ed811eb4d221ee9099
Joe Haig <joseph.haig@digital.justice.gov.uk>
2025-02-18 15:33:31 +0000

    Add error message based on status
    If the request failes before reaching the server the http status is used to
    display the error.

--- app/webpack/javascripts/modules/Modules.MultiFileUpload.js
+++ app/webpack/javascripts/modules/Modules.MultiFileUpload.js
@@ -17,19 +17,29 @@ moj.Modules.MultiFileUpload = {
         input.value = response.file.filename
         fields.appendChild(input)
       },
-      uploadFileErrorHook: function (_uploader, file, _jqXHR, _textStatus, errorThrown) {
+      uploadFileErrorHook: function (_uploader, file, jqXHR, _textStatus, errorThrown) {
         const input = document.createElement('input')
         input.type = 'hidden'
         input.name = 'message[document_ids][]'
         input.value = errorThrown
         fields.appendChild(input)
 
+        const httpStatus = {
+          408: 'Request timeout',
+          413: 'File is too large',
+          500: 'Internal Server Error',
+          502: 'Bad Gateway',
+          503: 'Service Unavailable',
+          504: 'Gateway Timeout',
+          505: 'HTTP Version Not Supported'
+        }
+
         const errorContainer = document.querySelector('.govuk-error-summary')
         const errors = errorContainer.querySelector('.govuk-list.govuk-error-summary__list')
         errorContainer.style.display = ''
         const error = document.createElement('span')
         error.style = 'color:#d4351c;font-weight:bold'
-        error.innerHTML = file.name + ' is ' + errorThrown + '.<br/>'
+        error.innerHTML = file.name + ': ' + httpStatus[jqXHR.status] + '.<br/>'
         errors.appendChild(error)
       },
       fileDeleteHook: function (_uploader, response) {

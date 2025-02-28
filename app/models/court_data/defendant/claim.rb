721504b88a7f863ee9ef32a5ef0f454802489a87
Joe Haig <joseph.haig@digital.justice.gov.uk>
2025-02-28 16:39:19 +0000

    Update MAAT reference matching
    The MAAT reference from Court Data was being taken from the
    'representation_order' field of the defendant. This is not always present and
    it should instead come from the 'laa_reference' option of the offence
    summaries. There may now be multiple references and these are all matched
    against all the references that the provider lists, not just the earliest.

--- app/models/court_data/defendant/claim.rb
+++ app/models/court_data/defendant/claim.rb
@@ -2,6 +2,7 @@ class CourtData
   class Defendant
     class Claim < Base
       def maat_reference = @defendant.earliest_representation_order.maat_reference
+      def maat_references = @defendant.representation_orders.map(&:maat_reference)
     end
   end
 end
--- app/models/court_data/defendant/hmcts.rb
+++ app/models/court_data/defendant/hmcts.rb
@@ -4,9 +4,16 @@ class CourtData
       delegate :id, to: :@defendant
 
       def maat_reference = @defendant.representation_order&.reference || 'No representation order recorded'
+      def maat_references = @defendant.representation_orders.map(&:reference)
       def start = @defendant.representation_order&.start
       def end = @defendant.representation_order&.end
       def contract_number = @defendant.representation_order&.contract_number
+
+      def maat_reference_list
+        return 'No representation orders recorded' if maat_references.empty?
+
+        maat_references.join(', ')
+      end
     end
   end
 end

91c3ec14bb6f63f7f08b4957158f83f856dfa561
Joe Haig <joseph.haig@digital.justice.gov.uk>
2025-03-28 11:21:35 +0000

    Target Ruby 3.4 with Rubocop
    Also make a few Rubocop fixes:
    
    * `publicise_errors` in `spec/factories/claim/base_claims.rb` refactored
      to resolve Metrics/AbsSize and Metrics/MethodLength.
    * Style/PercentLiteralDelimiters and Style/RescueStandardError fixed in
      various places.
    * Todo list updated

--- spec/helpers/feedback_helper_spec.rb
+++ spec/helpers/feedback_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe FeedbackHelper do
   describe '#referrer_is_claim?' do
-    %w(claims /claims claims/ /claims/).each do |path|
+    %w[claims /claims claims/ /claims/].each do |path|
       it "is truthy for path containing `#{path}`" do
         expect(helper.referrer_is_claim?(path)).to be_truthy
       end

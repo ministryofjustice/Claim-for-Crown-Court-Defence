a9b6d825b3aac013b20dfd8cb31866a5c7285fd0
VinceChiuMOJ <vince.chiu@digital.justice.gov.uk>
2025-04-03 11:46:47 +0100

    Configure Action Text to use an HTML5 standards-compliant sanitizer when it is supported on your platform.

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -261,7 +261,7 @@ end
 #
 # In previous versions of Rails, Action Text always used `Rails::HTML4::Sanitizer` as its vendor.
 #++
-# Rails.application.config.action_text.sanitizer_vendor = Rails::HTML::Sanitizer.best_supported_vendor
+Rails.application.config.action_text.sanitizer_vendor = Rails::HTML::Sanitizer.best_supported_vendor
 
 ###
 # Configure the log level used by the DebugExceptions middleware when logging

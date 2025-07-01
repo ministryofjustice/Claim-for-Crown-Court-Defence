b30a0fce13d3af8dd5553c88013364920024836a
VinceChiuMOJ <vince.chiu@digital.justice.gov.uk>
2025-07-01 09:15:12 +0100

    Rewrote the test scripts to take json file directly
    The original approach is too infeasible and not scalable.
    No more excel formula needed. Data exported from database directly to json file.
    The updated sql query is for CCCD database.

--- createTestCases/test_cases_query.sql
+++ createTestCases/test_cases_query.sql
@@ -1,34 +1,31 @@
-SELECT * 
-  FROM (SELECT  c.ID AS CASE_ID,
-  				fs.DESCRIPTION AS FEE_SCHEME,
-                c.COUR_COURT_CODE,
-                bs.SCENARIO AS BILL_SCENARIO,
-                oc.UNIQUE_CODE AS OFFENCE_UNIQUE_CODE,
-                c.CASE_NO,
-                b.VAT_INCLUDED,
-                c.TRIAL_DATE_START,
-                c.EST_TRIAL_LENGTH,
-                c.ACT_TRIAL_LENGTH,
-                b.PSTY_PERSON_TYPE,
-                c.CLIENT_FORENAME,
-                c.CLIENT_SURNAME,
-                c.CLIENT_DOB,
-                c.REP_ORD_DATE,
-                cr.MAAT_REFERENCE,
-                b.QUANTITY,
-                b.RATE,
-                b.BASIC_CASE_FEE,
-                bt.BILL_TYPE,
-                bst.BILL_SUB_TYPE
-  FROM BILLS b, CASES c, BILL_TYPES bt, BILL_SUB_TYPES bst, BILL_SCENARIOS bs, FEE_SCHEMES fs, CASES_REPORDERS cr, OFFENCE_CODES oc, OFFENCE_TYPES ot
-  WHERE b.CASE_ID = c.ID
-          AND b.BITY_BILL_TYPE = bt.BILL_TYPE
-          AND b.BIST_BILL_SUB_TYPE = bst.BILL_SUB_TYPE
-          AND b.BISC_BILL_SCENARIO_ID = bs.ID
-          AND b.FSTH_FEE_STRUCTURE_ID = fs.FESH_FIRST_ID
-          AND c.id = cr.CASE_ID
-          AND c.OFCD_ID = oc.id
-          AND c.OFTY_OFFENCE_TYPE = ot.OFFENCE_TYPE
-          AND b.CIS_EXTERNAL_REF IS NOT NULL
-  ORDER BY c.DATE_CREATED DESC)
-WHERE ROWNUM <= 10;
+select
+	c.id as case_id,
+	f.id as fee_id,
+	d.id as defendant_id,
+	c.court_id,
+	c.case_type_id,
+	c.offence_id,
+	c.case_number,
+	c.apply_vat, 
+	c.first_day_of_trial, 
+	c.trial_concluded_at,
+	c.estimated_trial_length, 
+	c.actual_trial_length,
+	c.advocate_category, 
+	d.first_name,
+	d.last_name,
+	d.date_of_birth,
+	ro.maat_reference,
+	ro.representation_order_date,
+	f.quantity,
+	f.rate,
+	f.amount,
+	f.fee_type_id
+from claims c, fees f, defendants d, representation_orders ro
+where 
+	c.state = 'authorised' and
+	f.claim_id  = c.id and
+	d.claim_id  = c.id and
+	ro.defendant_id = d.id
+order by c.created_at desc 
+limit 5;
--- createTestCases/test_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-LOG_FILE="create_test_cases_$(date +%Y%m%d%H%M).log"
-
-./create_test_cases.sh "advocate@example.com" "34" "12" "POFBPPCOC_12.2" "8010101012" "true" "2025-06-09" "1" "1" "Leading junior" "FIRST" "SMITH" "1954-06-19" "2025-06-09" "2161851" "1" "0" "BABAF" >> "$LOG_FILE" 2>&1
-
-echo "Log file created: $LOG_FILE"

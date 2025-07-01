6a919baa894c09818207f966b7d5284a143a60fe
VinceChiuMOJ <vince.chiu@digital.justice.gov.uk>
2025-07-01 09:15:12 +0100

    Rewrote the test scripts to take json file directly
    The original approach is too infeasible and not scalable.
    No more excel formula needed. Data exported from database directly to json file.
    The updated sql query is for CCCD database.

--- createTestCases/create_test_cases.sh
+++ createTestCases/create_test_cases.sh
@@ -1,119 +1,304 @@
-#!/bin/bash -e
-
+#!/bin/bash
+LOG_FILE="${0}_$(date +%Y%m%d%H%M).log"
+exec > "$LOG_FILE" 2>&1
 urlencode() {
   local string="$1"
-  local encoded_string=$(echo "$string" | tr -d '\n' | jq -sRr @uri)
-  echo "$encoded_string"
+  if [ -z "$string" ] || [ "$string" = "null" ]; then
+    echo ""
+  else
+    local encoded_string=$(echo "$string" | tr -d '\n' | jq -sRr @uri)
+    echo "$encoded_string"
+  fi
 }
 
-# Example usage:
-# ./create_test_cases.sh "advocate@example.com" "1" "12" "ABOCUT_C" "AGFS16S28" "true" "2023-10-16" "1" "1" "Junior" "Joe" "Bloggs" "2000-01-01" "2023-10-16" "6015685" "1" "1000" "MISTE"
-# ./create_test_cases.sh "advocate@example.com" "34" "12" "POFBPPCOC_12.2" "8010101012" "true" "2025-06-09" "1" "1" "Leading junior" "Joe" "Bloggs" "2000-01-01" "2025-06-09" "6015685" "1" "1000" "BABAF"
+# Check if a file was provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <json_file>"
+  exit 1
+fi
 
-API_KEY="10e76205-5fd3-49d9-895d-b9de5f415a6a" # PLEASE ADD YOUR API KEY
-API_DOMAIN="http://localhost:3001"
+# Check if the file exists and is a file
+if [ ! -f "$1" ]; then
+  echo "Error: $1 is not a file"
+  exit 1
+fi
 
-# Create AGFS 16 Claim PARAMS
-USER_EMAIL=$(urlencode "${1}") # advocate@example.com
-echo -e "\n==== USER_EMAIL: $USER_EMAIL"
-COURT_ID=$(urlencode "${2}") # c.COUR_COURT_CODE
-echo -e "\n==== COURT_ID: $COURT_ID"
-CASE_TYPE_ID=$(urlencode "${3}") # bs.SCENARIO AS BILL_SCENARIO
-echo -e "\n==== CASE_TYPE_ID: $CASE_TYPE_ID"
-OFFENCE_UNIQUE_CODE=$(urlencode "${4}") # oc.UNIQUE_CODE
-echo -e "\n==== OFFENCE_UNIQUE_CODE: $OFFENCE_UNIQUE_CODE"
-CASE_NUMBER=$(urlencode "${5}") # c.CASE_NO
-echo -e "\n==== CASE_NUMBER: $CASE_NUMBER"
-APPLY_VAT=$(urlencode "${6}") # b.VAT_INCLUDED
-echo -e "\n==== APPLY_VAT: $APPLY_VAT"
-DATE=$(urlencode "${7}") # c.TRIAL_DATE_START
-echo -e "\n==== DATE: $DATE"
-E_LENGTH=$(urlencode "${8}") # c.EST_TRIAL_LENGTH
-echo -e "\n==== E_LENGTH: $E_LENGTH"
-A_LENGTH=$(urlencode "${9}") # c.ACT_TRAIL_LENGTH
-echo -e "\n==== A_LENGTH: $A_LENGTH"
-ADVOCATE_TYPE=$(urlencode "${10}") # PSTY_PERSON_TYPE
-echo -e "\n==== ADVOCATE_TYPE: $ADVOCATE_TYPE"
+echo "!=== ${0} started..."
 
-# Create Defendant PARAMS
-FIRST=$(urlencode "${11}") # c.CLIENT_FORENAME
-echo -e "\n==== FIRST: $FIRST"
-LAST=$(urlencode "${12}") # c.CLIENT_SURNAME
-echo -e "\n==== LAST: $LAST"
-DOB=$(urlencode "${13}") # c.CLIENT_DOB
-echo -e "\n==== DOB: $DOB"
+API_KEY="10e76205-5fd3-49d9-895d-b9de5f415a6a" # ADD YOUR API KEY
+API_DOMAIN="http://localhost:3001" # ADD YOUR API DOMAIN
 
-# Create Rep Order PARAMS
-REP_ORDER_DATE=$(urlencode "${14}") # REP_ORD_DATE
-echo -e "\n==== REP_ORDER_DATE: $REP_ORDER_DATE"
-MAAT_REF=$(urlencode "${15}") # cr.MAAT_REFERENCE
-echo -e "\n==== MAAT_REF: $MAAT_REF"
+# Use jq to parse the JSON data
+DATA_FILE="$1"
+LAST_CASE_ID="00000"
+LAST_FEE_ID="00000"
+LAST_DEFENDANT_ID="00000"
+LAST_CLAIM_ID="00000"
+IS_ALL_FEE_ADDED="FALSE"
+IS_ALL_DEFNANT_ADDED="FALSE"
 
-# Create Fee PARAMS
-QUANTITY=$(urlencode "${16}") # b.QUANTITY
-echo -e "\n==== QUANTITY: $QUANTITY"
-RATE=$(urlencode "${17}") # b.RATE
-echo -e "\n==== RATE: $RATE"
-FEE_UNIQUE_CODE=$(urlencode "${18}") # bt.BILL_TYPE, bst.BILL_SUB_TYPE pair
-echo -e "\n==== FEE_UNIQUE_CODE: $FEE_UNIQUE_CODE"
+# Count the number of items in the second level
+ITEM_COUNT=$(jq '.[] | length' "$DATA_FILE" | head -n 1)
+echo -e "\n==== Number of items: $ITEM_COUNT"
 
-# PARAMS GET FROM API
-OFFENCE_ID="" # 1093, 1711
-CLAIM_ID=""
-DEFENDANT_ID=""
+for ((i=0; i<ITEM_COUNT; i++)); do
+    echo -e "\n==== Start of item: $i"
 
-# FLOW START
-echo -e "create_test_cases.sh started!"
-declare -a RESPONSE
+    CLAIM_FLAG="TRUE"
+    FEE_FLAG="TRUE"
+    DEFENDANT_FLAG="TRUE"
 
-# GET Offence
-echo -e "\n>>> Getting Offence..."
-RESPONSE=$(curl -s --location --globoff "$API_DOMAIN/api/offences?rep_order_date=$REP_ORDER_DATE&unique_code=$OFFENCE_UNIQUE_CODE&api_key=$API_KEY")
-echo -e "\nRESPONSE:$RESPONSE"
-echo -e "\n>>> Offence found successfully."
+    # Extract the values from the JSON file
+    case_id=$(jq -r '.[] | .['$i'].case_id' "$DATA_FILE")
+    fee_id=$(jq -r '.[] | .['$i'].fee_id' "$DATA_FILE")
+    defendant_id=$(jq -r '.[] | .['$i'].defendant_id' "$DATA_FILE")
+    court_id=$(jq -r '.[] | .['$i'].court_id' "$DATA_FILE")
+    case_type_id=$(jq -r '.[] | .['$i'].case_type_id' "$DATA_FILE")
+    offence_id=$(jq -r '.[] | .['$i'].offence_id' "$DATA_FILE")
+    case_number=$(jq -r '.[] | .['$i'].case_number' "$DATA_FILE")
+    apply_vat=$(jq -r '.[] | .['$i'].apply_vat' "$DATA_FILE")
+    first_day_of_trial=$(jq -r '.[] | .['$i'].first_day_of_trial' "$DATA_FILE")
+    trial_concluded_at=$(jq -r '.[] | .['$i'].trial_concluded_at' "$DATA_FILE")
+    estimated_trial_length=$(jq -r '.[] | .['$i'].estimated_trial_length' "$DATA_FILE")
+    actual_trial_length=$(jq -r '.[] | .['$i'].actual_trial_length' "$DATA_FILE")
+    advocate_category=$(jq -r '.[] | .['$i'].advocate_category' "$DATA_FILE")
+    trial_cracked_at_third=$(jq -r '.[] | .['$i'].trial_cracked_at_third' "$DATA_FILE")
+    trial_cracked_at=$(jq -r '.[] | .['$i'].trial_cracked_at' "$DATA_FILE")
+    trial_fixed_notice_at=$(jq -r '.[] | .['$i'].trial_fixed_notice_at' "$DATA_FILE")
+    trial_fixed_at=$(jq -r '.[] | .['$i'].trial_fixed_at' "$DATA_FILE")
+    first_name=$(jq -r '.[] | .['$i'].first_name' "$DATA_FILE")
+    last_name=$(jq -r '.[] | .['$i'].last_name' "$DATA_FILE")
+    date_of_birth=$(jq -r '.[] | .['$i'].date_of_birth' "$DATA_FILE")
+    maat_reference=$(jq -r '.[] | .['$i'].maat_reference' "$DATA_FILE")
+    representation_order_date=$(jq -r '.[] | .['$i'].representation_order_date' "$DATA_FILE")
+    quantity=$(jq -r '.[] | .['$i'].quantity' "$DATA_FILE")
+    rate=$(jq -r '.[] | .['$i'].rate' "$DATA_FILE")
+    amount=$(jq -r '.[] | .['$i'].amount' "$DATA_FILE")
+    fee_type_id=$(jq -r '.[] | .['$i'].fee_type_id' "$DATA_FILE")
 
-# Parse OFFENCE_ID from response
-OFFENCE_ID=$(jq -r '.[0].id' <<< "$RESPONSE")
-echo -e "\n==== OFFENCE_ID: $OFFENCE_ID"
-unset RESPONSE
+    # Create Claim PARAMS
+    USER_EMAIL="advocate@example.com"
+    echo -e "=> USER_EMAIL: $USER_EMAIL"
+    CASE_ID=$(urlencode "$case_id")
+    echo -e "=> CASE_ID: $CASE_ID"
+    FEE_ID=$(urlencode "$fee_id")
+    echo -e "=> FEE_ID: $FEE_ID"
+    DEFENDANT_ID=$(urlencode "$defendant_id")
+    echo -e "=> DEFENDANT_ID: $DEFENDANT_ID"
+    COURT_ID=$(urlencode "$court_id")
+    echo -e "=> COURT_ID: $COURT_ID"
+    CASE_TYPE_ID=$(urlencode "$case_type_id")
+    echo -e "=> CASE_TYPE_ID: $CASE_TYPE_ID"
+    OFFENCE_ID=$(urlencode "$offence_id")
+    echo -e "=> OFFENCE_ID: $OFFENCE_ID"
+    CASE_NUMBER=$(urlencode "$case_number")
+    echo -e "=> CASE_NUMBER: $CASE_NUMBER"
+    APPLY_VAT=$(urlencode "$apply_vat")
+    echo -e "=> APPLY_VAT: $APPLY_VAT"
+    FIRST_TRIAL_DATE=$(urlencode "$first_day_of_trial")
+    echo -e "=> FIRST_TRIAL_DATE: $FIRST_TRIAL_DATE"
+    TRIAL_CONCLUDED_DATE=$(urlencode "$trial_concluded_at")
+    echo -e "=> TRIAL_CONCLUDED_DATE: $TRIAL_CONCLUDED_DATE"
+    E_LENGTH=$(urlencode "$estimated_trial_length")
+    echo -e "=> E_LENGTH: $E_LENGTH"
+    A_LENGTH=$(urlencode "$actual_trial_length")
+    echo -e "=> A_LENGTH: $A_LENGTH"
+    ADVOCATE_TYPE=$(urlencode "$advocate_category")
+    echo -e "=> ADVOCATE_TYPE: $ADVOCATE_TYPE"
+    TRIAL_CRACKED_AT_THIRD=$(urlencode "$trial_cracked_at_third")
+    echo -e "=> TRIAL_CRACKED_AT_THIRD: $TRIAL_CRACKED_AT_THIRD"
+    TRIAL_CRACKED_AT=$(urlencode "$trial_cracked_at")
+    echo -e "=> TRIAL_CRACKED_AT: $TRIAL_CRACKED_AT"
+    TRIAL_FIXED_NOTICE_AT=$(urlencode "$trial_fixed_notice_at")
+    echo -e "=> TRIAL_FIXED_NOTICE_AT: $TRIAL_FIXED_NOTICE_AT"
+    TRIAL_FIXED_AT=$(urlencode "$trial_fixed_at")
+    echo -e "=> TRIAL_FIXED_AT: $TRIAL_FIXED_AT"
 
-# Create AGFS 16 Claim
-echo -e "\n>>> Creating AGFS 16 Claim..."
-RESPONSE=$(curl --location --globoff --request POST "$API_DOMAIN/api/external_users/claims/advocates/final?creator_email=$USER_EMAIL&court_id=$COURT_ID&case_type_id=$CASE_TYPE_ID&offence_id=$OFFENCE_ID&case_number=$CASE_NUMBER&apply_vat=$APPLY_VAT&first_day_of_trial=$DATE&estimated_length_of_trial=$E_LENGTH&trial_concluded_at=$DATE&actual_trial_length=$A_LENGTH&user_email=$USER_EMAIL&advocate_category=$ADVOCATE_TYPE&api_key=$API_KEY")
+    # Create Defendant PARAMS
+    FIRST=$(urlencode "$first_name")
+    echo -e "=> FIRST: $FIRST"
+    LAST=$(urlencode "$last_name")
+    echo -e "=> LAST: $LAST"
+    DOB=$(urlencode "$date_of_birth")
+    echo -e "=> DOB: $DOB"
 
-echo -e "\nRESPONSE:$RESPONSE"
-echo -e "\n>>> AGFS 16 Claim created successfully."
+    # Create Rep Order PARAMS
+    REP_ORDER_DATE=$(urlencode "$representation_order_date")
+    echo -e "=> REP_ORDER_DATE: $REP_ORDER_DATE"
+    MAAT_REF=$(urlencode "$maat_reference")
+    echo -e "=> MAAT_REF: $MAAT_REF"
 
-# Parse CLAIM_ID from response
-CLAIM_ID=$(jq -r '.id' <<< "$RESPONSE")
-echo -e "\n==== CLAIM_ID: $CLAIM_ID"
-unset RESPONSE
+    # Create Fee PARAMS
+    QUANTITY=$(urlencode "$quantity")
+    echo -e "=> QUANTITY: $QUANTITY"
+    RATE=$(urlencode "$rate")
+    echo -e "=> RATE: $RATE"
+    AMOUNT=$(urlencode "$amount")
+    echo -e "=> AMOUNT: $AMOUNT"
+    FEE_TYPE_ID=$(urlencode "$fee_type_id")
+    echo -e "=> FEE_TYPE_ID: $FEE_TYPE_ID"
 
-# Create Defendant
-echo -e "\n>>> Creating Defendant..."
-RESPONSE=$(curl -s --location --globoff --request POST "$API_DOMAIN/api/external_users/defendants?claim_id=$CLAIM_ID&first_name=$FIRST&last_name=$LAST&date_of_birth=$DOB&api_key=$API_KEY")
-echo -e "\nRESPONSE:$RESPONSE"
-echo -e "\n>>> Defendant created successfully."
+    # PARAMS GET FROM API
+    CLAIM_ID=""
+    DEFENDANT_ID=""
 
-# Parse DEFENDANT_ID from response
-DEFENDANT_ID=$(jq -r '.id' <<< "$RESPONSE")
-echo -e "\n==== DEFENDANT_ID: $DEFENDANT_ID"
-unset RESPONSE
+    # FLOW START
+    declare -a RESPONSE
 
-# Create Rep Order
-echo -e "\n>>> Creating Representation Order..."
-RESPONSE=$(curl -s --location --globoff --request POST "$API_DOMAIN/api/external_users/representation_orders?defendant_id=$DEFENDANT_ID&maat_reference=$MAAT_REF&representation_order_date=$REP_ORDER_DATE&api_key=$API_KEY")
-echo -e "\nRESPONSE:$RESPONSE"
-echo -e "\n>>> Representation Order created successfully."
+    # FLAGS SET
+    if [ "$CASE_ID" == "$LAST_CASE_ID" ]; then
+        CLAIM_ID=$LAST_CLAIM_ID
+        echo -e "Same Case ID as last item"
+        CLAIM_FLAG="FALSE"
+    else
+        IS_ALL_FEE_ADDED="FALSE"
+        IS_ALL_DEFNANT_ADDED="FALSE"
+    fi 
+    
+    if [ "$CASE_ID" == "$LAST_CASE_ID" ] && [ "$FEE_ID" == "$LAST_FEE_ID" ]; then
+        echo -e "Same Fee ID as last item"
+        FEE_FLAG="FALSE"
+        if [ "$IS_ALL_DEFNANT_ADDED" == "TRUE" ]; then
+            DEFENDANT_FLAG="FALSE"    
+        fi
+    elif [ "$CASE_ID" == "$LAST_CASE_ID" ] && [ "$FEE_ID" != "$LAST_FEE_ID" ]; then
+        echo -e "All Defendant ID are being added"
+        DEFENDANT_FLAG="FALSE"
+        IS_ALL_DEFNANT_ADDED="TRUE"
+    fi
 
-unset RESPONSE
+    # Create Claim
+    if [ "$CLAIM_FLAG" == "TRUE" ]; then
+        echo -e "\n>>> Creating Claim..."
 
-# Create Fee
-echo -e "\n>>> Creating Fee..."
-RESPONSE=$(curl -s --location --globoff --request POST "$API_DOMAIN/api/external_users/fees/?claim_id=$CLAIM_ID&fee_type_unique_code=$FEE_UNIQUE_CODE&amount="5000"&api_key=$API_KEY")
-echo -e "\nRESPONSE:$RESPONSE"
-echo -e "\n>>> Fee created successfully."
+        QUERY_PARAMS="creator_email=$USER_EMAIL&court_id=$COURT_ID&case_type_id=$CASE_TYPE_ID&offence_id=$OFFENCE_ID&case_number=$CASE_NUMBER&apply_vat=$APPLY_VAT&estimated_length_of_trial=$E_LENGTH&actual_trial_length=$A_LENGTH&user_email=$USER_EMAIL&advocate_category=$ADVOCATE_TYPE&api_key=$API_KEY"
 
-unset RESPONSE
+        if [ -n "$FIRST_TRIAL_DATE" ]; then
+            echo -e "First trial date: $FIRST_TRIAL_DATE"
+            QUERY_PARAMS+="&first_day_of_trial=$FIRST_TRIAL_DATE"
+        fi
 
-echo -e "\ncreate_test_cases.sh finished!"
+        if [ -n "$TRIAL_CONCLUDED_DATE" ]; then
+            echo -e "Trial concluded date: $TRIAL_CONCLUDED_DATE"
+            QUERY_PARAMS+="&trial_concluded_at=$TRIAL_CONCLUDED_DATE"
+        fi
+
+        if [ -n "$TRIAL_CRACKED_AT_THIRD" ]; then
+            echo -e "Trial cracked at third: $TRIAL_CRACKED_AT_THIRD"
+            QUERY_PARAMS+="&trial_cracked_at_third=$TRIAL_CRACKED_AT_THIRD"
+        fi
+
+        if [ -n "$TRIAL_CRACKED_AT" ]; then
+            echo -e "Trial cracked at: $TRIAL_CRACKED_AT"
+            QUERY_PARAMS+="&trial_cracked_at=$TRIAL_CRACKED_AT"
+        fi
+
+        if [ -n "$TRIAL_FIXED_NOTICE_AT" ]; then
+            echo -e "Trial fixed notice at: $TRIAL_FIXED_NOTICE_AT"
+            QUERY_PARAMS+="&trial_fixed_notice_at=$TRIAL_FIXED_NOTICE_AT"
+        fi
+
+        if [ -n "$TRIAL_FIXED_AT" ]; then
+            echo -e "Trial fixed at: $TRIAL_FIXED_AT"
+            QUERY_PARAMS+="&trial_fixed_at=$TRIAL_FIXED_AT"
+        fi
+
+        RESPONSE=$(curl --location --globoff --request POST "$API_DOMAIN/api/external_users/claims/advocates/final?$QUERY_PARAMS")
+
+        if jq -e 'type == "array" and .[0].error' <<< "$RESPONSE" > /dev/null; then
+            ERROR_MSG=$(jq -r '.[0].error' <<< "$RESPONSE")
+            echo -e "\nRESPONSE:$RESPONSE"
+            echo -e "\n>>> Error creating claim: $ERROR_MSG"
+        else
+            echo -e "\nRESPONSE:$RESPONSE"
+            echo -e "\n>>> Claim created successfully."
+
+            # Parse CLAIM_ID from response
+            CLAIM_ID=$(jq -r '.id' <<< "$RESPONSE")
+            echo -e "=> CLAIM_ID: $CLAIM_ID"
+        fi
+
+        unset RESPONSE
+    fi
+
+    # Create Defendant
+    if [ "$DEFENDANT_FLAG" == "TRUE" ]; then
+        echo -e "\n>>> Creating Defendant..."
+        RESPONSE=$(curl -s --location --globoff --request POST "$API_DOMAIN/api/external_users/defendants?claim_id=$CLAIM_ID&first_name=$FIRST&last_name=$LAST&date_of_birth=$DOB&api_key=$API_KEY")
+
+        echo -e "\nRESPONSE:$RESPONSE"
+
+        if jq -e 'type == "array" and .[0].error' <<< "$RESPONSE" > /dev/null; then
+            ERROR_MSG=$(jq -r '.[0].error' <<< "$RESPONSE")
+            echo -e "\n>>> Error creating claim: $ERROR_MSG"
+        else
+            echo -e "\n>>> Defendant created successfully."
+
+            # Parse DEFENDANT_ID from response
+            DEFENDANT_ID=$(jq -r '.id' <<< "$RESPONSE")
+            echo -e "=> DEFENDANT_ID: $DEFENDANT_ID"
+        fi
+
+        unset RESPONSE
+
+        # Create Rep Order
+        echo -e "\n>>> Creating Representation Order..."
+        RESPONSE=$(curl -s --location --globoff --request POST "$API_DOMAIN/api/external_users/representation_orders?defendant_id=$DEFENDANT_ID&maat_reference=$MAAT_REF&representation_order_date=$REP_ORDER_DATE&api_key=$API_KEY")
+        
+        echo -e "\nRESPONSE:$RESPONSE"
+
+        if jq -e 'type == "array" and .[0].error' <<< "$RESPONSE" > /dev/null; then
+            ERROR_MSG=$(jq -r '.[0].error' <<< "$RESPONSE")            
+            echo -e "\n>>> Error creating claim: $ERROR_MSG"
+        else
+            echo -e "\n>>> Representation Order created successfully."
+        fi
+
+        unset RESPONSE
+    fi
+
+    # Create Fee
+    if [ "$FEE_FLAG" == "TRUE" ]; then
+    echo -e "\n>>> Creating Fee..."
+    
+    QUERY_PARAMS="claim_id=$CLAIM_ID&fee_type_id=$FEE_TYPE_ID&api_key=$API_KEY"
+
+    if [ -n "$QUANTITY" ]; then
+        echo -e "Quantity: $QUANTITY"
+        QUERY_PARAMS+="&quantity=$QUANTITY" 
+    fi
+
+    if [ -n "$RATE" ]; then
+        echo -e "Rate: $RATE"
+        QUERY_PARAMS+="&rate=$RATE" 
+    fi
+
+    if [ -n "$AMOUNT" ]; then
+        echo -e "Amount: $AMOUNT"
+        QUERY_PARAMS+="&amount=$AMOUNT" 
+    fi
+
+    RESPONSE=$(curl -s --location --globoff --request POST "$API_DOMAIN/api/external_users/fees/?$QUERY_PARAMS")
+
+    echo -e "\nRESPONSE:$RESPONSE"
+
+    if jq -e 'type == "array" and .[0].error' <<< "$RESPONSE" > /dev/null; then
+        ERROR_MSG=$(jq -r '.[0].error' <<< "$RESPONSE")        
+        echo -e "\n>>> Error creating claim: $ERROR_MSG"
+    else
+        echo -e "\n>>> Fee created successfully."
+    fi
+
+    unset RESPONSE
+    fi
+
+    # PARAM SET
+    LAST_CLAIM_ID=$CLAIM_ID
+    LAST_CASE_ID=$case_id
+    LAST_FEE_ID=$fee_id
+    LAST_DEFENDANT_ID=$defendant_id
+
+    echo -e "\n==== End of item: $i"
+done
+
+echo -e "\n!=== ${0} ended..."

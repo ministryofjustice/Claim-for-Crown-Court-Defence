950b6bc3dfeb2a20ba1309448bca2158810bfaa8
Katherine Mccann <katherine.mccann@digital.justice.gov.uk>
2025-10-17 10:56:13 +0100

    Enable validation of migration timestamps.

--- config/initializers/new_framework_defaults_7_2.rb
+++ config/initializers/new_framework_defaults_7_2.rb
@@ -51,7 +51,7 @@
 # Applications with existing timestamped migrations that do not adhere to the
 # expected format can disable validation by setting this config to `false`.
 #++
-# Rails.application.config.active_record.validate_migration_timestamps = true
+Rails.application.config.active_record.validate_migration_timestamps = true
 
 ###
 # Controls whether the PostgresqlAdapter should decode dates automatically with manual queries.

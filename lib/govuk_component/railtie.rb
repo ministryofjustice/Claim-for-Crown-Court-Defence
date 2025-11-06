fc3f889fde930f0fc6bda6c0f382d95fa155596f
Katherine Mccann <katherine.mccann@digital.justice.gov.uk>
2025-11-06 15:31:50 +0000

    Remove customised summary_card and summary_list helper methods and associated railties Remove helper tests

--- lib/govuk_component/railtie.rb
+++ lib/govuk_component/railtie.rb
@@ -22,12 +22,6 @@ module GovukComponent
       end
     end
 
-    initializer 'govuk_component.summary_list_helpers' do
-      config.after_initialize do
-        ActiveSupport.on_load(:action_view) { include GovukComponent::SummaryListHelpers }
-      end
-    end
-
     initializer 'govuk_component.tag_helpers' do
       config.after_initialize do
         ActiveSupport.on_load(:action_view) { include GovukComponent::TagHelpers }
@@ -37,12 +31,6 @@ module GovukComponent
     initializer 'govuk_component.warning_text_helpers' do
       config.after_initialize do
         ActiveSupport.on_load(:action_view) { include GovukComponent::WarningTextHelpers }
-      end
-    end
-
-    initializer 'govuk_component.summary_card_helpers' do
-      config.after_initialize do
-        ActiveSupport.on_load(:action_view) { include GovukComponent::SummaryCardHelpers }
       end
     end
   end

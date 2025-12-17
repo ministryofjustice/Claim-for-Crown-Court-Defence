d1f36e49798da9caa5590caae257fe2107cad7b9
Joe Haig <joseph.haig@digital.justice.gov.uk>
2025-12-17 16:13:57 +0000

    Add rake task to create LGFS Fee Scheme 11
    LGFS fee scheme 10 is closed off the day before scheme 11 goes live and
    LGFS fee scheme 11 is added. Tasks are added to create the new fee
    scheme (db:lgfs_scheme_eleven:up) and to roll it back
    (db:lgfs_scheme_eleven:down).

--- db/seeds/schemas/add_lgfs_fee_scheme_11.rb
+++ db/seeds/schemas/add_lgfs_fee_scheme_11.rb
@@ -21,6 +21,18 @@ module Seeds
         STATUS
       end
 
+      def up
+        create_lgfs_scheme_eleven
+        # set_lgfs_scheme_eleven_offences
+        # create_lgfs_scheme_eleven_fee_types
+      end 
+
+      def down
+        # unset_lgfs_scheme_eleven_offences
+        # remove_lgfs_scheme_eleven_fee_type_roles
+        destroy_lgfs_scheme_eleven
+      end
+
       private
 
       def lgfs_fee_scheme_11
@@ -37,6 +49,43 @@ module Seeds
 
       def scheme_11_fee_type_count
         Fee::BaseFeeType.lgfs_scheme_11s.count
+      end
+
+      def create_lgfs_scheme_eleven
+        print "Finding LGFS scheme 10".yellow
+        lgfs_fee_scheme_ten = FeeScheme.find_by(name: 'LGFS', version: 10, start_date: Settings.lgfs_scheme_10_clair_release_date.beginning_of_day)
+        lgfs_fee_scheme_ten ? print("...found\n".green) : print("...not found\n".red)
+
+        print "Updating LGFS scheme 10 end date to #{Settings.lgfs_scheme_11_csfr.end_of_day-1.day}".yellow
+        if pretending?
+          print "...not updated\n".green if pretending?
+        else
+          if lgfs_fee_scheme_ten.update(end_date: Settings.lgfs_scheme_11_csfr.end_of_day - 1.day)
+            print "...updated\n".green
+          else
+            print "...failed to update: #{lgfs_fee_scheme_ten.errors.full_messages.join(', ')}\n".red
+          end
+        end
+
+        print "Finding or creating LGFS fee scheme 11 with start date #{Settings.lgfs_scheme_11_csfr.beginning_of_day}...".yellow
+        if pretending?
+          print "...not created\n".green if pretending?
+        else
+          FeeScheme.find_or_create_by(name: 'LGFS', version: 11, start_date: Settings.lgfs_scheme_11_csfr.beginning_of_day)
+          print "...created\n".green
+        end
+      end
+
+      def destroy_lgfs_scheme_eleven
+        if pretending?
+          puts "Would delete fee scheme 11: #{lgfs_fee_scheme_11&.attributes || 'does not exist'}".yellow
+          puts "Would update #{lgfs_fee_scheme_10.attributes} end date to nil".yellow
+        else
+          puts 'Deleted LGFS fee scheme 11'.green if lgfs_fee_scheme_11&.destroy
+          print "Updating LGFS scheme 10 end date to nil".yellow
+          lgfs_fee_scheme_10.update(end_date: nil)
+          print "...updated\n".green
+        end
       end
     end
   end
--- lib/tasks/lgfs_scheme_eleven.rake
+++ lib/tasks/lgfs_scheme_eleven.rake
@@ -7,5 +7,41 @@ namespace :db do
       adder = Seeds::Schemas::AddLGFSFeeScheme11.new(pretend: false)
       puts adder.status
     end
+
+    desc 'Create the db structures and records required for LGFS Fee Scheme 11 (CSFR - February 2026), pass \'true\' to run'
+    task :seed, [:not_pretend] => :environment do |_task, args|
+
+      # seed['true'] should seed, otherwise pretend
+      args.with_defaults(not_pretend: 'false')
+      not_pretend = !args.not_pretend.to_s.downcase.eql?('false')
+      pretend = !not_pretend
+
+      continue?('This will seed data for LGFS Fee Scheme 11 (CSFR - February 2026). Are you sure?') if not_pretend
+      puts "#{pretend ? 'pretending' : 'working'}...".yellow
+
+      log_level = ActiveRecord::Base.logger.level
+      ActiveRecord::Base.logger.level = 1
+      adder = Seeds::Schemas::AddLGFSFeeScheme11.new(pretend: pretend)
+      adder.up
+      ActiveRecord::Base.logger.level = log_level
+    end
+
+    desc 'Destroy the db structures and records required for LGFS Fee Scheme 11 (CSFR - February 2026), pass \'true\' to run'
+    task :rollback, [:not_pretend] => :environment do |_task, args|
+
+      # rollback['true'] should rollback, otherwise pretend
+      args.with_defaults(not_pretend: 'false')
+      not_pretend = !args.not_pretend.to_s.downcase.eql?('false')
+      pretend = !not_pretend
+
+      continue?('This will rollback data for LGFS Fee Scheme 11 (CSFR - February 2026). Are you sure?') if not_pretend
+      puts "#{pretend ? 'pretending' : 'working'}...".yellow
+
+      log_level = ActiveRecord::Base.logger.level
+      ActiveRecord::Base.logger.level = 1
+      adder = Seeds::Schemas::AddLGFSFeeScheme11.new(pretend: pretend)
+      adder.down
+      ActiveRecord::Base.logger.level = log_level
+    end
   end
 end

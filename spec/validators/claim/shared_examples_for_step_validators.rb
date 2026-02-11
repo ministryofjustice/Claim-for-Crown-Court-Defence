d9b57377406f3bf352d1b281e5ab0e2782d94641
Joe Haig <joseph.haig@digital.justice.gov.uk>
2026-02-11 16:29:33 +0000

    Refactor allow_any_instance_of in shared examples
    (WIP) Changes recommended by Copilot so it will require review.

--- spec/validators/claim/shared_examples_for_step_validators.rb
+++ spec/validators/claim/shared_examples_for_step_validators.rb
@@ -2,6 +2,10 @@
 # The current way only asserts that the provided fields are validated,
 # not that the other step fields aren't, so could lead to false positives.
 RSpec.shared_examples 'common partial validations' do |steps|
+  let(:validator) do
+    claim.class.validators.find { |v| v.is_a?(described_class) }
+  end
+
   context 'partial validation' do
     context 'from web' do
       before do
@@ -9,7 +13,7 @@ RSpec.shared_examples 'common partial validations' do |steps|
         steps.each_value do |fields|
           fields.each do |field|
             (fields - [field]).each do |other_field|
-              allow_any_instance_of(described_class).to receive(:validate_field).with(other_field)
+              allow(validator).to receive(:validate_field).with(other_field)
             end
           end
         end
@@ -19,7 +23,7 @@ RSpec.shared_examples 'common partial validations' do |steps|
         fields.each do |field|
           it "validates #{field} just for the #{step_name} step" do
             claim.form_step = step_name
-            expect_any_instance_of(described_class).to receive(:validate_field).with(field)
+            expect(validator).to receive(:validate_field).with(field)
             claim.valid?
           end
         end
@@ -33,7 +37,7 @@ RSpec.shared_examples 'common partial validations' do |steps|
 
       it 'validates all the attributes for all the steps' do
         steps.values.flatten.each do |attrib|
-          expect_any_instance_of(described_class).to receive(:validate_field).with(attrib)
+          expect(validator).to receive(:validate_field).with(attrib)
         end
 
         claim.valid?
@@ -46,6 +50,10 @@ end
 # The current way only asserts that the provided associations are validated,
 # not that the other step fields aren't, so could lead to false positives.
 RSpec.shared_examples 'common partial association validations' do |steps|
+  let(:validator) do
+    claim.class.validators.find { |v| v.is_a?(described_class) }
+  end
+
   context 'partial validation' do
     before do
       claim.force_validation = true
@@ -57,7 +65,7 @@ RSpec.shared_examples 'common partial association validations' do |steps|
         steps[:has_one].each_value do |associations|
           associations.each do |association_data|
             (associations - [association_data]).each do |other_association|
-              allow_any_instance_of(described_class).to receive(:validate_association_for).with(claim, other_association[:name])
+              allow(validator).to receive(:validate_association_for).with(claim, other_association[:name])
             end
           end
         end
@@ -68,10 +76,10 @@ RSpec.shared_examples 'common partial association validations' do |steps|
           association_name = association_data[:name]
           it "validates has_one association #{association_name} just for the #{step_name} step" do
             claim.form_step = step_name
-            expect_any_instance_of(described_class).to receive(:validate_association_for).with(claim, association_name)
+            expect(validator).to receive(:validate_association_for).with(claim, association_name)
 
             if association_data[:options] && association_data[:options][:presence]
-              expect_any_instance_of(described_class).to receive(:validate_presence).with(association_name, 'blank')
+              expect(validator).to receive(:validate_presence).with(association_name, 'blank')
             end
 
             claim.valid?
@@ -85,12 +93,12 @@ RSpec.shared_examples 'common partial association validations' do |steps|
           association_misc_fees = :misc_fees
           it "validates has_many association #{association_name} just for the #{step_name} step" do
             claim.form_step = step_name
-            expect_any_instance_of(described_class).to receive(:validate_collection_for).with(claim, association_name)
+            expect(validator).to receive(:validate_collection_for).with(claim, association_name)
             if association_data[:options] && association_data[:options][:presence]
               if association_name == association_misc_fees
-                expect_any_instance_of(described_class).to receive(:validate_presence).with(association_name, :blank)
+                expect(validator).to receive(:validate_presence).with(association_name, :blank)
               else
-                expect_any_instance_of(described_class).to receive(:validate_presence).with(association_name, 'blank')
+                expect(validator).to receive(:validate_presence).with(association_name, 'blank')
               end
             end
 
@@ -107,7 +115,7 @@ RSpec.shared_examples 'common partial association validations' do |steps|
 
       it 'validates all the has_one associations for all the steps' do
         steps[:has_one].values.flatten.each do |association_data|
-          expect_any_instance_of(described_class).to receive(:validate_association_for).with(claim, association_data[:name])
+          expect(validator).to receive(:validate_association_for).with(claim, association_data[:name])
         end
 
         claim.valid?
@@ -115,7 +123,7 @@ RSpec.shared_examples 'common partial association validations' do |steps|
 
       it 'validates all the has_many associations for all the steps' do
         steps[:has_many].values.flatten.each do |association_data|
-          expect_any_instance_of(described_class).to receive(:validate_collection_for).with(claim, association_data[:name])
+          expect(validator).to receive(:validate_collection_for).with(claim, association_data[:name])
         end
 
         claim.valid?

8f37062fdd50f49ca4c84370d2d7d0319c4299a3
Joe Haig <joseph.haig@digital.justice.gov.uk>
2026-02-13 16:08:55 +0000

    Remove Extensions::HashExtension#all_values_for
    The `all_values_for` method finds all values in a nested hash for a
    given key. In the only place it is used, in `transfer_brain_helpers.rb`,
    it specifically finds all values for the key `:bill_scenario` that all
    exist at the same level of nesting so it can be simplified.

--- lib/extensions/hash_extension.rb
+++ lib/extensions/hash_extension.rb
@@ -1,13 +1,5 @@
 module Extensions
   module HashExtension
-    def all_values_for(key)
-      each_with_object([]) do |(k, v), values|
-        values << v if k.eql?(key)
-        values.concat(v.select { |el| el.is_a?(Hash) }.flat_map { |el| el.all_values_for(key) }) if v.is_a? Array
-        values.concat(v.all_values_for(key)) if v.is_a? Hash
-      end
-    end
-
     def key_paths(path = [])
       each_with_object([]) do |(k, v), items|
         if v.is_a?(Hash)

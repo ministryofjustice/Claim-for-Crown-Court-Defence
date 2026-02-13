01938a6319125b6cd1e51ae2d9cdf127a200ffd4
Joe Haig <joseph.haig@digital.justice.gov.uk>
2026-02-13 15:16:45 +0000

    Some Rubocop fixes

--- spec/api/v2/mi/agfs_scheme_ten_claims_spec.rb
+++ spec/api/v2/mi/agfs_scheme_ten_claims_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe API::V2::MI::AGFSSchemeTenClaims do
   include ApiSpecHelper
   include ActiveSupport::Testing::TimeHelpers
 
-  let(:case_worker_admin) { create(:case_worker, :admin) }
-  let(:external_user) { create(:external_user) }
+  subject(:do_request) { get '/api/mi/scheme_10_claims', params.merge!(transitory_params), format: :json }
+
   let(:params) { { api_key: } }
   let(:transitory_params) { {} }
   let(:expected_json_keys) do
@@ -34,10 +34,8 @@ RSpec.describe API::V2::MI::AGFSSchemeTenClaims do
     # Included claims
     travel_to(DateTime.parse('1-APR-2018 11:00')) { create(:submitted_claim, :agfs_scheme_10) }
     travel_to(DateTime.parse('5-APR-2018 10:30')) { create(:submitted_claim, :agfs_scheme_10) }
-  end
 
-  def do_request
-    get '/api/mi/scheme_10_claims', params.merge!(transitory_params), format: :json
+    travel_to Date.parse('21-APR-2018')
   end
 
   RSpec.shared_examples 'MI JSON return' do
@@ -59,66 +57,60 @@ RSpec.describe API::V2::MI::AGFSSchemeTenClaims do
     end
   end
 
-  describe 'GET s10_claims' do
-    before { travel_to Date.parse('21-APR-2018') }
+  context 'when accessed by a CaseWorker' do
+    let(:api_key) { create(:case_worker, :admin).user.api_key }
 
-    context 'when accessed by a CaseWorker' do
-      let(:api_key) { case_worker_admin.user.api_key }
+    before { do_request }
 
-      before { do_request }
+    context 'with no format' do
+      it_behaves_like 'MI JSON return'
+    end
 
-      context 'with no format' do
-        it_behaves_like 'MI JSON return'
-      end
+    context 'with JSON format' do
+      let(:transitory_params) { { format: 'json' } }
 
-      context 'with JSON format' do
-        let(:transitory_params) { { format: 'json' } }
+      it_behaves_like 'MI JSON return'
 
-        it_behaves_like 'MI JSON return'
+      context 'when passed a date with no data' do
+        let(:transitory_params) { { format: 'json', date: '2018-04-04' } }
 
-        context 'when passed a date with no data' do
-          let(:transitory_params) { { format: 'json', date: '2018-04-04' } }
-
-          it 'returns an empty array' do
-            expect(JSON.parse(last_response.body).count).to eq 0
-          end
-        end
-      end
-
-      context 'with csv format' do
-        let(:transitory_params) { { format: 'csv' } }
-
-        it 'returns success' do
-          expect(last_response).to be_ok
-        end
-
-        it 'returns CSV' do
-          expect(last_response.headers['content-type']).to eq 'text/csv; utf-8'
-        end
-
-        it 'returns 2 rows of data and a header row' do
-          expect(CSV.parse(last_response.body).count).to eq 3
-        end
-
-        context 'when passed a date with no data' do
-          let(:transitory_params) { { format: 'csv', date: '2018-04-04' } }
-
-          it 'returns an empty array' do
-            expect(CSV.parse(last_response.body)).to eq [['No scheme ten claims filed on'], ['2018-04-04']]
-          end
+        it 'returns an empty array' do
+          expect(JSON.parse(last_response.body).count).to eq 0
         end
       end
     end
 
-    context 'when accessed by a ExternalUser' do
-      before { do_request }
+    context 'with csv format' do
+      let(:transitory_params) { { format: 'csv' } }
 
-      let(:api_key) { external_user.user.api_key }
+      it 'returns success' do
+        expect(last_response).to be_ok
+      end
 
-      it 'returns unauthorised' do
-        expect(last_response).to be_unauthorized
-        expect(last_response.body).to include('Unauthorised')
+      it 'returns CSV' do
+        expect(last_response.headers['content-type']).to eq 'text/csv; utf-8'
+      end
+
+      it 'returns 2 rows of data and a header row' do
+        expect(CSV.parse(last_response.body).count).to eq 3
+      end
+
+      context 'when passed a date with no data' do
+        let(:transitory_params) { { format: 'csv', date: '2018-04-04' } }
+
+        it 'returns an empty array' do
+          expect(CSV.parse(last_response.body)).to eq [['No scheme ten claims filed on'], ['2018-04-04']]
+        end
       end
     end
+  end
+
+  context 'when accessed by a ExternalUser' do
+    before { do_request }
+
+    let(:api_key) { create(:external_user).user.api_key }
+
+    it { expect(last_response).to be_unauthorized }
+    it { expect(last_response.body).to include('Unauthorised') }
   end
 end

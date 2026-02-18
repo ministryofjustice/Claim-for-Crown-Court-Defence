a50bf6fcf8666c8cdfd5f7a505b4fcb72e58a3a3
Joe Haig <joseph.haig@digital.justice.gov.uk>
2026-02-18 18:12:22 +0000

    (WIP) Modify navigation of claims for case workers

--- app/helpers/case_workers/claims_helper.rb
+++ app/helpers/case_workers/claims_helper.rb
@@ -23,20 +23,47 @@ module CaseWorkers::ClaimsHelper
     "#{claim_ids.index(@claim.id) + 1} of #{claim_count}"
   end
 
+  def not_first_claim?
+    (claim_ids.index(@claim.id) + 1) != 1
+  end
+
+  def not_last_claim?
+    (claim_ids.index(@claim.id) + 1) != claim_count.to_i
+  end
+
   def last_claim?
     (claim_ids.index(@claim.id) + 1) == claim_count.to_i
   end
 
-  def next_claim_link(text, options = {})
-    govuk_link_to text, case_workers_claim_path(claim_ids[claim_ids.index(@claim.id) + 1]), options
-  end
+  def next_claim_link
+    case_workers_claim_path(claim_ids[claim_ids.index(@claim.id) + 1], sort: sort_column, direction: sort_direction)
+  end  
+
+  def previous_claim_link
+    case_workers_claim_path(claim_ids[claim_ids.index(@claim.id) - 1], sort: sort_column, direction: sort_direction)
+  end  
 
   def claim_ids
-    session[:claim_ids]
+    @claim_ids ||= current_user.claims.allocated.order(sql_sort_column => sql_sort_direction).pluck(:id)
   end
 
   def claim_count
-    session[:claim_count]
+    @claim_count ||= claim_ids.count
+  end
+
+  def sql_sort_column
+    {
+      # 'type' => '???',
+      'case_number' => 'case_number',
+      # 'advocate' => '???',
+      # 'total_inc_vat' => '???',
+      # 'case_type' => '???',
+      'last_submitted_at' => 'last_submitted_at'
+    }[sort_column] || 'last_submitted_at'
+  end
+
+  def sql_sort_direction
+    %w[asc desc].include?(sort_direction) ? sort_direction : 'desc'
   end
 
   def format_miscellaneous_fee_names(claim)

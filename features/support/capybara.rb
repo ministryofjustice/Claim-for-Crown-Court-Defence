5c15d3d5c5a89122777cadee0fe3abe40e866ef9
Joe Haig <joseph.haig@digital.justice.gov.uk>
2026-03-10 12:27:34 +0000

    Increase default wait time

--- features/support/capybara.rb
+++ features/support/capybara.rb
@@ -27,7 +27,7 @@ end
 Capybara.javascript_driver = :headless_chrome
 
 Capybara.configure do |config|
-  config.default_max_wait_time = 10 # seconds
+  config.default_max_wait_time = 15 # seconds
 end
 
 if ENV['BROWSER'] == 'chrome'

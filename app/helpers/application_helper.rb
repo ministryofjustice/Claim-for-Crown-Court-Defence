1b7e3fba68062c8283abb6de159d13264f603ce8
Katherine Mccann <katherine.mccann@digital.justice.gov.uk>
2026-03-17 15:51:41 +0000

    Delete old style sheet components that loaded dynamically and replace with a helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -179,4 +179,24 @@ module ApplicationHelper
     tab = tab_present(path)
     request.GET[:tab] == tab
   end
+
+  def govuk_tag_active_user?(user)
+    user.active? && user.enabled? ? govuk_tag(text: 'Active', colour: "green") : govuk_tag(text: 'Inactive', colour: "red")
+  end
+
+  def state_colour
+    {
+      'authorised' => 'green',
+      'accepted' => 'green',
+      'allocated' => 'purple',
+      'draft' => 'blue',
+      'rejected' => 'red',
+      'refused' => 'red',
+      'unverified' => 'red',
+      'archived-pending-delete'=> 'dark-grey',
+      'redermination' => "black",
+      'part-authorised' => "blue",
+      'submitted' => "grey",
+    }
+  end
 end

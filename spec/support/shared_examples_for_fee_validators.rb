5a12162245418db0fba7ffed45fb66490c86add1
Roni Colling <veronica.colling@justice.gov.uk>
2026-04-17 11:04:50 +0100

    fix(error handling): remove syntax error

--- spec/support/shared_examples_for_fee_validators.rb
+++ spec/support/shared_examples_for_fee_validators.rb
@@ -19,6 +19,7 @@ RSpec.shared_examples 'common LGFS fee date validations' do
       expect(fee.errors[:date]).to include(match(/(.*?)(Fixed|Graduated) fee date cannot be too far in the future/))
     end
 
+    # as we've removed the 2 year limit, introduced a test to ensure that the error is same
     it 'error if > 2 years before the first repo order date should be same as other dates before earliest reporder' do
       allow(claim).to receive(:earliest_representation_order_date).and_return(Time.zone.today)
       allow(fee).to receive(:claim).and_return(claim)

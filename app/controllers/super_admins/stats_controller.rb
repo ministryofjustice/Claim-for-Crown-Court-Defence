9ea93883eae6571f39d0f9685ba6a39f7faebaaa
Joe Haig <joseph.haig@digital.justice.gov.uk>
2026-04-27 14:02:02 +0100

    Add scope to date fields
    A multi-part date field has field names `model[field(1i)]`, etc. When
    there is no model name, when the form is created with a url, then the
    field names are just `[field(1i)]`, etc. The square brackets causes
    problems with the parsing of the parameters in Rack 3 and the cleanest
    solution is to add a scope, which will allow the params to be parsed
    correctly.

--- app/controllers/super_admins/stats_controller.rb
+++ app/controllers/super_admins/stats_controller.rb
@@ -27,12 +27,24 @@ module SuperAdmins
     end
 
     def parse_time(date)
-      Time.zone.parse("#{params["#{date}(3i)"]}-#{params["#{date}(2i)"]}-#{params["#{date}(1i)"]}")
+      return if stats_params[:stats].nil?
+
+      Date.new(
+        stats_params[:stats]["#{date}(1i)"].to_i,
+        stats_params[:stats]["#{date}(2i)"].to_i,
+        stats_params[:stats]["#{date}(3i)"].to_i
+      )
+    rescue Date::Error
+      nil
     end
 
     def set_times
       @from = parse_time('date_from') || Time.current.at_beginning_of_month
       @to = parse_time('date_to')
+    end
+
+    def stats_params
+      params.permit(stats: %i[date_from date_to])
     end
   end
 end

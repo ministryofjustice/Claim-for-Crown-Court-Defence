3c2b010c17ff5ee9156882f14965fd693f829d29
VinceChiuMOJ <vince.chiu@digital.justice.gov.uk>
2026-05-19 23:46:44 +0100

    Add OmniAuth mock Entra ID login flow

---
+++ lib/omniauth/strategies/entra_mock.rb
@@ -0,0 +1,67 @@
+require 'json'
+require 'securerandom'
+
+module OmniAuth
+  module Strategies
+    class EntraMock
+      include OmniAuth::Strategy
+
+      option :name, 'entra_mock'
+
+      def request_phase
+        redirect callback_path
+      end
+
+      def callback_phase
+        return fail!('entra_mock_http_error') unless mock_http_success?
+
+        data = mock_data
+        env['omniauth.auth'] = OmniAuth::AuthHash.new(
+          provider: name,
+          uid: data['uid'] || data['oid'] || SecureRandom.uuid,
+          info: {
+            email: data['email'],
+            first_name: data['first_name'],
+            last_name: data['last_name']
+          },
+          extra: {
+            raw_info: data
+          }
+        )
+        call_app!
+      rescue StandardError => e
+        fail!('entra_mock_error', e)
+      end
+
+      private
+
+      def mock_data
+        json = ENV['ENTRA_MOCK_JSON']
+        return default_data if json.nil? || json.strip.empty?
+
+        JSON.parse(json)
+      rescue JSON::ParserError
+        default_data
+      end
+
+      def default_data
+        {
+          'uid' => 'mock-entra-uid',
+          'email' => 'caseworker@example.com',
+          'first_name' => 'Mock',
+          'last_name' => 'Caseworker',
+          'persona' => 'CaseWorker',
+          'roles' => ['case_worker'],
+          'location' => 'Mock Location'
+        }
+      end
+
+      def mock_http_success?
+        status = ENV['ENTRA_MOCK_HTTP_STATUS']
+        return true if status.nil? || status.strip.empty?
+
+        status.to_i == 200
+      end
+    end
+  end
+end

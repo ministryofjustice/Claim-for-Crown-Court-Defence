96fc17c2dd80af53cd37161e2ac1d132dcd3cc17
Roni Colling <veronica.colling@justice.gov.uk>
2026-06-09 08:47:51 +0100

    fix(test): add waits into upload test

--- features/page_objects/claim_form_page.rb
+++ features/page_objects/claim_form_page.rb
@@ -144,6 +144,7 @@ class ClaimFormPage < BasePage
     end
   end
 
+
   def check_evidence_checklist(count = 1)
     evidence_checklist.items_with_labels[0...count].each { |item| item.label.click }
   end

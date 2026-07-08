a62567459e4f96abeaea6607aabe382f1d0e9963
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2026-07-08 17:09:03 +0100

    WIP: allow additional prep fee on discontinuances

--- spec/services/claims/fetch_eligible_misc_fee_types_spec.rb
+++ spec/services/claims/fetch_eligible_misc_fee_types_spec.rb
@@ -381,7 +381,22 @@ RSpec.describe Claims::FetchEligibleMiscFeeTypes, type: :service do
             it { is_expected.not_to include(*supplementary_only_types) }
             it { is_expected.not_to include(*unused_materials_types) }
             it { is_expected.not_to include(*section_twenty_eight_types) }
-            it { is_expected.not_to include(*additional_prep_fee_types) }
+
+            context 'when prosecution evidence is present' do
+              before { claim.update!(prosecution_evidence: true) }
+
+              it { is_expected.to include(*additional_prep_fee_types) }
+            end
+
+            context 'when prosecution evidence is not present' do
+              before { claim.update!(prosecution_evidence: false) }
+
+              it { is_expected.not_to include(*additional_prep_fee_types) }
+            end
+
+            context 'when prosecution evidence is nil' do
+              it { is_expected.not_to include(*additional_prep_fee_types) }
+            end
           end
         end
       end

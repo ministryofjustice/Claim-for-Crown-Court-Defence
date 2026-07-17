a83fe67c764cc277d17d00e88590b5c54a4735d7
Katherine Mccann <katherine.mccann@digital.justice.gov.uk>
2026-07-17 08:56:02 +0100

    refactor: Use start_with? for has_one association check Updates the error_is_for_govuk_has_one_association? method to use start_with? instead of include?. This makes the check more specific by ensuring that the error attribute begins with the association name, which is a more accurate way to identify errors related to has_one associations. This also resolves a RuboCop offense.

--- app/presenters/error_message/presenter.rb
+++ app/presenters/error_message/presenter.rb
@@ -32,7 +32,7 @@ module ErrorMessage
     end
 
     def error_is_for_govuk_has_one_association?(attribute)
-      govuk_has_one_associations.any? { |association| attribute.to_s.include?(association) }
+      govuk_has_one_associations.any? { |association| attribute.to_s.start_with?(association) }
     end
 
     def govuk_has_one_associations

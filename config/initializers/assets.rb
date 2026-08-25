e0f69bbba792a7343cabd53e1355f7992f70f51f
Mark Whitaker <mark.whitaker@digital.justice.gov.uk>
2026-08-25 17:17:08 +0100

    Replace sprockets with propshaft and add cssbundling-rails
    - Replaces `sprockets-rails` gem with `propshaft` and `cssbundling-rails`
    - Removes `sprockets` manifest file (`app/assets/config/manifest.js`)
    - Removes `sprockets`-specific config from `development.rb` and `production.rb`
    - Removes `webpack` packs path from `assets.rb` initializer
    - Adds `propshaft` asset paths for `govuk-frontend` and `moj-frontend` in `application.rb`

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,8 +11,3 @@
 # Rails.application.config.assets.precompile += %w( adp_swagger_application.js *.png)
 # Rails.application.config.assets.precompile += %w( application.test.js ) if Rails.env.test?
 # Rails.application.config.assets.precompile += %w( pdf.css )
-Rails.application.config.assets.paths << Rails.root.join('node_modules', 'govuk-frontend', 'dist', 'govuk', 'assets')
-Rails.application.config.assets.paths << Rails.root.join('node_modules', 'govuk-frontend', 'dist', 'govuk', 'assets', 'images')
-Rails.application.config.assets.paths << Rails.root.join('app', 'webpack', 'packs')
-Rails.application.config.assets.paths << Rails.root.join('node_modules', '@ministryofjustice', 'frontend', 'moj', 'assets')
-Rails.application.config.assets.paths << Rails.root.join('node_modules', '@ministryofjustice', 'frontend', 'moj', 'assets', 'images')
